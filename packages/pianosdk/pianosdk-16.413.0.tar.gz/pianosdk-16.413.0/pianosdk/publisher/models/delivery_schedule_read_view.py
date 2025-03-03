f9b057addc5e1b1619229d98ad165e01097db25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:11:18 +0000

    Add package pianosdk/16.413.0/pianosdk-16.413.0.tar.gz

---
+++ packages/pianosdk/pianosdk-16.413.0.tar.gz/pianosdk-16.413.0/pianosdk/publisher/models/delivery_schedule_read_view.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime
+from pydantic.main import BaseModel
+from typing import Optional
+from pianosdk.publisher.models.delivery_period_read_view import DeliveryPeriodReadView
+from typing import List
+
+
+class DeliveryScheduleReadView(BaseModel):
+    main_period_pub_id: Optional[str] = None
+    delivery_periods: Optional['List[DeliveryPeriodReadView]'] = None
+
+
+DeliveryScheduleReadView.model_rebuild()
