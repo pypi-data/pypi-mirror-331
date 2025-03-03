4149a6c7484a01f64df36d92b6113712a80d480c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:05:02 +0000

    Add package pltstat/0.9.7/pltstat-0.9.7.tar.gz

---
+++ packages/pltstat/pltstat-0.9.7.tar.gz/pltstat-0.9.7/pltstat/__init__.py
@@ -0,0 +1,18 @@
+# Author: Dmitrii Beregovoi
+# License: BSD 3-Clause License, see LICENSE file
+
+# Copyright: (c) 2025 Dmitrii Beregovoi
+
+__version__ = "0.9.7"
+__author__ = "Dmitrii Beregovoi"
+__email__ = "dimaforth@gmail.com"
+__all__ = [
+    "__version__",
+    "corr_methods",
+    "cm",
+    "circle",
+    "in_out",
+    "singlefeat",
+    "twofeats",
+    "multfeats",
+]
