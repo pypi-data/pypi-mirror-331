138eacb2ee31d18e37f754aff929ae2408b90515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 16:03:44 +0000

    Add package ks_trade_api/0.0.72/ks_trade_api-0.0.72.tar.gz

---
+++ packages/ks_trade_api/ks_trade_api-0.0.72.tar.gz/ks_trade_api-0.0.72/__about__.py
@@ -0,0 +1,6 @@
+# __version__ = "0.0.58" # 周期增加月份支持
+# __version__ = "0.0.59" # 增加Indicator
+# __version__ = "0.0.60" # 增加datatimetype
+# __version__ = "0.0.61" # query_hisotry_n由decimal改为返回float
+# __version__ = "0.0.71" # DataTimeType -> Timing
+__version__ = "0.0.72" # MySubscribeType支持snapshot
