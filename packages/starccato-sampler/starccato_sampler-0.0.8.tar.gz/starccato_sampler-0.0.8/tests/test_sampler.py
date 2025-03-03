b08aab79b0399f2ee4ca7ab08ba5782440f7feb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:31 +0000

    Add package starccato-sampler/0.0.8/starccato_sampler-0.0.8.tar.gz

---
+++ packages/starccato-sampler/starccato_sampler-0.0.8.tar.gz/starccato_sampler-0.0.8/tests/test_sampler.py
@@ -0,0 +1,31 @@
+import jax
+import pytest
+from starccato_jax import StarccatoVAE
+from starccato_jax.data import load_training_data
+
+from starccato_sampler.sampler import sample
+
+RNG = jax.random.PRNGKey(0)
+
+
+@pytest.fixture
+def injection():
+    vae = StarccatoVAE()
+    # true_z = jax.random.normal(RNG, (1, vae.latent_dim))
+    true_z = jax.numpy.zeros((1, vae.latent_dim))
+    true_signal = vae.generate(z=true_z)[0]
+    return true_signal, true_z.ravel()
+
+
+def test_sampler(injection, outdir):
+    true_signal, true_z = injection
+    sample(
+        true_signal,
+        outdir=outdir,
+        num_warmup=50,
+        num_samples=100,
+        num_chains=1,
+        verbose=True,
+        stepping_stone_lnz=False,
+        truth=dict(signal=true_signal, latent=true_z),
+    )
