3d1fac75ae086510fbf73d2ea693583814c207ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:03:55 +0000

    Add package square-common-bl/2.2.0/square_common_bl-2.2.0-py3-none-any.whl

---
+++ packages/square-common-bl/square_common_bl-2.2.0-py3-none-any.whl/square_common_bl/routes/authentication.py
@@ -0,0 +1,324 @@
+import json
+from typing import Annotated
+
+from fastapi import APIRouter, Header, HTTPException, status
+from fastapi.responses import JSONResponse
+from requests import HTTPError
+from square_commons import get_api_output_in_standard_format
+
+from square_common_bl.configuration import (
+    global_object_square_logger,
+    global_object_square_authentication_helper,
+)
+from square_common_bl.messages import messages
+from square_common_bl.pydantic_models.authentication import (
+    DeleteUserV0,
+    UpdatePasswordV0,
+)
+
+router = APIRouter(
+    tags=["authentication"],
+)
+
+
+@router.delete("/delete_user/v0")
+@global_object_square_logger.async_auto_logger
+async def delete_user_v0(
+    body: DeleteUserV0,
+    access_token: Annotated[str, Header()],
+):
+
+    try:
+        """
+        validation
+        """
+        # pass
+        """
+        main process
+        """
+        response = global_object_square_authentication_helper.delete_user_v0(
+            password=body.password,
+            access_token=access_token,
+        )
+        """
+        return value
+        """
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response,
+        )
+    except HTTPError as http_error:
+        global_object_square_logger.logger.error(http_error, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_error.response.status_code,
+            content=json.loads(http_error.response.content),
+        )
+    except HTTPException as http_exception:
+        global_object_square_logger.logger.error(http_exception, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_exception.status_code, content=http_exception.detail
+        )
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"],
+            log=str(e),
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
+
+
+@router.patch("/update_username/v0")
+@global_object_square_logger.async_auto_logger
+async def update_username_v0(
+    new_username: str,
+    access_token: Annotated[str, Header()],
+):
+
+    try:
+        """
+        validation
+        """
+        # pass
+        """
+        main process
+        """
+        response = global_object_square_authentication_helper.update_username_v0(
+            new_username=new_username,
+            access_token=access_token,
+        )
+        """
+        return value
+        """
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response,
+        )
+    except HTTPError as http_error:
+        global_object_square_logger.logger.error(http_error, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_error.response.status_code,
+            content=json.loads(http_error.response.content),
+        )
+    except HTTPException as http_exception:
+        global_object_square_logger.logger.error(http_exception, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_exception.status_code, content=http_exception.detail
+        )
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"],
+            log=str(e),
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
+
+
+@router.patch("/update_password/v0")
+@global_object_square_logger.async_auto_logger
+async def update_password_v0(
+    body: UpdatePasswordV0,
+    access_token: Annotated[str, Header()],
+):
+    old_password = body.old_password
+    new_password = body.new_password
+    try:
+        """
+        validation
+        """
+        # pass
+        """
+        main process
+        """
+        response = global_object_square_authentication_helper.update_password_v0(
+            old_password=old_password,
+            new_password=new_password,
+            access_token=access_token,
+        )
+        """
+        return value
+        """
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response,
+        )
+    except HTTPError as http_error:
+        global_object_square_logger.logger.error(http_error, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_error.response.status_code,
+            content=json.loads(http_error.response.content),
+        )
+    except HTTPException as http_exception:
+        global_object_square_logger.logger.error(http_exception, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_exception.status_code, content=http_exception.detail
+        )
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"],
+            log=str(e),
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
+
+
+@router.get("/get_user_details/v0")
+@global_object_square_logger.async_auto_logger
+async def get_user_details_v0(
+    access_token: Annotated[str, Header()],
+):
+
+    try:
+        """
+        validation
+        """
+        # pass
+        """
+        main process
+        """
+        response = global_object_square_authentication_helper.get_user_details_v0(
+            access_token=access_token,
+        )
+        """
+        return value
+        """
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response,
+        )
+    except HTTPError as http_error:
+        global_object_square_logger.logger.error(http_error, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_error.response.status_code,
+            content=json.loads(http_error.response.content),
+        )
+    except HTTPException as http_exception:
+        global_object_square_logger.logger.error(http_exception, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_exception.status_code, content=http_exception.detail
+        )
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"],
+            log=str(e),
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
+
+
+@router.delete("/logout/all/v0")
+@global_object_square_logger.async_auto_logger
+async def logout_all_v0(
+    access_token: Annotated[str, Header()],
+):
+
+    try:
+        """
+        validation
+        """
+        # pass
+        """
+        main process
+        """
+        response = global_object_square_authentication_helper.logout_all_v0(
+            access_token=access_token,
+        )
+        """
+        return value
+        """
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response,
+        )
+    except HTTPError as http_error:
+        global_object_square_logger.logger.error(http_error, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_error.response.status_code,
+            content=json.loads(http_error.response.content),
+        )
+    except HTTPException as http_exception:
+        global_object_square_logger.logger.error(http_exception, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_exception.status_code, content=http_exception.detail
+        )
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"],
+            log=str(e),
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
