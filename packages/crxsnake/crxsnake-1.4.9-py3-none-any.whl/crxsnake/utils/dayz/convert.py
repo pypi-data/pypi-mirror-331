3cb0359248680a22947498e258d63f902e10991d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:05:26 +0000

    Add package crxsnake/1.4.9/crxsnake-1.4.9-py3-none-any.whl

---
+++ packages/crxsnake/crxsnake-1.4.9-py3-none-any.whl/crxsnake/utils/dayz/convert.py
@@ -0,0 +1,34 @@
+from hashlib import sha256, md5
+from base64 import b64encode
+
+
+def steam_to_be_guid(steam_id: int) -> str:
+    """Convert steam id to Battleye guid.
+    
+    Args:
+        steam_id (int)
+    Returns:
+        str: Battleye guid
+    """
+    parts = [0x42, 0x45] + [0] * 8
+    for i in range(2, 10):
+        steam_id, remainder = divmod(steam_id, 256)
+        parts[i] = remainder
+
+    byte_array = bytes(parts)
+    hash_object = md5(byte_array)
+    return hash_object.hexdigest()
+
+
+def steam_to_dayz_guid(steam_id: int) -> str:
+    """Convert steam id to Bohemia ID
+
+    Args:
+        steam_id (int)_
+
+    Returns:
+        str: Bohemia ID
+    """
+    hashed = sha256()
+    hashed.update(str(steam_id).encode("utf-8"))
+    return b64encode(hashed.digest()).decode("utf-8")
