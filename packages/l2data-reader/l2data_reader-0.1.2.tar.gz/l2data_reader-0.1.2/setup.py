dab642ebeccc68bb2b370fc5c6291e3955a1c43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:05:42 +0000

    Add package l2data-reader/0.1.2/l2data_reader-0.1.2.tar.gz

---
+++ packages/l2data-reader/l2data_reader-0.1.2.tar.gz/l2data_reader-0.1.2/setup.py
@@ -0,0 +1,29 @@
+"""
+Setup script for l2data_reader.
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="l2data_reader",
+    version="0.1.0",
+    author="Jason Jiang",
+    author_email="chinese88+3@2925.com",
+    description="A package for reading level 2 market data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JasonJiang0303/l2data-reader",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "protobuf>=3.12.0",
+    ],
+)
