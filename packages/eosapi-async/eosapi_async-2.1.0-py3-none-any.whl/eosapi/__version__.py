995bc628076fd8f86c896d060082358152d1aa05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:15 +0000

    Add package eosapi-async/2.1.0/eosapi_async-2.1.0-py3-none-any.whl

---
+++ packages/eosapi-async/eosapi_async-2.1.0-py3-none-any.whl/eosapi/__version__.py
@@ -0,0 +1,10 @@
+__title__ = "eosapi-async"
+__description__ = (
+    "a simple, high-level and lightweight eosio sdk write by python with async features"
+)
+__url__ = "https://github.com/alsekaram/eosapi_async"
+__author__ = "alsekaram"
+__author_email__ = "git@awl.su"
+__license__ = "MIT"
+
+__version__ = "2.1.0"
