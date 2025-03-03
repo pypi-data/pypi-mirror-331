26d5cc3024cde59fa45eaaac603e63932f5db776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:41 +0000

    Add package cameo-geo-query/1.0.31/cameo_geo_query-1.0.31.tar.gz

---
+++ packages/cameo-geo-query/cameo_geo_query-1.0.31.tar.gz/cameo_geo_query-1.0.31/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'cameo_geo_query',
+    version = '1.0.31',
+    description='This is cameo_geo_query',
+    url = '',
+    author = 'bear',
+    author_email='panda19931217@gmail.com',
+    license='BSD 2-clause',
+    packages=['cameo_geo_query'],
+    install_requires=[
+        'pandas',
+        'geopy',
+        'plotly',
+        'cameo-botrun-prompt-tools>=1.0.6'
+    ]
+)
