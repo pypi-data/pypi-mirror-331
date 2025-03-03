ab5efdb184d999093d1c52cd5933c9002e38b788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:35 +0000

    Add package depoc/0.0.7/depoc-0.0.7.tar.gz

---
+++ packages/depoc/depoc-0.0.7.tar.gz/depoc-0.0.7/depoc/__init__.py
@@ -0,0 +1,25 @@
+import json
+
+from depoc.core.requestor import Requestor
+from depoc.core.client import DepocClient
+from depoc.core.auth import Connection
+
+from depoc.services.user import User
+from depoc.services.owner import Owner
+from depoc.services.account import Account
+from depoc.services.business import Business
+from depoc.services.customer import Customer
+from depoc.services.supplier import Supplier
+
+
+token: str | None = None
+
+try:
+    with open('token.json', 'r') as f:
+        data: dict = json.load(f)
+        token = data.get('token')
+except (FileNotFoundError, json.JSONDecodeError, KeyError) as e:
+    pass
+
+# Constants
+BASE_URL: str = 'https://api.depoc.com.br'
