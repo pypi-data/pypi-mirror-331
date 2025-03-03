7afee2fc22fde44effae2e6f1a043b45ebb565d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:41 +0000

    Add package r3frame/2025.0.16/r3frame-2025.0.16.tar.gz

---
+++ packages/r3frame/r3frame-2025.0.16.tar.gz/r3frame-2025.0.16/r3frame/objects/__init__.py
@@ -0,0 +1,2 @@
+import r3frame.objects.game as game
+import r3frame.objects.world as world
---
+++ packages/r3frame/r3frame-2025.0.16.tar.gz/r3frame-2025.0.16/r3frame/objects/game/__init__.py
@@ -0,0 +1,3 @@
+from r3frame.objects.game.chain import Chain
+from r3frame.objects.game.base import Game_Object
+from r3frame.objects.game.animation import Animation
