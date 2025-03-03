183605532e201f600e4a8d9832c8a8c45fc4bbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:34 +0000

    Add package pydom/0.3.1/pydom-0.3.1.tar.gz

---
+++ packages/pydom/pydom-0.3.1.tar.gz/pydom-0.3.1/src/pydom/rendering/__init__.py
@@ -0,0 +1,57 @@
+from typing import overload, Literal, Union, Optional
+
+from pydom.types.rendering import RenderResultJSON
+
+from ..context import Context
+from .html import render_html
+from .json import render_json
+from ..types import Renderable, Primitive
+
+
+@overload
+def render(
+    element: Union[Renderable, Primitive],
+    *,
+    to: Literal["html"],
+    pretty: bool = False,
+    tab_indent: int = 1,
+    context: Optional[Context] = None,
+    **render_state_data,
+) -> str: ...
+
+
+@overload
+def render(
+    element: Union[Renderable, Primitive],
+    *,
+    to: Literal["json"],
+    context: Optional[Context] = None,
+    **render_state_data,
+) -> RenderResultJSON: ...
+
+
+@overload
+def render(
+    element: Union[Renderable, Primitive],
+    *,
+    pretty: bool = False,
+    tab_indent: int = 1,
+    context: Optional[Context] = None,
+    **render_state_data,
+) -> str: ...
+
+
+def render(
+    element: Union[Renderable, Primitive],
+    *,
+    to: Literal["json", "html"] = "html",
+    **kwargs,
+) -> Union[str, RenderResultJSON]:
+
+    if to == "json":
+        return render_json(element, **kwargs)
+    if to == "html":
+        return render_html(element, **kwargs)
+
+
+__all__ = ["render", "render_html", "render_json"]
---
+++ packages/pydom/pydom-0.3.1.tar.gz/pydom-0.3.1/src/pydom/rendering/transformers/property_transformer.py
@@ -0,0 +1,41 @@
+from typing import Any, Callable, List, Optional, Type, Union
+
+from pydom.context.transformers import PropertyTransformer
+from ...context.context import PropertyTransformerFunction, Context, get_context
+
+
+def property_transformer(
+    matcher: Union[Callable[[str, Any], bool], str],
+    *,
+    context: Optional[Context] = None,
+    before: Optional[List[Type[PropertyTransformer]]] = None,
+    after: Optional[List[Type[PropertyTransformer]]] = None,
+):
+    """
+    A decorator to register a function as a property transformer.
+
+    Args:
+        matcher: A callable that takes a key and a value and returns a boolean
+        indicating whether the transformer should be applied.
+        If a string is provided, it is assumed to be a key that should be matched exactly.
+        context: The context to register the transformer in. If not provided, the default context is used.
+
+    Returns:
+        A decorator that takes a transformer function and registers it.
+
+    Example:
+        >>> @property_transformer("class_name")
+        ... def class_name_mapper(key, value, element):
+        ...     if isinstance(class_name, list):
+        ...         class_name = " ".join(class_name)
+        ...
+        ...     element.props["class"] = " ".join(str(class_name).split())
+
+    """
+    context = get_context(context)
+
+    def decorator(func: PropertyTransformerFunction):
+        context.add_prop_transformer(matcher, func, before=before, after=after)
+        return func
+
+    return decorator
