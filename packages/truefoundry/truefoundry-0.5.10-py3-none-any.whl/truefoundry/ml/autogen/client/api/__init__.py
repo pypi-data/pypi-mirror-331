e28c7a027db64845f597b9f69ae64f5f2297f9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:58 +0000

    Add package truefoundry/0.5.10/truefoundry-0.5.10-py3-none-any.whl

---
+++ packages/truefoundry/truefoundry-0.5.10-py3-none-any.whl/truefoundry/ml/autogen/client/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from truefoundry.ml.autogen.client.api.auth_api import AuthApi
+from truefoundry.ml.autogen.client.api.deprecated_api import DeprecatedApi
+from truefoundry.ml.autogen.client.api.experiments_api import ExperimentsApi
+from truefoundry.ml.autogen.client.api.generate_code_snippet_api import (
+    GenerateCodeSnippetApi,
+)
+from truefoundry.ml.autogen.client.api.health_api import HealthApi
+from truefoundry.ml.autogen.client.api.metrics_api import MetricsApi
+from truefoundry.ml.autogen.client.api.mlfoundry_artifacts_api import (
+    MlfoundryArtifactsApi,
+)
+from truefoundry.ml.autogen.client.api.run_artifacts_api import RunArtifactsApi
+from truefoundry.ml.autogen.client.api.runs_api import RunsApi
