6311418677552c9dcf07d181edf346a0055e9d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:03:45 +0000

    Add package good-clickhouse/0.2.6/good_clickhouse-0.2.6-py3-none-any.whl

---
+++ packages/good-clickhouse/good_clickhouse-0.2.6-py3-none-any.whl/good_clickhouse/__init__.py
@@ -0,0 +1,18 @@
+from ._client import (
+    Clickhouse,
+    ClickhouseProvider,
+    ClickhouseAsync,
+    ClickhouseAsyncProvider,
+    ConnectionProfile,
+    NamedTupleCursor,
+)
+# from ._query import Query, query
+
+__all__ = [
+    "Clickhouse",
+    "ClickhouseProvider",
+    "ClickhouseAsync",
+    "ClickhouseAsyncProvider",
+    "ConnectionProfile",
+    "NamedTupleCursor",
+]
