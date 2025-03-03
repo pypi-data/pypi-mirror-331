798d1276673de12455f3bddbfd9551a2b0fb0ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:02 +0000

    Add package qn-talk/0.0.2/qn_talk-0.0.2.tar.gz

---
+++ packages/qn-talk/qn_talk-0.0.2.tar.gz/qn_talk-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qn_talk",
+    version="0.0.2",
+    author="qn",
+    author_email="654304502@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
