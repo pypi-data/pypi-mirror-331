9f9fe01077228ba7170018461011a9e5a6b32d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:21 +0000

    Add package pltstat/0.9.6/pltstat-0.9.6.tar.gz

---
+++ packages/pltstat/pltstat-0.9.6.tar.gz/pltstat-0.9.6/pltstat/tests.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
+
+from pltstat import singlefeat as sf
+from pltstat import twofeats as tf
+from pltstat import multfeats as mf
+from pltstat import circle as crl
