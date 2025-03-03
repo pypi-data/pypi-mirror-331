98aefa37491da61bbfd43c26aecc8db1335a90de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:41 +0000

    Add package add-confidence-score-and-coordinates/1.0.4/add_confidence_score_and_coordinates-1.0.4.tar.gz

---
+++ packages/add-confidence-score-and-coordinates/add_confidence_score_and_coordinates-1.0.4.tar.gz/add_confidence_score_and_coordinates-1.0.4/README.md
@@ -0,0 +1,2 @@
+# ocr-json-processor
+OCR JSON Processor is a Python package designed to streamline the process of extracting OCR data from PDF files and enriching OpenAI-generated JSON responses with confidence scores and coordinate values for each field.
---
+++ packages/add-confidence-score-and-coordinates/add_confidence_score_and_coordinates-1.0.4.tar.gz/add_confidence_score_and_coordinates-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="add_confidence_score_and_coordinates",
+    version="1.0.4",
+    description="A package for updating openai JSON response with confidence score and coordinates",
+    author="Shubham Kumar",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow",
+        "numpy",
+        "python-dotenv",
+        "azure-core",
+        "azure-ai-documentintelligence==1.0.0",
+        "amazon-textract-textractor==1.7.10",
+        "PyMuPDF",
+        "pytest",
+        "fastapi",
+        "fuzzywuzzy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
