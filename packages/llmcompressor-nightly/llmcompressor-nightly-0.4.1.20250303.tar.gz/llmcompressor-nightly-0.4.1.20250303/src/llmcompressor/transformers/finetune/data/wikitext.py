638080a10b5a0385a925ca699c8544a184dc2cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:35 +0000

    Add package llmcompressor-nightly/0.4.1.20250303/llmcompressor-nightly-0.4.1.20250303.tar.gz

---
+++ packages/llmcompressor-nightly/llmcompressor-nightly-0.4.1.20250303.tar.gz/llmcompressor-nightly-0.4.1.20250303/src/llmcompressor/transformers/finetune/data/wikitext.py
@@ -0,0 +1,30 @@
+from copy import deepcopy
+from typing import TYPE_CHECKING
+
+from llmcompressor.transformers.finetune.data import TextGenerationDataset
+from llmcompressor.typing import Processor
+
+if TYPE_CHECKING:
+    from llmcompressor.args import DatasetArguments
+
+
+@TextGenerationDataset.register(name="wikitext")
+class WikiTextDataset(TextGenerationDataset):
+    """
+    Child text generation class for the Open Platypus dataset
+
+    :param data_args: configuration settings for dataset loading
+    :param split: split from dataset to load, for instance `test` or `train[:5%]`
+    :param processor: processor or tokenizer to use on dataset
+    """
+
+    def __init__(self, data_args: "DatasetArguments", split: str, processor: Processor):
+        data_args = deepcopy(data_args)
+        data_args.dataset = "Salesforce/wikitext"
+        data_args.text_column = "text"
+
+        super().__init__(
+            data_args=data_args,
+            split=split,
+            processor=processor,
+        )
