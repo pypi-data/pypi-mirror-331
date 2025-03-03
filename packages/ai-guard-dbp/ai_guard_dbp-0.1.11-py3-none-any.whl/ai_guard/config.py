4e5b8a578fadd48fd31e5023a6e571c52397cf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:05:41 +0000

    Add package ai-guard-dbp/0.1.11/ai_guard_dbp-0.1.11-py3-none-any.whl

---
+++ packages/ai-guard-dbp/ai_guard_dbp-0.1.11-py3-none-any.whl/ai_guard/config.py
@@ -0,0 +1,6 @@
+# ai_guard/config.py
+
+# Define the thresholds for the evaluation metrics
+RAGASS_THRESHOLD = 0.5  # Example threshold for RAGASS
+BERTSCORE_THRESHOLD = 0.9  # Example threshold for BERTScore
+ROUGEL_THRESHOLD = 0.7  # Example threshold for ROUGE-L
