51053aee99f488b3c5fe2526aa76ceeb61f7f624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:09 +0000

    Add package fastlifeweb/0.25.2/fastlifeweb-0.25.2-py3-none-any.whl

---
+++ packages/fastlifeweb/fastlifeweb-0.25.2-py3-none-any.whl/fastlife/adapters/fastapi/routing/route.py
@@ -0,0 +1,47 @@
+"""HTTP Route."""
+
+from collections.abc import Callable, Coroutine
+from typing import TYPE_CHECKING, Any
+
+from fastapi.routing import APIRoute
+from starlette.requests import Request as StarletteRequest
+from starlette.responses import Response
+
+from fastlife.domain.model.request import GenericRequest
+
+if TYPE_CHECKING:
+    from fastlife.service.registry import DefaultRegistry  # coverage: ignore
+
+
+class Route(APIRoute):
+    """
+    Routing for fastlife application.
+
+    The fastlife router construct fastlife request object in order to
+    have the registry property available in every received request.
+    """
+
+    _registry: "DefaultRegistry"
+    """
+    The application registry.
+
+    this static variable is initialized by the configurator during
+    the startup and keep the registry during the lifetime of the application.
+
+    this variable should be accessed via the request object or the
+    {class}`fastlife.config.Registry` depenency injection.
+    """
+
+    def get_route_handler(
+        self,
+    ) -> Callable[[StarletteRequest], Coroutine[Any, Any, Response]]:
+        """
+        Replace the request object by the fastlife request associated with the registry.
+        """
+        orig_route_handler = super().get_route_handler()
+
+        async def route_handler(request: StarletteRequest) -> Response:
+            req = GenericRequest[Any, Any, Any](self._registry, request)
+            return await orig_route_handler(req)
+
+        return route_handler
---
+++ packages/fastlifeweb/fastlifeweb-0.25.2-py3-none-any.whl/fastlife/service/locale_negociator.py
@@ -0,0 +1,25 @@
+"""Find the localization gor the given request."""
+
+from collections.abc import Callable
+from typing import Any
+
+from fastlife.settings import Settings
+
+LocaleName = str
+"""The LocaleName is a locale such as en, fr that will be consume for translations."""
+
+from fastlife.adapters.fastapi.request import GenericRequest  # coverage: ignore
+
+LocaleNegociator = Callable[
+    [GenericRequest[Any, Any, Any]], LocaleName
+]  # coverage: ignore
+"""Interface to implement to negociate a locale"""  # coverage: ignore
+
+
+def default_negociator(settings: Settings) -> LocaleNegociator:
+    """The default local negociator return the locale set in the conf."""
+
+    def locale_negociator(request: "GenericRequest[Any, Any, Any]") -> str:
+        return settings.default_locale
+
+    return locale_negociator
