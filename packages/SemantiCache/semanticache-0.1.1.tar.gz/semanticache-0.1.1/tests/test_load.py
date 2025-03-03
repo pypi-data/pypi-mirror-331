7f548be83f013d5a655f0f78ebb01da6acd9437b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:18 +0000

    Add package SemantiCache/0.1.1/semanticache-0.1.1.tar.gz

---
+++ packages/SemantiCache/semanticache-0.1.1.tar.gz/semanticache-0.1.1/tests/test_load.py
@@ -0,0 +1,22 @@
+import time
+from semanticache import SemantiCache
+
+start = time.perf_counter()
+cache = SemantiCache(ttl=65, trim_by_size=False, log_level="WARNING")
+end = time.perf_counter() - start
+print("cache load time:: %s secs" % round(end, 2))
+
+
+def run(iter: int):
+    start = time.perf_counter()
+    cache.set("test", "test response")
+    ans = cache.get("test")
+    end = time.perf_counter() - start
+    print(ans, f"iter {iter} run time: {round(end, 2)} secs")
+    return end
+
+
+run_times = []
+for iter in range(10):
+    run_times.append(run(iter))
+print("average run time %s secs" % round(sum(run_times)/len(run_times), 2))
