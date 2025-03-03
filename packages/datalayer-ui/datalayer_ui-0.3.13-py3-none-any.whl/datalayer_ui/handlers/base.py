df4be96f81cda18a292eb4627f9022441909dd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:05:38 +0000

    Add package datalayer-ui/0.3.13/datalayer_ui-0.3.13-py3-none-any.whl

---
+++ packages/datalayer-ui/datalayer_ui-0.3.13-py3-none-any.whl/datalayer_ui/handlers/base.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2021-2024 Datalayer, Inc.
+#
+# Datalayer License
+
+# Copyright (c) Datalayer Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+"""Base handler."""
+
+from jupyter_server.base.handlers import JupyterHandler
+from jupyter_server.extension.handler import ExtensionHandlerMixin, ExtensionHandlerJinjaMixin
+
+
+# pylint: disable=W0223
+class BaseTemplateHandler(ExtensionHandlerJinjaMixin, ExtensionHandlerMixin, JupyterHandler):
+    """The Base handler for the templates."""
+    
---
+++ packages/datalayer-ui/datalayer_ui-0.3.13-py3-none-any.whl/datalayer_ui/handlers/index/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2021-2024 Datalayer, Inc.
+#
+# Datalayer License
+
+# Copyright (c) Datalayer Development Team.
+# Distributed under the terms of the Modified BSD License.
+
