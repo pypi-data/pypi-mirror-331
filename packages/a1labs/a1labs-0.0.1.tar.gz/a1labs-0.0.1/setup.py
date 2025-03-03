bb20fccd976cb54583f6aaff6a0d246074993948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:21 +0000

    Add package a1labs/0.0.1/a1labs-0.0.1.tar.gz

---
+++ packages/a1labs/a1labs-0.0.1.tar.gz/a1labs-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="a1labs",
+    version="0.0.1",
+    description="a1labs",
+    author="Anish Agrawal",
+    author_email="your.email@example.com",
+    packages=["a1labs"],
+)
