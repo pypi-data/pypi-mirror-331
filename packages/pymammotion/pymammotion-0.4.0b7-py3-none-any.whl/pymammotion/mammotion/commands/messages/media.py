75953f0d5d77c0d4f8bd92d8111d7a7efa58522f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 16:04:43 +0000

    Add package pymammotion/0.4.0b7/pymammotion-0.4.0b7-py3-none-any.whl

---
+++ packages/pymammotion/pymammotion-0.4.0b7-py3-none-any.whl/pymammotion/mammotion/commands/messages/media.py
@@ -0,0 +1,34 @@
+# === sendOrderMsg_Media ===
+from abc import ABC
+
+from pymammotion.mammotion.commands.abstract_message import AbstractMessage
+from pymammotion.proto import luba_msg_pb2, luba_mul_pb2
+from pymammotion.proto.luba_msg import MsgCmdType, MsgDevice
+from pymammotion.proto.luba_mul import MUL_LANGUAGE
+
+
+class MessageMedia(AbstractMessage, ABC):
+    def send_order_msg_media(self, mul):
+        luba_msg = luba_msg_pb2.LubaMsg(
+            msgtype=luba_msg_pb2.MSG_CMD_TYPE_MUL,
+            sender=luba_msg_pb2.DEV_MOBILEAPP,
+            rcver=self.get_msg_device(MsgCmdType.MSG_CMD_TYPE_MUL, MsgDevice.SOC_MODULE_MULTIMEDIA),
+            msgattr=luba_msg_pb2.MSG_ATTR_REQ,
+            seqs=1,
+            version=1,
+            subtype=1,
+            mul=mul,
+        )
+
+        return luba_msg.SerializeToString()
+
+    def set_car_volume(self, volume: int):
+        return self.send_order_msg_media(luba_mul_pb2.SocMul(set_audio=luba_mul_pb2.MulSetAudio(at_switch=volume)))
+
+    def set_car_voice_language(self, language_type: MUL_LANGUAGE | str | None):
+        return self.send_order_msg_media(
+            luba_mul_pb2.SocMul(set_audio=luba_mul_pb2.MulSetAudio(au_language=language_type))
+        )
+
+    def set_car_wiper(self, round_num: int):
+        return self.send_order_msg_media(luba_mul_pb2.SocMul(set_wiper=luba_mul_pb2.MulSetWiper(round=round_num)))
