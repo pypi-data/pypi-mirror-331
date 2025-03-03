b2cb4c67ed7a2a70a0d11ecdc9f948882617632a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 16:04:43 +0000

    Add package PySimpleGUI/5.0.99.99/pysimplegui-5.0.99.99-py3-none-any.whl

---
+++ packages/PySimpleGUI/pysimplegui-5.0.99.99-py3-none-any.whl/PySimpleGUI/PySimpleGUI.py
@@ -0,0 +1,8 @@
+print("PySimpleGUI is now located on a private PyPI server.  Please add to your pip command: -i https://PySimpleGUI.net/install")
+
+
+def main():
+    print("PySimpleGUI is now located on a private PyPI server.  Please add to your pip command: -i https://PySimpleGUI.net/install")
+
+if __name__ == "__main__":
+    main()
