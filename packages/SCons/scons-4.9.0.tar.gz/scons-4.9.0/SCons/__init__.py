116b4b99c91cc0d8db6b609cd4a02fdaafc6ca02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 04:03:53 +0000

    Add package SCons/4.9.0/scons-4.9.0.tar.gz

---
+++ packages/SCons/scons-4.9.0.tar.gz/scons-4.9.0/SCons/__init__.py
@@ -0,0 +1,9 @@
+__version__="4.9.0"
+__copyright__="Copyright (c) 2001 - 2025 The SCons Foundation"
+__developer__="bdbaddog"
+__date__="Sun, 02 Mar 2025 14:04:50 -0700"
+__buildsys__="M1Dog2021"
+__revision__="99a8c86de1ce91d23b102520e185c54ebd968924"
+__build__="99a8c86de1ce91d23b102520e185c54ebd968924"
+# make sure compatibility is always in place
+import SCons.compat  # noqa
