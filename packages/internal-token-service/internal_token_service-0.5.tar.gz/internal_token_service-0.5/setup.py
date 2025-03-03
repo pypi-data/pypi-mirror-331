168f51279c164107d7218c411d63062de384f9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:11:29 +0000

    Add package internal-token-service/0.5/internal_token_service-0.5.tar.gz

---
+++ packages/internal-token-service/internal_token_service-0.5.tar.gz/internal_token_service-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as f:
+    description = f.read()
+
+setup(
+    name='internal_token_service',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'PyJWT>=2.0.0'
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+    license='MIT',  # Add your license type here (e.g., MIT)
+    include_package_data=True,
+)
