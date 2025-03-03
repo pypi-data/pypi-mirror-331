00e8281f8a6784eb3013e390f793e4708ffaac94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 14:04:12 +0000

    Add package dbt-tidy/0.2.0/dbt_tidy-0.2.0.tar.gz

---
+++ packages/dbt-tidy/dbt_tidy-0.2.0.tar.gz/dbt_tidy-0.2.0/tidy/manifest/types.py
@@ -0,0 +1,10 @@
+from typing import Union
+from tidy.manifest.v10.manifest import ManifestV10
+from tidy.manifest.v11.manifest import ManifestV11
+from tidy.manifest.v12.manifest import ManifestV12
+
+ManifestType = Union[
+    ManifestV10,
+    ManifestV11,
+    ManifestV12,
+]
