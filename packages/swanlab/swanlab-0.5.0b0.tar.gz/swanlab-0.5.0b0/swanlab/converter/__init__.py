ddc77f4bbdf598d50ce318d7d639ed66c26be4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:05:37 +0000

    Add package swanlab/0.5.0b0/swanlab-0.5.0b0.tar.gz

---
+++ packages/swanlab/swanlab-0.5.0b0.tar.gz/swanlab-0.5.0b0/swanlab/converter/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024/6/14 00:51
+@File: __init__.py.py
+@IDE: pycharm
+@Description:
+    转换部分，兼容其他可视化工具，转换为swanlab格式
+"""
+
+
+# 使用延迟导入的方式
+def __getattr__(name):
+    if name == "TFBConverter":
+        from .tfb import TFBConverter
+
+        return TFBConverter
+    if name == "WandbConverter":
+        from .wb import WandbConverter
+
+        return WandbConverter
+
+    raise AttributeError(f"module 'convert' has no attribute '{name}'")
+
+
+__all__ = ["TFBConverter", "WandbConverter"]
