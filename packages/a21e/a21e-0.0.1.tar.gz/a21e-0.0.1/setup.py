b7bd9d895835cd7f3018448ef6379143894d5302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:04:30 +0000

    Add package a21e/0.0.1/a21e-0.0.1.tar.gz

---
+++ packages/a21e/a21e-0.0.1.tar.gz/a21e-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="a21e",
+    version="0.0.1",
+    description="a21e",
+    author="Anish Agrawal",
+    author_email="your.email@example.com",
+    packages=["a21e"],
+)
