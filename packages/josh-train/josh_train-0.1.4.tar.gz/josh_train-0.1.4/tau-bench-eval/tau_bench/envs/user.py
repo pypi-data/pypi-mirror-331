e6c9be74fbde8beec49d3a17229817615bdbcdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 15:05:27 +0000

    Add package josh-train/0.1.4/josh_train-0.1.4.tar.gz

---
+++ packages/josh-train/josh_train-0.1.4.tar.gz/josh_train-0.1.4/tau-bench-eval/tau_bench/envs/user.py
@@ -0,0 +1,100 @@
+# Copyright Sierra
+
+import abc
+from litellm import completion
+
+from typing import Optional, List, Dict, Any
+
+
+class BaseUserSimulationEnv(abc.ABC):
+    metadata = {}
+
+    @abc.abstractmethod
+    def reset(self, instruction: Optional[str] = None) -> str:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def step(self, content: str) -> str:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_total_cost(self) -> float:
+        raise NotImplementedError
+
+
+class HumanUserSimulationEnv(BaseUserSimulationEnv):
+    def reset(self, instruction: str) -> str:
+        return input(f"{instruction}\n")
+
+    def step(self, content: str) -> str:
+        return input(f"{content}\n")
+
+    def get_total_cost(self) -> float:
+        return 0
+
+
+def build_system_prompt(instruction: Optional[str]) -> str:
+    inst = ("\n\nInstruction: " + instruction + "\n") if instruction is not None else ""
+    return f"""You are an user interacting with an agent.{inst}
+Rules:
+- Just generate one line at a time to simulate the user's message.
+- Do not give away all the instruction at once. Only provide the information that is necessary for the current step.
+- Do not hallucinate information that is not provided in the instruction. For example, if the agent asks for the order id but it is not mentioned in the instruction, do not make up an order id, just say you do not remember or have it.
+- If the instruction goal is satisified, generate '###STOP###' as a standalone message without anything else to end the conversation.
+- Do not repeat the exact instruction in the conversation. Instead, use your own words to convey the same information.
+- Try to make the conversation as natural as possible, and stick to the personalities in the instruction.
+"""
+
+
+class LLMUserSimulationEnv(BaseUserSimulationEnv):
+    def __init__(self, model: str, provider: str) -> None:
+        super().__init__()
+        self.messages: List[Dict[str, Any]] = []
+        self.model = model
+        self.provider = provider
+        self.total_cost = 0.0
+        self.reset()
+
+    def reset(self, instruction: Optional[str] = None) -> str:
+        self.messages = [
+            {
+                "role": "system",
+                "content": build_system_prompt(instruction=instruction),
+            },
+            {"role": "user", "content": "Hi! How can I help you today?"},
+        ]
+        res = completion(
+            model=self.model, custom_llm_provider=self.provider, messages=self.messages, temperature=0.0
+        )
+        message = res.choices[0].message
+        self.messages.append(message.model_dump())
+        self.total_cost = res._hidden_params["response_cost"]
+        return message.content
+
+    def step(self, content: str) -> str:
+        self.messages.append({"role": "user", "content": content})
+        res = completion(
+            model=self.model, custom_llm_provider=self.provider, messages=self.messages, temperature=0.0
+        )
+        message = res.choices[0].message
+        self.messages.append(message.model_dump())
+        self.total_cost += res._hidden_params["response_cost"]
+        return message.content
+
+    def get_total_cost(self) -> float:
+        return self.total_cost
+
+
+def load_user(
+    user_strategy: str, model: Optional[str] = "gpt-4o", provider: Optional[str] = None
+) -> BaseUserSimulationEnv:
+    if user_strategy == "human":
+        return HumanUserSimulationEnv()
+    elif user_strategy == "llm":
+        if model is None:
+            raise ValueError("LLM user strategy requires a model")
+        if provider is None:
+            raise ValueError("LLM user strategy requires a model provider")
+        return LLMUserSimulationEnv(model=model, provider=provider)
+    else:
+        raise ValueError(f"Unknown user strategy {user_strategy}")
