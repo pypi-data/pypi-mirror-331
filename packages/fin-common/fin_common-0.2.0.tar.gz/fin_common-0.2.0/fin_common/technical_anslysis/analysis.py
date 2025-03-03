ee67b13566656e34cb1c6abd31773e931ea4aa32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:11:20 +0000

    Add package fin-common/0.2.0/fin_common-0.2.0.tar.gz

---
+++ packages/fin-common/fin_common-0.2.0.tar.gz/fin_common-0.2.0/fin_common/technical_anslysis/analysis.py
@@ -0,0 +1,32 @@
+from fin_common.YfinanceService import yf_get_info
+from fin_common.technical_anslysis.liquidity import compute_liquidity_analysis
+from fin_common.technical_anslysis.pattern import compute_pattern_analysis
+from fin_common.technical_anslysis.momentum import compute_momentum_analysis
+from fin_common.technical_anslysis.volatility import compute_volatility_analysis
+from fin_common.technical_anslysis.util import fetch_data
+from datetime import datetime
+
+def perform_technical_analysis(ticker: str):
+    # Perform technical analysis based on the analysis_type
+    # and return the analysis result
+
+    ## fetch relavant data for ticker
+    stock_data = fetch_data(ticker)
+    if stock_data is None:
+        return None
+    info = yf_get_info(ticker)
+    if info is None:
+        return None
+    elif info['sharesOutstanding'] != None:
+        stock_data['Shares_Outstanding'] = info['sharesOutstanding']
+
+    ## Send stock data for analysis
+    # TODO: add more technical analysis combinations
+    return {
+        "ticker": ticker,
+        "date": datetime.now().strftime('%Y-%m-%d'),
+        "volatility_analysis": compute_volatility_analysis(stock_data),
+        "momentum_analysis": compute_momentum_analysis(stock_data),
+        "pattern_analysis": compute_pattern_analysis(stock_data),
+        "liquidity_analysis": compute_liquidity_analysis(stock_data)
+    }
