a0e49efd2ef14f453dffe0903b2159c8c5efd955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:48 +0000

    Add package pybud-gui/3.0.2/pybud_gui-3.0.2-pp310-pypy310_pp73-manylinux_2_34_x86_64.whl

---
+++ packages/pybud-gui/pybud_gui-3.0.2-pp310-pypy310_pp73-manylinux_2_34_x86_64.whl/pybud/drawer/ansi.py
@@ -0,0 +1,21 @@
+import os
+from pybud._drawer import ansi
+
+AnsiGraphics = ansi.AnsiGraphics
+AnsiChar = ansi.AnsiChar
+AnsiString = ansi.AnsiString
+
+# this ugly, but I can't do nothing about it
+class AnsiGraphicMode:
+    BOLD = AnsiGraphics._from_bits(0b00000001)
+    FAINT = AnsiGraphics._from_bits(0b00000010)
+    ITALIC = AnsiGraphics._from_bits(0b00000100)
+    UNDERLINE = AnsiGraphics._from_bits(0b00001000)
+    BLINKING = AnsiGraphics._from_bits(0b00010000)
+    REVERSE = AnsiGraphics._from_bits(0b00100000)
+    HIDDEN = AnsiGraphics._from_bits(0b01000000)
+    STRIKE = AnsiGraphics._from_bits(0b10000000)
+
+def init():
+    """only for windows"""
+    os.system("")
