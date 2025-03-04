8c2b6d7be222a6e6520d04c36dee707f694124d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:30 +0000

    Add package onbbu/0.0.6/onbbu-0.0.6-py3-none-any.whl

---
+++ packages/onbbu/onbbu-0.0.6-py3-none-any.whl/onbbu/cli/cli_run_server.py
@@ -0,0 +1,29 @@
+import uvicorn
+from onbbu.settings.ConfigLoader import ConfigLoader
+from onbbu.settings.main import BASE_DIR
+
+
+def run_server():
+    """Ejecuta `internal/main.py` como servidor"""
+
+    module = ConfigLoader(BASE_DIR).load_python_config(
+        relative_path="internal/main.py",
+        attribute_name="server",
+        default=[],
+    )
+
+    if hasattr(module, "server"):
+        print("🚀 Iniciando FastAPI desde `internal/main.py`...")
+
+        print(f"🚀 Iniciando servidor en {module.server.host}:{module.server.port} ...")
+
+        uvicorn.run(
+            "internal.main:server.http",
+            host=module.server.host,
+            port=module.server.port,
+            reload=module.server.reload,
+            workers=module.server.workers,
+        )
+
+    else:
+        print("❌ `internal/main.py` no contiene una instancia `server`.")
