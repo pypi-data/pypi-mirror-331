234c2047f05a2c152427aebbcc079613413fff71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:06 +0000

    Add package plain.sessions/0.11.0/plain_sessions-0.11.0.tar.gz

---
+++ packages/plain.sessions/plain_sessions-0.11.0.tar.gz/plain_sessions-0.11.0/tests/app/urls.py
@@ -0,0 +1,17 @@
+from plain.urls import RouterBase, path, register_router
+from plain.views import View
+
+
+class IndexView(View):
+    def get(self):
+        # Store something so the session is saved
+        self.request.session["foo"] = "bar"
+        return "test"
+
+
+@register_router
+class Router(RouterBase):
+    namespace = ""
+    urls = [
+        path("", IndexView),
+    ]
