a7f21ffd569a3bfee201de463b9fb0fae7d1d30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 07:03:59 +0000

    Add package ezoff/0.1.27/ezoff-0.1.27-py3-none-any.whl

---
+++ packages/ezoff/ezoff-0.1.27-py3-none-any.whl/ezoff/ezoff.py
@@ -0,0 +1,12 @@
+"""
+Module for interacting with the EZOffice Inventory API
+"""
+
+from .assets import *
+from .exceptions import *
+from .groups import *
+from .inventory import *
+from .locations import *
+from .members import *
+from .projects import *
+from .workorders import *
