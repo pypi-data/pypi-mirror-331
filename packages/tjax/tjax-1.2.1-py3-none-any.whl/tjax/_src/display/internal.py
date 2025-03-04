b8ddd5a0ab694b94db0ad0548424680d0061dd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 15:04:45 +0000

    Add package tjax/1.2.1/tjax-1.2.1-py3-none-any.whl

---
+++ packages/tjax/tjax-1.2.1-py3-none-any.whl/tjax/_src/display/internal.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from rich.console import Console
+from rich.tree import Tree
+
+from .display_generic import display_generic
+
+# Global variables ---------------------------------------------------------------------------------
+global_console = Console()
+
+
+# Functions ----------------------------------------------------------------------------------------
+def internal_print_generic(*args: object,
+                           raise_on_nan: bool = True,
+                           console: Console | None = None,
+                           **kwargs: object) -> None:
+    if console is None:
+        console = global_console
+    found_nan = False
+    root = Tree("", hide_root=True)
+    for value in args:
+        _ = root.add(display_generic(value, seen=set()))
+        found_nan = found_nan or raise_on_nan and 'nan' in str(root)
+    for key, value in kwargs.items():
+        _ = root.add(display_generic(value, seen=set(), key=key))
+        found_nan = found_nan or raise_on_nan and 'nan' in str(root)
+    console.print(root)
+    if found_nan:
+        assert False  # noqa: PT015
---
+++ packages/tjax/tjax-1.2.1-py3-none-any.whl/tjax/_src/display/print_generic.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from typing import Any
+
+import numpy as np
+from jax import debug, tree
+from rich.console import Console
+
+from .internal import internal_print_generic
+
+
+def print_generic(*args: object,
+                  raise_on_nan: bool = True,
+                  immediate: bool = False,
+                  console: Console | None = None,
+                  **kwargs: object
+                  ) -> None:
+    """Uses internal_print_generic in a tapped function.
+
+    Args:
+        raise_on_nan: Assert if NaN is found in the output.
+        immediate: Print the value immediately even if they're tracers.  Do not wait for the
+            compiled function to be called.
+        console: The console that formats the output.
+        args: Positional arguments to be printed.  Only dynamic arguments are allowed.
+        kwargs: Keyword arguments to be printed.  Only static keys and dynamic values are allowed.
+    """
+    if immediate:
+        internal_print_generic(*args, raise_on_nan=raise_on_nan, console=console, **kwargs)
+        return
+
+    leaves, tree_def = tree.flatten((args, kwargs))
+
+    def fix(u_arg: Any, arg: Any) -> Any:
+        return (np.asarray(u_arg) if isinstance(arg, np.ndarray)
+                else int(u_arg.item()) if isinstance(arg, int)
+                else u_arg)
+
+    def callback(*callback_leaves: object) -> None:
+        unflattened_tree = tree.unflatten(tree_def, callback_leaves)
+        v_args, v_kwargs = tree.map(fix, unflattened_tree, (args, kwargs))
+        internal_print_generic(*v_args, raise_on_nan=raise_on_nan, console=console, **v_kwargs)
+
+    debug.callback(callback, *leaves, ordered=True)
