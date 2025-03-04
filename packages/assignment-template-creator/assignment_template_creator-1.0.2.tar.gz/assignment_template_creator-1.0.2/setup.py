2c075898935e1ca80e16e93f4b3a2e1ab18e1bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 15:04:27 +0000

    Add package assignment-template-creator/1.0.2/assignment_template_creator-1.0.2.tar.gz

---
+++ packages/assignment-template-creator/assignment_template_creator-1.0.2.tar.gz/assignment_template_creator-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="assignment_template_creator",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "colorama",
+        "python-docx"],
+    entry_points={
+        "console_scripts": [
+            "create_template=assignment_template_creator:main"]},
+    author="stasik",
+    description="the ultimate tool fpor CS assignment template generatipon.",
+    classifiers=[
+        "Programming Language :: Python :: 3", 
+        ],
+    )
