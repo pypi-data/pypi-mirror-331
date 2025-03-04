11751b5d567a0ac1a419f147ad5784ce88e4f9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:04:37 +0000

    Add package tppc/5.0.1/tppc-5.0.1.tar.gz

---
+++ packages/tppc/tppc-5.0.1.tar.gz/tppc-5.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tppc",
+    version="5.0.1",
+    author="Sean Tichenor",
+    description="T++ Quantum Execution Framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YOUR_GITHUB_USERNAME/tppc",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    entry_points={
+        "console_scripts": [
+            "tppc=tppc.__main__:main",
+        ],
+    },
+    install_requires=["pyyaml"],
+)
---
+++ packages/tppc/tppc-5.0.1.tar.gz/tppc-5.0.1/tppc/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from tppc import TPPC
+
+def main():
+    """Main function to execute the T++ interpreter."""
+    tppc = TPPC()
+    tppc.run()
+
+if __name__ == "__main__":
+    main()
