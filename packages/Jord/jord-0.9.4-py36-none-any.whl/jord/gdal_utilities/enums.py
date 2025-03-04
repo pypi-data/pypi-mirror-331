b32cebc9c0fe7d43d398b25365476edc6588498f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 18:06:09 +0000

    Add package Jord/0.9.4/jord-0.9.4-py36-none-any.whl

---
+++ packages/Jord/jord-0.9.4-py36-none-any.whl/jord/gdal_utilities/enums.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+__author__ = "heider"
+__doc__ = r"""
+
+           Created on 5/5/22
+           """
+
+from enum import Enum
+
+from jord.gdal_utilities.importing import GDAL
+
+__all__ = ["GDAL"]
+
+
+class GdalAccessEnum(Enum):
+    """
+    Enum for GDAL.Access
+    """
+
+    read_only = GDAL.GA_ReadOnly  # Default  = 0
+    """  Read-only access."""
+
+    update = GDAL.GA_Update
+    """  Update access."""
---
+++ packages/Jord/jord-0.9.4-py36-none-any.whl/jord/qlive_utilities/clients/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from .auto import *
+from .interfaced import *
