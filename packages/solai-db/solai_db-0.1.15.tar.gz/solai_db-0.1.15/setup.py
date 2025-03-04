8b9d5c867d3ef25bdad916b2a80f280c21404ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:05:20 +0000

    Add package solai-db/0.1.15/solai_db-0.1.15.tar.gz

---
+++ packages/solai-db/solai_db-0.1.15.tar.gz/solai_db-0.1.15/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="solai-db",
+    packages=find_packages(
+        include=["solai_db", "solai_db.*"]
+    ),  # This will include all subpackages
+    version="0.1.15",
+    author="SolAI",
+    description="The database for SolAI",
+    install_requires=[
+        "sqlalchemy>=2.0.0",
+        "psycopg2-binary>=2.9.0",
+    ],
+)
