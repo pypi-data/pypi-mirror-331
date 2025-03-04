4b90ed77641a2109e900f9f8dddbb5e2cd89d855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:47 +0000

    Add package pybud-gui/3.0.2/pybud_gui-3.0.2-cp313-cp313-macosx_11_0_universal2.whl

---
+++ packages/pybud-gui/pybud_gui-3.0.2-cp313-cp313-macosx_11_0_universal2.whl/pybud/callbacks/__init__.py
@@ -0,0 +1,96 @@
+from ..drawer import Drawer
+
+# base class for all callback contexts
+
+class CallbackContext:
+    """
+    `CallbackContext` stores the callback's identifier, whether or not the callback is cancelled, 
+    the result of the callback, and the information that is passed to the callback funtion.
+    """
+
+    def __init__(self, id: str = None):
+        self.id = id
+        self.result = None
+        self._is_cancelled = False
+
+    def get_callback_id(self) -> int:
+        return self.id
+    
+    def get_id(self) -> int:
+        return self.id
+    
+    def get_result(self):
+        pass
+    
+    def set_result(self, *args, **kwargs):
+        pass
+
+    def cancel(self) -> bool:
+        self._is_cancelled = True
+
+    def is_cancelled(self) -> bool:
+        return self._is_cancelled
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, result={self.result}, is_cancelled={self._is_cancelled}, ...)"
+
+# actual callback classes
+
+class OnUpdateContext(CallbackContext):
+    """
+    A child of `CallbackContext` for the update callback with id="on_update".
+    """
+    
+    def __init__(self, tick: int = None, key: str = None):
+        super().__init__(id = "on_update")
+        self.tick = tick
+        self.key = key
+
+    def get_tick(self) -> int:
+        return self.tick
+
+    def get_key(self) -> str | None:
+        return self.key
+
+class OnDrawContext(CallbackContext):
+    """
+    A child of `CallbackContext` for screens/widgets closing callback with id="on_draw".
+    """
+
+    def __init__(self, drawer: Drawer):
+        super().__init__(id = "on_draw")
+        self.drawer = drawer
+
+    def get_drawer(self):
+        return self.drawer
+
+class OnFocusAddedContext(CallbackContext):
+    """
+    A child of `CallbackContext` for screens closing callback with id="on_focus_added".
+    """
+
+    def __init__(self):
+        super().__init__(id = "on_focus_added")
+
+
+class OnFocusLostContext(CallbackContext):
+    """
+    A child of `CallbackContext` for screens closing callback with id="on_focus_lost".
+    """
+
+    def __init__(self):
+        super().__init__(id = "on_focus_lost")
+
+
+class OnKeyboardInputContext(CallbackContext):
+    """
+    A child of `CallbackContext` for screens closing callback with id="on_keyboard_input".
+    """
+
+    def __init__(self, key: str):
+        super().__init__(id = "on_keyboard_input")
+        assert key is not None, "Expected `key` to be of type `str` but got `None`."
+        self.key = key
+    
+    def get_key(self):
+        return self.key
---
+++ packages/pybud-gui/pybud_gui-3.0.2-cp313-cp313-macosx_11_0_universal2.whl/pybud/session.py
@@ -0,0 +1,179 @@
+import time
+from threading import Thread
+
+from readchar import key as KeyPress
+from readchar import readkey
+
+from .drawer import Drawer, color, ansi
+from .window import Window
+
+from .datatypes import Size, Color
+
+from .callbacks import OnUpdateContext
+
+def get_admination_at(tick, n = 3, animation = "▁▂▃▄▅▆▆▅▄▃▂▁▂ "):
+    return animation[tick % (len(animation)-n):tick % (len(animation)-n)+n]
+
+class UpdateHandler:
+    def __init__(self, update_fn, tps: int = 20):
+        self.tps = tps
+        self.closed = False
+        self.tick = 0
+
+        self.tickupdate_thread: Thread = None
+
+        self.update_fn = update_fn
+    
+    def start(self):
+        if self.tickupdate_thread is None:
+            self.tickupdate_thread = Thread(target=self.do_tick_updates)
+            self.tickupdate_thread.start()
+        self.do_key_updates()
+
+    def stop(self):
+        self.closed = True
+        while self.tickupdate_thread.is_alive():
+            time.sleep(0.01)
+        self.tickupdate_thread = None
+
+    def is_running(self):
+        return (not self.closed) and (self.tickupdate_thread is not None) and self.tickupdate_thread.is_alive()
+    
+    def do_tick_updates(self):
+        start_time = time.time()
+        self.tick = 0
+        while not self.closed:
+            self.update_fn(OnUpdateContext(self.tick))
+
+            schedule_ahead = start_time + (self.tick / self.tps) - time.time()
+            
+            # if we're too far off the schedule, reset, instead of sprinting
+            if schedule_ahead > 5:
+                # TODO: display a warning
+                start_time = time.time()
+
+            time.sleep(max(0, schedule_ahead))
+            self.tick += 1
+
+    def do_key_updates(self):
+        while not self.closed:
+            try:
+                key = readkey()
+            except KeyboardInterrupt:
+                key = KeyPress.CTRL_C
+            if self.closed:
+                break
+            self.update_fn(OnUpdateContext(self.tick, key = key))
+
+
+class Session:
+    """
+    A `Session` is main handler for all `Window`s, each `Session` contains a list of ordered windows.
+
+    Additonally creates a `Drawer` instance and draws `Window`s in order,
+    Also, adds functionality for a `Window` to appear on top of other `Window`s,
+    Lastly, handles keyboard updates and tick updates for all windows with an instance of `UpdateHandler`.
+    """
+
+    def __init__(
+        self,
+        size: Size | tuple[int, int],
+        background: Color | tuple[int, int, int],
+        color_mode: color.ColorMode = None,
+        allow_resize: bool = False
+    ):
+        if not isinstance(size, (Size, tuple)):
+            raise TypeError(
+                f"Expected size to be of type `datatypes.Size` or `tuple[int, int]` but got {type(size)}."
+            )
+        if isinstance(size, tuple):
+            size = Size(*size)
+
+        self.size = size
+
+        if not isinstance(background, (Color, tuple)):
+            raise TypeError(
+                f"Expected background to be of type `datatypes.Color` or `tuple[int, int, int]` but got {type(background)}."
+            )
+        if isinstance(background, tuple):
+            background = Color(*background)
+
+        self.background = background
+
+        if allow_resize:
+            raise NotImplementedError("`allow_resize` is not yet implemented but will be released in a future update.")
+
+        self.allow_resize = allow_resize
+        
+        self.color_mode = color.ColorMode.TRUECOLOR if color_mode is None else color_mode
+        
+        self.update_handler = UpdateHandler(update_fn=self.update)
+
+        self.window_buffer: list[Window] = []
+
+        self.drawer = Drawer(width=size.width, height=size.height, plane_color=background.get_rgb())
+        
+        self.draw_lock = False
+    
+    def add_window(self, window: Window):
+        if window in self.window_buffer:
+            self.window_buffer.remove(window)
+        self.window_buffer.append(window)
+        self.bring_window_to_front(window)
+        self.update_focus()
+    
+    def bring_window_to_front(self, window: Window):
+        # TODO: Don't update unnecessary widgets
+        for w in self.window_buffer:
+            if w is window:
+                w._set_depth(0)
+            else:
+                w._set_depth(1+w._get_depth())
+
+    def update_focus(self):
+        for i, window in enumerate(self.window_buffer):
+            if (i + 1) == len(self.window_buffer):
+                window.focus()
+            else:
+                window.unfocus()
+
+    def show(self):
+        for window in self.window_buffer:
+            window.open()
+        self.update_handler.start()
+        while self.update_handler.is_running():
+            time.sleep(0.01)
+
+    def close(self):
+        print(("\r" + (" " * self.size.width) + "\n") * (self.size.height) , end="")
+        print(f"\033[{self.size.height}F", end="")
+        self.update_handler.stop()
+
+    def update(self, context: OnUpdateContext):
+        for window in reversed(self.window_buffer):
+            if window.is_in_focus:
+                window.update(context)
+        self.draw()
+
+    def clear(self):
+        self.drawer.clear()
+    
+    def draw(self):
+        if not self.draw_lock:
+            self.draw_lock = True
+            any_window_is_open = False
+            
+            depth_order_windows = sorted(self.window_buffer, key = lambda x: x._get_depth())
+            for window in depth_order_windows:
+                if window.is_open:
+                    any_window_is_open = True
+                    window.draw(self.drawer)
+            if not any_window_is_open:
+                self.close()
+                self.draw_lock = False
+                return
+            
+            self.drawer.place(ansi.AnsiString(get_admination_at(self.update_handler.tick)), pos=(0, self.size.width-4), assign = False)
+            print(self.drawer.render(self.color_mode)[:-1], end="")
+            print(f"\033[{self.size.height-1}F", end="")
+            self.draw_lock = False
