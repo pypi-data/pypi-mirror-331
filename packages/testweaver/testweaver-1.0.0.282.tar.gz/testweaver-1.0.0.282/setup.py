76f06918f2d74e8c4fbbe729042c7671617d2279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 15:04:50 +0000

    Add package testweaver/1.0.0.282/testweaver-1.0.0.282.tar.gz

---
+++ packages/testweaver/testweaver-1.0.0.282.tar.gz/testweaver-1.0.0.282/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testweaver', # no kebab-casing, or the import won't work 
+    version='1.0.0.282',
+    author='Albert Willett Jr',
+    author_email='albert.willett2@gmail.com',
+    description='A module for generating input handling test cases',
+    packages=find_packages(),
+    install_requires=[
+        "basicfeatureflags",
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3.12',
+    ],
+)
