e20819b126e644b007f656809cbf7fbcf7e0418c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 15:04:15 +0000

    Add package auxknow/0.0.8/auxknow-0.0.8-py3-none-any.whl

---
+++ packages/auxknow/auxknow-0.0.8-py3-none-any.whl/auxknow/constants.py
@@ -0,0 +1,10 @@
+"""
+A module to represent various constants used in the application.
+"""
+
+DEFAULT_ANSWER_LENGTH_PARAGRAPHS: int = 3
+DEFAULT_LINES_PER_PARAGRAPH: int = 5
+MAX_ANSWER_LENGTH_PARAGRAPHS: int = 8
+MAX_LINES_PER_PARAGRAPH: int = 10
+DEFAULT_PERPLEXITY_MODEL: str = "sonar-pro"
+MAX_CONTEXT_TOKENS: int = 4096
