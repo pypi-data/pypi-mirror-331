1365beff02fcc6e14c6231e1fb0f583a1777adb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 06:03:53 +0000

    Add package ontol/2.10.1/ontol-2.10.1-py3-none-any.whl

---
+++ packages/ontol/ontol-2.10.1-py3-none-any.whl/ontol/__init__.py
@@ -0,0 +1,38 @@
+from .oast import (
+    ASTNode,
+    Ontology,
+    Term,
+    Function,
+    Relationship,
+    Meta,
+    FunctionArgument,
+    RelationshipType,
+    FunctionAttributes,
+    RelationshipAttributes,
+    RelationshipDirection,
+    TermAttributes,
+)
+from .parser import Parser
+from .plantuml import PlantUML
+from .serializer import JSONSerializer
+from .cli import CLI
+
+
+__all__ = (
+    'ASTNode',
+    'Ontology',
+    'Term',
+    'Function',
+    'Relationship',
+    'FunctionAttributes',
+    'RelationshipAttributes',
+    'RelationshipDirection',
+    'TermAttributes',
+    'Meta',
+    'FunctionArgument',
+    'RelationshipType',
+    'Parser',
+    'PlantUML',
+    'JSONSerializer',
+    'CLI',
+)
