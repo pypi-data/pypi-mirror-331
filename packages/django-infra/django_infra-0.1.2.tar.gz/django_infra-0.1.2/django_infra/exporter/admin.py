75364569c43d86ac6535cd1ca469f8f1fbb161aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:45 +0000

    Add package django-infra/0.1.2/django_infra-0.1.2.tar.gz

---
+++ packages/django-infra/django_infra-0.1.2.tar.gz/django_infra-0.1.2/django_infra/exporter/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import QueryExport
+
+
+@admin.register(QueryExport)
+class ExporterAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+
+    )
+    list_filter = ("state",)
+    search_fields = ("id",)
+
