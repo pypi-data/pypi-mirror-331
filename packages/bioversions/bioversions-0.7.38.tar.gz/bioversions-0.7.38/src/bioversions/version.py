320dc9a15dcf2e6f6370633cdecffb019abbd06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 14:05:31 +0000

    Add package bioversions/0.7.38/bioversions-0.7.38.tar.gz

---
+++ packages/bioversions/bioversions-0.7.38.tar.gz/bioversions-0.7.38/src/bioversions/version.py
@@ -0,0 +1,25 @@
+"""Version information for bioversions."""
+
+import os
+from subprocess import CalledProcessError, check_output
+
+__all__ = [
+    "VERSION",
+]
+
+VERSION = "0.7.38"
+
+
+def get_git_hash() -> str:
+    """Get the bioversions git hash."""
+    with open(os.devnull, "w") as devnull:
+        try:
+            ret = check_output(
+                ["git", "rev-parse", "HEAD"],
+                cwd=os.path.dirname(__file__),
+                stderr=devnull,
+            )
+        except CalledProcessError:
+            return "UNHASHED"
+        else:
+            return ret.strip().decode("utf-8")[:8]
