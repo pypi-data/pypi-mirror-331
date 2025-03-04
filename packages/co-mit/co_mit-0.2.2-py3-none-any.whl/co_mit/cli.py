3e9f6e85ae7394cd1cfeb3ba06474e4dff38c471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 16:04:36 +0000

    Add package co-mit/0.2.2/co_mit-0.2.2-py3-none-any.whl

---
+++ packages/co-mit/co_mit-0.2.2-py3-none-any.whl/co_mit/cli.py
@@ -0,0 +1,52 @@
+import asyncio
+import os
+
+import rich
+import rich_click as click
+
+from . import commit, config
+
+
+@click.command()
+@click.option(
+    "--openai-key",
+    "-k",
+    type=click.STRING,
+    help="OpenAI API key. Can also set with OPENAI_API_KEY environment variable.",
+)
+@click.option(
+    "--example",
+    "-e",
+    type=click.STRING,
+    help="Example input to generate a commit message from.",
+)
+@click.option(
+    "--quiet",
+    "-q",
+    is_flag=True,
+    help="Suppress all output other than final commit message. Useful for scripting. Can also set with CO_MIT_QUIET environment variable.",
+)
+@click.option(
+    "--version",
+    "-v",
+    is_flag=True,
+    help="Show version information.",
+)
+def main(
+    openai_key: str | None, example: str | None, quiet: bool, version: bool
+) -> None:
+    """Helps with git commits."""
+
+    if version:
+        from . import __about__
+
+        click.echo(f"co-mit version {__about__.__version__}")
+        return
+
+    if openai_key:
+        os.environ["OPENAI_API_KEY"] = openai_key
+    if quiet:
+        config.Config.quiet = quiet
+    else:
+        rich.print("Generating commit message...")
+    asyncio.run(commit.co_mit(example))
