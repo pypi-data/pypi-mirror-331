f53110e1c45a458d55b68c05be772c9a6fa0ec46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:05:28 +0000

    Add package oxgram/0.0.6/oxgram-0.0.6.tar.gz

---
+++ packages/oxgram/oxgram-0.0.6.tar.gz/oxgram-0.0.6/Oxgram/exceptions/exceptions.py
@@ -0,0 +1,26 @@
+class ExpiredPremium(Exception):
+    pass
+
+class LimitedMessage(Exception):
+    pass
+
+class SecuredMessage(Exception):
+    pass
+
+class Accessblocked(Exception):
+    pass
+
+class CancelledTask(Exception):
+    pass
+
+class InvalidReply(Exception):
+    pass
+
+class Positivemode(Exception):
+    pass
+
+class Negativemode(Exception):
+    pass
+
+class Cancelled(Exception):
+    pass
