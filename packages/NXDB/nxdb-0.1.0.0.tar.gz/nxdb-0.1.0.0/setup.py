380b4b020803877e6a8d146d87a386a4d2ac76c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:03:57 +0000

    Add package NXDB/0.1/nxdb-0.1.0.0.tar.gz

---
+++ packages/NXDB/nxdb-0.1.0.0.tar.gz/nxdb-0.1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NXDB",
+    version="0.1.0.0",
+    packages=find_packages(),
+    install_requires=["pyyaml"],
+    entry_points={
+        "console_scripts": [
+            'nxdb-cli = nxdb.cli:main'
+        ]
+    },
+    author="VladosNX",
+    author_email="email.name@email.com"
+)
+
+# Run `python3 setup.py sdist bdist_wheel`
