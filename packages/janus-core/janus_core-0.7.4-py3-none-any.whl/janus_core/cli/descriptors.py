d020a3738d89d3740c8c7742ae7b34ec11c9199c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:47 +0000

    Add package janus-core/0.7.4/janus_core-0.7.4-py3-none-any.whl

---
+++ packages/janus-core/janus_core-0.7.4-py3-none-any.whl/janus_core/cli/descriptors.py
@@ -0,0 +1,192 @@
+"""Set up MLIP descriptors commandline interface."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Annotated
+
+from typer import Context, Option, Typer
+from typer_config import use_config
+
+from janus_core.cli.types import (
+    Architecture,
+    CalcKwargs,
+    Device,
+    LogPath,
+    ModelPath,
+    ReadKwargsAll,
+    StructPath,
+    Summary,
+    WriteKwargs,
+)
+from janus_core.cli.utils import yaml_converter_callback
+
+app = Typer()
+
+
+@app.command()
+@use_config(yaml_converter_callback)
+def descriptors(
+    # numpydoc ignore=PR02
+    ctx: Context,
+    struct: StructPath,
+    invariants_only: Annotated[
+        bool,
+        Option(help="Only calculate invariant descriptors."),
+    ] = True,
+    calc_per_element: Annotated[
+        bool,
+        Option(help="Calculate mean descriptors for each element."),
+    ] = False,
+    calc_per_atom: Annotated[
+        bool,
+        Option(help="Calculate descriptors for each atom."),
+    ] = False,
+    arch: Architecture = "mace_mp",
+    device: Device = "cpu",
+    model_path: ModelPath = None,
+    out: Annotated[
+        Path | None,
+        Option(
+            help=(
+                "Path to save structure with calculated descriptors. Default is "
+                "inferred from name of structure file."
+            ),
+        ),
+    ] = None,
+    read_kwargs: ReadKwargsAll = None,
+    calc_kwargs: CalcKwargs = None,
+    write_kwargs: WriteKwargs = None,
+    log: LogPath = None,
+    tracker: Annotated[
+        bool, Option(help="Whether to save carbon emissions of calculation")
+    ] = True,
+    summary: Summary = None,
+) -> None:
+    """
+    Calculate MLIP descriptors for the given structure(s).
+
+    Parameters
+    ----------
+    ctx
+        Typer (Click) Context. Automatically set.
+    struct
+        Path of structure to simulate.
+    invariants_only
+        Whether only the invariant descriptors should be returned. Default is True.
+    calc_per_element
+        Whether to calculate mean descriptors for each element. Default is False.
+    calc_per_atom
+        Whether to calculate descriptors for each atom. Default is False.
+    arch
+        MLIP architecture to use for single point calculations.
+        Default is "mace_mp".
+    device
+        Device to run model on. Default is "cpu".
+    model_path
+        Path to MLIP model. Default is `None`.
+    out
+        Path to save structure with calculated results. Default is inferred from name
+        of the structure file.
+    read_kwargs
+        Keyword arguments to pass to ase.io.read. By default,
+            read_kwargs["index"] is ":".
+    calc_kwargs
+        Keyword arguments to pass to the selected calculator. Default is {}.
+    write_kwargs
+        Keyword arguments to pass to ase.io.write when saving results. Default is {}.
+    log
+        Path to write logs to. Default is inferred from the name of the structure file.
+    tracker
+        Whether to save carbon emissions of calculation in log file and summary.
+        Default is True.
+    summary
+        Path to save summary of inputs, start/end time, and carbon emissions. Default
+        is inferred from the name of the structure file.
+    config
+        Path to yaml configuration file to define the above options. Default is None.
+    """
+    from janus_core.calculations.descriptors import Descriptors
+    from janus_core.cli.utils import (
+        carbon_summary,
+        check_config,
+        end_summary,
+        parse_typer_dicts,
+        save_struct_calc,
+        start_summary,
+    )
+
+    # Check options from configuration file are all valid
+    check_config(ctx)
+
+    [read_kwargs, calc_kwargs, write_kwargs] = parse_typer_dicts(
+        [read_kwargs, calc_kwargs, write_kwargs]
+    )
+
+    # Check optimized structure path not duplicated
+    if "filename" in write_kwargs:
+        raise ValueError("'filename' must be passed through the --out option")
+
+    # Set default filname for writing structure with descriptors if not specified
+    if out:
+        write_kwargs["filename"] = out
+
+    log_kwargs = {"filemode": "w"}
+    if log:
+        log_kwargs["filename"] = log
+
+    # Dictionary of inputs for Descriptors class
+    descriptors_kwargs = {
+        "struct": struct,
+        "arch": arch,
+        "device": device,
+        "model_path": model_path,
+        "read_kwargs": read_kwargs,
+        "calc_kwargs": calc_kwargs,
+        "attach_logger": True,
+        "log_kwargs": log_kwargs,
+        "track_carbon": tracker,
+        "invariants_only": invariants_only,
+        "calc_per_element": calc_per_element,
+        "calc_per_atom": calc_per_atom,
+        "write_results": True,
+        "write_kwargs": write_kwargs,
+    }
+
+    # Initialise descriptors
+    descript = Descriptors(**descriptors_kwargs)
+
+    # Set summary and log files
+    summary = descript._build_filename(
+        "descriptors-summary.yml", filename=summary
+    ).absolute()
+    log = descript.log_kwargs["filename"]
+
+    # Store inputs for yaml summary
+    inputs = descriptors_kwargs.copy()
+
+    # Add structure, MLIP information, and log to inputs
+    save_struct_calc(
+        inputs=inputs,
+        struct=descript.struct,
+        struct_path=struct,
+        arch=arch,
+        device=device,
+        model_path=model_path,
+        read_kwargs=read_kwargs,
+        calc_kwargs=calc_kwargs,
+        log=log,
+    )
+
+    # Save summary information before calculation begins
+    start_summary(command="descriptors", summary=summary, inputs=inputs)
+
+    # Calculate descriptors
+    descript.run()
+
+    # Save carbon summary
+    if tracker:
+        carbon_summary(summary=summary, log=log)
+
+    # Time after calculation has finished
+    end_summary(summary)
---
+++ packages/janus-core/janus_core-0.7.4-py3-none-any.whl/janus_core/cli/utils.py
@@ -0,0 +1,311 @@
+"""Utility functions for CLI."""
+
+from __future__ import annotations
+
+from collections.abc import Sequence
+import datetime
+import logging
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+from typer_config import conf_callback_factory, yaml_loader
+import yaml
+
+if TYPE_CHECKING:
+    from ase import Atoms
+    from typer import Context
+
+    from janus_core.cli.types import TyperDict
+    from janus_core.helpers.janus_types import (
+        Architectures,
+        ASEReadArgs,
+        Devices,
+        MaybeSequence,
+    )
+
+
+def dict_paths_to_strs(dictionary: dict) -> None:
+    """
+    Recursively iterate over dictionary, converting Path values to strings.
+
+    Parameters
+    ----------
+    dictionary
+        Dictionary to be converted.
+    """
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            dict_paths_to_strs(value)
+        elif isinstance(value, Path):
+            dictionary[key] = str(value)
+
+
+def dict_tuples_to_lists(dictionary: dict) -> None:
+    """
+    Recursively iterate over dictionary, converting tuple values to lists.
+
+    Parameters
+    ----------
+    dictionary
+        Dictionary to be converted.
+    """
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            dict_paths_to_strs(value)
+        elif isinstance(value, tuple):
+            dictionary[key] = list(value)
+
+
+def dict_remove_hyphens(dictionary: dict) -> dict:
+    """
+    Recursively iterate over dictionary, replacing hyphens with underscores in keys.
+
+    Parameters
+    ----------
+    dictionary
+        Dictionary to be converted.
+
+    Returns
+    -------
+    dict
+        Dictionary with hyphens in keys replaced with underscores.
+    """
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            dictionary[key] = dict_remove_hyphens(value)
+    return {k.replace("-", "_"): v for k, v in dictionary.items()}
+
+
+def set_read_kwargs_index(read_kwargs: dict[str, Any]) -> None:
+    """
+    Set default read_kwargs["index"] to final image and check its value is an integer.
+
+    To ensure only a single Atoms object is read, slices such as ":" are forbidden.
+
+    Parameters
+    ----------
+    read_kwargs
+        Keyword arguments to be passed to ase.io.read. If specified,
+        read_kwargs["index"] must be an integer, and if not, a default value
+        of -1 is set.
+    """
+    read_kwargs.setdefault("index", -1)
+    try:
+        int(read_kwargs["index"])
+    except ValueError as e:
+        raise ValueError("`read_kwargs['index']` must be an integer") from e
+
+
+def parse_typer_dicts(typer_dicts: list[TyperDict]) -> list[dict]:
+    """
+    Convert list of TyperDict objects to list of dictionaries.
+
+    Parameters
+    ----------
+    typer_dicts
+        List of TyperDict objects to convert.
+
+    Returns
+    -------
+    list[dict]
+        List of converted dictionaries.
+
+    Raises
+    ------
+    ValueError
+        If items in list are not converted to dicts.
+    """
+    for i, typer_dict in enumerate(typer_dicts):
+        typer_dicts[i] = typer_dict.value if typer_dict else {}
+        if not isinstance(typer_dicts[i], dict):
+            raise ValueError(
+                f"""{typer_dicts[i]} must be passed as a dictionary wrapped in quotes.\
+ For example, "{{'key': value}}" """
+            )
+    return typer_dicts
+
+
+def yaml_converter_loader(config_file: str) -> dict[str, Any]:
+    """
+    Load yaml configuration and replace hyphens with underscores.
+
+    Parameters
+    ----------
+    config_file
+        Yaml configuration file to read.
+
+    Returns
+    -------
+    dict[str, Any]
+        Dictionary with loaded configuration.
+    """
+    if not config_file:
+        return {}
+
+    config = yaml_loader(config_file)
+    # Replace all "-"" with "_" in conf
+    return dict_remove_hyphens(config)
+
+
+yaml_converter_callback = conf_callback_factory(yaml_converter_loader)
+
+
+def start_summary(*, command: str, summary: Path, inputs: dict) -> None:
+    """
+    Write initial summary contents.
+
+    Parameters
+    ----------
+    command
+        Name of CLI command being used.
+    summary
+        Path to summary file being saved.
+    inputs
+        Inputs to CLI command to save.
+    """
+    save_info = {
+        "command": f"janus {command}",
+        "start_time": datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"),
+        "inputs": inputs,
+    }
+    with open(summary, "w", encoding="utf8") as outfile:
+        yaml.dump(save_info, outfile, default_flow_style=False)
+
+
+def carbon_summary(*, summary: Path, log: Path) -> None:
+    """
+    Calculate and write carbon tracking summary.
+
+    Parameters
+    ----------
+    summary
+        Path to summary file being saved.
+    log
+        Path to log file with carbon emissions saved.
+    """
+    with open(log, encoding="utf8") as file:
+        logs = yaml.safe_load(file)
+
+    emissions = sum(
+        lg["message"]["emissions"]
+        for lg in logs
+        if isinstance(lg["message"], dict) and "emissions" in lg["message"]
+    )
+
+    with open(summary, "a", encoding="utf8") as outfile:
+        yaml.dump({"emissions": emissions}, outfile, default_flow_style=False)
+
+
+def end_summary(summary: Path) -> None:
+    """
+    Write final time to summary and close.
+
+    Parameters
+    ----------
+    summary
+        Path to summary file being saved.
+    """
+    with open(summary, "a", encoding="utf8") as outfile:
+        yaml.dump(
+            {"end_time": datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")},
+            outfile,
+            default_flow_style=False,
+        )
+    logging.shutdown()
+
+
+def save_struct_calc(
+    *,
+    inputs: dict,
+    struct: MaybeSequence[Atoms],
+    struct_path: Path,
+    arch: Architectures,
+    device: Devices,
+    model_path: str,
+    read_kwargs: ASEReadArgs,
+    calc_kwargs: dict[str, Any],
+    log: Path,
+) -> None:
+    """
+    Add structure and calculator input information to a dictionary.
+
+    Parameters
+    ----------
+    inputs
+        Inputs dictionary to add information to.
+    struct
+        Structure to be simulated.
+    struct_path
+        Path of structure file.
+    arch
+        MLIP architecture.
+    device
+        Device to run calculations on.
+    model_path
+        Path to MLIP model.
+    read_kwargs
+        Keyword arguments to pass to ase.io.read.
+    calc_kwargs
+        Keyword arguments to pass to the calculator.
+    log
+        Path to log file.
+    """
+    from ase import Atoms
+
+    # Clean up duplicate parameters
+    for key in (
+        "struct",
+        "struct_path",
+        "arch",
+        "device",
+        "model_path",
+        "read_kwargs",
+        "calc_kwargs",
+        "log_kwargs",
+    ):
+        inputs.pop(key, None)
+
+    if isinstance(struct, Atoms):
+        inputs["struct"] = {
+            "n_atoms": len(struct),
+            "struct_path": struct_path,
+            "formula": struct.get_chemical_formula(),
+        }
+    elif isinstance(struct, Sequence):
+        inputs["traj"] = {
+            "length": len(struct),
+            "struct_path": struct_path,
+            "struct": {
+                "n_atoms": len(struct[0]),
+                "formula": struct[0].get_chemical_formula(),
+            },
+        }
+
+    inputs["calc"] = {
+        "arch": arch,
+        "device": device,
+        "model_path": model_path,
+        "read_kwargs": read_kwargs,
+        "calc_kwargs": calc_kwargs,
+    }
+
+    inputs["log"] = log
+
+    # Convert all paths to strings in inputs nested dictionary
+    dict_paths_to_strs(inputs)
+
+
+def check_config(ctx: Context) -> None:
+    """
+    Check options in configuration file are valid options for CLI command.
+
+    Parameters
+    ----------
+    ctx
+        Typer (Click) Context within command.
+    """
+    # Compare options from config file (default_map) to function definition (params)
+    for option in ctx.default_map:
+        # Check options individually so can inform user of specific issue
+        if option not in ctx.params:
+            raise ValueError(f"'{option}' in configuration file is not a valid option")
