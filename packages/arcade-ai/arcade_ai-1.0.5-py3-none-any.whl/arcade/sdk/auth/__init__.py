d3edc1ae90c41fa77ea90ad9959f034620d9ddb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 14:05:31 +0000

    Add package arcade-ai/1.0.5/arcade_ai-1.0.5-py3-none-any.whl

---
+++ packages/arcade-ai/arcade_ai-1.0.5-py3-none-any.whl/arcade/sdk/auth/__init__.py
@@ -0,0 +1,29 @@
+from arcade.core.auth import (
+    Atlassian,
+    Discord,
+    Dropbox,
+    GitHub,
+    Google,
+    LinkedIn,
+    OAuth2,
+    Slack,
+    Spotify,
+    ToolAuthorization,
+    X,
+    Zoom,
+)
+
+__all__ = [
+    "Atlassian",
+    "Discord",
+    "Dropbox",
+    "GitHub",
+    "Google",
+    "LinkedIn",
+    "OAuth2",
+    "Slack",
+    "Spotify",
+    "ToolAuthorization",
+    "X",
+    "Zoom",
+]
