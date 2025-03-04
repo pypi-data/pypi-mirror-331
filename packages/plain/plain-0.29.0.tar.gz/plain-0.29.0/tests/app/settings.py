64af41953d0b684a8b785e4df81e1a77e935249a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:05 +0000

    Add package plain/0.29.0/plain-0.29.0.tar.gz

---
+++ packages/plain/plain-0.29.0.tar.gz/plain-0.29.0/tests/app/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = "secret"
+DEBUG = True
+
+INSTALLED_PACKAGES = [
+    "app.test",
+]
+
+EXPLICIT_SETTING = "explicitly changed"
+ENV_OVERRIDDEN_SETTING = "explicitly overridden"
---
+++ packages/plain/plain-0.29.0.tar.gz/plain-0.29.0/tests/app/urls.py
@@ -0,0 +1,15 @@
+from plain.urls import RouterBase, path, register_router
+from plain.views import View
+
+
+class TestView(View):
+    def get(self):
+        return "Hello, world!"
+
+
+@register_router
+class Router(RouterBase):
+    namespace = ""
+    urls = [
+        path("", TestView),
+    ]
