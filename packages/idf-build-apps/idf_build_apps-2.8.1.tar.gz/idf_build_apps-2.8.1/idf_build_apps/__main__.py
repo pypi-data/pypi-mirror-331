39f7cc29d01875b4100066513f3f64f51a8173ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 18:05:44 +0000

    Add package idf-build-apps/2.8.1/idf_build_apps-2.8.1.tar.gz

---
+++ packages/idf-build-apps/idf_build_apps-2.8.1.tar.gz/idf_build_apps-2.8.1/idf_build_apps/__main__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+from .main import (
+    main,
+)
+
+if __name__ == '__main__':
+    main()
