f90b749b2c3505af807edcb68e44054724a1cc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 07:03:52 +0000

    Add package changesets/0.3.0/changesets-0.3.0.tar.gz

---
+++ packages/changesets/changesets-0.3.0.tar.gz/changesets-0.3.0/src/changesets/__init__.py
@@ -0,0 +1,39 @@
+import importlib
+import platform
+import subprocess
+import sys
+from pathlib import Path
+
+
+def get_path() -> Path:
+    plat = ""
+    machine = ""
+    match sys.platform:
+        case "linux":
+            plat = "linux"
+        case "darwin":
+            plat = "darwin"
+        case "win32":
+            plat = "win32"
+        case _:
+            raise ValueError(f"Unsupported platform: {sys.platform}")
+    match platform.machine().lower():
+        case "x86_64" | "amd64":
+            machine = "x64"
+        case "arm64" | "aarch64":
+            machine = "arm64"
+        case _:
+            raise ValueError(f"Unsupported architecture: {platform.machine()}")
+    mod = f"changesets_{plat}_{machine}"
+    module = importlib.import_module(mod)
+    return module.get_path()
+
+
+def run() -> None:
+    path = get_path()
+    path.chmod(0o774)
+    subprocess.run([path, *sys.argv[1:]], check=True)
+
+
+if __name__ == "__main__":
+    run()
