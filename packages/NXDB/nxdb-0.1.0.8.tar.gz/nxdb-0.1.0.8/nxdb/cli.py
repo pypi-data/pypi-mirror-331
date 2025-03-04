4856babf7e47e70c668a3b86ade63ee5b8f1ea7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:04:12 +0000

    Add package NXDB/0.1.0.8/nxdb-0.1.0.8.tar.gz

---
+++ packages/NXDB/nxdb-0.1.0.8.tar.gz/nxdb-0.1.0.8/nxdb/cli.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+def cmd_help():
+    print('create [filename] - create a new database')
+
+def require_args(count):
+    if len(sys.argv) < count:
+        cmd_help()
+        sys.exit()
+
+def cmd_create():
+    require_args(3)
+    filename = sys.argv[2] + '.nxdb'
+    if os.path.exists(filename):
+        print(f"{filename} already exists")
+        sys.exit(1)
+    else:
+        with open(filename, mode='wb') as file:
+            file.write(b"DOTNXDB\n")
+        print(f"Saved to {filename}")
+cmds = {
+    'help': cmd_help,
+    'create': cmd_create
+}
+
+def main():
+    require_args(2)
+    cmd = sys.argv[1]
+    if not cmd or not cmd in cmds:
+        cmd_help()
+        sys.exit()
+    cmds[cmd]()
+
+main()
