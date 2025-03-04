76c454d5d08e8157134e879fb5c4073d23b080db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:35 +0000

    Add package argus-server/1.32.0/argus_server-1.32.0.tar.gz

---
+++ packages/argus-server/argus_server-1.32.0.tar.gz/argus_server-1.32.0/src/argus/auth/V1/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework import routers
+
+from . import views as views_V1
+from .. import views
+
+
+router = routers.SimpleRouter()
+router.register(r"phone-number", views_V1.PhoneNumberViewV1, basename="phone_number")
+
+
+app_name = "auth"
+urlpatterns = [
+    # path("login/", django_views.LoginView.as_view(redirect_authenticated_user=True), name="login"),
+    path("user/", views_V1.CurrentUserViewV1.as_view(), name="current-user"),
+    path("users/<int:pk>/", views.BasicUserDetail.as_view(), name="user"),
+] + router.urls
---
+++ packages/argus-server/argus_server-1.32.0.tar.gz/argus_server-1.32.0/src/argus/util/utils.py
@@ -0,0 +1,37 @@
+import importlib
+
+
+class AttrGetter:
+    def __init__(self, attr_name: str):
+        self.attr_name = attr_name
+
+    def __call__(self, obj):
+        return getattr(obj, self.attr_name)
+
+    @property
+    def query(self):
+        return self.attr_name
+
+
+class NestedAttrGetter(AttrGetter):
+    def __init__(self, attr_name: str):
+        super().__init__(attr_name)
+        self._attr_names = attr_name.split(".")
+        self._attr_query = "__".join(self._attr_names)
+
+    def __call__(self, obj):
+        obj_attr = obj
+        for attr_name in self._attr_names:
+            obj_attr = getattr(obj_attr, attr_name)
+        return obj_attr
+
+    @property
+    def query(self):
+        return self._attr_query
+
+
+def import_class_from_dotted_path(dotted_path: str):
+    module_name, class_name = dotted_path.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    class_ = getattr(module, class_name)
+    return class_
