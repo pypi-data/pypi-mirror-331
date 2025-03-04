d8ab937f310c2976ea0960b73b61c55aa1951ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:41 +0000

    Add package symfem/2025.3.0/symfem-2025.3.0-py3-none-any.whl

---
+++ packages/symfem/symfem-2025.3.0-py3-none-any.whl/symfem/elements/bdfm.py
@@ -0,0 +1,138 @@
+"""Brezzi-Douglas-Fortin-Marini elements.
+
+This element's definition appears in https://doi.org/10.1051/m2an/1987210405811
+(Brezzi, Douglas, Fortin, Marini, 1987) and
+https://doi.org/10.1007/978-1-4612-3172-1 (Brezzi, Fortin, 1991)
+"""
+
+import typing
+
+from symfem.elements.dpc import DPC, VectorDPC
+from symfem.elements.lagrange import Lagrange
+from symfem.elements.nedelec import NedelecFirstKind
+from symfem.finite_element import CiarletElement
+from symfem.functionals import IntegralMoment, ListOfFunctionals, NormalIntegralMoment
+from symfem.functions import FunctionInput
+from symfem.moments import make_integral_moment_dofs
+from symfem.polynomials import polynomial_set_vector
+from symfem.references import NonDefaultReferenceError, Reference
+from symfem.symbols import x
+
+__all__ = ["bdfm_polyset", "BDFM"]
+
+
+def bdfm_polyset(reference: Reference, order: int) -> typing.List[FunctionInput]:
+    """Create the polynomial basis for a BDFM element.
+
+    Args:
+        reference: The reference cell
+        order: The polynomial order
+
+    Returns:
+        The polynomial basis
+    """
+    dim = reference.tdim
+    pset: typing.List[FunctionInput] = []
+    pset += polynomial_set_vector(dim, dim, order - 1)
+    if reference.name == "quadrilateral":
+        for i in range(1, order + 1):
+            j = order - i
+            pset.append((x[0] ** i * x[1] ** j, 0))
+            pset.append((0, x[1] ** i * x[0] ** j))
+    elif reference.name == "triangle":
+        for i in range(order - 1):
+            p = x[0] ** i * x[1] ** (order - 2 - i)
+            pset.append((x[0] * (x[0] + x[1]) * p, 0))
+            pset.append((0, x[1] * (x[0] + x[1]) * p))
+        p = x[0] ** (order - 1)
+        pset.append((x[0] * p, x[1] * p))
+    elif reference.name == "hexahedron":
+        for i in range(1, order + 1):
+            for j in range(order + 1 - i):
+                k = order - i - j
+                pset.append((x[0] ** i * x[1] ** j * x[2] ** k, 0, 0))
+                pset.append((0, x[1] ** i * x[0] ** j * x[2] ** k, 0))
+                pset.append((0, 0, x[2] ** i * x[0] ** j * x[1] ** k))
+    elif reference.name == "tetrahedron":
+        for i in range(order - 1):
+            for j in range(order - i - 1):
+                p = x[0] ** i * x[1] ** j * x[2] ** (order - 2 - i - j)
+                pset.append((x[0] * (x[0] + x[1] + x[2]) * p, 0, 0))
+                pset.append((0, x[1] * (x[0] + x[1] + x[2]) * p, 0))
+                pset.append((0, 0, x[2] * (x[0] + x[1] + x[2]) * p))
+        for i in range(order):
+            p = x[0] ** i * x[1] ** (order - 1 - i)
+            pset.append((x[0] * p, x[1] * p, x[2] * p))
+        for i in range(1, order):
+            p = x[0] ** i * x[1] ** (order - 1 - i)
+            pset.append((p * (x[0] + x[1]), 0, x[1] * p))
+
+    return pset
+
+
+class BDFM(CiarletElement):
+    """Brezzi-Douglas-Fortin-Marini Hdiv finite element."""
+
+    def __init__(self, reference: Reference, order: int, variant: str = "equispaced"):
+        """Create the element.
+
+        Args:
+            reference: The reference element
+            order: The polynomial order
+            variant: The variant of the element
+        """
+        if reference.vertices != reference.reference_vertices:
+            raise NonDefaultReferenceError()
+
+        poly = bdfm_polyset(reference, order)
+
+        dofs: ListOfFunctionals = []
+        if reference.name in ["triangle", "tetrahedron"]:
+            dofs = make_integral_moment_dofs(
+                reference,
+                facets=(NormalIntegralMoment, Lagrange, order - 1, {"variant": variant}),
+                cells=(IntegralMoment, NedelecFirstKind, order - 1, {"variant": variant}),
+            )
+        else:
+            dofs = make_integral_moment_dofs(
+                reference,
+                facets=(NormalIntegralMoment, DPC, order - 1, {"variant": variant}),
+                cells=(IntegralMoment, VectorDPC, order - 2, {"variant": variant}),
+            )
+        self.variant = variant
+
+        super().__init__(reference, order, poly, dofs, reference.tdim, reference.tdim)
+
+    def init_kwargs(self) -> typing.Dict[str, typing.Any]:
+        """Return the kwargs used to create this element.
+
+        Returns:
+            Keyword argument dictionary
+        """
+        return {"variant": self.variant}
+
+    @property
+    def lagrange_subdegree(self) -> int:
+        if self.reference.name in ["triangle", "tetrahedron"]:
+            return self.order - 1
+        else:
+            return self.order // self.reference.tdim
+
+    @property
+    def lagrange_superdegree(self) -> typing.Optional[int]:
+        return self.order
+
+    @property
+    def polynomial_subdegree(self) -> int:
+        return self.order - 1
+
+    @property
+    def polynomial_superdegree(self) -> typing.Optional[int]:
+        return self.order
+
+    names = ["Brezzi-Douglas-Fortin-Marini", "BDFM"]
+    references = ["triangle", "quadrilateral", "hexahedron", "tetrahedron"]
+    min_order = 1
+    continuity = "H(div)"
+    value_type = "vector"
+    last_updated = "2024.10"
---
+++ packages/symfem/symfem-2025.3.0-py3-none-any.whl/symfem/elements/trimmed_serendipity.py
@@ -0,0 +1,245 @@
+"""Trimmed serendipity elements on tensor products.
+
+These elements' definitions appear in https://doi.org/10.1137/16M1073352
+(Cockburn, Fu, 2017) and https://doi.org/10.1090/mcom/3354
+(Gilette, Kloefkorn, 2018)
+"""
+
+import typing
+
+from symfem.elements.dpc import DPC, VectorDPC
+from symfem.finite_element import CiarletElement
+from symfem.functionals import (
+    IntegralAgainst,
+    IntegralMoment,
+    ListOfFunctionals,
+    NormalIntegralMoment,
+    TangentIntegralMoment,
+)
+from symfem.functions import FunctionInput, ScalarFunction, VectorFunction
+from symfem.moments import make_integral_moment_dofs
+from symfem.polynomials import polynomial_set_vector
+from symfem.references import NonDefaultReferenceError, Reference
+from symfem.symbols import t, x
+
+__all__ = ["TrimmedSerendipityHcurl", "TrimmedSerendipityHdiv"]
+
+
+class TrimmedSerendipityHcurl(CiarletElement):
+    """Trimmed serendipity Hcurl finite element."""
+
+    def __init__(self, reference: Reference, order: int, variant: str = "equispaced"):
+        """Create the element.
+
+        Args:
+            reference: The reference element
+            order: The polynomial order
+            variant: The variant of the element
+        """
+        if reference.vertices != reference.reference_vertices:
+            raise NonDefaultReferenceError()
+
+        poly: typing.List[FunctionInput] = []
+        poly += polynomial_set_vector(reference.tdim, reference.tdim, order - 1)
+        if reference.tdim == 2:
+            poly += [
+                (x[0] ** j * x[1] ** (order - j), -(x[0] ** (j + 1)) * x[1] ** (order - 1 - j))
+                for j in range(order)
+            ]
+            if order == 1:
+                poly += [(x[1], x[0])]
+            else:
+                poly += [
+                    (x[1] ** order, order * x[0] * x[1] ** (order - 1)),
+                    (order * x[0] ** (order - 1) * x[1], x[0] ** order),
+                ]
+        else:
+            for i in range(order):
+                for j in range(order - i):
+                    for dim in range(3):
+                        if i == 0 or dim != 0:
+                            p = VectorFunction(tuple(x)).cross(
+                                VectorFunction(
+                                    [
+                                        x[0] ** i * x[1] ** j * x[2] ** (order - 1 - i - j)
+                                        if d == dim
+                                        else 0
+                                        for d in range(3)
+                                    ]
+                                )
+                            )
+                            poly.append(p)
+
+            if order == 1:
+                poly += [ScalarFunction(x[0] * x[1] * x[2] ** order).grad(3)]
+            else:
+                poly += [
+                    ScalarFunction(x[0] * x[1] * x[2] ** order).grad(3),
+                    ScalarFunction(x[0] * x[1] ** order * x[2]).grad(3),
+                    ScalarFunction(x[0] ** order * x[1] * x[2]).grad(3),
+                ]
+            for i in range(order + 1):
+                poly.append(ScalarFunction(x[0] * x[1] ** i * x[2] ** (order - i)).grad(3))
+                if i != 1:
+                    poly.append(ScalarFunction(x[1] * x[0] ** i * x[2] ** (order - i)).grad(3))
+                    if order - i != 1:
+                        poly.append(ScalarFunction(x[2] * x[0] ** i * x[1] ** (order - i)).grad(3))
+            for i in range(order):
+                p = x[0] * x[1] ** i * x[2] ** (order - 1 - i)
+                poly.append((0, -x[2] * p, x[1] * p))
+                p = x[1] * x[0] ** i * x[2] ** (order - 1 - i)
+                poly.append((-x[2] * p, 0, x[0] * p))
+                if order > 1:
+                    p = x[2] * x[0] ** i * x[1] ** (order - 1 - i)
+                    poly.append((-x[1] * p, x[0] * p, 0))
+
+        dofs: ListOfFunctionals = []
+        dofs += make_integral_moment_dofs(
+            reference,
+            edges=(TangentIntegralMoment, DPC, order - 1, {"variant": variant}),
+            faces=(IntegralMoment, VectorDPC, order - 3, "contravariant", {"variant": variant}),
+            volumes=(IntegralMoment, VectorDPC, order - 5, "contravariant", {"variant": variant}),
+        )
+        if order >= 2:
+            for f_n in range(reference.sub_entity_count(2)):
+                for i in range(order):
+                    f = ScalarFunction(x[0] ** (order - 1 - i) * x[1] ** i).grad(2)
+                    f2 = VectorFunction((f[1], -f[0])).subs(x, tuple(t))
+                    dofs.append(
+                        IntegralAgainst(reference, f2, entity=(2, f_n), mapping="contravariant")
+                    )
+
+        super().__init__(reference, order, poly, dofs, reference.tdim, reference.tdim)
+        self.variant = variant
+
+    def init_kwargs(self) -> typing.Dict[str, typing.Any]:
+        """Return the kwargs used to create this element.
+
+        Returns:
+            Keyword argument dictionary
+        """
+        return {"variant": self.variant}
+
+    @property
+    def lagrange_subdegree(self) -> int:
+        return (self.order + self.reference.tdim - 1) // (self.reference.tdim + 1)
+
+    @property
+    def lagrange_superdegree(self) -> typing.Optional[int]:
+        return self.order
+
+    @property
+    def polynomial_subdegree(self) -> int:
+        return self.order - 1
+
+    @property
+    def polynomial_superdegree(self) -> typing.Optional[int]:
+        return self.order + self.reference.tdim - 2
+
+    names = ["trimmed serendipity Hcurl", "TScurl"]
+    references = ["quadrilateral", "hexahedron"]
+    min_order = 1
+    continuity = "H(curl)"
+    value_type = "vector"
+    last_updated = "2023.06"
+
+
+class TrimmedSerendipityHdiv(CiarletElement):
+    """Trimmed serendipity Hdiv finite element."""
+
+    def __init__(self, reference: Reference, order: int, variant: str = "equispaced"):
+        """Create the element.
+
+        Args:
+            reference: The reference element
+            order: The polynomial order
+            variant: The variant of the element
+        """
+        if reference.vertices != reference.reference_vertices:
+            raise NonDefaultReferenceError()
+
+        poly: typing.List[FunctionInput] = []
+        poly += polynomial_set_vector(reference.tdim, reference.tdim, order - 1)
+        if reference.tdim == 2:
+            poly += [
+                (x[0] ** (j + 1) * x[1] ** (order - 1 - j), x[0] ** j * x[1] ** (order - j))
+                for j in range(order)
+            ]
+            if order == 1:
+                poly += [(x[0], -x[1])]
+            else:
+                poly += [
+                    (order * x[0] * x[1] ** (order - 1), -(x[1] ** order)),
+                    (-(x[0] ** order), order * x[0] ** (order - 1) * x[1]),
+                ]
+        else:
+            for i in range(order):
+                for j in range(order - i):
+                    p = x[0] ** i * x[1] ** j * x[2] ** (order - 1 - i - j)
+                    poly.append((x[0] * p, x[1] * p, x[2] * p))
+            for i in range(order):
+                p = x[0] * x[1] ** i * x[2] ** (order - 1 - i)
+                poly.append(VectorFunction((0, -x[2] * p, x[1] * p)).curl())
+                p = x[1] * x[0] ** i * x[2] ** (order - 1 - i)
+                poly.append(VectorFunction((-x[2] * p, 0, x[0] * p)).curl())
+                if order > 1:
+                    p = x[2] * x[0] ** i * x[1] ** (order - 1 - i)
+                    poly.append(VectorFunction((-x[1] * p, x[0] * p, 0)).curl())
+
+        dofs: ListOfFunctionals = []
+        dofs += make_integral_moment_dofs(
+            reference,
+            facets=(NormalIntegralMoment, DPC, order - 1, {"variant": variant}),
+            cells=(IntegralMoment, VectorDPC, order - 3, "covariant", {"variant": variant}),
+        )
+        if order >= 2:
+            if reference.tdim == 2:
+                fs = [
+                    ScalarFunction(x[0] ** (order - 1 - i) * x[1] ** i).grad(2)
+                    for i in range(order)
+                ]
+            else:
+                fs = [
+                    ScalarFunction(x[0] ** (order - 1 - i - j) * x[1] ** i * x[2] ** j).grad(3)
+                    for i in range(order)
+                    for j in range(order - i)
+                ]
+            for f in fs:
+                f2 = f.subs(x, tuple(t))
+                dofs.append(
+                    IntegralAgainst(reference, f2, entity=(reference.tdim, 0), mapping="covariant")
+                )
+
+        super().__init__(reference, order, poly, dofs, reference.tdim, reference.tdim)
+        self.variant = variant
+
+    def init_kwargs(self) -> typing.Dict[str, typing.Any]:
+        """Return the kwargs used to create this element.
+
+        Returns:
+            Keyword argument dictionary
+        """
+        return {"variant": self.variant}
+
+    @property
+    def lagrange_subdegree(self) -> int:
+        return (self.order + 1) // (self.reference.tdim + 1)
+
+    @property
+    def lagrange_superdegree(self) -> typing.Optional[int]:
+        return self.order
+
+    @property
+    def polynomial_subdegree(self) -> int:
+        return self.order - 1
+
+    @property
+    def polynomial_superdegree(self) -> typing.Optional[int]:
+        return self.order
+
+    names = ["trimmed serendipity Hdiv", "TSdiv"]
+    references = ["quadrilateral", "hexahedron"]
+    min_order = 1
+    continuity = "H(div)"
+    value_type = "vector"
+    last_updated = "2023.06"
