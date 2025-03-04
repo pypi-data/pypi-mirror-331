f2ceea0dc130c7b493c0647390ec6c98d1402c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:04:43 +0000

    Add package humanloop/0.8.26/humanloop-0.8.26.tar.gz

---
+++ packages/humanloop/humanloop-0.8.26.tar.gz/humanloop-0.8.26/src/humanloop/otel/constants.py
@@ -0,0 +1,10 @@
+# Attribute name prefix on Humanloop spans for file-related attributes + path
+HUMANLOOP_FILE_KEY = "humanloop.file"
+# Attribute name prefix on Humanloop spans for log-related attributes
+HUMANLOOP_LOG_KEY = "humanloop.log"
+HUMANLOOP_FILE_TYPE_KEY = "humanloop.file.type"
+HUMANLOOP_PATH_KEY = "humanloop.file.path"
+# Required for the exporter to know when to mark the Flow Log as complete
+HUMANLOOP_FLOW_PREREQUISITES_KEY = "humanloop.flow.prerequisites"
+HUMANLOOP_INTERCEPTED_HL_CALL_SPAN_NAME = "humanloop_intercepted_hl_call"
+HUMANLOOP_INTERCEPTED_HL_CALL_RESPONSE = "intercepted_call_response"
