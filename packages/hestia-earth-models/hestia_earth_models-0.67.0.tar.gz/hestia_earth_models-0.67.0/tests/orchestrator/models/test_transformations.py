8ab22b083e311a081852318d9b40cb9c02f2355d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 18:06:01 +0000

    Add package hestia-earth-models/0.67.0/hestia_earth_models-0.67.0.tar.gz

---
+++ packages/hestia-earth-models/hestia_earth_models-0.67.0.tar.gz/hestia_earth_models-0.67.0/tests/orchestrator/models/test_transformations.py
@@ -0,0 +1,29 @@
+from unittest.mock import patch
+import json
+import os
+
+from tests.utils import fixtures_path
+from hestia_earth.orchestrator.models.transformations import run, _include_practice
+
+folder_path = os.path.join(fixtures_path, 'orchestrator', 'transformation')
+
+
+@patch('hestia_earth.orchestrator.strategies.merge._merge_version', return_value='0.0.0')
+def test_run(*args):
+    with open(os.path.join(folder_path, 'config.json'), encoding='utf-8') as f:
+        config = json.load(f)
+    with open(os.path.join(folder_path, 'cycle.jsonld'), encoding='utf-8') as f:
+        cycle = json.load(f)
+    with open(os.path.join(folder_path, 'result.jsonld'), encoding='utf-8') as f:
+        expected = json.load(f)
+
+    result = run(config.get('models'), cycle)
+    assert result == expected
+
+
+def test_include_practice():
+    term = {'@id': 'genericCropProduct', 'termType': 'crop'}
+    assert not _include_practice({'term': term})
+
+    term = {'@id': 'yieldOfPrimaryAquacultureProductLiveweightPerM2', 'termType': 'aquacultureManagement'}
+    assert _include_practice({'term': term}) is True
