6dead01611cd1a1199543c27485d684514f92775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:24 +0000

    Add package pyvfg/3.1.1/pyvfg-3.1.1.tar.gz

---
+++ packages/pyvfg/pyvfg-3.1.1.tar.gz/pyvfg-3.1.1/tests/versions/test_vfg_0_2_0.py
@@ -0,0 +1,22 @@
+import json
+import os
+import pathlib
+
+from pyvfg import vfg_from_json
+
+from .. import package_dir
+
+
+def test_vfg_020_from_json():
+    file_path = (
+        package_dir / "tests/fixtures/models" / "sprinkler/sprinkler_vfg_0_2_0.json"
+    )
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    vfg = vfg_from_json(data)
+
+    assert vfg.version == "0.4.0"
+    for factor, factor_data in zip(vfg.factors, data["factors"]):
+        assert factor.variables == factor_data["variables"]
+        # assert factor.values == factor_data["values"]
