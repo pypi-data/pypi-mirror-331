30dc57e70629034d067738f07c302b53e38429bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 17:04:35 +0000

    Add package twat-mp/2.5.4/twat_mp-2.5.4-py3-none-any.whl

---
+++ packages/twat-mp/twat_mp-2.5.4-py3-none-any.whl/twat_mp/__init__.py
@@ -0,0 +1,25 @@
+"""
+twat-mp - Parallel processing utilities for twat using pathos and aiomultiprocess.
+"""
+
+from twat_mp.mp import MultiPool, ProcessPool, ThreadPool, amap, imap, mmap, pmap
+
+__version__ = "1.8.0"
+
+__all__ = [
+    "MultiPool",
+    "ProcessPool",
+    "ThreadPool",
+    "__version__",
+    "amap",
+    "imap",
+    "mmap",
+    "pmap",
+]
+
+try:
+    from twat_mp.async_mp import AsyncMultiPool, apmap
+
+    __all__ += ["AsyncMultiPool", "apmap"]
+except ImportError:
+    pass  # aiomultiprocess is not installed
