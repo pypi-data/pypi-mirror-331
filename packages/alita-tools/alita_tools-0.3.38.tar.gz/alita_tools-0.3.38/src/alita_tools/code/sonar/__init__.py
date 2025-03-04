6a8ccb2a08bd12ead1471a4a200bf4947300e24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:11:50 +0000

    Add package alita-tools/0.3.38/alita_tools-0.3.38.tar.gz

---
+++ packages/alita-tools/alita_tools-0.3.38.tar.gz/alita_tools-0.3.38/src/alita_tools/code/sonar/__init__.py
@@ -0,0 +1,54 @@
+from typing import List, Literal
+from langchain_core.tools import BaseToolkit, BaseTool
+from pydantic import create_model, BaseModel, ConfigDict, Field
+
+from .api_wrapper import SonarApiWrapper
+from ...base.tool import BaseAction
+
+name = "sonar"
+
+
+def get_tools(tool):
+    return SonarToolkit().get_toolkit(
+        selected_tools=tool['settings'].get('selected_tools', []),
+        url=tool['settings']['url'],
+        sonar_token=tool['settings']['sonar_token'],
+        sonar_project_name=tool['settings']['sonar_project_name']
+    ).get_tools()
+
+
+class SonarToolkit(BaseToolkit):
+    tools: list[BaseTool] = []
+
+    @staticmethod
+    def toolkit_config_schema() -> BaseModel:
+        selected_tools = {x['name']: x['args_schema'].schema() for x in SonarApiWrapper.model_construct().get_available_tools()}
+        return create_model(
+            name,
+            url=(str, Field(description="SonarQube Server URL")),
+            sonar_token=(str, Field(description="SonarQube user token for authentication", json_schema_extra={'secret': True})),
+            sonar_project_name=(str, Field(description="Project name of the desired repository")),
+            selected_tools=(List[Literal[tuple(selected_tools)]], Field(default=[], json_schema_extra={'args_schemas': selected_tools})),
+            __config__=ConfigDict(json_schema_extra={'metadata': {"label": "Sonar", "icon_url": None}})
+        )
+
+    @classmethod
+    def get_toolkit(cls, selected_tools: list[str] | None = None, **kwargs):
+        if selected_tools is None:
+            selected_tools = []
+        sonar_api_wrapper = SonarApiWrapper(**kwargs)
+        available_tools = sonar_api_wrapper.get_available_tools()
+        tools = []
+        for tool in available_tools:
+            if selected_tools and tool["name"] not in selected_tools:
+                continue
+            tools.append(BaseAction(
+                api_wrapper=sonar_api_wrapper,
+                name=tool["name"],
+                description=tool["description"],
+                args_schema=tool["args_schema"]
+            ))
+        return cls(tools=tools)
+
+    def get_tools(self) -> list[BaseTool]:
+        return self.tools
