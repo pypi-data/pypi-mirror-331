44344f2902b4e633a439d02735311822a30e0352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:13:13 +0000

    Add package byteplus-python-sdk-v2/1.0.7/byteplus-python-sdk-v2-1.0.7.tar.gz

---
+++ packages/byteplus-python-sdk-v2/byteplus-python-sdk-v2-1.0.7.tar.gz/byteplus-python-sdk-v2-1.0.7/byteplussdkcore/__init__.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+
+from byteplussdkcore.configuration import Configuration
+from byteplussdkcore.api_client import ApiClient
+from byteplussdkcore.universal import UniversalApi, UniversalInfo
+from byteplussdkcore.flatten import Flatten
