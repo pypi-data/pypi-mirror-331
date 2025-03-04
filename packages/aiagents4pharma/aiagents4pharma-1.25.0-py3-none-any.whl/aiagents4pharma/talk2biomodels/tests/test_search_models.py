1c7f8c02501b7f28c57922722eee17343ff41cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:11:58 +0000

    Add package aiagents4pharma/1.25.0/aiagents4pharma-1.25.0-py3-none-any.whl

---
+++ packages/aiagents4pharma/aiagents4pharma-1.25.0-py3-none-any.whl/aiagents4pharma/talk2biomodels/tests/test_search_models.py
@@ -0,0 +1,36 @@
+'''
+Test cases for Talk2Biomodels search models tool.
+'''
+
+from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+from ..agents.t2b_agent import get_app
+
+def test_search_models_tool():
+    '''
+    Test the search_models tool.
+    '''
+    unique_id = 12345
+    app = get_app(unique_id)
+    config = {"configurable": {"thread_id": unique_id}}
+    # Update state
+    app.update_state(config,
+            {"llm_model": ChatNVIDIA(model="meta/llama-3.3-70b-instruct")})
+    prompt = "Search for models on Crohn's disease."
+    # Test the tool get_modelinfo
+    response = app.invoke(
+                        {"messages": [HumanMessage(content=prompt)]},
+                        config=config
+                    )
+    # Extract the assistant artifact which contains
+    # all the search results
+    found_model_537 = False
+    for msg in response["messages"]:
+        if isinstance(msg, ToolMessage) and msg.name == "search_models":
+            msg_artifact = msg.artifact
+            for model in msg_artifact["dic_data"]:
+                if model["id"] == "BIOMD0000000537":
+                    found_model_537 = True
+            break
+    # Check if the model BIOMD0000000537 is found
+    assert found_model_537
