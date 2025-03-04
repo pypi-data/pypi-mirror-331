48365340f6c0a65c63b63453f5f97d4216df66c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:52 +0000

    Add package Piff/1.5.1/piff-1.5.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/Piff/piff-1.5.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/piff/model.py
@@ -0,0 +1,235 @@
+# Copyright (c) 2016 by Mike Jarvis and the other collaborators on GitHub at
+# https://github.com/rmjarvis/Piff  All rights reserved.
+#
+# Piff is free software: Redistribution and use in source and binary forms
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the disclaimer given in the accompanying LICENSE
+#    file.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the disclaimer given in the documentation
+#    and/or other materials provided with the distribution.
+
+"""
+.. module:: model
+"""
+
+from .star import Star
+
+
+class Model(object):
+    """The base class for modeling a single PSF (i.e. no interpolation yet)
+
+    This is essentially an abstract base class intended to define the methods that should be
+    implemented by any derived class.
+    """
+    # This class-level dict will store all the valid model types.
+    # Each subclass should set a cls._type_name, which is the name that should
+    # appear in a config dict.  These will be the keys of valid_model_types.
+    # The values in this dict will be the Model sub-classes.
+    valid_model_types = {}
+
+    @classmethod
+    def process(cls, config_model, logger=None):
+        """Parse the model field of the config dict.
+
+        :param config_model:    The configuration dict for the model field.
+        :param logger:          A logger object for logging debug info. [default: None]
+
+        :returns: a Model instance
+        """
+        # Get the class to use for the model
+        if 'type' not in config_model:
+            raise ValueError("config['model'] has no type field")
+
+        model_type = config_model['type']
+        if model_type not in Model.valid_model_types:
+            raise ValueError("type %s is not a valid model type. "%model_type +
+                             "Expecting one of %s"%list(Model.valid_model_types.keys()))
+
+        model_class = Model.valid_model_types[model_type]
+
+        # Read any other kwargs in the model field
+        kwargs = model_class.parseKwargs(config_model, logger)
+
+        # Build model object
+        model = model_class(**kwargs)
+
+        return model
+
+    def set_num(self, num):
+        """If there are multiple components involved in the fit, set the number to use
+        for this model.
+        """
+        self._num = num
+
+    @classmethod
+    def __init_subclass__(cls):
+        # Classes that don't want to register a type name can either not define _type_name
+        # or set it to None.
+        if hasattr(cls, '_type_name') and cls._type_name is not None:
+            if cls._type_name in Model.valid_model_types:
+                raise ValueError('Model type %s already registered'%cls._type_name +
+                                 'Maybe you subclassed and forgot to set _type_name?')
+            Model.valid_model_types[cls._type_name] = cls
+
+    @classmethod
+    def parseKwargs(cls, config_model, logger=None):
+        """Parse the model field of a configuration dict and return the kwargs to use for
+        initializing an instance of the class.
+
+        The base class implementation just returns the kwargs as they are, but derived classes
+        might want to override this if they need to do something more sophisticated with them.
+
+        :param config_model:    The model field of the configuration dict, config['model']
+        :param logger:          A logger object for logging debug info. [default: None]
+
+        :returns: a kwargs dict to pass to the initializer
+        """
+        kwargs = {}
+        kwargs.update(config_model)
+        kwargs.pop('type', None)
+        kwargs['logger'] = logger
+        return kwargs
+
+    def initialize(self, star, logger=None, default_init=None):
+        """Initialize a star to work with the current model.
+
+        :param star:            A Star instance with the raw data.
+        :param logger:          A logger object for logging debug info. [default: None]
+        :param default_init:    The default initilization method if the user doesn't specify one.
+                                [default: None]
+
+        :returns:       Star instance with the appropriate initial fit values
+        """
+        raise NotImplementedError("Derived classes must define the initialize function")
+
+    def normalize(self, star):
+        """Make sure star.fit.params are normalized properly.
+
+        Note: This modifies the input star in place.
+        """
+        # This is by default a no op.  Some models may need to do something to noramlize the
+        # parameter values in star.fit.
+        pass
+
+    def fit(self, star, convert_func=None):
+        """Fit the Model to the star's data to yield iterative improvement on
+        its PSF parameters, their uncertainties, and flux (and center, if free).
+        The returned star.fit.alpha will be inverse covariance of solution if
+        it is estimated, else is None.
+
+        :param star:            A Star instance
+        :param convert_func:    An optional function to apply to the profile being fit before
+                                drawing it onto the image.  This is used by composite PSFs to
+                                isolate the effect of just this model component. [default: None]
+
+        :returns:      New Star instance with updated fit information
+        """
+        raise NotImplementedError("Derived classes must define the fit function")
+
+    def draw(self, star, copy_image=True):
+        """Draw the model on the given image.
+
+        :param star:        A Star instance with the fitted parameters to use for drawing and a
+                            data field that acts as a template image for the drawn model.
+        :param copy_image:  If False, will use the same image object.
+                            If True, will copy the image and then overwrite it.
+                            [default: True]
+
+        :returns: a new Star instance with the data field having an image of the drawn model.
+        """
+        params = star.fit.get_params(self._num)
+        prof = self.getProfile(params).shift(star.fit.center) * star.fit.flux
+        if copy_image:
+            image = star.image.copy()
+        else:
+            image = star.image
+        prof.drawImage(image, method=self._method, center=star.image_pos)
+        return Star(star.data.withNew(image=image), star.fit)
+
+    def write(self, writer, name):
+        """Write a Model via a Writer object.
+
+        Note: this only writes the initialization kwargs to the fits extension, not the parameters.
+
+        The base class implemenation works if the class has a self.kwargs attribute and these
+        are all simple values (str, float, or int)
+
+        :param writer:      A writer object that encapsulates the serialization format.
+        :param name:        A name to associate with this model in the serialized output.
+        """
+        # First write the basic kwargs that works for all Model classes
+        model_type = self._type_name
+        writer.write_struct(name, dict(self.kwargs, type=model_type))
+        # Now do any class-specific steps.
+        with writer.nested(name) as w:
+            self._finish_write(w)
+
+    def _finish_write(self, writer):
+        """Finish the writing process with any class-specific steps.
+
+        The base class implementation doesn't do anything, which is often appropriate, but
+        this hook exists in case any Model classes need to write extra information to the
+        fits file.
+
+        :param writer:      A writer object that encapsulates the serialization format.
+        """
+        pass
+
+    @classmethod
+    def read(cls, reader, name):
+        """Read a Model from a FITS file.
+
+        Note: the returned Model will not have its parameters set.  This just initializes a fresh
+        model that can be used to interpret interpolated vectors.
+
+        :param reader:      A reader object that encapsulates the serialization format.
+        :param name:        Name associated with this model in the serialized output.
+
+        :returns: a model built with a information in the FITS file
+        """
+        kwargs = reader.read_struct(name)
+        assert kwargs is not None
+        assert 'type' in kwargs
+        model_type = kwargs.pop('type')
+
+        # Check that model_type is a valid Model type.
+        if model_type not in Model.valid_model_types:
+            raise ValueError("model type %s is not a valid Piff Model"%model_type)
+        model_cls = Model.valid_model_types[model_type]
+
+        if 'force_model_center' in kwargs: # pragma: no cover
+            # old version of this parameter name.
+            kwargs['centered'] = kwargs.pop('force_model_center')
+        model_cls._fix_kwargs(kwargs)
+        model = model_cls(**kwargs)
+        model._finish_read(reader)
+        return model
+
+    @classmethod
+    def _fix_kwargs(cls, kwargs):
+        """Fix the kwargs read in from an input file.
+
+        This is intended to make it easier to preserve backwards compatibility if a class
+        has changed something about the kwargs, this provides a way for old parameter names
+        or defaults to be updated for a newer version of Piff than the one that wrong them.
+
+        Usually, this is a no op.
+
+        :param kwargs:  The old kwargs read in from a previous version Piff output file.
+        """
+        pass
+
+    def _finish_read(self, reader):
+        """Finish the reading process with any class-specific steps.
+
+        The base class implementation doesn't do anything, which is often appropriate, but
+        this hook exists in case any Model classes need to read extra information from the
+        fits file.
+
+        :param reader:      A reader object that encapsulates the serialization format.
+        """
+        pass
---
+++ packages/Piff/piff-1.5.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/piff/simplepsf.py
@@ -0,0 +1,280 @@
+# Copyright (c) 2016 by Mike Jarvis and the other collaborators on GitHub at
+# https://github.com/rmjarvis/Piff  All rights reserved.
+#
+# Piff is free software: Redistribution and use in source and binary forms
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the disclaimer given in the accompanying LICENSE
+#    file.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the disclaimer given in the documentation
+#    and/or other materials provided with the distribution.
+
+"""
+.. module:: psf
+"""
+
+import numpy as np
+import galsim
+
+from .model import Model
+from .interp import Interp
+from .outliers import Outliers
+from .psf import PSF
+
+class SimplePSF(PSF):
+    """A PSF class that uses a single model and interpolator.
+
+    A SimplePSF is built from a Model and an Interp object.
+    The model defines the functional form of the surface brightness profile, and the
+    interpolator defines how the parameters of the model vary across the field of view.
+
+    Use type name "Simple" in a config field to use this psf type, or leave off the type
+    name, as this is the default PSF type.
+
+    :param model:       A Model instance used for modeling the surface brightness profile.
+    :param interp:      An Interp instance used to interpolate across the field of view.
+    :param outliers:    Optionally, an Outliers instance used to remove outliers.
+                        [default: None]
+    :param chisq_thresh: Change in reduced chisq at which iteration will terminate.
+                        [default: 0.1]
+    :param max_iter:    Maximum number of iterations to try. [default: 30]
+    """
+    _type_name = 'Simple'
+
+    def __init__(self, model, interp, outliers=None, chisq_thresh=0.1, max_iter=30):
+        self.model = model
+        self.interp = interp
+        self.outliers = outliers
+        self.chisq_thresh = chisq_thresh
+        self.max_iter = max_iter
+        self.kwargs = {
+            # Use 0 here for things that will get overwritten in _finish_read.
+            'model': 0,
+            'interp': 0,
+            'outliers': 0,
+            'chisq_thresh': self.chisq_thresh,
+            'max_iter': self.max_iter,
+        }
+        self.chisq = 0.
+        self.last_delta_chisq = 0.
+        self.dof = 0
+        self.nremoved = 0
+        self.niter = 0
+
+        # Run this by default on construction.
+        # If this is a component, it will be overwritten by a higher level composite class.
+        self.set_num(None)
+
+    def set_num(self, num):
+        """If there are multiple components involved in the fit, set the number to use
+        for this model.
+        """
+        from .model import Model
+        from .interp import Interp
+        self._num = num
+        # Note: they might be 0 if this is part of a read process, and they haven't been
+        # overwritten yet.
+        if isinstance(self.model, Model):
+            self.model.set_num(num)
+        if isinstance(self.interp, Interp):
+            self.interp.set_num(num)
+
+    @property
+    def interp_property_names(self):
+        return self.interp.property_names
+
+    @classmethod
+    def parseKwargs(cls, config_psf, logger):
+        """Parse the psf field of a configuration dict and return the kwargs to use for
+        initializing an instance of the class.
+
+        :param config_psf:      The psf field of the configuration dict, config['psf']
+        :param logger:          A logger object for logging debug info. [default: None]
+
+        :returns: a kwargs dict to pass to the initializer
+        """
+        from .model import Model
+        from .interp import Interp
+        from .outliers import Outliers
+
+        kwargs = {}
+        kwargs.update(config_psf)
+        kwargs.pop('type',None)
+
+        for key in ['model', 'interp']:
+            if key not in kwargs:  # pragma: no cover
+                # This actually is covered, but for some reason, codecov thinks it isn't.
+                raise ValueError("%s field is required in psf field for type=Simple"%key)
+
+        # make a Model object to use for the individual stellar fitting
+        model = Model.process(kwargs.pop('model'), logger=logger)
+        kwargs['model'] = model
+
+        # make an Interp object to use for the interpolation
+        interp = Interp.process(kwargs.pop('interp'), logger=logger)
+        kwargs['interp'] = interp
+
+        if 'outliers' in kwargs:
+            outliers = Outliers.process(kwargs.pop('outliers'), logger=logger)
+            kwargs['outliers'] = outliers
+
+        return kwargs
+
+    def initialize_params(self, stars, logger=None, default_init=None):
+        nremoved = 0
+
+        logger.debug("Initializing models")
+        # model.initialize may fail
+        new_stars = []
+        for star in stars:
+            try:
+                star = self.model.initialize(star, logger=logger, default_init=default_init)
+            except Exception as e:
+                logger.warning("Failed initializing star at %s. Excluding it.", star.image_pos)
+                logger.warning("  -- Caught exception: %s",e)
+                nremoved += 1
+                star = star.flag_if(True)
+            new_stars.append(star)
+        if nremoved == 0:
+            logger.debug("No stars removed in initialize step")
+        else:
+            logger.info("Removed %d stars in initialize", nremoved)
+
+        logger.debug("Initializing interpolator")
+        stars = self.interp.initialize(new_stars, logger=logger)
+
+        # For basis models, we can compute a quadratic form for chisq, and if we are using
+        # a basis interpolator, then we can use it.  It's kind of ugly to query this, but
+        # the double dispatch makes it tricky to implement this with class heirarchy, so for
+        # now we just check if we have all the required parts to use the quadratic form
+        if hasattr(self.interp, 'degenerate_points'):
+            self.quadratic_chisq = hasattr(self.model, 'chisq') and self.interp.degenerate_points
+            self.degenerate_points = self.interp.degenerate_points
+        else:
+            self.quadratic_chisq = False
+            self.degenerate_points = False
+
+        return stars, nremoved
+
+    def single_iteration(self, stars, logger, convert_funcs, draw_method):
+
+        # Perform the fit or compute design matrix as appropriate using just non-reserve stars
+        fit_fn = self.model.chisq if self.quadratic_chisq else self.model.fit
+
+        nremoved = 0  # For this iteration
+        use_stars = []  # Just the stars we want to use for fitting.
+        all_stars = []  # All the stars (with appropriate flags as necessary)
+        for k, star in enumerate(stars):
+            if not star.is_flagged and not star.is_reserve:
+                try:
+                    convert_func = None if convert_funcs is None else convert_funcs[k]
+                    star = fit_fn(star, logger=logger, convert_func=convert_func,
+                                  draw_method=draw_method)
+                    use_stars.append(star)
+                except Exception as e:
+                    logger.warning("Failed fitting star at %s.", star.image_pos)
+                    logger.warning("Excluding it from this iteration.")
+                    logger.warning("  -- Caught exception: %s", e)
+                    nremoved += 1
+                    star = star.flag_if(True)
+            all_stars.append(star)
+
+        if len(use_stars) == 0:
+            raise RuntimeError("No stars left to fit.  Cannot find PSF model.")
+
+        # Perform the interpolation, again using just non-reserve stars
+        logger.debug("             Calculating the interpolation")
+        self.interp.solve(use_stars, logger=logger)
+
+        # Propagate that solution to all the stars' parameters, including reserve stars.
+        all_stars = self.interp.interpolateList(all_stars)
+
+        return all_stars, nremoved
+
+    @property
+    def fit_center(self):
+        """Whether to fit the center of the star in reflux.
+
+        This is generally set in the model specifications.
+        If all component models includes a shift, then this is False.
+        Otherwise it is True.
+        """
+        return self.model._centered
+
+    @property
+    def include_model_centroid(self):
+        """Whether a model that we want to center can have a non-zero centroid during iterations.
+        """
+        return self.model._centered and self.model._model_can_be_offset
+
+    def interpolateStarList(self, stars):
+        """Update the stars to have the current interpolated fit parameters according to the
+        current PSF model.
+
+        :param stars:       List of Star instances to update.
+
+        :returns:           List of Star instances with their fit parameters updated.
+        """
+        stars = self.interp.interpolateList(stars)
+        for star in stars:
+            self.model.normalize(star)
+        return stars
+
+    def interpolateStar(self, star):
+        """Update the star to have the current interpolated fit parameters according to the
+        current PSF model.
+
+        :param star:        Star instance to update.
+
+        :returns:           Star instance with its fit parameters updated.
+        """
+        star = self.interp.interpolate(star)
+        self.model.normalize(star)
+        return star
+
+    def _drawStar(self, star):
+        return self.model.draw(star)
+
+    def _getRawProfile(self, star):
+        return self.model.getProfile(star.fit.get_params(self._num)), self.model._method
+
+    def _finish_write(self, writer, logger):
+        """Finish the writing process with any class-specific steps.
+
+        :param writer:      A writer object that encapsulates the serialization format.
+        :param logger:      A logger object for logging debug info.
+        """
+        logger = galsim.config.LoggerWrapper(logger)
+        chisq_dict = {
+            'chisq' : self.chisq,
+            'last_delta_chisq' : self.last_delta_chisq,
+            'dof' : self.dof,
+            'nremoved' : self.nremoved,
+            'niter' : self.niter,
+        }
+        writer.write_struct('chisq', chisq_dict)
+        logger.debug("Wrote the chisq info to %s", writer.get_full_name('chisq'))
+        self.model.write(writer, 'model')
+        logger.debug("Wrote the PSF model to %s", writer.get_full_name('model'))
+        self.interp.write(writer, 'interp')
+        logger.debug("Wrote the PSF interp to %s", writer.get_full_name('interp'))
+        if self.outliers:
+            self.outliers.write(writer, 'outliers')
+            logger.debug("Wrote the PSF outliers to %s", writer.get_full_name('outliers'))
+
+    def _finish_read(self, reader, logger):
+        """Finish the reading process with any class-specific steps.
+
+        :param reader:      A reader object that encapsulates the serialization format.
+        :param name:        Name associated with this PSF in the serialized output.
+        :param logger:      A logger object for logging debug info.
+        """
+        chisq_dict = reader.read_struct('chisq')
+        for key in chisq_dict:
+            setattr(self, key, chisq_dict[key])
+        self.model = Model.read(reader, 'model')
+        self.interp = Interp.read(reader, 'interp')
+        self.outliers = Outliers.read(reader, 'outliers')
