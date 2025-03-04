565f593950d352d0b9d9df701fb321e4a1b204b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:22 +0000

    Add package metaflow-checkpoint/0.1.9/metaflow_checkpoint-0.1.9.tar.gz

---
+++ packages/metaflow-checkpoint/metaflow_checkpoint-0.1.9.tar.gz/metaflow_checkpoint-0.1.9/metaflow_extensions/obcheckpoint/plugins/__init__.py
@@ -0,0 +1,20 @@
+STEP_DECORATORS_DESC = [
+    (
+        "checkpoint",
+        ".machine_learning_utilities.checkpoints.decorator.CheckpointDecorator",
+    ),
+    (
+        "model",
+        ".machine_learning_utilities.modeling_utils.decorator.ModelDecorator",
+    ),
+    (
+        "huggingface_hub",
+        ".machine_learning_utilities.hf_hub.decorator.HuggingfaceHubDecorator",
+    ),
+]
+
+###
+# CONFIGURE: Similar to datatools, you can make visible under metaflow.plugins.* other
+#            submodules not referenced in this file
+###
+__mf_promote_submodules__ = []
---
+++ packages/metaflow-checkpoint/metaflow_checkpoint-0.1.9.tar.gz/metaflow_checkpoint-0.1.9/metaflow_extensions/obcheckpoint/toplevel/toplevel.py
@@ -0,0 +1,13 @@
+__mf_extensions__ = "obcheckpoint"
+
+from ..plugins.machine_learning_utilities.checkpoints.final_api import Checkpoint
+from ..plugins.machine_learning_utilities.datastructures import load_model
+
+try:
+    import pkg_resources
+
+    __version__ = pkg_resources.get_distribution("metaflow-checkpoint").version
+except:
+    # this happens on remote environments since the job package
+    # does not have a version
+    __version__ = None
