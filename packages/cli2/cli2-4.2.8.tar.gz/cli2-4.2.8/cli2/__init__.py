ed7e605ef21e8feb7730e58092006ce214673234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:12 +0000

    Add package cli2/4.2.8/cli2-4.2.8.tar.gz

---
+++ packages/cli2/cli2-4.2.8.tar.gz/cli2-4.2.8/cli2/__init__.py
@@ -0,0 +1,56 @@
+# flake8: noqa
+from .cli import (
+    cmd,
+    arg,
+    hide,
+    retrieve,
+    Argument,
+    Command,
+    Group,
+    EntryPoint,
+)
+from .asyncio import async_resolve, async_run
+from .colors import colors as c
+
+from .configuration import Configuration, cfg
+try:
+    from .client import (
+        ClientError,
+        ResponseError,
+        TokenGetError,
+        RefusedResponseError,
+        RetriesExceededError,
+        FieldError,
+        FieldValueError,
+        FieldExternalizeError,
+        Client,
+        ClientCommand,
+        DateTimeField,
+        Field,
+        Handler,
+        JSONStringField,
+        Model,
+        ModelCommand,
+        Paginator,
+        Related,
+    )
+except ImportError:
+    raise
+    # httpx not installed
+    pass
+from .display import diff, diff_data, render, print, highlight, yaml_highlight
+from .lock import Lock
+from .log import configure, log
+from .table import Table
+
+
+def which(cmd):
+    """ Wrapper around shutil.which, and also check for ~/.local/bin. """
+    import shutil
+    path = shutil.which(cmd)
+    if path:
+        return path
+
+    path = Path(os.getenv('HOME')) / '.local/bin' / cmd
+    if path.exists():
+        return str(path)
