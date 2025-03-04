687dfe5b5106a5f8472e42fdbefa95cf7c676579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 18:05:28 +0000

    Add package nonebot-plugin-ernie/0.3.1/nonebot_plugin_ernie-0.3.1.tar.gz

---
+++ packages/nonebot-plugin-ernie/nonebot_plugin_ernie-0.3.1.tar.gz/nonebot_plugin_ernie-0.3.1/nonebot_plugin_ernie/config.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class PluginConfig(BaseModel):
+    """Plugin Config Here"""    
+    wenxin_ak: str = ""
+    wenxin_sk: str = ""
+    wenxin_model: str ="ernie-4.0-turbo-8k"
+    wenxin_sendpic: bool = False
