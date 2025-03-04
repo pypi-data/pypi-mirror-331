7f6e98aef92fc643786f86d9159b74b3f160d3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:49 +0000

    Add package free-llm-toolbox/0.1.1/free_llm_toolbox-0.1.1.tar.gz

---
+++ packages/free-llm-toolbox/free_llm_toolbox-0.1.1.tar.gz/free_llm_toolbox-0.1.1/src/__init__.py
@@ -0,0 +1,5 @@
+"""
+Free LLM Toolbox - A Python package for easy interaction with various LLMs and Vision models
+"""
+# Ce fichier redirige l'importation vers le sous-package free_llm_toolbox
+from .free_llm_toolbox import *
