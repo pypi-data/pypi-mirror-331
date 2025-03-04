60bfaca46b70930267b9159b4306d75b189cf64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 06:05:15 +0000

    Add package jsonrpclib-pelix/0.4.3.4/jsonrpclib_pelix-0.4.3.4.tar.gz

---
+++ packages/jsonrpclib-pelix/jsonrpclib_pelix-0.4.3.4.tar.gz/jsonrpclib_pelix-0.4.3.4/run_tests.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Script to execute tests in Docker
+#
+
+echo "Installing dependencies..."
+pip install pytest coverage || exit 1
+export COVERAGE_PROCESS_START=".coveragerc"
+
+echo "Initial tests..."
+export JSONRPCLIB_TEST_EXPECTED_LIB=json
+coverage run -m pytest || exit 1
+
+echo "orJson tests..."
+pip install orjson && (
+    export JSONRPCLIB_TEST_EXPECTED_LIB=orjson
+    coverage run -m pytest tests/test_jsonlib.py || exit 1
+    pip uninstall -y orjson
+)
+
+echo "uJson tests..."
+pip install ujson && (
+    export JSONRPCLIB_TEST_EXPECTED_LIB=ujson
+    coverage run -m pytest tests/test_jsonlib.py || exit 1
+    pip uninstall -y ujson
+)
+
+echo "cJson tests..."
+pip install python-cjson && (
+    export JSONRPCLIB_TEST_EXPECTED_LIB=cjson
+    coverage run -m pytest tests/test_jsonlib.py || exit 1
+    pip uninstall -y python-cjson
+)
+
+echo "simplejson tests..."
+pip install simplejson && (
+    export JSONRPCLIB_TEST_EXPECTED_LIB=simplejson
+    coverage run -m pytest tests/test_jsonlib.py || exit 1
+    pip uninstall -y simplejson
+)
+
+echo "Combine results..."
+coverage combine || exit $?
+coverage report
---
+++ packages/jsonrpclib-pelix/jsonrpclib_pelix-0.4.3.4.tar.gz/jsonrpclib_pelix-0.4.3.4/tests/test_cgi.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -- Content-Encoding: UTF-8 --
+"""
+Tests the CGI request handler
+
+:license: Apache License 2.0
+"""
+
+from __future__ import print_function
+
+# Standard library
+import os
+import random
+import threading
+import unittest
+
+try:
+    from http.server import HTTPServer, CGIHTTPRequestHandler
+except ImportError:
+    from BaseHTTPServer import HTTPServer  # type: ignore
+    from CGIHTTPServer import CGIHTTPRequestHandler  # type: ignore
+
+# JSON-RPC library
+from jsonrpclib import ServerProxy
+
+# ------------------------------------------------------------------------------
+
+
+class CGIHandlerTests(unittest.TestCase):
+    """
+    These tests verify that the CGI request handler works correctly
+    """
+
+    def test_server(self):
+        """
+        Tests the CGI request handler
+        """
+        # Move the parent directory of "cgi-bin"
+        old_dir = os.getcwd()
+        try:
+            # Setup server
+            os.chdir(os.path.dirname(__file__))
+            server = HTTPServer(("localhost", 0), CGIHTTPRequestHandler)
+
+            # Serve in a thread
+            thread = threading.Thread(target=server.serve_forever)
+            thread.daemon = True
+            thread.start()
+
+            # Find its port
+            port = server.socket.getsockname()[1]
+
+            # Make the client
+            client = ServerProxy(
+                "http://localhost:{0}/cgi-bin/cgi_server.py".format(port)
+            )
+
+            # Check call
+            for _ in range(2):
+                rand1, rand2 = random.random(), random.random()
+                result = client.add(rand1, rand2)
+                self.assertEqual(result, rand1 + rand2)
+
+            # Close server
+            server.shutdown()
+            server.server_close()
+            thread.join()
+        finally:
+            os.chdir(old_dir)
