3e3ecce5add225da9f40ae731d8bb5d0e55e4b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:13 +0000

    Add package skyfire-sdk/0.6.11/skyfire_sdk-0.6.11-py3-none-any.whl

---
+++ packages/skyfire-sdk/skyfire_sdk-0.6.11-py3-none-any.whl/skyfire_sdk/api/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+# import apis into api package
+from skyfire_sdk.api.api_ninja_api import APINinjaApi
+from skyfire_sdk.api.email_api import EmailApi
+from skyfire_sdk.api.gift_card_api import GiftCardApi
+from skyfire_sdk.api.net_nut_api import NetNutApi
+from skyfire_sdk.api.open_ai_api import OpenAIApi
+from skyfire_sdk.api.open_router_api import OpenRouterApi
+from skyfire_sdk.api.organization_api import OrganizationApi
+from skyfire_sdk.api.payments_api import PaymentsApi
+from skyfire_sdk.api.perplexity_api import PerplexityApi
+from skyfire_sdk.api.pricing_culture_api import PricingCultureApi
+from skyfire_sdk.api.venice_api import VeniceApi
+from skyfire_sdk.api.vetric_api import VetricApi
+from skyfire_sdk.api.wallet_management_api import WalletManagementApi
+
