fa5a71430b9d6631eb9d77bb0a4aa6e192849929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:11:55 +0000

    Add package torchdiy/0.8.2/torchdiy-0.8.2.tar.gz

---
+++ packages/torchdiy/torchdiy-0.8.2.tar.gz/torchdiy-0.8.2/torchdiy/__init__.py
@@ -0,0 +1,15 @@
+import torch
+from . import nn
+# from . import optim
+# from torch import optim
+import torch.optim as optim
+from . import utils
+from .loss import logsumexp
+# import torch.utils as utils
+from . import utils
+from torch import *
+from torch.utils import *
+from . import transformers
+
+# __all__ = ['nn', 'optim', 'utils', 'logsumexp']
+__all__ = ['nn', 'optim', 'utils', 'logsumexp']
