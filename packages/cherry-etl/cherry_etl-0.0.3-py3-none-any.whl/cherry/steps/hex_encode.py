4e8fd177e96c4b4c694dcceb506327365894eef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:04:42 +0000

    Add package cherry-etl/0.0.3/cherry_etl-0.0.3-py3-none-any.whl

---
+++ packages/cherry-etl/cherry_etl-0.0.3-py3-none-any.whl/cherry/steps/hex_encode.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from copy import deepcopy
+
+from ..config import HexEncodeConfig
+from cherry_core import hex_encode, prefix_hex_encode
+import pyarrow as pa
+
+
+def execute(data: Dict[str, pa.Table], config: HexEncodeConfig) -> Dict[str, pa.Table]:
+    data = deepcopy(data)
+
+    decode_fn = prefix_hex_encode if config.prefixed else hex_encode
+
+    table_names = data.keys() if config.tables is None else config.tables
+
+    for table_name in table_names:
+        batches = data[table_name].to_batches()
+        out_batches = []
+
+        for batch in batches:
+            out_batches.append(decode_fn(batch))
+
+        data[table_name] = pa.Table.from_batches(out_batches)
+
+    return data
