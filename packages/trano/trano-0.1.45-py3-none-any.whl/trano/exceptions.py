a61b97ce29efcc21789283a59dda3e477f435fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:18 +0000

    Add package trano/0.1.45/trano-0.1.45-py3-none-any.whl

---
+++ packages/trano/trano-0.1.45-py3-none-any.whl/trano/exceptions.py
@@ -0,0 +1,24 @@
+class WrongSystemFlowError(Exception):
+    """Exception raised for wrong system flow."""
+
+
+class ControllerBusPortError(Exception):
+    """Exception raised for controller bus port error."""
+
+
+class IncompatiblePortsError(Exception): ...
+
+
+class DockerNotInstalledError(Exception): ...
+
+
+class DockerClientError(Exception): ...
+
+
+class ConnectionLimitReached(Exception): ...  # noqa: N818
+
+
+class NoConnectionFoundError(Exception): ...
+
+
+class ContainerNotFoundError(Exception): ...
