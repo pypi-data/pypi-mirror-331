7af19a16a8386fc710031d57d68263e4715aa075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:46 +0000

    Add package NEWNAMEsalahaldin/0.1/newnamesalahaldin-0.1.tar.gz

---
+++ packages/NEWNAMEsalahaldin/newnamesalahaldin-0.1.tar.gz/newnamesalahaldin-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="NEWNAMEsalahaldin",
+    version="0.1",
+    description="Gaussian distributions",
+    packages=["distributions"],
+    authorname="salahaldin almughayyar",
+    zip_safe=False,
+)
