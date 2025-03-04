ad2ed3bf9612022b956d14c88819b87ccbff2f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 06:04:19 +0000

    Add package libapt/0.1.0/libapt-0.1.0.tar.gz

---
+++ packages/libapt/libapt-0.1.0.tar.gz/libapt-0.1.0/README.md
@@ -0,0 +1,3 @@
+# libapt
+
+Libapt provides an interface with Debian APT package repositories.
---
+++ packages/libapt/libapt-0.1.0.tar.gz/libapt-0.1.0/example/main.py
@@ -0,0 +1,13 @@
+"""
+Demo for using libapt.
+"""
+
+
+def main():
+    """
+    Example for ...
+    """
+
+
+if __name__ == "__main__":
+    main()
