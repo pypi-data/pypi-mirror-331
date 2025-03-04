d6a2095a89935d7756d318432a7eb36ce066dd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:26 +0000

    Add package sensor-routing/0.1.12/sensor_routing-0.1.12.tar.gz

---
+++ packages/sensor-routing/sensor_routing-0.1.12.tar.gz/sensor_routing-0.1.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sensor_routing',
+    version='0.1.12',
+    description='Sensor routing',
+    author='Can',
+    author_email='can.topaclioglu@ufz.de',
+    packages=find_packages(),
+    python_requires='>=3.12',
+)
