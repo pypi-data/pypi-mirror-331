10e877c6eb0c1652353be57d149e5e4ba576710b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 18:05:04 +0000

    Add package visionone-filesecurity/1.4.1/visionone-filesecurity-1.4.1.tar.gz

---
+++ packages/visionone-filesecurity/visionone-filesecurity-1.4.1.tar.gz/visionone-filesecurity-1.4.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("VERSION", "r") as fh:
+    package_version = fh.read().strip()
+setup(install_requires=['grpcio==1.60.0', 'protobuf==4.25.1'],
+    name="visionone-filesecurity",
+    version=package_version,
+    author="Trend Micro VisionOne File Security Team",
+    description="Trend Micro VisionOne File Security SDK for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trendmicro/tm-v1-fs-python-sdk",
+    packages=find_namespace_packages(exclude=["tests*", "examples"]),
+    package_data={"amaas": ["grpc/protos/*"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
