d2eaa0db77d46e828adeb94f708b7236fdacc867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 05:04:10 +0000

    Add package cr/1.9/cr-1.9-py3-none-any.whl

---
+++ packages/cr/cr-1.9-py3-none-any.whl/cr/__init__.py
@@ -0,0 +1,87 @@
+import enum
+import logging
+
+
+VERSION = "1.9"
+
+
+DOCS_LINK = "https://www.codered.cloud/cli/"
+
+
+# Logger for this module.
+LOGGER = logging.getLogger("cr")
+
+
+USER_AGENT = f"CodeRed-CLI/{VERSION} ({DOCS_LINK})"
+
+
+PROD_BRANCHES = [
+    "live",
+    "main",
+    "master",
+    "prod",
+    "production",
+    "release",
+]
+
+
+STAGING_BRANCHES = [
+    "dev",
+    "develop",
+    "development",
+    "gold",
+    "pre-prod",
+    "pre-production",
+    "pre-release",
+    "preprod",
+    "preproduction",
+    "prerelease",
+    "stage",
+    "staging",
+    "test",
+    "testing",
+]
+
+
+class ConfigurationError(Exception):
+    """
+    Raised when project does not match expected configuration.
+    """
+
+    pass
+
+
+class UserCancelError(Exception):
+    """
+    Raised when a user intentionally cancels the current operation.
+    """
+
+    pass
+
+
+class AppType(enum.Enum):
+    CODEREDCMS = "coderedcms"
+    DJANGO = "django"
+    HTML = "html"
+    WAGTAIL = "wagtail"
+    WORDPRESS = "wordpress"
+
+    def __str__(self):
+        return self.value
+
+
+class DatabaseType(enum.Enum):
+    MARIADB = "mariadb"
+    MYSQL = "mysql"
+    POSTGRES = "postgres"
+
+    def __str__(self):
+        return self.value
+
+
+class Env(enum.Enum):
+    PROD = "prod"
+    STAGING = "staging"
+
+    def __str__(self):
+        return self.value
