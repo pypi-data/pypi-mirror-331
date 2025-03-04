23f71ea99057751083d5cb0b55bd5f17dbc311e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:03:55 +0000

    Add package besapi/3.8.1/besapi-3.8.1.tar.gz

---
+++ packages/besapi/besapi-3.8.1.tar.gz/besapi-3.8.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+file to configure python build and packaging for pip
+"""
+
+try:
+    from setuptools import setup
+except (ImportError, ModuleNotFoundError):
+    from distutils.core import setup
+
+setup(
+    name="besapi",
+    # version= moved to setup.cfg
+    author="Matt Hansen, James Stewart",
+    author_email="hansen.m@psu.edu, james@jgstew.com",
+    description="Library for working with the BigFix REST API",
+    license="MIT",
+    keywords="bigfix iem tem rest api",
+    url="https://github.com/jgstew/besapi",
+    # long_description= moved to setup.cfg
+    packages=["besapi", "bescli"],
+    package_data={"besapi": ["schemas/*.xsd"]},
+    install_requires=["requests", "lxml", "cmd2"],
+    include_package_data=True,
+    package_dir={"": "src"},
+)
