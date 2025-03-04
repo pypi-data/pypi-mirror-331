f82cd3ef2d568e16c5387a153f20348a972aa136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 06:04:15 +0000

    Add package napoleon-network/0.1.5/napoleon_network-0.1.5.tar.gz

---
+++ packages/napoleon-network/napoleon_network-0.1.5.tar.gz/napoleon_network-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="napoleon-network",
+    version="0.1.5",
+    license="MIT",
+    author="Matheo",
+    author_email="m.besson2008@gmail.com",
+    description="Une librairie pour creer une IA basique",
+    url="https://github.com/MathNosseb/Napoleon",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
