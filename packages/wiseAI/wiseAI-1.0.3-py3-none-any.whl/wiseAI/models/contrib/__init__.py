70ad60da4ce5ce1e99b55ecf286b6327eac92b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 14:05:31 +0000

    Add package wiseAI/1.0.3/wiseAI-1.0.3-py3-none-any.whl

---
+++ packages/wiseAI/wiseAI-1.0.3-py3-none-any.whl/wiseAI/models/contrib/__init__.py
@@ -0,0 +1,9 @@
+from .automl_binary import AutoBinaryML
+from .automl_multiclass import AutoMultiClassML
+from .automl_multilabel import MultiClassImbalanceChain
+
+__all__ = [
+    "AutoBinaryML",
+    "AutoMultiClassML",
+    "MultiClassImbalanceChain",
+]
