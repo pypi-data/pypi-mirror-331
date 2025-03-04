a90ce39057ae2a948db65c47b850ce2ef847cd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 03:12:45 +0000

    Add package django-infra/0.1.2/django_infra-0.1.2-py3-none-any.whl

---
+++ packages/django-infra/django_infra-0.1.2-py3-none-any.whl/django_infra/env/__init__.py
@@ -0,0 +1,4 @@
+from .shell import load_env_val
+from .shell import run_command
+
+__all__ = ['load_env_val', 'run_command']
---
+++ packages/django-infra/django_infra-0.1.2-py3-none-any.whl/django_infra/exporter/views.py
@@ -0,0 +1,35 @@
+from rest_framework.serializers import ModelSerializer
+
+from django_infra.api.views import FilteredPartialResponseModelViewSet
+from django_infra.exporter.models import QueryExport
+from django_infra.api import filters
+
+
+class QueryExportSerializer(ModelSerializer):
+    class Meta:
+        model=QueryExport
+        fields = '__all__'
+        read_only_fields= ('state','file', 'format', 'id', 'metadata')
+
+class QueryExportViewSet(FilteredPartialResponseModelViewSet):
+    model = QueryExport
+    serializer_class = QueryExportSerializer
+    permission_classes = []
+    filters = [
+        filters.OrderingFilter(
+            name='sort',
+            fields=[
+                filters.FilterField(internal='id'),
+            ]
+        ),
+        filters.ExactMatchFilter(
+            fields=[
+                filters.TypedFilterField(type_=str, internal='state')
+            ]
+        ),
+        filters.IContainsFilter(
+            fields=[
+                filters.MultiInternalFilterField(internal=['file'], external='search')
+            ]
+        )
+    ]
