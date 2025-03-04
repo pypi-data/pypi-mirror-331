dcbda10391f4e0605d4d1eeefe06637cfed4a0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 17:05:20 +0000

    Add package datalayer-ui/0.3.14/datalayer_ui-0.3.14-py3-none-any.whl

---
+++ packages/datalayer-ui/datalayer_ui-0.3.14-py3-none-any.whl/datalayer_ui/handlers/index/handler.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2021-2024 Datalayer, Inc.
+#
+# Datalayer License
+
+# Copyright (c) Datalayer Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+"""Index handler."""
+
+import tornado
+
+from datalayer_ui.handlers.base import BaseTemplateHandler
+
+
+# pylint: disable=W0223
+class IndexHandler(BaseTemplateHandler):
+    """The handler for the index."""
+
+    @tornado.web.authenticated
+    def get(self):
+        """The index page."""
+        self.write(self.render_template("index.html"))
