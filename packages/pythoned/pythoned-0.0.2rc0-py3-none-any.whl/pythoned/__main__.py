d463d5850e132d2d2d850ca51beb1cde769769ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 15:04:16 +0000

    Add package pythoned/0.0.2rc0/pythoned-0.0.2rc0-py3-none-any.whl

---
+++ packages/pythoned/pythoned-0.0.2rc0-py3-none-any.whl/pythoned/__main__.py
@@ -0,0 +1,19 @@
+import argparse
+import sys
+
+from pythoned.edit import edit
+
+
+def main() -> int:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("expression")
+
+    args = arg_parser.parse_args()
+    expression: str = args.expression
+    for output_line in edit(sys.stdin, expression):
+        print(output_line, end="")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
