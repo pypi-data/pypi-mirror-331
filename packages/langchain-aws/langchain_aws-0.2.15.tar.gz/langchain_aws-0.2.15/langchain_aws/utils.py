00f531284bbd83c8635731d01ba84d97693dc895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 17:04:39 +0000

    Add package langchain-aws/0.2.15/langchain_aws-0.2.15.tar.gz

---
+++ packages/langchain-aws/langchain_aws-0.2.15.tar.gz/langchain_aws-0.2.15/langchain_aws/utils.py
@@ -0,0 +1,55 @@
+import re
+from typing import Any, List
+
+from packaging import version
+
+
+def enforce_stop_tokens(text: str, stop: List[str]) -> str:
+    """Cut off the text as soon as any stop words occur."""
+    return re.split("|".join(stop), text, maxsplit=1)[0]
+
+
+def anthropic_tokens_supported() -> bool:
+    """Check if all requirements for Anthropic count_tokens() are met."""
+    try:
+        import anthropic
+    except ImportError:
+        return False
+
+    if version.parse(anthropic.__version__) > version.parse("0.38.0"):
+        return False
+
+    try:
+        import httpx
+
+        if version.parse(httpx.__version__) > version.parse("0.27.2"):
+            raise ImportError()
+    except ImportError:
+        raise ImportError("httpx<=0.27.2 is required.")
+
+    return True
+
+
+def _get_anthropic_client() -> Any:
+    import anthropic
+
+    return anthropic.Anthropic()
+
+
+def get_num_tokens_anthropic(text: str) -> int:
+    """Get the number of tokens in a string of text."""
+    client = _get_anthropic_client()
+    return client.count_tokens(text=text)
+
+
+def get_token_ids_anthropic(text: str) -> List[int]:
+    """Get the token ids for a string of text."""
+    client = _get_anthropic_client()
+    tokenizer = client.get_tokenizer()
+    encoded_text = tokenizer.encode(text)
+    return encoded_text.ids
+
+
+def thinking_in_params(params: dict) -> bool:
+    """Check if the thinking parameter is enabled in the request."""
+    return params.get("thinking", {}).get("type") == "enabled"
