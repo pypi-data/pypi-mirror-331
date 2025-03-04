9aee79ce06c80849da75d615f98b453634265350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:03:54 +0000

    Add package hots/1.0.0/hots-1.0.0.tar.gz

---
+++ packages/hots/hots-1.0.0.tar.gz/hots-1.0.0/setup.py
@@ -0,0 +1,12 @@
+"""
+=========
+hots setup
+=========
+
+Provide packaging operations through ``python setup.py [command]`` and
+``pip install options .[...]``
+"""
+
+from setuptools import setup
+
+setup()
