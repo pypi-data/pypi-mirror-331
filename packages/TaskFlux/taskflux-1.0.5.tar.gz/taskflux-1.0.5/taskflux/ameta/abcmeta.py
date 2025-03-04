f1b00c0296b6455c1ce35b9757fe77503646a8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 14:05:06 +0000

    Add package TaskFlux/1.0.5/taskflux-1.0.5.tar.gz

---
+++ packages/TaskFlux/taskflux-1.0.5.tar.gz/taskflux-1.0.5/taskflux/ameta/abcmeta.py
@@ -0,0 +1,135 @@
+# -*- encoding: utf-8 -*-
+
+import abc
+
+from taskflux.logger import *
+from taskflux.queue import *
+from taskflux.rpc_proxy import *
+from taskflux.interface import *
+
+__all__ = ['ServiceConstructor', 'WorkerConstructor']
+
+
+class Meta(abc.ABC):
+
+    @property
+    @abc.abstractmethod
+    def rpc_call(self): ...
+
+    @property
+    @abc.abstractmethod
+    def rpc_proxy_call(self): ...
+
+    @property
+    @abc.abstractmethod
+    def submit_task(self): ...
+
+    @property
+    @abc.abstractmethod
+    def send_message(self): ...
+
+    @property
+    @abc.abstractmethod
+    def rabbit_send_message(self): ...
+
+    @property
+    @abc.abstractmethod
+    def logger(self): ...
+
+    @property
+    @abc.abstractmethod
+    def name(self): ...
+
+
+class ServiceMeta(Meta):
+
+    @property
+    @abc.abstractmethod
+    def service_name(self): ...
+
+    @property
+    @abc.abstractmethod
+    def service_version(self): ...
+
+    @property
+    @abc.abstractmethod
+    def service_ipaddr(self): ...
+
+
+class WorkerMeta(Meta):
+
+    @property
+    @abc.abstractmethod
+    def worker_name(self): ...
+
+    @property
+    @abc.abstractmethod
+    def worker_version(self): ...
+
+    @property
+    @abc.abstractmethod
+    def worker_ipaddr(self): ...
+
+    @abc.abstractmethod
+    def run(self, data): ...
+
+
+class ServiceConstructor(ServiceMeta):
+    """
+    ServiceConstructor is a class that represents a constructor for a service.
+    It contains attributes such as name, logger, service_name, service_ipaddr, service_version, and functions.
+    """
+    functions: list = []
+    name: str = None
+    service_name: str = None
+    service_ipaddr: str = None
+    service_version: str = None
+
+    logger: logger = None
+    rpc_call: remote_call = None
+    rpc_proxy_call: proxy_call = None
+    rabbit_send_message: send_message = None
+    submit_task: databases_submit_task = None
+    send_message: databases_send_message = None
+
+    @classmethod
+    def setattr(cls, name, value):
+        setattr(cls, name, value)
+
+    def __call__(self):
+        return self
+
+
+class WorkerConstructor(WorkerMeta):
+    """
+    WorkerConstructor is a class that represents a constructor for a worker.
+    It contains attributes such as name, logger, worker_name, worker_ipaddr, worker_version, and functions.
+    It also contains a method called run, which takes in a body and runs the worker with the given body.
+    """
+    functions: list = []
+
+    worker_name: str = None
+    worker_ipaddr: str = None
+    worker_version: str = None
+
+    name: str = None
+    logger: logger = None
+    rpc_call: remote_call = None
+    rpc_proxy_call: proxy_call = None
+    rabbit_send_message: send_message = None
+    submit_task: databases_submit_task = None
+    send_message: databases_send_message = None
+
+    @classmethod
+    def setattr(cls, name, value):
+        setattr(cls, name, value)
+
+    def run(self, body):
+        """
+        Runs the worker with the given body.
+        Args:
+            body (dict): The body of the worker.
+        """
+
+    def __call__(self):
+        return self
