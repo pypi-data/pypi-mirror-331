3f75432c3fc4b86f95005432c9f248a3aef83ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 04:04:25 +0000

    Add package gladify/0.2.2/gladify-0.2.2.tar.gz

---
+++ packages/gladify/gladify-0.2.2.tar.gz/gladify-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gladify",
+    version="0.2.2",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "gladify.GladUI": ["GladUI.png"],  # Updated path
+    },
+    install_requires=[],
+    author="Navthej",
+    author_email="gladgamingstudio@gmail.com",
+    description="A Python package for various utilities",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,  # Ensures files are extracted
+)
