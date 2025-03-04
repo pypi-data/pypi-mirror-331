9d03b03b6c0b13de20a631ee32a9539ac9ab50dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 07:04:11 +0000

    Add package morethemes/0.3.0/morethemes-0.3.0.tar.gz

---
+++ packages/morethemes/morethemes-0.3.0.tar.gz/morethemes-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="morethemes",
+    version="0.3.0",
+    packages=["morethemes"],
+    description="More themes for matplotlib",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Joseph Barbier",
+    author_email="joseph.barbierdarnal@gmail.com",
+    url="https://github.com/JosephBARBIERDARNAL/morethemes",
+    install_requires=["matplotlib"],
+    include_package_data=True,
+)
