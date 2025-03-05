c68a297583d94713e134861816131c7a4cc9583c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:05:35 +0000

    Add package aa-sov-timer/2.3.3/aa_sov_timer-2.3.3.tar.gz

---
+++ packages/aa-sov-timer/aa_sov_timer-2.3.3.tar.gz/aa_sov_timer-2.3.3/sovtimer/constants.py
@@ -0,0 +1,25 @@
+"""
+Constants we use throughout the app
+"""
+
+# Standard Library
+import os
+
+# Alliance Auth
+from esi import __version__ as esi_version
+
+# AA Sovereignty Timer
+from sovtimer import __version__
+
+APP_NAME = "aa-sov-timer"
+PACKAGE_NAME = "sovtimer"
+GITHUB_URL = f"https://github.com/ppfeufer/{APP_NAME}"
+USER_AGENT = f"{APP_NAME}/{__version__} +{GITHUB_URL} via django-esi/{esi_version}"
+
+# aa-sov-timer/sovtimer
+APP_BASE_DIR = os.path.join(os.path.dirname(__file__))
+# aa-sov-timer/sovtimer/static/sovtimer
+APP_STATIC_DIR = os.path.join(APP_BASE_DIR, "static", PACKAGE_NAME)
+
+# All internal URLs need to start with this prefix
+INTERNAL_URL_PREFIX = "-"
