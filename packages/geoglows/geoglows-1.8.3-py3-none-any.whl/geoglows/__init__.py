6bf20213da6d85fa772cd26efc2e1089bd2ede98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 07:05:25 +0000

    Add package geoglows/1.8.3/geoglows-1.8.3-py3-none-any.whl

---
+++ packages/geoglows/geoglows-1.8.3-py3-none-any.whl/geoglows/__init__.py
@@ -0,0 +1,17 @@
+import geoglows.bias
+import geoglows._plots as plots
+import geoglows.data
+import geoglows.analyze
+import geoglows.streams
+import geoglows.tables
+import geoglows.streamflow
+
+from ._constants import get_metadata_table_path, set_metadata_table_path
+
+__all__ = [
+    'bias', 'plots', 'data', 'analyze', 'streams', 'tables', 'streamflow',
+    'get_metadata_table_path', 'set_metadata_table_path',
+]
+__version__ = '1.8.3'
+__author__ = 'Riley Hales'
+__license__ = 'BSD 3-Clause Clear License'
---
+++ packages/geoglows/geoglows-1.8.3-py3-none-any.whl/geoglows/_constants.py
@@ -0,0 +1,37 @@
+import os
+
+ODP_CORE_S3_BUCKET_URI = 's3://geoglows-v2'
+ODP_FORECAST_S3_BUCKET_URI = 's3://geoglows-v2-forecasts'
+ODP_RETROSPECTIVE_S3_BUCKET_URI = 's3://geoglows-v2-retrospective'
+ODP_S3_BUCKET_REGION = 'us-west-2'
+
+METADATA_TABLE_ENV_KEY = 'PYGEOGLOWS_METADATA_TABLE_PATH'
+TRANSFORMER_TABLE_ENV_KEY = 'PYGEOGLOWS_TRANSFORMER_TABLE_URI'
+SFDC_ZARR_ENV_KEY = 'PYGEOGLOWS_SFDC_ZARR_URI'
+
+
+def get_sfdc_zarr_uri() -> str:
+    return os.getenv(SFDC_ZARR_ENV_KEY, 's3://rfs-v2/transformers/sfdc.zarr')
+
+
+def set_sfdc_zarr_uri(uri: str) -> None:
+    os.environ[SFDC_ZARR_ENV_KEY] = uri
+    return
+
+
+def get_transformer_table_uri() -> str:
+    return os.getenv(TRANSFORMER_TABLE_ENV_KEY, 's3://rfs-v2/transformers/transformer_table.parquet')
+
+
+def set_transformer_table_uri(uri: str) -> None:
+    os.environ[TRANSFORMER_TABLE_ENV_KEY] = uri
+    return
+
+
+def get_metadata_table_path() -> str:
+    return os.getenv(METADATA_TABLE_ENV_KEY, os.path.join(os.path.dirname(__file__), 'data', 'metadata-tables.parquet'))
+
+
+def set_metadata_table_path(path: str) -> None:
+    os.environ[METADATA_TABLE_ENV_KEY] = path
+    return
