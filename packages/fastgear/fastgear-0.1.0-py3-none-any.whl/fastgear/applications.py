2c8b62bfd4b47e6a9bc5468990d292295b2b34ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:48 +0000

    Add package fastgear/0.1.0/fastgear-0.1.0-py3-none-any.whl

---
+++ packages/fastgear/fastgear-0.1.0-py3-none-any.whl/fastgear/applications.py
@@ -0,0 +1,27 @@
+import contextlib
+
+from fastapi import FastAPI
+from fastapi_pagination import add_pagination
+
+from fastgear.handlers.http_exceptions_handler import HttpExceptionsHandler
+from fastgear.middlewares.db_session_middleware import (
+    AsyncDBSessionMiddleware,
+    SyncDBSessionMiddleware,
+)
+
+UTILS_CALLABLES = {
+    "http_exceptions_handler": lambda app, *args, **kwargs: HttpExceptionsHandler(app),
+    "http_db_session_middleware": lambda app, *args, **kwargs: app.add_middleware(
+        SyncDBSessionMiddleware, *args, **kwargs,
+    ),
+    "http_db_async_session_middleware": lambda app, *args, **kwargs: app.add_middleware(
+        AsyncDBSessionMiddleware, *args, **kwargs,
+    ),
+    "pagination": lambda app, *args, **kwargs: add_pagination(app),
+}
+
+
+def apply_utils(app: FastAPI, utils: list[str], *args, **kwargs) -> None:
+    for util in utils:
+        with contextlib.suppress(KeyError):
+            UTILS_CALLABLES[util](app, *args, **kwargs)
---
+++ packages/fastgear/fastgear-0.1.0-py3-none-any.whl/fastgear/middlewares/db_session_middleware.py
@@ -0,0 +1,46 @@
+from contextvars import ContextVar
+
+from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+
+from fastgear.common.database.sqlalchemy.session import (
+    AsyncDatabaseSessionFactory,
+    SyncDatabaseSessionFactory,
+)
+
+# Unified context variable for both sync and async sessions
+db_session: ContextVar[Session | AsyncSession | None] = ContextVar("db_session", default=None)
+
+
+class BaseDBSessionMiddleware(BaseHTTPMiddleware):
+    def __init__(
+        self,
+        app: FastAPI,
+        session_factory: SyncDatabaseSessionFactory | AsyncDatabaseSessionFactory,
+    ) -> None:
+        super().__init__(app)
+        self.session_factory = session_factory
+
+
+class SyncDBSessionMiddleware(BaseDBSessionMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        # Synchronous session management
+        with self.session_factory.get_session() as session:
+            db_session.set(session)
+            response = await call_next(request)
+            db_session.set(None)  # Clear the context variable after the request
+        return response
+
+
+class AsyncDBSessionMiddleware(BaseDBSessionMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        # Asynchronous session management
+        async with self.session_factory.get_async_session() as session:
+            db_session.set(session)
+            response = await call_next(request)
+            db_session.set(None)  # Clear the context variable after the request
+        return response
