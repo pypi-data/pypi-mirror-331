f08657bce79f93835fc581d72c9f492a8fce8de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:04:57 +0000

    Add package sketchy-pros-compiler/1.0.2/sketchy_pros_compiler-1.0.2.tar.gz

---
+++ packages/sketchy-pros-compiler/sketchy_pros_compiler-1.0.2.tar.gz/sketchy_pros_compiler-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sketchy_pros_compiler",
+    version="1.0.2",
+    scripts=['pros_sketchy'],
+    description="A build system wrapper for PROS robotics projects",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Zimeng Xiong",
+    url="https://github.com/zxzimeng/sketchy_pros_compiler",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+    python_requires=">=3.6",
+)
