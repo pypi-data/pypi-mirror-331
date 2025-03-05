5ef72149b2cf20cd106148a91edd3963fd82a399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:05:02 +0000

    Add package ul-api-utils/8.1.12/ul_api_utils-8.1.12-py3-none-any.whl

---
+++ packages/ul-api-utils/ul_api_utils-8.1.12-py3-none-any.whl/ul_api_utils/api_resource/db_types.py
@@ -0,0 +1,9 @@
+from typing import Union, Dict, Any, Tuple, Optional, List
+
+from pydantic import BaseModel
+from ul_db_utils.model.base_model import BaseModel as DbBaseModel
+from flask_sqlalchemy import Model
+
+
+TDictable = Union[Dict[str, Any], BaseModel, Tuple[Any, ...], DbBaseModel, Model]  # TODO: remove DbBaseModel/Model from it BECAUSE IT loads sqlalchemy (>20mb of code)
+TPayloadInputUnion = Union[Optional[TDictable], List[TDictable]]
