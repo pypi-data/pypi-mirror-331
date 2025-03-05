fb56f101e8740404503cec59e331cb5d15e753d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:32 +0000

    Add package cloudvision/1.23.2/cloudvision-1.23.2-py3-none-any.whl

---
+++ packages/cloudvision/cloudvision-1.23.2-py3-none-any.whl/cloudvision/cvlib/constants.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2023 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
+
+MAINLINE_WS_ID = ""
+
+# System Dynamic Arguments for actions
+BUILD_ID_ARG = "BuildID"
+INPUT_PATH_ARG = "InputPath"
+STUDIO_ID_ARG = "StudioID"
+STUDIO_IDS_ARG = "StudioIDs"
+WORKSPACE_ID_ARG = "WorkspaceID"
+
+# General timeout for all requests
+TIMEOUT_REQUEST = 60  # 1 min in seconds
