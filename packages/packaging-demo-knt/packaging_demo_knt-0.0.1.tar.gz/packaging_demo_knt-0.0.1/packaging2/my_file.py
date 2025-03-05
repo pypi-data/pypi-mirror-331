e08bac50f54aa696c4ba5603575f71b3a6c1c199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 14:04:27 +0000

    Add package packaging-demo-knt/0.0.1/packaging_demo_knt-0.0.1.tar.gz

---
+++ packages/packaging-demo-knt/packaging_demo_knt-0.0.1.tar.gz/packaging_demo_knt-0.0.1/packaging2/my_file.py
@@ -0,0 +1,11 @@
+# from my_other_file import CONSTANT
+# from my_folder.my_nested_file import CONSTANT as CONSTANT2
+
+
+# print(CONSTANT)
+# print(CONSTANT2)
+
+import numpy as np
+
+MY_ARRAY = np.array([1, 2, 3, 4, 5])
+print(f"My array: {MY_ARRAY}")
