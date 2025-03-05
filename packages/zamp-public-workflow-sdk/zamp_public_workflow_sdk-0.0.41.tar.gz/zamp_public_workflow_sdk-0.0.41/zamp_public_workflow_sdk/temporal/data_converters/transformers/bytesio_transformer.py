1745aab73d6bdec46250999ad10a4ca913b0e6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:28 +0000

    Add package zamp-public-workflow-sdk/0.0.41/zamp_public_workflow_sdk-0.0.41.tar.gz

---
+++ packages/zamp-public-workflow-sdk/zamp_public_workflow_sdk-0.0.41.tar.gz/zamp_public_workflow_sdk-0.0.41/zamp_public_workflow_sdk/temporal/data_converters/transformers/bytesio_transformer.py
@@ -0,0 +1,16 @@
+from zamp_public_workflow_sdk.temporal.data_converters.transformers.base import BaseTransformer
+from typing import Any
+import base64
+from io import BytesIO
+
+class BytesIOTransformer(BaseTransformer):
+    def __init__(self):
+        super().__init__()
+        self.should_serialize = lambda value: isinstance(value, BytesIO) 
+        self.should_deserialize = lambda value, type_hint: type_hint is BytesIO
+
+    def _serialize_internal(self, value: Any) -> Any:
+        return base64.b64encode(value.getvalue()).decode("utf-8")
+
+    def _deserialize_internal(self, value: Any, type_hint: Any) -> Any:
+        return BytesIO(base64.b64decode(value))
