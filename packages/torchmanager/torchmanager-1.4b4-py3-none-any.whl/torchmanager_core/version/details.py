c60173299260e594e30d70224c57d67bef96b3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 08:05:06 +0000

    Add package torchmanager/1.4b4/torchmanager-1.4b4-py3-none-any.whl

---
+++ packages/torchmanager/torchmanager-1.4b4-py3-none-any.whl/torchmanager_core/version/details.py
@@ -0,0 +1,6 @@
+from .version import Version
+
+
+API = Version("v1.4")
+CURRENT = Version("v1.4b4")
+DESCRIPTION: str = f"PyTorch Training Manager {CURRENT}"
---
+++ packages/torchmanager/torchmanager-1.4b4-py3-none-any.whl/torchmanager_core/view/logging.py
@@ -0,0 +1,48 @@
+from logging import *  # type: ignore
+
+logger = getLogger('torchmanager')
+logger.setLevel(INFO)
+
+# initialize console
+def add_console(console: StreamHandler = StreamHandler(), formatter = Formatter("%(message)s")) -> None:
+    """
+    Add console handler to the logger
+
+    - Parameters:
+        - console: A `logging.StreamHandler` of console handler
+    """
+    # check if console handler exists
+    handlers = logger.handlers
+    contains_console = any(type(handler) is type(console) for handler in handlers)
+
+    # set console
+    console.setLevel(INFO)
+    console.setFormatter(formatter)
+
+    # add console handler
+    if not contains_console:
+        logger.addHandler(console)
+    else:
+        for i, handler in enumerate(logger.handlers):
+            if type(handler) is type(console):
+                logger.handlers[i] = console
+                break
+
+def set_log_path(log_path: str) -> Formatter:
+    """
+    Set log path for torchmanager logger
+
+    - Parameters:
+        - log_path: A `str` of log file path
+    - Returns: A `logging.Formatter` of log formatter with the log path
+    """
+    # initialize formatter
+    formatter = Formatter("%(message)s")
+
+    # loop through handlers
+    for i, handler in enumerate(logger.handlers):
+        if isinstance(handler, FileHandler):
+            handler.baseFilename = log_path
+            handler.setFormatter(formatter)
+            logger.handlers[i] = handler
+    return formatter
