3dd0a50e7b568083d46c4a4e7d817ef99a899f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:55 +0000

    Add package fans/0.0.56/fans-0.0.56-py3-none-any.whl

---
+++ packages/fans/fans-0.0.56-py3-none-any.whl/fans/ports.py
@@ -0,0 +1,24 @@
+from fans.bunch import bunch
+
+
+ports = bunch({
+    'quantix':              6562,
+
+    'winslave':             15500,
+
+    'stome_desktop_back':   15510,
+    'stome_desktop_web':    15511,
+
+    'ocal_elevated':        16450,
+    'ocal_unelevated':      16451,
+
+    'auth_back':            16510,
+    'auth_web':             16511,
+    'auth_res_back':        16512,
+    'auth_res_web':         16513,
+
+    'doy_back':             16520,
+    'doy_web':              16521,
+
+    'duf_back':             16530,
+})
