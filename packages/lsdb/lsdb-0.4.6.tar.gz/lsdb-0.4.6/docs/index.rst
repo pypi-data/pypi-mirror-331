59f6079013dfc2115a8e6117770b8529be04e501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:53 +0000

    Add package lsdb/0.4.6/lsdb-0.4.6.tar.gz

---
+++ packages/lsdb/lsdb-0.4.6.tar.gz/lsdb-0.4.6/docs/index.rst
@@ -0,0 +1,88 @@
+.. lsdb documentation main file.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+
+LSDB
+========================================================================================
+
+LSDB (Large Survey DataBase) is a python framework that enables simple, fast spatial analysis of extremely
+large astronomical catalogs (e.g. querying and crossmatching ~10⁹ sources). It aims to address large-scale
+data processing challenges, in particular those brought up by `LSST <https://www.lsst.org/about>`_.
+
+Built on top of Dask to efficiently scale and parallelize operations across multiple distributed workers, it
+uses the `HATS <https://hats.readthedocs.io/en/stable/>`_ data format to efficiently perform spatial
+operations.
+
+.. figure:: _static/gaia.png
+   :class: no-scaled-link
+   :scale: 80 %
+   :align: center
+   :alt: A possible HEALPix distribution for Gaia DR3
+
+   A possible HEALPix distribution for Gaia DR3.
+
+
+Using this Guide
+-------------------------------------------------------------------------------
+.. grid:: 1 1 2 2
+
+   .. grid-item-card:: Getting Started
+       :link: getting-started
+       :link-type: doc
+
+       Installation and QuickStart Guide
+
+   .. grid-item-card:: Tutorials
+       :link: tutorials
+       :link-type: doc
+
+       Learn the LSDB features by working through our guides
+
+.. grid:: 1 1 2 2
+
+   .. grid-item-card:: API Reference
+       :link: autoapi/index
+       :link-type: doc
+
+       The detailed API documentation
+
+   .. grid-item-card:: Example Science Notebooks
+       :link: science-notebooks
+       :link-type: doc
+
+       Jupyter notebooks going over some example scientific use cases
+
+.. toctree::
+   :hidden:
+
+   Home page <self>
+   Getting Started <getting-started>
+   Tutorials <tutorials>
+   Advanced Topics<advanced-topics>
+   Example Science Notebooks<science-notebooks>
+   API Reference <autoapi/index>
+
+.. toctree::
+   :hidden:
+
+   Contact us <contact>
+
+.. toctree::
+   :hidden:
+   :caption: Developers
+
+   Contribution Guide <developer/contributing>
+
+Acknowledgements
+-------------------------------------------------------------------------------
+
+This project is supported by Schmidt Sciences.
+
+This project is based upon work supported by the National Science Foundation
+under Grant No. AST-2003196.
+
+This project acknowledges support from the DIRAC Institute in the Department of 
+Astronomy at the University of Washington. The DIRAC Institute is supported 
+through generous gifts from the Charles and Lisa Simonyi Fund for Arts and 
+Sciences, and the Washington Research Foundation.
---
+++ packages/lsdb/lsdb-0.4.6.tar.gz/lsdb-0.4.6/tests/conftest.py
@@ -0,0 +1,331 @@
+from pathlib import Path
+
+import hats as hc
+import pandas as pd
+import pytest
+from hats.pixel_math import spatial_index_to_healpix
+from hats.pixel_math.spatial_index import SPATIAL_INDEX_COLUMN, healpix_to_spatial_index
+
+import lsdb
+
+DATA_DIR_NAME = "data"
+SMALL_SKY_DIR_NAME = "small_sky"
+SMALL_SKY_LEFT_XMATCH_NAME = "small_sky_left_xmatch"
+SMALL_SKY_SOURCE_DIR_NAME = "small_sky_source"
+SMALL_SKY_SOURCE_MARGIN_NAME = "small_sky_source_margin"
+SMALL_SKY_ORDER3_SOURCE_MARGIN_NAME = "small_sky_order3_source_margin"
+SMALL_SKY_XMATCH_NAME = "small_sky_xmatch"
+SMALL_SKY_XMATCH_MARGIN_NAME = "small_sky_xmatch_margin"
+SMALL_SKY_TO_XMATCH_NAME = "small_sky_to_xmatch"
+SMALL_SKY_TO_XMATCH_SOFT_NAME = "small_sky_to_xmatch_soft"
+SMALL_SKY_ORDER1_DIR_NAME = "small_sky_order1"
+SMALL_SKY_ORDER1_NO_PANDAS_DIR_NAME = "small_sky_order1_no_pandas_meta"
+SMALL_SKY_ORDER1_DEFAULT_COLS_DIR_NAME = "small_sky_order1_default_columns"
+SMALL_SKY_ORDER1_SOURCE_NAME = "small_sky_order1_source"
+SMALL_SKY_ORDER1_SOURCE_MARGIN_NAME = "small_sky_order1_source_margin"
+SMALL_SKY_TO_ORDER1_SOURCE_NAME = "small_sky_to_o1source"
+SMALL_SKY_TO_ORDER1_SOURCE_SOFT_NAME = "small_sky_to_o1source_soft"
+SMALL_SKY_NO_METADATA = "small_sky_no_metadata"
+XMATCH_CORRECT_FILE = "xmatch_correct.csv"
+XMATCH_CORRECT_005_FILE = "xmatch_correct_0_005.csv"
+XMATCH_CORRECT_002_005_FILE = "xmatch_correct_002_005.csv"
+XMATCH_CORRECT_05_2_3N_MARGIN_FILE = "xmatch_correct_05_2_3n_margin.csv"
+XMATCH_CORRECT_3N_2T_FILE = "xmatch_correct_3n_2t.csv"
+XMATCH_CORRECT_3N_2T_NO_MARGIN_FILE = "xmatch_correct_3n_2t_no_margin.csv"
+XMATCH_CORRECT_3N_2T_NEGATIVE_FILE = "xmatch_correct_3n_2t_negative.csv"
+XMATCH_MOCK_FILE = "xmatch_mock.csv"
+TEST_DIR = Path(__file__).parent
+
+
+@pytest.fixture
+def test_data_dir():
+    return Path(TEST_DIR) / DATA_DIR_NAME
+
+
+@pytest.fixture
+def small_sky_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_DIR_NAME
+
+
+@pytest.fixture
+def small_sky_left_xmatch_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_LEFT_XMATCH_NAME
+
+
+@pytest.fixture
+def small_sky_xmatch_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_XMATCH_NAME
+
+
+@pytest.fixture
+def small_sky_xmatch_margin_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_XMATCH_MARGIN_NAME
+
+
+@pytest.fixture
+def small_sky_to_xmatch_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_TO_XMATCH_NAME
+
+
+@pytest.fixture
+def small_sky_to_xmatch_soft_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_TO_XMATCH_SOFT_NAME
+
+
+@pytest.fixture
+def small_sky_order1_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_ORDER1_DIR_NAME
+
+
+@pytest.fixture
+def small_sky_order1_default_cols_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_ORDER1_DEFAULT_COLS_DIR_NAME
+
+
+@pytest.fixture
+def small_sky_order1_no_pandas_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_ORDER1_NO_PANDAS_DIR_NAME
+
+
+@pytest.fixture
+def small_sky_order1_source_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_ORDER1_SOURCE_NAME
+
+
+@pytest.fixture
+def small_sky_source_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_SOURCE_DIR_NAME
+
+
+@pytest.fixture
+def small_sky_source_catalog(small_sky_source_dir):
+    return lsdb.read_hats(small_sky_source_dir)
+
+
+@pytest.fixture
+def small_sky_order1_source_margin_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_ORDER1_SOURCE_MARGIN_NAME
+
+
+@pytest.fixture
+def small_sky_to_order1_source_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_TO_ORDER1_SOURCE_NAME
+
+
+@pytest.fixture
+def small_sky_to_order1_source_soft_dir(test_data_dir):
+    return test_data_dir / SMALL_SKY_TO_ORDER1_SOURCE_SOFT_NAME
+
+
+@pytest.fixture
+def small_sky_hats_catalog(small_sky_dir):
+    return hc.read_hats(small_sky_dir)
+
+
+@pytest.fixture
+def small_sky_order1_id_index_dir(test_data_dir):
+    return test_data_dir / "small_sky_order1_id_index"
+
+
+@pytest.fixture
+def small_sky_catalog(small_sky_dir):
+    return lsdb.read_hats(small_sky_dir)
+
+
+@pytest.fixture
+def small_sky_left_xmatch_catalog(small_sky_left_xmatch_dir):
+    return lsdb.read_hats(small_sky_left_xmatch_dir)
+
+
+@pytest.fixture
+def small_sky_xmatch_catalog(small_sky_xmatch_dir):
+    return lsdb.read_hats(small_sky_xmatch_dir)
+
+
+@pytest.fixture
+def small_sky_xmatch_margin_catalog(small_sky_xmatch_margin_dir):
+    return lsdb.read_hats(small_sky_xmatch_margin_dir)
+
+
+@pytest.fixture
+def small_sky_xmatch_with_margin(small_sky_xmatch_dir, small_sky_xmatch_margin_dir):
+    return lsdb.read_hats(small_sky_xmatch_dir, margin_cache=small_sky_xmatch_margin_dir)
+
+
+@pytest.fixture
+def small_sky_to_xmatch_catalog(small_sky_to_xmatch_dir):
+    return lsdb.read_hats(small_sky_to_xmatch_dir)
+
+
+@pytest.fixture
+def small_sky_to_xmatch_soft_catalog(small_sky_to_xmatch_soft_dir):
+    return lsdb.read_hats(small_sky_to_xmatch_soft_dir)
+
+
+@pytest.fixture
+def small_sky_order1_hats_catalog(small_sky_order1_dir):
+    return hc.read_hats(small_sky_order1_dir)
+
+
+@pytest.fixture
+def small_sky_order1_catalog(small_sky_order1_dir):
+    return lsdb.read_hats(small_sky_order1_dir)
+
+
+@pytest.fixture
+def small_sky_order1_source_with_margin(small_sky_order1_source_dir, small_sky_order1_source_margin_dir):
+    return lsdb.read_hats(small_sky_order1_source_dir, margin_cache=small_sky_order1_source_margin_dir)
+
+
+@pytest.fixture
+def small_sky_order1_source_margin_catalog(small_sky_order1_source_margin_dir):
+    return lsdb.read_hats(small_sky_order1_source_margin_dir)
+
+
+@pytest.fixture
+def small_sky_to_o1source_catalog(small_sky_to_order1_source_dir):
+    return lsdb.read_hats(small_sky_to_order1_source_dir)
+
+
+@pytest.fixture
+def small_sky_to_o1source_soft_catalog(small_sky_to_order1_source_soft_dir):
+    return lsdb.read_hats(small_sky_to_order1_source_soft_dir)
+
+
+@pytest.fixture
+def small_sky_order1_df(test_data_dir):
+    return pd.read_csv(test_data_dir / "raw" / "small_sky" / "small_sky.csv")
+
+
+@pytest.fixture
+def small_sky_source_df(test_data_dir):
+    return pd.read_csv(test_data_dir / "raw" / "small_sky_source" / "small_sky_source.csv")
+
+
+@pytest.fixture
+def small_sky_source_margin_catalog(test_data_dir):
+    return lsdb.read_hats(test_data_dir / SMALL_SKY_SOURCE_MARGIN_NAME)
+
+
+@pytest.fixture
+def small_sky_order3_source_margin_catalog(test_data_dir):
+    return lsdb.read_hats(test_data_dir / SMALL_SKY_ORDER3_SOURCE_MARGIN_NAME)
+
+
+@pytest.fixture
+def small_sky_with_nested_sources(small_sky_order1_catalog, small_sky_order1_source_with_margin):
+    return small_sky_order1_catalog.join_nested(
+        small_sky_order1_source_with_margin, left_on="id", right_on="object_id", nested_column_name="sources"
+    )
+
+
+@pytest.fixture
+def small_sky_no_metadata_dir(test_data_dir):
+    return test_data_dir / "raw" / SMALL_SKY_NO_METADATA
+
+
+@pytest.fixture
+def xmatch_expected_dir(test_data_dir):
+    return test_data_dir / "raw" / "xmatch_expected"
+
+
+@pytest.fixture
+def xmatch_correct(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_CORRECT_FILE)
+
+
+@pytest.fixture
+def xmatch_correct_005(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_CORRECT_005_FILE)
+
+
+@pytest.fixture
+def xmatch_correct_002_005(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_CORRECT_002_005_FILE)
+
+
+@pytest.fixture
+def xmatch_correct_05_2_3n_margin(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_CORRECT_05_2_3N_MARGIN_FILE)
+
+
+@pytest.fixture
+def xmatch_correct_3n_2t(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_CORRECT_3N_2T_FILE)
+
+
+@pytest.fixture
+def xmatch_correct_3n_2t_no_margin(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_CORRECT_3N_2T_NO_MARGIN_FILE)
+
+
+@pytest.fixture
+def xmatch_correct_3n_2t_negative(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_CORRECT_3N_2T_NEGATIVE_FILE)
+
+
+@pytest.fixture
+def xmatch_mock(xmatch_expected_dir):
+    return pd.read_csv(xmatch_expected_dir / XMATCH_MOCK_FILE)
+
+
+@pytest.fixture
+def cone_search_expected_dir(test_data_dir):
+    return test_data_dir / "raw" / "cone_search_expected"
+
+
+@pytest.fixture
+def cone_search_expected(cone_search_expected_dir):
+    return pd.read_csv(cone_search_expected_dir / "catalog.csv", index_col=SPATIAL_INDEX_COLUMN)
+
+
+@pytest.fixture
+def cone_search_margin_expected(cone_search_expected_dir):
+    return pd.read_csv(cone_search_expected_dir / "margin.csv", index_col=SPATIAL_INDEX_COLUMN)
+
+
+# pylint: disable=import-outside-toplevel
+def pytest_collection_modifyitems(items):
+    """Modify tests that use the `lsst-sphgeom` package to only run when that
+    package has been installed in the development environment.
+
+    If we detect that we can import `lsst-sphgeom`, this method exits early
+    and does not modify any test items.
+    """
+    try:
+        # pylint: disable=unused-import
+        from lsst.sphgeom import ConvexPolygon
+
+        return
+    except ImportError:
+        pass
+
+    for item in items:
+        if any(item.iter_markers(name="sphgeom")):
+            item.add_marker(pytest.mark.skip(reason="lsst-sphgeom is not installed"))
+
+
+@pytest.fixture
+def assert_divisions_are_correct():
+    def assert_divisions_are_correct(catalog):
+        # Check that number of divisions == number of pixels + 1
+        hp_pixels = [None] * len(catalog._ddf_pixel_map)
+        for pix, index in catalog._ddf_pixel_map.items():
+            hp_pixels[index] = pix
+        if len(hp_pixels) == 0:
+            # Special case if there are no partitions.
+            assert catalog._ddf.divisions == (None, None)
+            return
+        assert len(catalog._ddf.divisions) == len(hp_pixels) + 1
+        # Check that the divisions are not None
+        assert None not in catalog._ddf.divisions
+        # Check that divisions belong to the correct pixel
+        for division, hp_pixel in zip(catalog._ddf.divisions, hp_pixels):
+            div_pixel = spatial_index_to_healpix([division], target_order=hp_pixel.order)
+            assert hp_pixel.pixel == div_pixel
+        # The last division corresponds to the largest healpix value
+        assert catalog._ddf.divisions[-1] == healpix_to_spatial_index(
+            hp_pixels[-1].order, hp_pixels[-1].pixel + 1
+        )
+
+    return assert_divisions_are_correct
