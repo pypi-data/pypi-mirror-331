d1f6bbe33c87c101bd6aafd8b31981c2211ae649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:46 +0000

    Add package pyperfcheck/0.2.0/pyperfcheck-0.2.0.tar.gz

---
+++ packages/pyperfcheck/pyperfcheck-0.2.0.tar.gz/pyperfcheck-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyperfcheck',
+    version='0.2.0',  # Update with the current version
+    description='A high-performance benchmarking library for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Janardhan Medathati',
+    author_email='7janardhan7@gmail.com',
+    url='https://github.com/Janardhan11/pyperfcheck',
+    packages=find_packages(),
+    install_requires=[
+        'psutil',
+        'matplotlib',
+        'tabulate',
+        'asyncio',  # if async is required
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
