448cfe76601358fc70d0aeca4e3348d40bb20494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:39 +0000

    Add package djangaec/2.0.0rc3/djangaec-2.0.0rc3.tar.gz

---
+++ packages/djangaec/djangaec-2.0.0rc3.tar.gz/djangaec-2.0.0rc3/setup.py
@@ -0,0 +1,58 @@
+import os
+from setuptools import setup, find_packages
+
+
+NAME = 'djangaec'
+PACKAGES = find_packages()
+DESCRIPTION = 'Django integration with Google App Engine GC Custom Build'
+URL = "https://github.com/potatolondon/djangae"
+LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+AUTHOR = 'GC'
+
+EXTRAS = {
+    "test": ["beautifulsoup4"],
+}
+
+setup(
+    name=NAME,
+    version='2.0.0rc3',
+    packages=PACKAGES,
+
+    # metadata for upload to PyPI
+    author=AUTHOR,
+    author_email='kotarskip@googl.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords=["django", "Google App Engine", "GAE"],
+    url=URL,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+
+    include_package_data=True,
+    # dependencies
+    install_requires=[
+        'django>=2.2,<=3.3.4',
+        'django-gcloud-connectors>=0.3.4',
+        'google-api-python-client>=1.7.11',
+        'google-cloud-tasks>=1.5.0,<2.0.0',
+        'psutil>=5.7.3',
+        # requests required by cloud storage file backend
+        'requests>=2.22.0',
+        # required for iap backend
+        'cryptography==3.4.6',
+        'python-jose[cryptography]==3.2.0',
+    ],
+    extras_require=EXTRAS,
+    tests_require=EXTRAS['test'],
+)
