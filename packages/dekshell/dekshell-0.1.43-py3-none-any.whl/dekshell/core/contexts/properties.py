d424145c38b69a81bd3fffbd945c31431825e3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:05:12 +0000

    Add package dekshell/0.1.43/dekshell-0.1.43-py3-none-any.whl

---
+++ packages/dekshell/dekshell-0.1.43-py3-none-any.whl/dekshell/core/contexts/properties.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import shutil
+from sysconfig import get_paths
+from importlib import metadata
+from dektools.module import ModuleProxy
+from ..redirect import shell_name
+from ...utils.serializer import serializer
+
+current_shell = shutil.which(shell_name, path=get_paths()['scripts'])
+
+
+def make_shell_properties(shell):
+    return {
+        'shell': shell,
+        'shr': f'{shell} r',
+        'shrf': f'{shell} rf',
+        'shrfc': f'{shell} rfc',
+        'shrs': f'{shell} rs',
+        'shrrs': f'{shell} rrs',
+    }
+
+
+package_name = __name__.partition(".")[0]
+
+default_properties = {
+    '__meta__': {
+        'name': package_name,
+        'version': metadata.version(package_name)
+    },
+    'python': sys.executable,
+    **make_shell_properties(current_shell),
+    'pid': os.getpid(),
+    'pname': os.path.basename(sys.executable),
+
+    'os_name': os.name,
+    'platform': sys.platform,
+    'os_win': os.name == "nt",
+    'ops': os.pathsep,
+    'home': os.path.expanduser('~'),
+
+    'ser': serializer,
+
+    'mp': ModuleProxy(),
+}
