b5fa69bf8bc42c25c04396a689bbd6dad50234e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:12 +0000

    Add package diracx-core/0.0.1a27/diracx_core-0.0.1a27.tar.gz

---
+++ packages/diracx-core/diracx_core-0.0.1a27.tar.gz/diracx_core-0.0.1a27/src/diracx/core/models.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+from datetime import datetime
+from enum import StrEnum
+
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+
+
+class ScalarSearchOperator(StrEnum):
+    EQUAL = "eq"
+    NOT_EQUAL = "neq"
+    GREATER_THAN = "gt"
+    LESS_THAN = "lt"
+    LIKE = "like"
+
+
+class VectorSearchOperator(StrEnum):
+    IN = "in"
+    NOT_IN = "not in"
+
+
+class ScalarSearchSpec(TypedDict):
+    parameter: str
+    operator: ScalarSearchOperator
+    value: str | int
+
+
+class VectorSearchSpec(TypedDict):
+    parameter: str
+    operator: VectorSearchOperator
+    values: list[str] | list[int]
+
+
+SearchSpec = ScalarSearchSpec | VectorSearchSpec
+
+
+class SortDirection(StrEnum):
+    ASC = "asc"
+    DESC = "desc"
+
+
+# TODO: TypedDict vs pydantic?
+class SortSpec(TypedDict):
+    parameter: str
+    direction: SortDirection
+
+
+class TokenResponse(BaseModel):
+    # Based on RFC 6749
+    access_token: str
+    expires_in: int
+    token_type: str = "Bearer"  # noqa: S105
+    refresh_token: str | None = None
+
+
+class JobStatus(StrEnum):
+    SUBMITTING = "Submitting"
+    RECEIVED = "Received"
+    CHECKING = "Checking"
+    STAGING = "Staging"
+    WAITING = "Waiting"
+    MATCHED = "Matched"
+    RUNNING = "Running"
+    STALLED = "Stalled"
+    COMPLETING = "Completing"
+    DONE = "Done"
+    COMPLETED = "Completed"
+    FAILED = "Failed"
+    DELETED = "Deleted"
+    KILLED = "Killed"
+    RESCHEDULED = "Rescheduled"
+
+
+class JobMinorStatus(StrEnum):
+    MAX_RESCHEDULING = "Maximum of reschedulings reached"
+    RESCHEDULED = "Job Rescheduled"
+
+
+class JobStatusUpdate(BaseModel):
+    Status: JobStatus | None = None
+    MinorStatus: str | None = None
+    ApplicationStatus: str | None = None
+    Source: str = "Unknown"
+
+
+class LimitedJobStatusReturn(BaseModel):
+    Status: JobStatus
+    MinorStatus: str
+    ApplicationStatus: str
+
+
+class JobStatusReturn(LimitedJobStatusReturn):
+    StatusTime: datetime
+    Source: str
+
+
+class SetJobStatusReturn(BaseModel):
+    class SetJobStatusReturnSuccess(BaseModel):
+        """Successful new status change."""
+
+        Status: JobStatus | None = None
+        MinorStatus: str | None = None
+        ApplicationStatus: str | None = None
+        HeartBeatTime: datetime | None = None
+        StartExecTime: datetime | None = None
+        EndExecTime: datetime | None = None
+        LastUpdateTime: datetime | None = None
+
+    success: dict[int, SetJobStatusReturnSuccess]
+    failed: dict[int, dict[str, str]]
+
+
+class UserInfo(BaseModel):
+    sub: str  # dirac generated vo:sub
+    preferred_username: str
+    dirac_group: str
+    vo: str
+
+
+class ChecksumAlgorithm(StrEnum):
+    SHA256 = "sha256"
+
+
+class SandboxFormat(StrEnum):
+    TAR_BZ2 = "tar.bz2"
+
+
+class SandboxInfo(BaseModel):
+    checksum_algorithm: ChecksumAlgorithm
+    checksum: str = Field(pattern=r"^[0-9a-fA-F]{64}$")
+    size: int = Field(ge=1)
+    format: SandboxFormat
+
+
+class SandboxType(StrEnum):
+    Input = "Input"
+    Output = "Output"
---
+++ packages/diracx-core/diracx_core-0.0.1a27.tar.gz/diracx_core-0.0.1a27/src/diracx/core/settings.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+__all__ = (
+    "SqlalchemyDsn",
+    "LocalFileUrl",
+    "ServiceSettingsBase",
+)
+
+import contextlib
+from collections.abc import AsyncIterator
+from pathlib import Path
+from typing import Annotated, Any, Self, TypeVar
+
+from authlib.jose import JsonWebKey
+from cryptography.fernet import Fernet
+from pydantic import (
+    AnyUrl,
+    BeforeValidator,
+    FileUrl,
+    SecretStr,
+    TypeAdapter,
+    UrlConstraints,
+)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+T = TypeVar("T")
+
+
+class SqlalchemyDsn(AnyUrl):
+    _constraints = UrlConstraints(
+        allowed_schemes=[
+            "sqlite+aiosqlite",
+            "mysql+aiomysql",
+            # The real scheme is with an underscore, (oracle+oracledb_async)
+            # but pydantic does not validate it, so we use this hack
+            "oracle+oracledb-async",
+        ]
+    )
+
+
+class _TokenSigningKey(SecretStr):
+    jwk: JsonWebKey
+
+    def __init__(self, data: str):
+        super().__init__(data)
+        self.jwk = JsonWebKey.import_key(self.get_secret_value())
+
+
+def _maybe_load_key_from_file(value: Any) -> Any:
+    """Load private keys from files if needed."""
+    if isinstance(value, str) and not value.strip().startswith("-----BEGIN"):
+        url = TypeAdapter(LocalFileUrl).validate_python(value)
+        if not url.scheme == "file":
+            raise ValueError("Only file:// URLs are supported")
+        if url.path is None:
+            raise ValueError("No path specified")
+        value = Path(url.path).read_text()
+    return value
+
+
+TokenSigningKey = Annotated[
+    _TokenSigningKey, BeforeValidator(_maybe_load_key_from_file)
+]
+
+
+class FernetKey(SecretStr):
+    fernet: Fernet
+
+    def __init__(self, data: str):
+        super().__init__(data)
+        self.fernet = Fernet(self.get_secret_value())
+
+
+def _apply_default_scheme(value: str) -> str:
+    """Applies the default file:// scheme if not present."""
+    if isinstance(value, str) and "://" not in value:
+        value = f"file://{value}"
+    return value
+
+
+LocalFileUrl = Annotated[FileUrl, BeforeValidator(_apply_default_scheme)]
+
+
+class ServiceSettingsBase(BaseSettings):
+    model_config = SettingsConfigDict(frozen=True)
+
+    @classmethod
+    def create(cls) -> Self:
+        raise NotImplementedError("This should never be called")
+
+    @contextlib.asynccontextmanager
+    async def lifetime_function(self) -> AsyncIterator[None]:
+        """A context manager that can be used to run code at startup and shutdown."""
+        yield
+
+
+class DevelopmentSettings(ServiceSettingsBase):
+    """Settings for the Development Configuration that can influence run time."""
+
+    model_config = SettingsConfigDict(env_prefix="DIRACX_DEV_")
+
+    # When then to true (only for demo/CI), crash if an access policy isn't
+    # called
+    crash_on_missed_access_policy: bool = False
