48dec41ad6ae40db1c095f0d73c032836f93a822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 08:04:54 +0000

    Add package just-agents-core/0.6.4/just_agents_core-0.6.4-py3-none-any.whl

---
+++ packages/just-agents-core/just_agents_core-0.6.4-py3-none-any.whl/just_agents/interfaces/protocol_adapter.py
@@ -0,0 +1,82 @@
+from abc import ABC, abstractmethod
+from pydantic import BaseModel
+from typing import Callable, Coroutine, Union, AsyncGenerator, List, Sequence, ClassVar, Type, TypeVar, Generic, Any, Optional
+
+from just_agents.interfaces.function_call import IFunctionCall
+
+BaseModelResponse = TypeVar('BaseModelResponse', bound=BaseModel)
+BaseModelStreamResponse = TypeVar('BaseModelStreamResponse', bound=BaseModel)
+AbstractMessage = TypeVar("AbstractMessage")
+
+ModelResponseCallback=Callable[...,BaseModelResponse]
+MessageUnpackCallback=Callable[[BaseModelResponse], AbstractMessage]
+ExecuteToolCallback=Callable[[Sequence[IFunctionCall]],List[AbstractMessage]]
+
+class IProtocolAdapter(ABC, Generic[BaseModelResponse, AbstractMessage, BaseModelStreamResponse]):
+    """
+    Class that is required to wrap the model protocol
+    """
+    stop: ClassVar[str] = "[DONE]"
+    
+    function_convention: ClassVar[Type[IFunctionCall[Any]]]
+
+    @abstractmethod
+    def tool_from_function(self, tool: Callable, **kwargs) -> dict:
+        raise NotImplementedError("You need to implement tool_from_function first!")
+    
+    @abstractmethod
+    def sanitize_args(self, *args, **kwargs) -> tuple:
+        raise NotImplementedError("You need to implement sanitize_args first!")
+    
+    @abstractmethod
+    def completion(self, *args, **kwargs) -> BaseModelResponse:
+        raise NotImplementedError("You need to implement completion first!")
+
+    async def async_completion(self, *args, **kwargs) \
+            -> Coroutine[Any,Any,Union[BaseModelResponse, AsyncGenerator, Any]]:
+        raise NotImplementedError("You need to implement async_completion first!")
+
+    @abstractmethod
+    def finish_reason_from_response(self, response: Union[AbstractMessage, BaseModelResponse, BaseModelStreamResponse]) -> Any:
+        raise NotImplementedError("You need to implement finish_reason_from_response first!")
+
+    @abstractmethod
+    def message_from_response(self, response: Union[AbstractMessage, BaseModelResponse, BaseModelStreamResponse]) -> AbstractMessage:
+        raise NotImplementedError("You need to implement message_from_response first!")
+
+    @abstractmethod
+    def content_from_delta(self, delta: AbstractMessage) -> str:
+        raise NotImplementedError("You need to implement content_from_delta first!")
+
+    @abstractmethod
+    def tool_calls_from_message(self, message: AbstractMessage) -> List[IFunctionCall[AbstractMessage]]:
+        raise NotImplementedError("You need to implement tool_calls_from_response first!")
+
+    @abstractmethod
+    def response_from_deltas(self, deltas: List[AbstractMessage]) -> BaseModelResponse:
+        raise NotImplementedError("You need to implement message_from_deltas first!")
+
+    @abstractmethod
+    def message_as_chunk(self, index: int, delta: AbstractMessage, model: str, role: Optional[str] = None) -> AbstractMessage:
+        raise NotImplementedError("You need to implement message_as_chunk first!")
+
+    @abstractmethod
+    def get_supported_params(self, model_name: str) -> Optional[list]:
+        raise NotImplementedError("You need to implement get_supported_params first!")
+
+    @property
+    @abstractmethod
+    def debug_enabled(self) -> bool:
+        raise NotImplementedError("You need to implement debug_enabled first!")
+
+    @abstractmethod
+    def enable_debug(self) -> None:
+        raise NotImplementedError("You need to implement enable_debug first!")
+
+    @abstractmethod
+    def enable_logging(self) -> None:
+        raise NotImplementedError("You need to implement enable_logging first!")
+
+    @abstractmethod
+    def disable_logging(self) -> None:
+        raise NotImplementedError("You need to implement disable_logging first!")
