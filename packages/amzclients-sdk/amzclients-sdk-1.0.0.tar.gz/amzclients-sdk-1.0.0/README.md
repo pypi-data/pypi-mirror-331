85eecb74617996e226d29b4526824a031a3200de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:41 +0000

    Add package amzclients-sdk/1.0.0/amzclients-sdk-1.0.0.tar.gz

---
+++ packages/amzclients-sdk/amzclients-sdk-1.0.0.tar.gz/amzclients-sdk-1.0.0/README.md
@@ -0,0 +1,16 @@
+# amzclients-sdk
+
+
+This is the client module of Amazon Web Services Python SDK.
+
+Amazon Web Services Python SDK is the software development kit. It makes things easy to integrate your Python application,
+library, or script with Amazon Web Services services.
+
+This module works on Python versions:
+
+   * 2.7 and greater, including 3.x
+
+
+Documentation:
+
+Please visit https://docs.aws.amazon.com/
---
+++ packages/amzclients-sdk/amzclients-sdk-1.0.0.tar.gz/amzclients-sdk-1.0.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
+PACKAGE = "amzclients_sdk"
+DESCRIPTION = "cloud clients for AWS Lambda functions"
+VERSION = '1.0.0'
+
+
+setup_args = {
+    'version': VERSION,
+    'description': DESCRIPTION,
+    'license': "Apache License 2.0",
+    'long_description': readme(),
+    'long_description_content_type': 'text/markdown',
+    'packages': find_packages(exclude=["tests*"]),
+    'platforms': 'any',
+    'install_requires': ["boto3>=1.37.0","aws-consoler>=1.1.0","snapshot-photo>=0.0.3"],
+    'classifiers': (
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Topic :: Software Development',
+    )
+}
+
+setup(name='amzclients-sdk', **setup_args)
