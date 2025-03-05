9ffaaa747c267596b17fd1ac22e7ff8d4e3bb918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:06:22 +0000

    Add package transipy/1.0.6/transipy-1.0.6.tar.gz

---
+++ packages/transipy/transipy-1.0.6.tar.gz/transipy-1.0.6/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+def get_requirements(path: str):
+    return [l.strip() for l in open(path)]
+
+setup(
+    name='transipy',
+    version='1.0.6',
+    description='Transipy is your one-stop solution for lightning-fast and accurate document translation.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=', '.join(['Hien Vu', '']),
+    author_email=', '.join(['hienvq23@gmail.com', '']),
+    url='https://github.com/NeiH4207/transipy',
+    packages=find_packages(),
+    keywords='google translate, translation, document translation, language translation',
+    install_requires=get_requirements("requirements.txt"),
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'transipy=transipy:main'
+        ],
+    },
+)
