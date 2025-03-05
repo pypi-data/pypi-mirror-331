693f12224b367f236203ec4030970729de44626b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:53 +0000

    Add package aiotdlib/0.26.1/aiotdlib-0.26.1-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/aiotdlib/aiotdlib-0.26.1-py3-none-manylinux_2_28_x86_64.whl/aiotdlib/api/functions/get_web_page_instant_view.py
@@ -0,0 +1,29 @@
+# =============================================================================== #
+#                                                                                 #
+#    This file has been generated automatically!! Do not change this manually!    #
+#                                                                                 #
+# =============================================================================== #
+from __future__ import annotations
+
+import typing
+
+from pydantic import Field
+
+from ..types.base import *
+
+
+class GetWebPageInstantView(BaseObject):
+    """
+    Returns an instant view version of a web page if available. Returns a 404 error if the web page has no instant view page
+
+    :param url: The web page URL
+    :type url: :class:`String`
+    :param force_full: Pass true to get full instant view for the web page
+    :type force_full: :class:`Bool`
+    """
+
+    ID: typing.Literal["getWebPageInstantView"] = Field(
+        "getWebPageInstantView", validation_alias="@type", alias="@type"
+    )
+    url: String
+    force_full: Bool = False
