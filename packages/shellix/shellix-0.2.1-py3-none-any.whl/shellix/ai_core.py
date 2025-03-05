7f63128e3ddbafb8473378f2ff2ed5d9f0915bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:31 +0000

    Add package shellix/0.2.1/shellix-0.2.1-py3-none-any.whl

---
+++ packages/shellix/shellix-0.2.1-py3-none-any.whl/shellix/ai_core.py
@@ -0,0 +1,90 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from langchain_community.tools import TavilySearchResults
+from shellix.shell_tool import ShellTool
+from shellix.write_tool import write_file, modify_file, read_file
+from datetime import datetime
+import json
+import os
+
+
+def load_tools(credentials):
+    search_tool = TavilySearchResults(
+        max_results=10,
+        search_depth="advanced",
+        include_answer=True,
+        include_raw_content=False,
+        include_images=False,
+        tavily_api_key=credentials['TAVILY_KEY'],
+    )
+    shell_tool = ShellTool()
+    tools = [shell_tool, search_tool, write_file, modify_file, read_file]
+    return tools
+
+
+def get_directory_contents(current_directory):
+    try:
+        entries = os.listdir(current_directory)
+        files_list = [f for f in entries if os.path.isfile(os.path.join(current_directory, f))]
+        folders_list = [d for d in entries if os.path.isdir(os.path.join(current_directory, d))]
+        return files_list, folders_list
+    except Exception as e:
+        print(f"Error accessing directory: {e}")
+        return [], []
+
+
+def load_memory():
+    try:
+        with open('.shellix_memory.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+def save_memory(memory):
+    with open('.shellix_memory.json', 'w') as file:
+        json.dump(memory, file, indent=4)
+
+
+def process_input(input_str, credentials, current_directory):
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    folder_path = os.path.abspath(current_directory)
+    files_list, folders_list = get_directory_contents(current_directory)
+
+    memory = load_memory()
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", f"""
+            You are a helpful console assistant called Shellix. 
+
+            Current Date: {current_date}
+            Current Directory: {folder_path}
+            Files in Directory: {', '.join(files_list)}
+            Folders in Directory: {', '.join(folders_list)}
+
+            Your output and tool call results will be outputted to user terminal.
+            Minimize comments in output code and provide clear responses overall. 
+            Think about how can you use shell or search tool to accomplish the task if you don't have information directly provided.
+            When asked to do something, likely the user wants you to apply a command or modify project files.
+            Feel free to traverse the current folder with 'ls' to accomplish your tasks. Don't ask for confirmations to modify project files.
+            """),
+            ("placeholder", "{messages}"),
+        ]
+    )
+
+    tools = load_tools(credentials)
+
+    model = ChatOpenAI(model=credentials['OPENAI_MODEL'], temperature=0, api_key=credentials['OPENAI_KEY'],
+                         streaming=True)
+    langgraph_agent_executor = create_react_agent(model, tools, prompt=prompt)
+
+    # Convert memory (list of dicts) into a list of tuples
+    converted_memory = [(msg["role"], msg["content"]) for msg in memory]
+    messages = langgraph_agent_executor.invoke({"messages": converted_memory + [("human", input_str)]})
+
+    memory.append({"role": "human", "content": input_str})
+    memory.append({"role": "assistant", "content": messages["messages"][-1].content})
+    save_memory(memory)
+    print(messages["messages"][-1].content)
---
+++ packages/shellix/shellix-0.2.1-py3-none-any.whl/shellix/write_tool.py
@@ -0,0 +1,43 @@
+from langchain_core.tools import tool
+
+
+@tool
+def write_file(file_name: str, file_content: str) -> bool:
+    """Writes full content to a file, please note that the full file content has to be provided."""
+    print(f"Writing {file_name}...")
+    with open(file_name, "w") as f:
+        f.write(file_content)
+    return f"{file_name} written successfully"
+
+
+@tool
+def modify_file(file_name: str, substring_search: str, replacement: str) -> str:
+    """Use to modify a part of file. Finds a specific substring in a file and replaces it with another string."""
+    print(f"Modifying {file_name} Searching for '{substring_search}'...")
+    try:
+        with open(file_name, "r") as f:
+            content = f.read()
+    except FileNotFoundError:
+        print(f"File {file_name} not found.")
+        return f"File {file_name} not found."
+
+    occurrences = content.count(substring_search)
+    if occurrences == 0:
+        print(f"Substring '{substring_search}' not found in {file_name}.")
+        return "No occurrences found to replace."
+    elif occurrences > 1:
+        print(f"More than one occurrence of '{substring_search}' found in {file_name}. Clarify search.")
+        return "Error: More than one occurrence found. Clarify search."
+
+    modified_content = content.replace(substring_search, replacement)
+    with open(file_name, "w") as f:
+        f.write(modified_content)
+    print(f"Replaced '{substring_search}' with '{replacement}' in {file_name}.")
+    return f"Replaced '{substring_search}' with '{replacement}' in {file_name}."
+
+def read_file(file_name: str) -> str:
+    """Reads the full content of a file."""
+    print(f"Reading {file_name}...")
+    with open(file_name, "r") as f:
+        content = f.read()
+    return content
