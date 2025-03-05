0b46f576ca2705a586b99871df95d8d9cd0d01c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:05:48 +0000

    Add package msgspec-python313-pre/0.0.1/msgspec-python313-pre-0.0.1.tar.gz

---
+++ packages/msgspec-python313-pre/msgspec-python313-pre-0.0.1.tar.gz/msgspec-python313-pre-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="msgspec-python313-pre",
+    version="0.0.1",
+    author="",
+    author_email="",
+    description="A security place-holder package",
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+)
