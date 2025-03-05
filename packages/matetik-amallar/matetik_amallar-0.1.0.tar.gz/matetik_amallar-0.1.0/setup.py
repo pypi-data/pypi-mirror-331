9073b12bdc65e97c619381456aceca7c5da9e065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:04:59 +0000

    Add package matetik-amallar/0.1.0/matetik_amallar-0.1.0.tar.gz

---
+++ packages/matetik-amallar/matetik_amallar-0.1.0.tar.gz/matetik_amallar-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='matetik_amallar',
+version='0.1.0',
+author='Walter',
+author_email='Abdullabek710@gmail.com',
+description='2 ta son bilan amallar bajaradi',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.10',
+)
