e75d7f0d22076c1a3e73f518002211ebd56da73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:06:01 +0000

    Add package xdsl/0.29.0/xdsl-0.29.0-py3-none-any.whl

---
+++ packages/xdsl/xdsl-0.29.0-py3-none-any.whl/tests/dialects/test_riscv.py
@@ -0,0 +1,262 @@
+import pytest
+
+from xdsl.context import Context
+from xdsl.dialects import riscv
+from xdsl.dialects.builtin import (
+    IntAttr,
+    IntegerAttr,
+    ModuleOp,
+    NoneAttr,
+    Signedness,
+    i32,
+)
+from xdsl.parser import Parser
+from xdsl.transforms.canonicalization_patterns.riscv import get_constant_value
+from xdsl.utils.exceptions import ParseError, VerifyException
+from xdsl.utils.test_value import TestSSAValue
+
+
+def test_add_op():
+    a1 = TestSSAValue(riscv.Registers.A1)
+    a2 = TestSSAValue(riscv.Registers.A2)
+    add_op = riscv.AddOp(a1, a2, rd=riscv.Registers.A0)
+    a0 = add_op.rd
+
+    assert a1.type is add_op.rs1.type
+    assert a2.type is add_op.rs2.type
+    assert isinstance(a0.type, riscv.IntRegisterType)
+    assert isinstance(a1.type, riscv.IntRegisterType)
+    assert isinstance(a2.type, riscv.IntRegisterType)
+    assert a0.type.spelling.data == "a0"
+    assert a0.type.index == IntAttr(10)
+    assert a1.type.spelling.data == "a1"
+    assert a1.type.index == IntAttr(11)
+    assert a2.type.spelling.data == "a2"
+    assert a2.type.index == IntAttr(12)
+
+    # Registers that aren't predefined should not have an index.
+    assert isinstance(riscv.IntRegisterType.infinite_register(1).index, NoneAttr)
+
+
+def test_csr_op():
+    a1 = TestSSAValue(riscv.Registers.A1)
+    zero = TestSSAValue(riscv.Registers.ZERO)
+    csr = IntegerAttr(16, i32)
+    # CsrrwOp
+    riscv.CsrrwOp(rs1=a1, csr=csr, rd=riscv.Registers.A2).verify()
+    riscv.CsrrwOp(rs1=a1, csr=csr, rd=riscv.Registers.ZERO).verify()
+    riscv.CsrrwOp(rs1=a1, csr=csr, writeonly=True, rd=riscv.Registers.ZERO).verify()
+    with pytest.raises(VerifyException):
+        riscv.CsrrwOp(rs1=a1, csr=csr, writeonly=True, rd=riscv.Registers.A2).verify()
+    # CsrrsOp
+    riscv.CsrrsOp(rs1=a1, csr=csr, rd=riscv.Registers.A2).verify()
+    riscv.CsrrsOp(rs1=zero, csr=csr, rd=riscv.Registers.A2).verify()
+    riscv.CsrrsOp(rs1=zero, csr=csr, readonly=True, rd=riscv.Registers.A2).verify()
+    with pytest.raises(VerifyException):
+        riscv.CsrrsOp(rs1=a1, csr=csr, readonly=True, rd=riscv.Registers.A2).verify()
+    # CsrrcOp
+    riscv.CsrrcOp(rs1=a1, csr=csr, rd=riscv.Registers.A2).verify()
+    riscv.CsrrcOp(rs1=zero, csr=csr, rd=riscv.Registers.A2).verify()
+    riscv.CsrrcOp(rs1=zero, csr=csr, readonly=True, rd=riscv.Registers.A2).verify()
+    with pytest.raises(VerifyException):
+        riscv.CsrrcOp(rs1=a1, csr=csr, readonly=True, rd=riscv.Registers.A2).verify()
+    # CsrrwiOp
+    riscv.CsrrwiOp(
+        csr=csr, immediate=IntegerAttr(0, i32), rd=riscv.Registers.A2
+    ).verify()
+    riscv.CsrrwiOp(
+        csr=csr, immediate=IntegerAttr(0, i32), rd=riscv.Registers.ZERO
+    ).verify()
+    riscv.CsrrwiOp(
+        csr=csr, immediate=IntegerAttr(0, i32), writeonly=True, rd=riscv.Registers.ZERO
+    ).verify()
+    with pytest.raises(VerifyException):
+        riscv.CsrrwiOp(
+            csr=csr,
+            immediate=IntegerAttr(0, i32),
+            writeonly=True,
+            rd=riscv.Registers.A2,
+        ).verify()
+    # CsrrsiOp
+    riscv.CsrrsiOp(
+        csr=csr, immediate=IntegerAttr(0, i32), rd=riscv.Registers.A2
+    ).verify()
+    riscv.CsrrsiOp(
+        csr=csr, immediate=IntegerAttr(1, i32), rd=riscv.Registers.A2
+    ).verify()
+    # CsrrciOp
+    riscv.CsrrciOp(
+        csr=csr, immediate=IntegerAttr(0, i32), rd=riscv.Registers.A2
+    ).verify()
+    riscv.CsrrsiOp(
+        csr=csr, immediate=IntegerAttr(1, i32), rd=riscv.Registers.A2
+    ).verify()
+
+
+def test_comment_op():
+    comment_op = riscv.CommentOp("my comment")
+
+    assert comment_op.comment.data == "my comment"
+
+    code = riscv.riscv_code(ModuleOp([comment_op]))
+    assert code == "    # my comment\n"
+
+
+def test_label_op_without_comment():
+    label_str = "mylabel"
+    label_op = riscv.LabelOp(label_str)
+
+    assert label_op.label.data == label_str
+
+    code = riscv.riscv_code(ModuleOp([label_op]))
+    assert code == f"{label_str}:\n"
+
+
+def test_label_op_with_comment():
+    label_str = "mylabel"
+    label_op = riscv.LabelOp(f"{label_str}", comment="my label")
+
+    assert label_op.label.data == label_str
+
+    code = riscv.riscv_code(ModuleOp([label_op]))
+    assert code == f"{label_str}:                                         # my label\n"
+
+
+def test_return_op():
+    return_op = riscv.EbreakOp(comment="my comment")
+
+    assert return_op.comment is not None
+
+    assert return_op.comment.data == "my comment"
+
+    code = riscv.riscv_code(ModuleOp([return_op]))
+    assert code == "    ebreak                                       # my comment\n"
+
+
+def test_immediate_i_inst():
+    # I-Type - 12-bits signed immediate
+    lb, ub = Signedness.SIGNED.value_range(12)
+    a1 = TestSSAValue(riscv.Registers.A1)
+
+    with pytest.raises(VerifyException):
+        riscv.AddiOp(a1, ub, rd=riscv.Registers.A0)
+
+    with pytest.raises(VerifyException):
+        riscv.AddiOp(a1, lb - 1, rd=riscv.Registers.A0)
+
+    riscv.AddiOp(a1, ub - 1, rd=riscv.Registers.A0)
+    riscv.AddiOp(a1, lb, rd=riscv.Registers.A0)
+
+
+def test_immediate_s_inst():
+    # S-Type - 12-bits signed immediate
+    lb, ub = Signedness.SIGNED.value_range(12)
+    a1 = TestSSAValue(riscv.Registers.A1)
+    a2 = TestSSAValue(riscv.Registers.A2)
+
+    with pytest.raises(VerifyException):
+        riscv.SwOp(a1, a2, ub)
+
+    with pytest.raises(VerifyException):
+        riscv.SwOp(a1, a2, lb - 1)
+
+    riscv.SwOp(a1, a2, ub - 1)
+    riscv.SwOp(a1, a2, lb)
+
+
+def test_immediate_u_j_inst():
+    # U-Type and J-Type - 20-bits immediate
+    lb, ub = Signedness.SIGNLESS.value_range(20)
+    assert ub == 1048576
+    assert lb == -524288
+
+    with pytest.raises(VerifyException):
+        riscv.LuiOp(ub)
+
+    with pytest.raises(VerifyException):
+        riscv.LuiOp(lb - 1)
+
+    riscv.LuiOp(ub - 1)
+    riscv.LuiOp(lb)
+
+
+def test_immediate_jalr_inst():
+    # Jalr - 12-bits immediate
+    a1 = TestSSAValue(riscv.Registers.A1)
+
+    with pytest.raises(VerifyException):
+        riscv.JalrOp(a1, 1 << 12, rd=riscv.Registers.A0)
+
+    with pytest.raises(VerifyException):
+        riscv.JalrOp(a1, -(1 << 12) - 2, rd=riscv.Registers.A0)
+
+    riscv.JalrOp(a1, (1 << 11) - 1, rd=riscv.Registers.A0)
+
+
+def test_immediate_pseudo_inst():
+    lb, ub = Signedness.SIGNLESS.value_range(32)
+    assert ub == 4294967296
+    assert lb == -2147483648
+
+    # Pseudo-Instruction with custom handling
+    with pytest.raises(VerifyException):
+        riscv.LiOp(ub, rd=riscv.Registers.A0)
+
+    with pytest.raises(VerifyException):
+        riscv.LiOp(lb - 1, rd=riscv.Registers.A0)
+
+    riscv.LiOp(ub - 1, rd=riscv.Registers.A0)
+    riscv.LiOp(lb, rd=riscv.Registers.A0)
+
+
+def test_immediate_shift_inst():
+    # Shift instructions (SLLI, SRLI, SRAI) - 5-bits immediate
+    a1 = TestSSAValue(riscv.Registers.A1)
+
+    with pytest.raises(VerifyException):
+        riscv.SlliOp(a1, 1 << 5, rd=riscv.Registers.A0)
+
+    with pytest.raises(VerifyException):
+        riscv.SlliOp(a1, -1, rd=riscv.Registers.A0)
+
+    riscv.SlliOp(a1, (1 << 5) - 1, rd=riscv.Registers.A0)
+
+
+def test_float_register():
+    with pytest.raises(VerifyException, match="not in"):
+        riscv.IntRegisterType("ft9")
+    with pytest.raises(VerifyException, match="not in"):
+        riscv.FloatRegisterType("a0")
+
+    a1 = TestSSAValue(riscv.Registers.A1)
+    a2 = TestSSAValue(riscv.Registers.A2)
+    with pytest.raises(VerifyException, match="Operation does not verify"):
+        riscv.FAddSOp(a1, a2).verify()
+
+    f1 = TestSSAValue(riscv.Registers.FT0)
+    f2 = TestSSAValue(riscv.Registers.FT1)
+    riscv.FAddSOp(f1, f2).verify()
+
+
+def test_riscv_parse_immediate_value():
+    ctx = Context()
+    ctx.load_dialect(riscv.RISCV)
+
+    prog = """riscv.jalr %0, 1.1, !riscv.reg : (!riscv.reg) -> ()"""
+    parser = Parser(ctx, prog)
+    with pytest.raises(ParseError, match="Expected immediate"):
+        parser.parse_operation()
+
+
+def test_asm_section():
+    section = riscv.AssemblySectionOp("section")
+    section.verify()
+
+
+def test_get_constant_value():
+    li_op = riscv.LiOp(1)
+    li_val = get_constant_value(li_op.rd)
+    assert li_val == IntegerAttr.from_int_and_width(1, 32)
+    zero_op = riscv.GetRegisterOp(riscv.Registers.ZERO)
+    zero_val = get_constant_value(zero_op.res)
+    assert zero_val == IntegerAttr.from_int_and_width(0, 32)
---
+++ packages/xdsl/xdsl-0.29.0-py3-none-any.whl/xdsl/dialects/x86/register.py
@@ -0,0 +1,350 @@
+from __future__ import annotations
+
+from abc import ABC
+
+from xdsl.backend.register_type import RegisterType
+from xdsl.irdl import (
+    irdl_attr_definition,
+)
+from xdsl.utils.exceptions import VerifyException
+
+
+class X86RegisterType(RegisterType, ABC):
+    """
+    The abstract class for all x86 register types.
+    """
+
+    def verify(self) -> None:
+        name = self.spelling.data
+        if not self.is_allocated:
+            return
+        if name not in type(self).abi_index_by_name():
+            raise VerifyException(f"{name} not in {self.instruction_set_name()}")
+
+
+# See https://wiki.osdev.org/X86-64_Instruction_Encoding#Registers
+X86_INDEX_BY_NAME = {
+    "rax": 0,
+    "rcx": 1,
+    "rdx": 2,
+    "rbx": 3,
+    "rsp": 4,
+    "rbp": 5,
+    "rsi": 6,
+    "rdi": 7,
+    "eax": 0,
+    "ecx": 1,
+    "edx": 2,
+    "ebx": 3,
+    "esp": 4,
+    "ebp": 5,
+    "esi": 6,
+    "edi": 7,
+    "r8": 8,
+    "r9": 9,
+    "r10": 10,
+    "r11": 11,
+    "r12": 12,
+    "r13": 13,
+    "r14": 14,
+    "r15": 15,
+}
+
+
+@irdl_attr_definition
+class GeneralRegisterType(X86RegisterType):
+    """
+    A scalar x86 register type representing general registers.
+    """
+
+    name = "x86.reg"
+
+    @classmethod
+    def instruction_set_name(cls) -> str:
+        return "x86"
+
+    @classmethod
+    def abi_index_by_name(cls) -> dict[str, int]:
+        return X86_INDEX_BY_NAME
+
+    @classmethod
+    def infinite_register_prefix(cls):
+        return "inf_reg_"
+
+
+UNALLOCATED_GENERAL = GeneralRegisterType.unallocated()
+RAX = GeneralRegisterType("rax")
+RCX = GeneralRegisterType("rcx")
+RDX = GeneralRegisterType("rdx")
+RBX = GeneralRegisterType("rbx")
+RSP = GeneralRegisterType("rsp")
+RBP = GeneralRegisterType("rbp")
+RSI = GeneralRegisterType("rsi")
+RDI = GeneralRegisterType("rdi")
+
+EAX = GeneralRegisterType("eax")
+ECX = GeneralRegisterType("ecx")
+EDX = GeneralRegisterType("edx")
+EBX = GeneralRegisterType("ebx")
+ESP = GeneralRegisterType("esp")
+EBP = GeneralRegisterType("ebp")
+ESI = GeneralRegisterType("esi")
+EDI = GeneralRegisterType("edi")
+
+R8 = GeneralRegisterType("r8")
+R9 = GeneralRegisterType("r9")
+R10 = GeneralRegisterType("r10")
+R11 = GeneralRegisterType("r11")
+R12 = GeneralRegisterType("r12")
+R13 = GeneralRegisterType("r13")
+R14 = GeneralRegisterType("r14")
+R15 = GeneralRegisterType("r15")
+
+RFLAGS_INDEX_BY_NAME = {
+    "rflags": 0,
+}
+
+
+@irdl_attr_definition
+class RFLAGSRegisterType(X86RegisterType):
+    """
+    A scalar x86 register type representing the RFLAGS register.
+    """
+
+    name = "x86.rflags"
+
+    @classmethod
+    def instruction_set_name(cls) -> str:
+        return "x86"
+
+    @classmethod
+    def abi_index_by_name(cls) -> dict[str, int]:
+        return RFLAGS_INDEX_BY_NAME
+
+    @classmethod
+    def infinite_register_prefix(cls):
+        return "inf_rflags_"
+
+
+UNALLOCATED_RFLAGS = RFLAGSRegisterType.unallocated()
+RFLAGS = RFLAGSRegisterType("rflags")
+
+
+class X86VectorRegisterType(X86RegisterType):
+    """
+    The abstract class for all x86 vector register types.
+    """
+
+    pass
+
+
+@irdl_attr_definition
+class SSERegisterType(X86VectorRegisterType):
+    """
+    An x86 register type for SSE instructions.
+    """
+
+    name = "x86.ssereg"
+
+    @classmethod
+    def instruction_set_name(cls) -> str:
+        return "SSE"
+
+    @classmethod
+    def abi_index_by_name(cls) -> dict[str, int]:
+        return SSE_INDEX_BY_NAME
+
+    @classmethod
+    def infinite_register_prefix(cls):
+        return "inf_sse_"
+
+
+# See https://wiki.osdev.org/X86-64_Instruction_Encoding#Registers
+SSE_INDEX_BY_NAME = {
+    "xmm0": 0,
+    "xmm1": 1,
+    "xmm2": 2,
+    "xmm3": 3,
+    "xmm4": 4,
+    "xmm5": 5,
+    "xmm6": 6,
+    "xmm7": 7,
+    "xmm8": 8,
+    "xmm9": 9,
+    "xmm10": 10,
+    "xmm11": 11,
+    "xmm12": 12,
+    "xmm13": 13,
+    "xmm14": 14,
+    "xmm15": 15,
+}
+
+UNALLOCATED_SSE = SSERegisterType.unallocated()
+XMM0 = SSERegisterType("xmm0")
+XMM1 = SSERegisterType("xmm1")
+XMM2 = SSERegisterType("xmm2")
+XMM3 = SSERegisterType("xmm3")
+XMM4 = SSERegisterType("xmm4")
+XMM5 = SSERegisterType("xmm5")
+XMM6 = SSERegisterType("xmm6")
+XMM7 = SSERegisterType("xmm7")
+XMM8 = SSERegisterType("xmm8")
+XMM9 = SSERegisterType("xmm9")
+XMM10 = SSERegisterType("xmm10")
+XMM11 = SSERegisterType("xmm11")
+XMM12 = SSERegisterType("xmm12")
+XMM13 = SSERegisterType("xmm13")
+XMM14 = SSERegisterType("xmm14")
+XMM15 = SSERegisterType("xmm15")
+
+
+@irdl_attr_definition
+class AVX2RegisterType(X86VectorRegisterType):
+    """
+    An x86 register type for AVX2 instructions.
+    """
+
+    name = "x86.avx2reg"
+
+    @classmethod
+    def instruction_set_name(cls) -> str:
+        return "AVX2"
+
+    @classmethod
+    def abi_index_by_name(cls) -> dict[str, int]:
+        return AVX2_INDEX_BY_NAME
+
+    @classmethod
+    def infinite_register_prefix(cls):
+        return "inf_avx2_"
+
+
+# See https://wiki.osdev.org/X86-64_Instruction_Encoding#Registers
+AVX2_INDEX_BY_NAME = {
+    "ymm0": 0,
+    "ymm1": 1,
+    "ymm2": 2,
+    "ymm3": 3,
+    "ymm4": 4,
+    "ymm5": 5,
+    "ymm6": 6,
+    "ymm7": 7,
+    "ymm8": 8,
+    "ymm9": 9,
+    "ymm10": 10,
+    "ymm11": 11,
+    "ymm12": 12,
+    "ymm13": 13,
+    "ymm14": 14,
+    "ymm15": 15,
+}
+
+UNALLOCATED_AVX2 = AVX2RegisterType.unallocated()
+YMM0 = AVX2RegisterType("ymm0")
+YMM1 = AVX2RegisterType("ymm1")
+YMM2 = AVX2RegisterType("ymm2")
+YMM3 = AVX2RegisterType("ymm3")
+YMM4 = AVX2RegisterType("ymm4")
+YMM5 = AVX2RegisterType("ymm5")
+YMM6 = AVX2RegisterType("ymm6")
+YMM7 = AVX2RegisterType("ymm7")
+YMM8 = AVX2RegisterType("ymm8")
+YMM9 = AVX2RegisterType("ymm9")
+YMM10 = AVX2RegisterType("ymm10")
+YMM11 = AVX2RegisterType("ymm11")
+YMM12 = AVX2RegisterType("ymm12")
+YMM13 = AVX2RegisterType("ymm13")
+YMM14 = AVX2RegisterType("ymm14")
+YMM15 = AVX2RegisterType("ymm15")
+
+
+@irdl_attr_definition
+class AVX512RegisterType(X86VectorRegisterType):
+    """
+    An x86 register type for AVX512 instructions.
+    """
+
+    name = "x86.avx512reg"
+
+    @classmethod
+    def instruction_set_name(cls) -> str:
+        return "AVX512"
+
+    @classmethod
+    def abi_index_by_name(cls) -> dict[str, int]:
+        return X86AVX512_INDEX_BY_NAME
+
+    @classmethod
+    def infinite_register_prefix(cls):
+        return "inf_avx512_"
+
+
+# See https://wiki.osdev.org/X86-64_Instruction_Encoding#Registers
+X86AVX512_INDEX_BY_NAME = {
+    "zmm0": 0,
+    "zmm1": 1,
+    "zmm2": 2,
+    "zmm3": 3,
+    "zmm4": 4,
+    "zmm5": 5,
+    "zmm6": 6,
+    "zmm7": 7,
+    "zmm8": 8,
+    "zmm9": 9,
+    "zmm10": 10,
+    "zmm11": 11,
+    "zmm12": 12,
+    "zmm13": 13,
+    "zmm14": 14,
+    "zmm15": 15,
+    "zmm16": 16,
+    "zmm17": 17,
+    "zmm18": 18,
+    "zmm19": 19,
+    "zmm20": 20,
+    "zmm21": 21,
+    "zmm22": 22,
+    "zmm23": 23,
+    "zmm24": 24,
+    "zmm25": 25,
+    "zmm26": 26,
+    "zmm27": 27,
+    "zmm28": 28,
+    "zmm29": 29,
+    "zmm30": 30,
+    "zmm31": 31,
+}
+
+UNALLOCATED_AVX512 = AVX512RegisterType.unallocated()
+ZMM0 = AVX512RegisterType("zmm0")
+ZMM1 = AVX512RegisterType("zmm1")
+ZMM2 = AVX512RegisterType("zmm2")
+ZMM3 = AVX512RegisterType("zmm3")
+ZMM4 = AVX512RegisterType("zmm4")
+ZMM5 = AVX512RegisterType("zmm5")
+ZMM6 = AVX512RegisterType("zmm6")
+ZMM7 = AVX512RegisterType("zmm7")
+ZMM8 = AVX512RegisterType("zmm8")
+ZMM9 = AVX512RegisterType("zmm9")
+ZMM10 = AVX512RegisterType("zmm10")
+ZMM11 = AVX512RegisterType("zmm11")
+ZMM12 = AVX512RegisterType("zmm12")
+ZMM13 = AVX512RegisterType("zmm13")
+ZMM14 = AVX512RegisterType("zmm14")
+ZMM15 = AVX512RegisterType("zmm15")
+ZMM16 = AVX512RegisterType("zmm16")
+ZMM17 = AVX512RegisterType("zmm17")
+ZMM18 = AVX512RegisterType("zmm18")
+ZMM19 = AVX512RegisterType("zmm19")
+ZMM20 = AVX512RegisterType("zmm20")
+ZMM21 = AVX512RegisterType("zmm21")
+ZMM22 = AVX512RegisterType("zmm22")
+ZMM23 = AVX512RegisterType("zmm23")
+ZMM24 = AVX512RegisterType("zmm24")
+ZMM25 = AVX512RegisterType("zmm25")
+ZMM26 = AVX512RegisterType("zmm26")
+ZMM27 = AVX512RegisterType("zmm27")
+ZMM28 = AVX512RegisterType("zmm28")
+ZMM29 = AVX512RegisterType("zmm29")
+ZMM30 = AVX512RegisterType("zmm30")
+ZMM31 = AVX512RegisterType("zmm31")
