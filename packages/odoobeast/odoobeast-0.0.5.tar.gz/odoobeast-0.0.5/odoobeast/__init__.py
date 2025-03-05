c19433eb0e4f88e795393b0fca248beaa4208242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:38 +0000

    Add package odoobeast/0.0.5/odoobeast-0.0.5.tar.gz

---
+++ packages/odoobeast/odoobeast-0.0.5.tar.gz/odoobeast-0.0.5/odoobeast/__init__.py
@@ -0,0 +1,5 @@
+from .core import OdooBeast
+from .models import prebuilt_models
+from .tests import test_nlp, test_ml, test_memory, test_response, test_error
+
+__all__ = ["OdooBeast", "prebuilt_models", "test_nlp", "test_ml", "test_memory", "test_response", "test_error"]
