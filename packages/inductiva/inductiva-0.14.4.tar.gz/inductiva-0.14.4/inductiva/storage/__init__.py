4059ffc9fba7f50e5b7e13eed09448cfd0451e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 03:11:46 +0000

    Add package inductiva/0.14.4/inductiva-0.14.4.tar.gz

---
+++ packages/inductiva/inductiva-0.14.4.tar.gz/inductiva-0.14.4/inductiva/storage/__init__.py
@@ -0,0 +1,15 @@
+"""Storage related functions."""
+from .storage import (
+    ExportDestination,
+    StorageOperation,
+    export,
+    get_signed_urls,
+    get_space_used,
+    get_zip_contents,
+    listdir,
+    remove_workspace,
+    upload,
+    upload_from_url,
+    ZipArchiveInfo,
+    ZipFileInfo,
+)
