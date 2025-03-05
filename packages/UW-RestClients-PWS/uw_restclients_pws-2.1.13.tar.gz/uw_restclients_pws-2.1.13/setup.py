d6fd40b2ff664580c829ef4cf0526fc403f7cec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 08:05:02 +0000

    Add package UW-RestClients-PWS/2.1.13/uw_restclients_pws-2.1.13.tar.gz

---
+++ packages/UW-RestClients-PWS/uw_restclients_pws-2.1.13.tar.gz/uw_restclients_pws-2.1.13/setup.py
@@ -0,0 +1,44 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from setuptools import setup
+
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/uw-restclients-pws>`_.
+"""
+
+version_path = 'uw_pws/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+url = "https://github.com/uw-it-aca/uw-restclients-pws"
+setup(
+    name='UW-RestClients-PWS',
+    version=VERSION,
+    packages=['uw_pws'],
+    author="UW-IT T&LS",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[
+        'uw-restclients-core',
+        'nameparser',
+    ],
+    license='Apache License, Version 2.0',
+    description=('A library for connecting to the Person Web Service at the '
+                 'University of Washington'),
+    long_description=README,
+    url=url,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
+
+
