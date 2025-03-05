b6af67ca46af34a80a33702aa1639aaa5bc20437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 07:05:07 +0000

    Add package hdf5-nuclear-data-reader/0.1.0/hdf5_nuclear_data_reader-0.1.0-cp38-cp38-win_amd64.whl

---
+++ packages/hdf5-nuclear-data-reader/hdf5_nuclear_data_reader-0.1.0-cp38-cp38-win_amd64.whl/hdf5_nuclear_data_reader/__init__.py
@@ -0,0 +1,5 @@
+from .hdf5_nuclear_data_reader import *
+
+__doc__ = hdf5_nuclear_data_reader.__doc__
+if hasattr(hdf5_nuclear_data_reader, "__all__"):
+    __all__ = hdf5_nuclear_data_reader.__all__
