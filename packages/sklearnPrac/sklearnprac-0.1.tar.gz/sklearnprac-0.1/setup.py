dc28ae03fdb3966fd0f34b82ea055eb17fc48d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 05:04:58 +0000

    Add package sklearnPrac/0.1/sklearnprac-0.1.tar.gz

---
+++ packages/sklearnPrac/sklearnprac-0.1.tar.gz/sklearnprac-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sklearnPrac",  # Aapka package name
+    version="0.1",  # Version update karein
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package that pastes predefined Python code into the current file",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/sklearnPra/",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
