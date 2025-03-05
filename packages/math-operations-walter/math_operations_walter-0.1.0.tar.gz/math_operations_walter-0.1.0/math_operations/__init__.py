164ea804a3c1260f5a1c8919551a29e69d22f285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:35 +0000

    Add package math-operations-walter/0.1.0/math_operations_walter-0.1.0.tar.gz

---
+++ packages/math-operations-walter/math_operations_walter-0.1.0.tar.gz/math_operations_walter-0.1.0/math_operations/__init__.py
@@ -0,0 +1,1 @@
+from .amallar import add, subtract, multiply, divide
---
+++ packages/math-operations-walter/math_operations_walter-0.1.0.tar.gz/math_operations_walter-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+name="math_operations_walter",
+version="0.1.0",
+description="Test",
+author='Walter',
+author_email='Abdullabek710@gmail.com',
+packages=find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires=">=3.11",
+)
