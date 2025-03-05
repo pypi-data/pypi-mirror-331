67db0ee43b746a71de77daa038aaa0fa959d4643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 15:04:58 +0000

    Add package myfeeds-ai/1.62.0/myfeeds_ai-1.62.0-py3-none-any.whl

---
+++ packages/myfeeds-ai/myfeeds_ai-1.62.0-py3-none-any.whl/myfeeds_ai/data_feeds/Data_Feeds__Shared_Constants.py
@@ -0,0 +1,13 @@
+S3_BUCKET_PREFIX__DATA_FEEDS        = 'data-feeds'
+S3_BUCKET_SUFFIX__HACKER_NEWS       = 'data'
+
+S3_FILE_NAME__CONTENT               = 'content'
+S3_FILE_NAME__LATEST__VERSIONS      = 'latest-versions'
+S3_FILE_NAME__RAW__FEED_XML         = 'feed-xml'
+S3_FILE_NAME__RAW__FEED_DATA        = 'feed-data'
+S3_FILE_NAME__RAW__CONTENT          = 'raw-content'
+
+S3_FOLDER_NAME__ARTICLES            = 'articles'
+S3_FOLDER_NAME__LATEST              = 'latest'
+
+S3_FOLDER__ROOT_FOLDER__PUBLIC_DATA = 'public-data'
