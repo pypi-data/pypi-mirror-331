ec9ad7f7e89748cd9040c3744e5e8c4673035623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 15:05:05 +0000

    Add package techbureau-symbol-sdk-python/3.2.3.dev0/techbureau_symbol_sdk_python-3.2.3.dev0.tar.gz

---
+++ packages/techbureau-symbol-sdk-python/techbureau_symbol_sdk_python-3.2.3.dev0.tar.gz/techbureau_symbol_sdk_python-3.2.3.dev0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['symbolchain',
+   'symbolchain.facade',
+   'symbolchain.nc',
+   'symbolchain.nem',
+   'symbolchain.external',
+   'symbolchain.sc',
+   'symbolchain.symbol']
+
+package_data = \
+{'': ['*']}
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup_kwargs = {
+    'name': 'techbureau-symbol-sdk-python',
+    'version': '3.2.3.dev',
+    'description': 'Symbol SDK',
+    'long_description': 'This is symbol project core sdk python library.',
+    'author': 'Techbureau Contributors',
+    'author_email': 'development@techbureau.jp',
+    'maintainer': 'Techbureau Contributors',
+    'maintainer_email': 'development@techbureau.jp',
+    'url': 'https://github.com/tech-bureau-jp/symbol/tree/dev/sdk/python',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': requirements,
+    'python_requires': '>=3.7,<4.0',
+}
+
+setup(**setup_kwargs)
