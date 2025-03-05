2ed349b7d889583d336bc41118ab78e49dbfaa40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 05:04:26 +0000

    Add package uipath_sdk/0.0.66/uipath_sdk-0.0.66-py3-none-any.whl

---
+++ packages/uipath_sdk/uipath_sdk-0.0.66-py3-none-any.whl/uipath_sdk/_services/__init__.py
@@ -0,0 +1,15 @@
+from .actions_service import ActionsService
+from .api_client import ApiClient
+from .assets_service import AssetsService
+from .buckets_service import BucketsService
+from .context_grounding_service import ContextGroundingService
+from .processes_service import ProcessesService
+
+__all__ = [
+    "ActionsService",
+    "AssetsService",
+    "ProcessesService",
+    "BucketsService",
+    "ContextGroundingService",
+    "ApiClient",
+]
