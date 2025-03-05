35a29acfc7e5216978fd54cb82b7e075719bac12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:05:40 +0000

    Add package usethis/0.5.6/usethis-0.5.6.tar.gz

---
+++ packages/usethis/usethis-0.5.6.tar.gz/usethis-0.5.6/src/usethis/_integrations/toml/errors.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from usethis.errors import UsethisError
+
+
+class TOMLError(UsethisError):
+    """Base class for TOML-related errors."""
+
+
+class TOMLValueAlreadySetError(TOMLError):
+    """Raised when a value is unexpectedly already set in the TOML file."""
+
+
+class TOMLValueMissingError(TOMLError):
+    """Raised when a value is unexpectedly missing from the TOML file."""
+
+
+class TOMLNotFoundError(FileNotFoundError, TOMLError):
+    """Raised when a TOML file is unexpectedly not found."""
+
+
+class TOMLDecodeError(TOMLError):
+    """Raised when a TOML file is unexpectedly not decodable."""
+
+
+class UnexpectedTOMLOpenError(TOMLError):
+    """Raised when the TOML file is unexpectedly opened."""
+
+
+class UnexpectedTOMLCloseError(TOMLError):
+    """Raised when the TOML file is unexpectedly closed."""
+
+
+class UnexpectedTOMLIOError(TOMLError):
+    """Raised when an unexpected attempt is made to read or write the TOML file."""
