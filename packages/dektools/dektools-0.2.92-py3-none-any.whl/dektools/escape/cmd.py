8d526ea236e47f58b1cd535d33d3298f06fcfa6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:49 +0000

    Add package dektools/0.2.92/dektools-0.2.92-py3-none-any.whl

---
+++ packages/dektools/dektools-0.2.92-py3-none-any.whl/dektools/escape/cmd.py
@@ -0,0 +1,11 @@
+import re
+import os
+
+if os.name == 'nt':
+    from ._cmd_win import escape_cmd_argument_script as shell_quote  # noqa
+else:
+    def shell_quote(s):
+        return re.sub(r"(!|\$|#|&|\"|\'|\(|\)|\||<|>|`|\|;)", r"\\\1", s)
+
+# win_cmd_escaper
+from ._cmd_win import escape_powershell_argument_script as powershell_quote  # noqa
