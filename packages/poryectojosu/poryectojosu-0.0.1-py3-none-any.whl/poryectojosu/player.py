8a670d49824ab220da5cecf990ce1270dca77c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 03:11:48 +0000

    Add package poryectojosu/0.0.1/poryectojosu-0.0.1-py3-none-any.whl

---
+++ packages/poryectojosu/poryectojosu-0.0.1-py3-none-any.whl/poryectojosu/player.py
@@ -0,0 +1,28 @@
+"""
+Esta es la documentacion de player
+
+"""
+
+class player:
+
+    """
+    Esta clase crea un reproductor de musica
+    
+    """
+    def play (self, song):
+        """
+        Reproduce la cancion que recibio en el constructor
+
+        Parameters:
+        song (str): este es un string con el path de la cancions
+
+        Returns:
+        int: devuelve uno si reproduce con exito
+        """
+        print ("reproduciendo cancion")
+
+    def play (self):
+        """
+        Detiene la reproduccion
+        """
+        print("stopping")
