f2a46785b6d1eea4adc99fc371d55e837d6e2abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 15:04:56 +0000

    Add package dotflow/0.8.2.dev1/dotflow-0.8.2.dev1.tar.gz

---
+++ packages/dotflow/dotflow-0.8.2.dev1.tar.gz/dotflow-0.8.2.dev1/dotflow/core/models/__init__.py
@@ -0,0 +1,10 @@
+"""Types __init__ module."""
+
+from dotflow.core.models.execution import TypeExecution
+from dotflow.core.models.status import TaskStatus
+
+
+__all__ = [
+    "TypeExecution",
+    "TaskStatus"
+]
