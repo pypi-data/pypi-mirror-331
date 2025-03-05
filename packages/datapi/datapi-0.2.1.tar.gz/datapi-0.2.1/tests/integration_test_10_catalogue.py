c5237191d7718dcd239f15c5991a997edd9ce1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:05:34 +0000

    Add package datapi/0.2.1/datapi-0.2.1.tar.gz

---
+++ packages/datapi/datapi-0.2.1.tar.gz/datapi-0.2.1/tests/integration_test_10_catalogue.py
@@ -0,0 +1,108 @@
+import pytest
+
+from datapi import ApiClient, Collection, processing
+
+
+@pytest.fixture
+def collection(api_anon_client: ApiClient) -> Collection:
+    return api_anon_client.get_collection("test-adaptor-mars")
+
+
+def test_catalogue_collections(api_anon_client: ApiClient) -> None:
+    collections = api_anon_client.get_collections()
+    assert collections.next is not None
+
+    collection_ids = list(collections.collection_ids)
+    while len(collection_ids) != collections.json["numberMatched"]:
+        assert (next_collections := collections.next) is not None
+        collections = next_collections
+        collection_ids.extend(collections.collection_ids)
+    assert "reanalysis-era5-single-levels" in collection_ids
+
+
+def test_catalogue_collections_limit(api_anon_client: ApiClient) -> None:
+    collections = api_anon_client.get_collections(limit=1)
+    assert len(collections.collection_ids) == 1
+
+
+def test_catalogue_collections_sortby(api_anon_client: ApiClient) -> None:
+    collections_title = api_anon_client.get_collections(sortby="title")
+    collections_relevance = api_anon_client.get_collections(sortby="relevance")
+    assert collections_title.collection_ids != collections_relevance.collection_ids
+
+
+def test_catalogue_collections_query(api_anon_client: ApiClient) -> None:
+    collections = api_anon_client.get_collections(query="test")
+    assert collections.collection_ids
+
+
+def test_catalogue_collections_keywords(api_anon_client: ApiClient) -> None:
+    collections = api_anon_client.get_collections(keywords=["Product type: Reanalysis"])
+    assert collections.collection_ids
+
+
+def test_catalogue_collection_begin_datetime(collection: Collection) -> None:
+    assert collection.begin_datetime is not None
+    assert collection.begin_datetime.isoformat() == "1959-01-01T00:00:00+00:00"
+
+
+def test_catalogue_collection_end_datetime(collection: Collection) -> None:
+    assert collection.end_datetime is not None
+    assert collection.end_datetime.isoformat() == "2023-05-09T00:00:00+00:00"
+
+
+def test_catalogue_collection_bbox(collection: Collection) -> None:
+    assert collection.bbox == (0, -89, 360, 89)
+
+
+def test_catalogue_collection_id(collection: Collection) -> None:
+    assert collection.id == "test-adaptor-mars"
+
+
+def test_catalogue_collection_json(collection: Collection) -> None:
+    assert isinstance(collection.json, dict)
+
+
+def test_catalogue_collection_process(collection: Collection) -> None:
+    process = collection.process
+    assert isinstance(process, processing.Process)
+    assert process.id == "test-adaptor-mars"
+
+
+def test_catalogue_collection_url(collection: Collection) -> None:
+    assert collection.url.endswith("collections/test-adaptor-mars")
+
+
+def test_catalogue_get_licences(api_anon_client: ApiClient) -> None:
+    all_licences = api_anon_client.get_licences(scope="all")
+    assert all("id" in licence and "revision" in licence for licence in all_licences)
+
+    portal_licences = api_anon_client.get_licences(scope="portal")
+    assert portal_licences
+    for portal_licence in portal_licences:
+        assert portal_licence in all_licences
+
+
+def test_catalogue_form(collection: Collection) -> None:
+    assert set(collection.form[0]) == {
+        "css",
+        "default",
+        "details",
+        "help",
+        "id",
+        "label",
+        "name",
+        "required",
+        "type",
+    }
+
+
+def test_catalogue_constraints(collection: Collection) -> None:
+    assert set(collection.constraints[0]) == {
+        "day",
+        "month",
+        "product_type",
+        "time",
+        "variable",
+        "year",
+    }
---
+++ packages/datapi/datapi-0.2.1.tar.gz/datapi-0.2.1/tests/integration_test_30_remote.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+import contextlib
+import datetime
+import os
+import pathlib
+import time
+import uuid
+from typing import Any
+
+import pytest
+from requests import HTTPError
+
+from datapi import ApiClient, Remote
+
+does_not_raise = contextlib.nullcontext
+
+
+@pytest.fixture
+def remote(api_anon_client: ApiClient) -> Remote:
+    return api_anon_client.submit("test-adaptor-dummy", {"size": 1})
+
+
+def test_remote_delete(remote: Remote) -> None:
+    result = remote.delete()
+    time.sleep(1)
+    assert result["status"] == "dismissed"
+
+    with pytest.raises(HTTPError, match="404 Client Error"):
+        remote.status
+
+
+def test_remote_download(remote: Remote, tmp_path: pathlib.Path) -> None:
+    target = str(tmp_path / "dummy.grib")
+    result = remote.download(target=target)
+    assert result == target
+    assert os.path.getsize(result) == 1
+
+
+def test_remote_collection_id(remote: Remote) -> None:
+    assert remote.collection_id == "test-adaptor-dummy"
+
+
+def test_remote_json(remote: Remote) -> None:
+    assert isinstance(remote.json, dict)
+
+
+def test_remote_request(remote: Remote) -> None:
+    assert remote.request == {
+        "elapsed": 0.0,
+        "format": "grib",
+        "size": 1,
+    }
+
+
+def test_remote_request_uid(remote: Remote) -> None:
+    assert uuid.UUID(remote.request_uid)
+
+
+def test_remote_status(remote: Remote) -> None:
+    assert remote.status in ("accepted", "running", "successful")
+
+
+def test_remote_failed(api_anon_client: ApiClient) -> None:
+    remote = api_anon_client.submit("test-adaptor-dummy", {"format": "foo"})
+    with pytest.raises(HTTPError, match="400 Client Error: Bad Request"):
+        remote.download()
+    assert remote.status == "failed"
+
+
+@pytest.mark.parametrize(
+    "cleanup,raises",
+    [
+        (True, pytest.raises(HTTPError, match="404 Client Error")),
+        (False, does_not_raise()),
+    ],
+)
+def test_remote_cleanup(
+    api_root_url: str,
+    api_anon_key: str,
+    cleanup: bool,
+    raises: contextlib.nullcontext[Any],
+) -> None:
+    client = ApiClient(
+        url=api_root_url, key=api_anon_key, cleanup=cleanup, maximum_tries=0
+    )
+    remote = client.submit("test-adaptor-dummy", {})
+    request_uid = remote.request_uid
+    del remote
+    time.sleep(1)
+
+    client = ApiClient(url=api_root_url, key=api_anon_key, maximum_tries=0)
+    with raises:
+        client.get_remote(request_uid)
+
+
+def test_remote_datetimes(api_anon_client: ApiClient) -> None:
+    request = {"elapsed": 1, "_timestamp": datetime.datetime.now().isoformat()}
+    remote = api_anon_client.submit("test-adaptor-dummy", request)
+    assert remote.results_ready is False
+    assert isinstance(remote.creation_datetime, datetime.datetime)
+    assert remote.end_datetime is None
+
+    remote.make_results()
+    assert remote.start_datetime is not None
+    assert remote.end_datetime is not None
+    assert remote.creation_datetime < remote.start_datetime < remote.end_datetime
