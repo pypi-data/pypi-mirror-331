96011dabb6887acfed3bd2f9d3b869b33239e0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:49 +0000

    Add package gamr-backend-api-service/1.0.5/gamr_backend_api_service-1.0.5-py3-none-any.whl

---
+++ packages/gamr-backend-api-service/gamr_backend_api_service-1.0.5-py3-none-any.whl/gamr_backend_api_service/__main__.py
@@ -0,0 +1,16 @@
+import uvicorn
+
+from gamr_backend_api_service.api import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "gamr_backend_api_service.api:create_app",
+        factory=True,
+        host="0.0.0.0",
+        port=8080,
+        reload=True,
+    )
+
+# uvicorn gamr_backend_api_service.api:create_app --host 0.0.0.0 --port 8000 --reload
---
+++ packages/gamr-backend-api-service/gamr_backend_api_service-1.0.5-py3-none-any.whl/gamr_backend_api_service/api/routes/base.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends, Response
+
+from gamr_backend_api_service.auth.token import TokenGenerator
+from gamr_backend_api_service.data.user_manager import UserManager
+from gamr_backend_api_service.model_services import ModelService, get_ai_model_interface
+from gamr_backend_api_service.models import ImageData, User
+
+router = APIRouter()
+
+
+@router.get("/")
+async def root():
+    return {"message": "access the /detect_objects endpoint"}
+
+
+@router.post("/token")
+async def token(user: User):
+    token_generator = TokenGenerator()
+    _token = token_generator.get_token(user)
+    return {"token": _token, "type": "BEARER"}
+
+
+@router.post("/detect_objects")
+async def detect_objects(
+    image_data: ImageData,
+    current_user: User = Depends(TokenGenerator().get_user_from_token),
+) -> Response:
+    user_manager = UserManager()
+    user_manager.validate_user_exists(current_user)
+    model_interface = get_ai_model_interface(ModelService[image_data.model_service])
+    image_response = model_interface().predict(image_data=image_data)  # type: ignore
+
+    return image_response.response
