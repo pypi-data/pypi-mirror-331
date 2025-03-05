6e627277019acdb8abaaf1bd7023d7cc3e477ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:43 +0000

    Add package gpt-po-translator/0.2.18/gpt_po_translator-0.2.18.tar.gz

---
+++ packages/gpt-po-translator/gpt_po_translator-0.2.18.tar.gz/gpt_po_translator-0.2.18/setup.py
@@ -0,0 +1,58 @@
+"""
+Setup script for the gpt-po-translator package.
+This script is used to install the package and its dependencies.
+"""
+
+from setuptools import find_packages, setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='gpt-po-translator',
+    use_scm_version=True,
+    setup_requires=['setuptools-scm==8.1.0'],
+    author='Bram Mittendorff',
+    author_email='bram@pescheck.io',
+    description='A CLI tool for translating .po files using GPT models.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pescheckit/python-gpt-po',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'polib==1.2.0',
+        'openai==1.58.1',
+        'python-dotenv==1.0.0',
+        'tenacity==9.0.0',
+        'pycountry==24.6.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'gpt-po-translator=python_gpt_po.po_translator:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Text Processing :: Linguistic',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3 :: Only',
+        'Natural Language :: English',
+        'Natural Language :: Dutch',
+        'Environment :: Console',
+        'Typing :: Typed'
+    ],
+    python_requires='>=3.8',
+)
