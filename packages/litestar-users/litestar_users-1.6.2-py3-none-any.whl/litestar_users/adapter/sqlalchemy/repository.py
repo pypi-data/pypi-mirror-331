7161f549c97896365ef04fb7a0a1163ea01d7f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:49 +0000

    Add package litestar-users/1.6.2/litestar_users-1.6.2-py3-none-any.whl

---
+++ packages/litestar-users/litestar_users-1.6.2-py3-none-any.whl/litestar_users/adapter/sqlalchemy/repository.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Generic
+
+from advanced_alchemy.repository import SQLAlchemyAsyncRepository
+from litestar.exceptions import ImproperlyConfiguredException
+
+from litestar_users.adapter.sqlalchemy.protocols import SQLARoleT, SQLAUserT
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+__all__ = ["SQLAlchemyRoleRepository", "SQLAlchemyUserRepository"]
+
+
+class SQLAlchemyUserRepository(SQLAlchemyAsyncRepository[SQLAUserT], Generic[SQLAUserT]):
+    """SQLAlchemy implementation of user persistence layer."""
+
+    def __init__(self, session: AsyncSession, model_type: type[SQLAUserT], **kwargs: Any) -> None:
+        """Repository for users.
+
+        Args:
+            session: Session managing the unit-of-work for the operation.
+            model_type: A subclass of `SQLAlchemyUserModel`.
+            kwargs: Additional keyword arguments to pass to superclass.
+        """
+        self.model_type = model_type
+        super().__init__(session=session, **kwargs)
+
+    async def _update(self, user: SQLAUserT, data: dict[str, Any]) -> SQLAUserT:
+        for key, value in data.items():
+            setattr(user, key, value)
+
+        if self.auto_commit:
+            await self.session.commit()
+
+        return user
+
+
+class SQLAlchemyRoleRepository(SQLAlchemyAsyncRepository[SQLARoleT], Generic[SQLARoleT, SQLAUserT]):
+    """SQLAlchemy implementation of role persistence layer."""
+
+    def __init__(self, session: AsyncSession, model_type: type[SQLARoleT], **kwargs: Any) -> None:
+        """Repository for users.
+
+        Args:
+            session: Session managing the unit-of-work for the operation.
+            model_type: A subclass of `SQLAlchemyRoleModel`.
+            kwargs: Additional keyword arguments to pass to superclass.
+        """
+        self.model_type = model_type
+        super().__init__(session=session)
+
+    async def assign_role(self, user: SQLAUserT, role: SQLARoleT) -> SQLAUserT:
+        """Add a role to a user.
+
+        Args:
+            user: The user to receive the role.
+            role: The role to add to the user.
+        """
+        if not hasattr(user, "roles"):
+            raise ImproperlyConfiguredException("User.roles is not set")
+        user.roles.append(role)  # pyright: ignore
+
+        if self.auto_commit:
+            await self.session.commit()
+
+        return user
+
+    async def revoke_role(self, user: SQLAUserT, role: SQLARoleT) -> SQLAUserT:
+        """Revoke a role from a user.
+
+        Args:
+            user: The user to revoke the role from.
+            role: The role to revoke from the user.
+        """
+        if not hasattr(user, "roles"):
+            raise ImproperlyConfiguredException("User.roles is not set")
+        user.roles.remove(role)  # pyright: ignore
+        if self.auto_commit:
+            await self.session.commit()
+        return user
