fb25e185a0319f713b82b122a7229ebe5b21337b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 08:04:48 +0000

    Add package bakesite/0.6.0/bakesite-0.6.0-py3-none-any.whl

---
+++ packages/bakesite/bakesite-0.6.0-py3-none-any.whl/bakesite/server.py
@@ -0,0 +1,18 @@
+import functools
+import http.server
+import socketserver
+
+import click
+
+
+DEFAULT_PORT = 8200  # 8 is standard, 200 is the temp for deliciousness
+
+
+def serve(port=DEFAULT_PORT):
+    Handler = functools.partial(
+        http.server.SimpleHTTPRequestHandler, directory="./_site"
+    )
+
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+        click.echo(f"Serving your baked site at port http://localhost:{port}")
+        httpd.serve_forever()
