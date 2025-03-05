d66767476edaad819802d9df05b780243487ccc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 08:05:03 +0000

    Add package dash-builder/0.1.2/dash_builder-0.1.2.tar.gz

---
+++ packages/dash-builder/dash_builder-0.1.2.tar.gz/dash_builder-0.1.2/tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from typer.testing import CliRunner
+
+from src.dash_builder import DashPage, DashView
+
+
+@pytest.fixture()
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+@pytest.fixture()
+def test_view() -> type[DashView]:
+    class TestView(DashView):
+        @classmethod
+        def create(cls, id: str, **kwargs):
+            return []
+
+    return TestView
+
+
+@pytest.fixture()
+def test_page() -> type[DashPage]:
+    class TestPage(DashPage):
+        @classmethod
+        def valid_layout(cls, **kwargs):
+            return []
+
+    return TestPage
+
+
+@pytest.fixture()
+def error_page() -> type[DashPage]:
+    class TestPage(DashPage):
+        @classmethod
+        def valid_layout(cls, **kwargs):
+            return 1 / 0
+
+    return TestPage
