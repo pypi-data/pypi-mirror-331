13d4b58b56816dabfc15664b9d5a39fcfc2a7c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:05:25 +0000

    Add package tasfiya/0.1.1/tasfiya-0.1.1.tar.gz

---
+++ packages/tasfiya/tasfiya-0.1.1.tar.gz/tasfiya-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tasfiya",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="SHAH MAKHDUM SHAJON",
+    author_email="shahmakhdumshajon@gmail.com",
+    description="JSON-XD, JSON-LITE FACEBOOK ID CRACKING TOOL HELPERS",
+    url="https://github.com/SHAJON-404/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
