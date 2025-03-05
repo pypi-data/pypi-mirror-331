4db095200236f6d94f565bb1f1298e6afa0b9d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 07:06:35 +0000

    Add package simplelabel/1.2.0b0/simplelabel-1.2.0b0-py3-none-any.whl

---
+++ packages/simplelabel/simplelabel-1.2.0b0-py3-none-any.whl/simplelabel/__init__.py
@@ -0,0 +1,11 @@
+from .core.rule import Rule
+from .core.parser import RuleParser
+from .core.mapper import FieldMap
+from .core.mapper import FieldMapper
+from .core.mapper import DEFAULT_FIELD_MAP
+from .utils.label import DataLabelEngine
+from .utils.generator import SQLGenerator
+from .utils.generator import FieldsIdentifier
+
+__appname__ = "simplelabel"
+__version__ = "1.2.0-beta"
