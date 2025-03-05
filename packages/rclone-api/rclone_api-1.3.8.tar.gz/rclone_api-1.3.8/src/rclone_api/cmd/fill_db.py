3550f186f75c88a3beea92ff61fad75bc1bb84c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 14:04:50 +0000

    Add package rclone-api/1.3.8/rclone_api-1.3.8.tar.gz

---
+++ packages/rclone-api/rclone_api-1.3.8.tar.gz/rclone_api-1.3.8/src/rclone_api/cmd/fill_db.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from rclone_api import Rclone
+
+# load_dotenv()
+
+
+# DB_URL = "sqlite:///data.db"
+
+# os.environ["DB_URL"] = "sqlite:///data.db"
+
+
+def _db_url_from_env_or_raise() -> str:
+    load_dotenv()
+    db_url = os.getenv("DB_URL")
+    if db_url is None:
+        raise ValueError("DB_URL not set")
+    return db_url
+
+
+@dataclass
+class Args:
+    config: Path
+    path: str
+
+    def __post_init__(self):
+        if not self.config.exists():
+            raise FileNotFoundError(f"Config file not found: {self.config}")
+
+
+def fill_db(rclone: Rclone, path: str):
+    """List files in a remote path."""
+    # db = DB(_db_url_from_env_or_raise())
+    db_url = _db_url_from_env_or_raise()
+    rclone.save_to_db(src=path, db_url=db_url, fast_list=True)
+
+
+def _parse_args() -> Args:
+    parser = argparse.ArgumentParser(description="List files in a remote path.")
+    parser.add_argument(
+        "--config", help="Path to rclone config file", type=Path, default="rclone.conf"
+    )
+    parser.add_argument("path", help="Remote path to list")
+    tmp = parser.parse_args()
+    return Args(config=tmp.config, path=tmp.path)
+
+
+def main() -> int:
+    """Main entry point."""
+    args = _parse_args()
+    path = args.path
+    rclone = Rclone(Path(args.config))
+    fill_db(rclone, path)
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    cwd = Path(".").absolute()
+    print(f"cwd: {cwd}")
+    sys.argv.append("dst:TorrentBooks/meta")
+    main()
