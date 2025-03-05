eed28781f0fef8b0e8c9d18d6d080d4ae7356051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:16 +0000

    Add package semi-colon/0.2.2/semi_colon-0.2.2.tar.gz

---
+++ packages/semi-colon/semi_colon-0.2.2.tar.gz/semi_colon-0.2.2/src/semi_colon/__init__.py
@@ -0,0 +1,6 @@
+from .main import download, imshows
+from .house import load_boston
+from .img2data import digit_split
+
+__all__ = ["download", 'imshows', 'load_boston', 'digit_split']
+__version__ = '0.2.2'
