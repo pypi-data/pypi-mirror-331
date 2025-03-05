dbf45e270da4e48744b21b58137a613634d7887a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:56 +0000

    Add package coincident/0.2/coincident-0.2-py3-none-any.whl

---
+++ packages/coincident/coincident-0.2-py3-none-any.whl/coincident/io/__init__.py
@@ -0,0 +1,9 @@
+"""
+coincident: Search and analysis of STV Precursor Coincident Datasets
+"""
+
+from __future__ import annotations
+
+from coincident.io import download, sliderule, xarray
+
+__all__ = ["sliderule", "xarray", "download"]
