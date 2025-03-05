742849fbefbb68c4d26cf2ca0abc24e5fa48412d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:06:34 +0000

    Add package decentra-vision/2.0.0/decentra_vision-2.0.0.tar.gz

---
+++ packages/decentra-vision/decentra_vision-2.0.0.tar.gz/decentra_vision-2.0.0/README.md
@@ -0,0 +1,3 @@
+# DecentrAI Vision
+
+Decentra Vision is a Python library for which contains useful methods for python image manipulation.
---
+++ packages/decentra-vision/decentra_vision-2.0.0.tar.gz/decentra_vision-2.0.0/decentra_vision/constants.py
@@ -0,0 +1,21 @@
+## BGRs
+DARK_BLUE          = (109, 45, 11)
+BLUE               = (255, 0, 0)
+LIGHT_BLUE         = (230, 200, 100)
+ORANGE             = (19, 149, 250)
+WHITE              = (255, 255, 255)
+YELLOW             = (98, 222, 250)
+RED                = (100, 70, 248)
+GREEN              = (20, 180, 20)
+DARK_GREEN         = (20, 120, 20)
+PALE_GREEN         = (60, 230, 170)
+DEEP_RED           = (20, 20, 170)
+BROWN              = (42, 42, 165)
+PURPLE             = (211, 0, 148)
+
+
+
+BLUR_ADAPTIVE = 'BLUR_ADAPTIVE'
+BLUR_RAW = 'BLUR_RAW'
+FACE = 'FACE'
+PERSON = 'PERSON'
