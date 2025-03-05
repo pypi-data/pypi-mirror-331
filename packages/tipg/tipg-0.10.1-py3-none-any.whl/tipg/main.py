9923524d62fa55510b41a64da9d3965478ee26b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:06:12 +0000

    Add package tipg/0.10.1/tipg-0.10.1-py3-none-any.whl

---
+++ packages/tipg/tipg-0.10.1-py3-none-any.whl/tipg/main.py
@@ -0,0 +1,154 @@
+"""tipg app."""
+
+from contextlib import asynccontextmanager
+from typing import Any, List
+
+import jinja2
+
+from tipg import __version__ as tipg_version
+from tipg.collections import register_collection_catalog
+from tipg.database import close_db_connection, connect_to_db
+from tipg.errors import DEFAULT_STATUS_CODES, add_exception_handlers
+from tipg.factory import Endpoints
+from tipg.middleware import CacheControlMiddleware, CatalogUpdateMiddleware
+from tipg.settings import APISettings, CustomSQLSettings, DatabaseSettings
+
+from fastapi import FastAPI, Request
+
+from starlette.middleware.cors import CORSMiddleware
+from starlette.templating import Jinja2Templates
+from starlette_cramjam.middleware import CompressionMiddleware
+
+settings = APISettings()
+db_settings = DatabaseSettings()
+custom_sql_settings = CustomSQLSettings()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """FastAPI Lifespan."""
+    # Create Connection Pool
+    await connect_to_db(
+        app,
+        schemas=db_settings.schemas,
+        user_sql_files=custom_sql_settings.sql_files,
+    )
+
+    # Register Collection Catalog
+    await register_collection_catalog(
+        app,
+        schemas=db_settings.schemas,
+        tables=db_settings.tables,
+        exclude_tables=db_settings.exclude_tables,
+        exclude_table_schemas=db_settings.exclude_table_schemas,
+        functions=db_settings.functions,
+        exclude_functions=db_settings.exclude_functions,
+        exclude_function_schemas=db_settings.exclude_function_schemas,
+        spatial=db_settings.only_spatial_tables,
+        spatial_extent=db_settings.spatial_extent,
+        datetime_extent=db_settings.datetime_extent,
+    )
+
+    yield
+
+    # Close the Connection Pool
+    await close_db_connection(app)
+
+
+app = FastAPI(
+    title=settings.name,
+    version=tipg_version,
+    openapi_url="/api",
+    docs_url="/api.html",
+    lifespan=lifespan,
+    root_path=settings.root_path,
+)
+
+# custom template directory
+templates_location: List[Any] = (
+    [jinja2.FileSystemLoader(settings.template_directory)]
+    if settings.template_directory
+    else []
+)
+# default template directory
+templates_location.append(jinja2.PackageLoader(__package__, "templates"))
+
+jinja2_env = jinja2.Environment(loader=jinja2.ChoiceLoader(templates_location))
+templates = Jinja2Templates(env=jinja2_env)
+
+ogc_api = Endpoints(
+    title=settings.name,
+    templates=templates,
+    with_tiles_viewer=settings.add_tiles_viewer,
+)
+app.include_router(ogc_api.router)
+
+# Set all CORS enabled origins
+if settings.cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.cors_origins,
+        allow_credentials=True,
+        allow_methods=["GET"],
+        allow_headers=["*"],
+    )
+
+app.add_middleware(CacheControlMiddleware, cachecontrol=settings.cachecontrol)
+app.add_middleware(CompressionMiddleware, compression_level=6)
+
+if settings.catalog_ttl:
+    app.add_middleware(
+        CatalogUpdateMiddleware,
+        func=register_collection_catalog,
+        ttl=settings.catalog_ttl,
+        schemas=db_settings.schemas,
+        tables=db_settings.tables,
+        exclude_tables=db_settings.exclude_tables,
+        exclude_table_schemas=db_settings.exclude_table_schemas,
+        functions=db_settings.functions,
+        exclude_functions=db_settings.exclude_functions,
+        exclude_function_schemas=db_settings.exclude_function_schemas,
+        spatial=db_settings.only_spatial_tables,
+        spatial_extent=db_settings.spatial_extent,
+        datetime_extent=db_settings.datetime_extent,
+    )
+
+add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
+
+@app.get(
+    "/healthz",
+    description="Health Check.",
+    summary="Health Check.",
+    operation_id="healthCheck",
+    tags=["Health Check"],
+)
+def ping():
+    """Health check."""
+    return {"ping": "pong!"}
+
+
+if settings.debug:
+
+    @app.get("/rawcatalog", tags=["debug"])
+    async def raw_catalog(request: Request):
+        """Return parsed catalog data for testing."""
+        return request.app.state.collection_catalog
+
+    @app.get("/refresh", tags=["debug"])
+    async def refresh(request: Request):
+        """Return parsed catalog data for testing."""
+        await register_collection_catalog(
+            request.app,
+            schemas=db_settings.schemas,
+            tables=db_settings.tables,
+            exclude_tables=db_settings.exclude_tables,
+            exclude_table_schemas=db_settings.exclude_table_schemas,
+            functions=db_settings.functions,
+            exclude_functions=db_settings.exclude_functions,
+            exclude_function_schemas=db_settings.exclude_function_schemas,
+            spatial=db_settings.only_spatial_tables,
+            spatial_extent=db_settings.spatial_extent,
+            datetime_extent=db_settings.datetime_extent,
+        )
+        return request.app.state.collection_catalog
