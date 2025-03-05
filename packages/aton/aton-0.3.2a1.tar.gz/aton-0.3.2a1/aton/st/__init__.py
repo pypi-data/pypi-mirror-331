a30f4a325fd7e1ecd93e691ca2b2e9602ea8aaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:27 +0000

    Add package aton/0.3.2a1/aton-0.3.2a1.tar.gz

---
+++ packages/aton/aton-0.3.2a1.tar.gz/aton-0.3.2a1/aton/st/__init__.py
@@ -0,0 +1,20 @@
+"""
+# System Tools
+
+Common system tools across all ATON subpackages.
+
+
+# Index
+
+| | |  
+| --- | --- |  
+| `aton.st.file`  | File manipulation |  
+| `aton.st.call`  | Run bash scripts and related |  
+| `aton.st.alias` | Useful dictionaries for user input correction |  
+
+"""
+
+from . import file
+from . import call
+from . import alias
+
