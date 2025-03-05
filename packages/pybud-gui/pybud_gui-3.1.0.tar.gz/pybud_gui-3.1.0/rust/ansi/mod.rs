e1c798ae8e322ca0a875927674b9e98dd8ff404f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 08:04:57 +0000

    Add package pybud-gui/3.1.0/pybud_gui-3.1.0.tar.gz

---
+++ packages/pybud-gui/pybud_gui-3.1.0.tar.gz/pybud_gui-3.1.0/rust/ansi/mod.rs
@@ -0,0 +1,197 @@
+use pyo3::prelude::*;
+use bitflags::bitflags;
+
+pub mod char;
+pub mod string;
+pub mod drawer;
+
+// Types
+#[pyclass]
+#[derive(Clone, Copy, PartialEq)]
+pub struct AnsiColor(pub u8, pub u8, pub u8);
+
+fn calc_legacy_color(c: u8) -> u8 {
+    /* 
+    legacy colors have an estimated range from about 48 to 236, 
+    this function translates a single color to a legacy color of base 6
+    */
+
+    // const K: f32 = 5.0 / 187.0; // constant ratio
+    
+    if c >= 48 { ((c - 48) * 5) / 187 } else { 0 }
+}
+
+impl AnsiColor {
+    #[inline]
+    fn get_ground_code(ground: &ColorGround) -> &str {
+        match ground {
+            ColorGround::BACK => "48",
+            ColorGround::FORE => "38",
+        }
+    }
+
+    fn truecolor_render(&self, ground: &ColorGround) -> String {
+        format!("\x1b[{};2;{};{};{}m", Self::get_ground_code(ground), self.0, self.1, self.2)
+    }
+
+    fn limited_render(&self, ground: &ColorGround) -> String {
+        
+        let r = calc_legacy_color(self.0);
+        let g = calc_legacy_color(self.1);
+        let b = calc_legacy_color(self.2);
+        let color_code = 16 + (r * 36) + (g * 6) + b;
+
+        format!("\x1b[{};5;{}m", Self::get_ground_code(ground), color_code)
+    }
+}
+
+#[pymethods]
+impl AnsiColor {
+    #[new]
+    fn new(r: u8, g: u8, b: u8) -> Self {
+        Self {0: r, 1: g, 2:b}
+    }
+
+    pub fn to_string(&self, mode: &ColorMode, ground: &ColorGround) -> String {
+        match mode {
+            ColorMode::TRUECOLOR => self.truecolor_render(ground),
+            ColorMode::LIMITED => self.limited_render(ground)
+        }
+    }
+
+    // python __eq__ magic function
+    pub fn __eq__(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+
+#[pyclass(eq, eq_int)]
+#[derive(Clone, Copy, PartialEq)]
+pub enum ColorGround {
+    BACK,
+    FORE
+}
+#[pyclass(eq, eq_int)]
+#[derive(Clone, Copy, PartialEq)]
+pub enum ColorMode {
+    LIMITED,
+    TRUECOLOR
+}
+
+const ANSIRESET: &str = "\x1b[0m";
+//const RESET_FOREGROUND: &str = "\x1b[0;39m";
+//const RESET_BACKGROUND: &str = "\x1b[0;49m";
+
+bitflags! {
+    #[pyclass]
+    #[derive(Clone, Copy, PartialEq)]
+    pub struct AnsiGraphics: u8 {
+        const BOLD      = 0b00000001;
+        const FAINT     = 0b00000010;
+        const ITALIC    = 0b00000100;
+        const UNDERLINE = 0b00001000;
+        const BLINKING  = 0b00010000;
+        const REVERSE   = 0b00100000;
+        const HIDDEN    = 0b01000000;
+        const STRIKE    = 0b10000000;
+    }
+}
+
+impl AnsiGraphics {
+    const IDX2ANSI: [(&'static str, &'static str); 8] = [
+        // set    ,    reset  
+        ("\x1b[1m", "\x1b[22m"), // 0 -> BOLD
+        ("\x1b[2m", "\x1b[22m"), // 1 -> FAINT
+        ("\x1b[3m", "\x1b[23m"), // 2 -> ITALIC
+        ("\x1b[4m", "\x1b[24m"), // 3 -> UNDERLINE
+        ("\x1b[5m", "\x1b[25m"), // 4 -> BLINKING
+        ("\x1b[7m", "\x1b[27m"), // 5 -> REVERSE
+        ("\x1b[8m", "\x1b[28m"), // 6 -> HIDDEN
+        ("\x1b[9m", "\x1b[29m"), // 7 -> STRIKE
+        ];
+    
+    const NAME2IDX: [(&'static str, u8); 8] = [
+        // set    ,    reset  
+        ("BOLD", 0), 
+        ("FAINT", 1), 
+        ("ITALIC", 2), 
+        ("UNDERLINE", 3), 
+        ("BLINKING", 4),
+        ("REVERSE", 5),
+        ("HIDDEN", 6),
+        ("STRIKE", 7),
+        ];
+
+    #[inline]
+    pub fn get_mode(name: &str, reset: bool) -> &'static str {
+        let mut i: usize = 0;
+        let idx = loop {
+            if i < Self::NAME2IDX.len(){
+                let mapping = Self::NAME2IDX[i];
+                if mapping.0 == name.to_uppercase() {
+                    break mapping.1 as i8
+                }
+                i += 1;
+            } else {
+                break -1
+            }
+        };
+
+        if idx == -1{
+            print!("Could not find mode with name \"{}\".", name);
+            panic!()
+        }
+
+        let idx: usize = idx as usize;
+
+        let graphic_ansi_codes = match Self::IDX2ANSI.get(idx) {
+            None => {("", "")},
+            Some(t) => {*t}
+        };
+
+        match reset {
+            false => {graphic_ansi_codes.0},
+            true => {graphic_ansi_codes.1}
+        }
+    }
+}
+
+#[pymethods]
+impl AnsiGraphics {
+    #[new]
+    #[inline]
+    fn new() -> Self {
+        Self::empty()
+    }
+
+    #[staticmethod]
+    #[inline]
+    fn _from_bits(bits: u8) -> Option<Self> {
+        Self::from_bits(bits)
+    }
+
+    pub fn to_string(&self, reset: bool) -> String {
+        let mut result = String::new();
+
+        for mode in self.iter_names() {
+            result.push_str(Self::get_mode(mode.0, reset))
+        }
+
+        result
+    }
+
+    // python __str__ magic function
+    pub fn __str__(&self) -> String {
+        self.to_string(false)
+    }
+
+    // python __eq__ magic function
+    pub fn __eq__(&self, other: &Self) -> bool {
+        self == other
+    }
+
+    // python __or__ magic function
+    pub fn __or__(&self, other: &Self) -> Self {
+        *self | *other
+    }
+}
---
+++ packages/pybud-gui/pybud_gui-3.1.0.tar.gz/pybud_gui-3.1.0/rust/ansi/string.rs
@@ -0,0 +1,234 @@
+use pyo3::prelude::*;
+
+use std::ops::Add;
+
+use crate::ansi::AnsiGraphics;
+
+use super::{ColorGround, ColorMode, ANSIRESET};
+use super::char::AnsiChar;
+
+#[pyclass]
+#[derive(Clone, PartialEq)]
+pub struct AnsiString {
+    #[pyo3(get, set)]
+    pub vec: Vec<AnsiChar>,
+}
+
+fn min(a: usize, b: usize) -> usize {
+    if a > b {b} else {a}
+}
+
+// non-python methods
+impl AnsiString {
+    pub fn len(&self) -> usize {
+        self.vec.len()
+    }
+
+    // non-optimized to_string, each character is rendered individually
+    pub fn to_string_noopt(&self, mode: &ColorMode) -> String {
+        let mut _string = String::new();
+        for ac in &self.vec.clone() {
+            _string.push_str(ac.to_string(mode).as_str());
+            _string.push_str(ANSIRESET);
+        }
+        _string
+    }
+
+    #[inline]
+    pub fn new_fore(str: &str, fore: (u8, u8, u8)) -> AnsiString {
+        AnsiString::new(str, Some(fore), None)
+    }
+
+    #[inline]
+    pub fn new_back(str: &str, back: (u8, u8, u8)) -> AnsiString {
+        AnsiString::new(str, None, Some(back))
+    }
+
+    #[inline]
+    pub fn new_colorless(str: &str) -> AnsiString {
+        AnsiString::new(str, None, None)
+    }
+}
+
+// python methods
+#[pymethods]
+impl AnsiString {
+    #[new]
+    #[pyo3(signature = (s, fore=None, back=None))]
+    #[inline]
+    pub fn new(s: &str, fore: Option<(u8, u8, u8)>, back: Option<(u8, u8, u8)>) -> Self {
+        let mut vec: Vec<AnsiChar> = Vec::with_capacity(s.len());
+    
+        for c in s.chars() {
+            vec.push(AnsiChar::new(c, fore, back));
+        }
+
+        Self {vec: vec}
+    }
+
+    // optimized to_string
+    pub fn to_string(&self, mode: &ColorMode) -> String {
+        if self.vec.len() == 0 {
+            return String::new();
+        }
+        // add the first character unoptimized
+        let mut _string = self.vec[0].to_string(mode);
+
+        for i in 1..self.vec.len() {
+            // current AnsiChar
+            let cac = &self.vec[i];
+            // current fore color
+            let cfc = &cac.fore_color;
+            // current back color
+            let cbc = &cac.back_color;
+            // current AnsiGraphics
+            let cag = &cac.graphics;
+
+            // previus AnsiChar
+            let pac = &self.vec[i-1];
+            // previus fore color
+            let pfc = &pac.fore_color;
+            // previus back color
+            let pbc = &pac.back_color;
+            // previus AnsiGraphics
+            let pag = &pac.graphics;
+
+            // if color is changed, apply changes
+            if (pbc != cbc) || (pfc != cfc) {
+                // reset ansi
+                _string.push_str(ANSIRESET);
+                /* to reset background and foreground sperately:
+                _string.push_str(RESET_BACKGROUND);
+                _string.push_str(RESET_FOREGROUND);*/
+
+                // set background if exists
+                _string.push_str(match cbc {
+                    None => {String::new()},
+                    Some(c ) => {c.to_string(mode, &ColorGround::BACK)},
+                }.as_str());
+
+                // set foreground if exists
+                _string.push_str(match cfc {
+                    None => {String::new()},
+                    Some(c) => {c.to_string(mode, &ColorGround::FORE)},
+                }.as_str());
+
+                // write current AnsiGraphics AGAIN
+                _string.push_str(cag.to_string(false).as_str());
+            } else if pag != cag {
+                // reset previus AnsiGraphics
+                _string.push_str(pag.to_string(true).as_str());
+                // write current AnsiGraphics
+                _string.push_str(cag.to_string(false).as_str());
+
+            }
+            
+            _string.push(cac.char);
+        }
+        // append reset token and return
+        _string + "\x1b[0m"
+    }
+
+    pub fn split_at(&self, mid: usize) -> (AnsiString, AnsiString){
+        let vecs = self.vec.split_at(mid);
+        (
+            Self {vec: vecs.0.to_vec()},
+            Self {vec: vecs.1.to_vec()}
+        )
+    }
+
+    pub fn cut_at(&self, end: usize) -> AnsiString{
+        let vecs = self.vec.split_at(end);
+        Self {vec: vecs.0.to_vec()}
+    }
+
+    // main function for writing text
+    pub fn place(&mut self, text: &AnsiString, pos: usize, assign: bool) {
+        assert!(pos < self.len());
+        // starting index
+        let si = pos;
+        // endiing index
+        let ei = min(pos + text.len(), self.vec.len());
+
+        for i in si..ei {
+            let ac = &text.vec[i - si];
+            if assign {
+                self.vec[i] = ac.clone();
+            } else {
+                self.vec[i] = AnsiChar {
+                    char: ac.char,
+                    fore_color: ac.fore_color,
+                    back_color: if ac.back_color == None {self.vec[i].back_color} else {ac.back_color},
+                    graphics: ac.graphics.clone(),
+                }
+            }
+        }
+    }
+
+    pub fn place_str(&mut self, str: &str, pos: usize) {
+        // TODO: negative positon values
+        assert!(pos < self.len());
+
+        let astr = AnsiString::new_colorless(str);
+        self.place(&astr, pos, false);
+        
+    }
+
+    pub fn center_place(&mut self, astr: &AnsiString, assign: bool) {
+        assert!(self.len() > astr.len());
+
+        let pos: usize = (self.len() - astr.len()) / 2;
+
+        self.place(astr, pos, assign);
+    }
+
+    pub fn center_place_str(&mut self, str: &str) {
+        assert!(self.len() > str.len());
+
+        let pos: usize = (self.len() - str.len()) / 2;
+
+        let astr = AnsiString::new_colorless(str);
+        self.place(&astr, pos, false);
+    }
+
+    pub fn add_graphics(&mut self, agm: AnsiGraphics) {
+        for ac in &mut self.vec {
+            ac.graphics = ac.graphics | agm;
+        }
+    }
+
+    // python operation add
+    pub fn __add__(&mut self, other: &Self) -> Self {
+        let mut r = self.clone();
+        r.vec.append(&mut other.vec.clone());
+
+        r
+    }
+
+    // python len function
+    pub fn __len__(&self) -> usize {
+        self.len()
+    }
+
+    // python __str__ magic function
+    pub fn __str__(&self) -> String {
+        self.to_string(&ColorMode::TRUECOLOR)
+    }
+
+    // python __eq__ magic function
+    pub fn __eq__(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+
+impl Add for AnsiString {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        let mut new_vec: Vec<AnsiChar> = Vec::with_capacity(self.vec.len() + other.vec.len());
+        new_vec.append(&mut self.vec.clone());
+        new_vec.append(&mut other.vec.clone());
+
+        AnsiString { vec: new_vec }
+    }
+}
