3cc30a11c5668f31c10120f7447f78c8901f47c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 05:05:04 +0000

    Add package airflow-provider-demo-ml-framework/0.1.5/airflow_provider_demo_ml_framework-0.1.5-py3-none-any.whl

---
+++ packages/airflow-provider-demo-ml-framework/airflow_provider_demo_ml_framework-0.1.5-py3-none-any.whl/ml_demo_provider/__init__.py
@@ -0,0 +1,14 @@
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-demo-ml-framework",
+        "name": "DEMO ML Framework Airflow Provider",
+        "description": "DEMO ML Framework Airflow provider.",
+        "versions": ["0.1.5"],
+        "connection-types": [
+            {
+                "hook-class-name": "ml_demo_provider.hooks.ml_demo_framework.DatasetStorageHook",
+                "connection-type": "http",
+            },
+        ],
+        "extra-links": [],
+    }
