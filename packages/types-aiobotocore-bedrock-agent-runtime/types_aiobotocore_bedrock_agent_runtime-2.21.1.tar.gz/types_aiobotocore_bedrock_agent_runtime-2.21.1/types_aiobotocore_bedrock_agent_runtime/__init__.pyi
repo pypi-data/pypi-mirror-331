ffd810ffbd554cf09690c878b4cd666fae6307b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 14:04:32 +0000

    Add package types-aiobotocore-bedrock-agent-runtime/2.21.1/types_aiobotocore_bedrock_agent_runtime-2.21.1.tar.gz

---
+++ packages/types-aiobotocore-bedrock-agent-runtime/types_aiobotocore_bedrock_agent_runtime-2.21.1.tar.gz/types_aiobotocore_bedrock_agent_runtime-2.21.1/types_aiobotocore_bedrock_agent_runtime/__init__.pyi
@@ -0,0 +1,43 @@
+"""
+Main interface for bedrock-agent-runtime service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_bedrock_agent_runtime import (
+        AgentsforBedrockRuntimeClient,
+        Client,
+        GetAgentMemoryPaginator,
+        RerankPaginator,
+        RetrievePaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("bedrock-agent-runtime") as client:
+        client: AgentsforBedrockRuntimeClient
+        ...
+
+
+    get_agent_memory_paginator: GetAgentMemoryPaginator = client.get_paginator("get_agent_memory")
+    rerank_paginator: RerankPaginator = client.get_paginator("rerank")
+    retrieve_paginator: RetrievePaginator = client.get_paginator("retrieve")
+    ```
+"""
+
+from .client import AgentsforBedrockRuntimeClient
+from .paginator import GetAgentMemoryPaginator, RerankPaginator, RetrievePaginator
+
+Client = AgentsforBedrockRuntimeClient
+
+__all__ = (
+    "AgentsforBedrockRuntimeClient",
+    "Client",
+    "GetAgentMemoryPaginator",
+    "RerankPaginator",
+    "RetrievePaginator",
+)
