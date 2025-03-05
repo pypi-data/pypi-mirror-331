050ce01dab408b993f2e6bbc9453df94f45c99df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:48 +0000

    Add package depoc/0.1.3a1/depoc-0.1.3a1.tar.gz

---
+++ packages/depoc/depoc-0.1.3a1.tar.gz/depoc-0.1.3a1/depoc/cli.py
@@ -0,0 +1,27 @@
+import click
+
+from .commands import account
+from .commands import login
+from .commands import logout
+from .commands import me
+from .commands import finance
+from .commands import contact
+from .commands import receivable
+from .commands import payable
+from .commands import report
+
+@click.group()
+def main() -> None:
+   pass
+ 
+main.add_command(account)
+main.add_command(login)
+main.add_command(logout)
+main.add_command(me)
+main.add_command(finance.bank)
+main.add_command(finance.category)
+main.add_command(finance.transaction)
+main.add_command(contact)
+main.add_command(receivable)
+main.add_command(payable)
+main.add_command(report)
