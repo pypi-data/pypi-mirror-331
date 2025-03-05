ceff6da456e25b68d881f9c6ee9dcdc38355a278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 07:06:23 +0000

    Add package bergcastro-image-processing/0.1.1/bergcastro_image_processing-0.1.1.tar.gz

---
+++ packages/bergcastro-image-processing/bergcastro_image_processing-0.1.1.tar.gz/bergcastro_image_processing-0.1.1/README.md
@@ -0,0 +1,27 @@
+# package_name
+
+Description. 
+The package package_name is used to:
+	- transformar imagens
+	- combinar imagens
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install bergcastro-image-processing
+```
+
+## Usage
+
+```python
+from image_processing.processing import combination
+combination.find_difference()
+```
+
+## Author
+Lindemberg Castro
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/bergcastro-image-processing/bergcastro_image_processing-0.1.1.tar.gz/bergcastro_image_processing-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="bergcastro_image_processing",  
+    version="0.1.1", 
+    author="Lindemberg Castro",  
+    author_email="lindembergncastro@outlook.com", 
+    description="Pacote de processamento de imagens", 
+    long_description=long_description,  
+    long_description_content_type="text/markdown",  
+    url="https://github.com/BergCastro/image-processing-package",  
+    packages=find_packages(),  
+    install_requires=requirements,
+    classifiers=[  
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',  
+)
