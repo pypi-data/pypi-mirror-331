1fea2c3021686f9bbc26f75a9bc2b0abba833a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:04:55 +0000

    Add package clang-repl-kernel/1.3.18/clang_repl_kernel-1.3.18.tar.gz

---
+++ packages/clang-repl-kernel/clang_repl_kernel-1.3.18.tar.gz/clang_repl_kernel-1.3.18/clang_repl_kernel/__init__.py
@@ -0,0 +1,7 @@
+"""An example Jupyter kernel"""
+
+__version__ = '1.3.18'
+
+from .downloader import list, download, get_dll_or_download, is_done
+from .kernel import ClangReplKernel, PlatformPath, ClangReplConfig, find_prog,WinShell, BashShell, Shell
+from .install import install_bundles
