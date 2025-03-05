082de008ca0348010e34fe33370b395247966bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:15 +0000

    Add package bedrocked/0.1.1/bedrocked-0.1.1-py3-none-any.whl

---
+++ packages/bedrocked/bedrocked-0.1.1-py3-none-any.whl/bedrocked/reporting/reported.py
@@ -0,0 +1,5 @@
+from enum import Enum, auto
+from bedrocked.reporting.logged import logger
+
+class ReportTypes(Enum):
+    logger.error("NotYetImplementedYet: 'Reporter' features are unavailable.")
