05f2bb3d48e755b1fde1645efcee410a6614b1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 07:06:20 +0000

    Add package tacoq/0.2.0/tacoq-0.2.0.tar.gz

---
+++ packages/tacoq/tacoq-0.2.0.tar.gz/tacoq-0.2.0/src/worker/__init__.py
@@ -0,0 +1,4 @@
+from worker.client import WorkerApplication, TaskNotRegisteredError
+from worker.config import WorkerApplicationConfig
+
+__all__ = ["WorkerApplication", "WorkerApplicationConfig", "TaskNotRegisteredError"]
