ef86e5fed2f0676f798e01d429d966da5fd6484f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 04:04:46 +0000

    Add package middleware-io/2.1.2rc4/middleware_io-2.1.2rc4-py3-none-any.whl

---
+++ packages/middleware-io/middleware_io-2.1.2rc4-py3-none-any.whl/middleware/__init__.py
@@ -0,0 +1,60 @@
+from middleware.distro import mw_tracker, record_exception
+from typing import Collection
+import sys
+import threading
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+from opentelemetry import trace
+from typing import Optional
+from middleware.options import (
+    MWOptions,
+    DETECT_AWS_BEANSTALK,
+    DETECT_AWS_EC2,
+    DETECT_AWS_ECS,
+    DETECT_AWS_EKS,
+    DETECT_AWS_LAMBDA,
+    DETECT_AZURE_APP_SERVICE,
+    DETECT_AZURE_FUNCTIONS,
+    DETECT_AZURE_VM,
+    DETECT_GCP,
+    DETECT_ENVVARS,
+)
+
+__all__ = [
+    "mw_tracker",
+    "record_exception",
+    "MWOptions",
+    "DETECT_AWS_BEANSTALK",
+    "DETECT_AWS_EC2",
+    "DETECT_AWS_ECS",
+    "DETECT_AWS_EKS",
+    "DETECT_AWS_LAMBDA",
+    "DETECT_AZURE_APP_SERVICE",
+    "DETECT_AZURE_FUNCTIONS",
+    "DETECT_AZURE_VM",
+    "DETECT_GCP",
+    "DETECT_ENVVARS",
+]
+
+tracer = trace.get_tracer(__name__)
+
+# Hook for threading exceptions (Python 3.8+)
+def thread_excepthook(args):
+    record_exception(args.exc_type, args.exc_value, args.exc_traceback)
+
+class ExceptionInstrumentor(BaseInstrumentor):
+    def instrumentation_dependencies(self) -> Collection[str]:
+        """Return dependencies if this instrumentor requires any."""
+        return []
+    
+    def _instrument(self, **kwargs):
+        """Automatically sets sys.excepthook when the instrumentor is loaded."""
+        sys.excepthook = record_exception
+        threading.excepthook = thread_excepthook
+
+    def _uninstrument(self, **kwargs):
+        """Restores default sys.excepthook if needed."""
+        sys.excepthook = sys.__excepthook__
+
+# Load the instrumentor
+ExceptionInstrumentor().instrument()
+
