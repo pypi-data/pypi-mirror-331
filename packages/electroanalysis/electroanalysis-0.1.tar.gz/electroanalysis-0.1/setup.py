a623ee2302996eda8ef651f1547bc631524b42f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:05:43 +0000

    Add package electroanalysis/0.1/electroanalysis-0.1.tar.gz

---
+++ packages/electroanalysis/electroanalysis-0.1.tar.gz/electroanalysis-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="electroanalysis",
+    version="0.1",
+    packages = find_packages(),
+    install_requires=["numpy",
+        "opencv-python",
+        "pandas",
+        "scipy"],
+    author="Egwer David Sierra Jaraba",
+    description="Un paquete para analizar bandas de electroforesis",
+    #long_description=open("README.md").read(),
+    #long_description_content_type="text/markdown",
+    url="https://github.com/esierraj/electrophoresis_analysis",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+        ],
+)
