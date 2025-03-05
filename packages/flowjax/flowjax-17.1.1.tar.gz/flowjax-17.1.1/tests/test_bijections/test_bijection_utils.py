2a1f70698575ec76ed6b31a4653035fe831bea5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:24 +0000

    Add package flowjax/17.1.1/flowjax-17.1.1.tar.gz

---
+++ packages/flowjax/flowjax-17.1.1.tar.gz/flowjax-17.1.1/tests/test_bijections/test_bijection_utils.py
@@ -0,0 +1,34 @@
+import jax.numpy as jnp
+import pytest
+from equinox import EquinoxRuntimeError
+
+from flowjax.bijections import Affine, Indexed, Permute
+
+test_cases = {
+    # name: idx, expected
+    "int": (1, jnp.array([False, True, False, False])),
+    "bool_array": (
+        jnp.array([True, False, True, False]),
+        jnp.array([True, False, True, False]),
+    ),
+    "int_arry": (jnp.array([0, 2]), jnp.array([True, False, True, False])),
+}
+
+
+@pytest.mark.parametrize(
+    ("idx", "expected"),
+    test_cases.values(),
+    ids=test_cases.keys(),
+)
+def test_partial(idx, expected):
+    "Check values only change where we expect."
+    x = jnp.zeros(4)
+    shape = x[idx].shape
+    bijection = Indexed(Affine(jnp.ones(shape)), idx, x.shape)
+    y = bijection.transform(x)
+    assert jnp.all((x != y) == expected)
+
+
+def test_Permute_argcheck():
+    with pytest.raises(EquinoxRuntimeError):
+        Permute(jnp.array([0, 0]))
