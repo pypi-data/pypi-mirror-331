10066fdc98fc28f864ae5875dfad42f5209b4d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 05:05:09 +0000

    Add package gamr-backend-api-service/1.0.4/gamr_backend_api_service-1.0.4-py3-none-any.whl

---
+++ packages/gamr-backend-api-service/gamr_backend_api_service-1.0.4-py3-none-any.whl/gamr_backend_api_service/models/image_data.py
@@ -0,0 +1,27 @@
+import base64
+from pathlib import Path
+from typing import Self
+
+import numpy as np
+from pydantic import BaseModel, Field, model_validator
+
+
+ALLOWED_EXTENSIONS = [".png", ".jpg", ".jpeg"]
+
+
+class ImageData(BaseModel):
+    filename: str = Field(min_length=1)
+    image_bytes: bytes
+    model_service: str
+
+    @model_validator(mode="after")
+    def validate_filename(self) -> Self:
+        if Path(self.filename).suffix.lower() not in ALLOWED_EXTENSIONS:
+            raise ValueError("Wrong filename format")
+
+        return self
+
+    @property
+    def list_encoded_image(self) -> list:
+        image_bytes_decoded = base64.b64decode(self.image_bytes)
+        return np.frombuffer(image_bytes_decoded, dtype=np.uint8).tolist()
---
+++ packages/gamr-backend-api-service/gamr_backend_api_service-1.0.4-py3-none-any.whl/gamr_backend_api_service/models/user.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, model_validator
+
+from gamr_backend_api_service.models.errors import UserBlank
+
+
+class User(BaseModel):
+    username: str
+
+    @model_validator(mode="after")
+    def username_not_blank(self):
+        if not self.username:
+            raise UserBlank
+        return self
