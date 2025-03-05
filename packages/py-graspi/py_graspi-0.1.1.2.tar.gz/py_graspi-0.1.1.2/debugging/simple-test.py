27a0c60409bfde12c6682484df540ef8df2b38d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 16:05:13 +0000

    Add package py-graspi/0.1.1.2/py_graspi-0.1.1.2.tar.gz

---
+++ packages/py-graspi/py_graspi-0.1.1.2.tar.gz/py_graspi-0.1.1.2/debugging/simple-test.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath('../src'))
+
+import src.igraph_testing as ig
+import src.descriptors
+
+def main():
+
+    filename = sys.argv[1]
+    # # dimension = sys.argv[2]
+    # graph_type = sys.argv[2]
+    # functionality = sys.argv[3]
+
+    g,is_2D,black_vertices,white_vertices, black_green,black_interface_red, white_interface_blue, dim,interface_edge_comp_paths, shortest_path_to_red, shortest_path_to_blue, CT_n_D_adj_An, CT_n_A_adj_Ca = ig.generateGraph(filename)
+    descript = src.descriptors.descriptors(g, filename, black_vertices, white_vertices, black_green, black_interface_red, white_interface_blue, dim, interface_edge_comp_paths, shortest_path_to_red, shortest_path_to_blue, CT_n_D_adj_An, CT_n_A_adj_Ca)
+    expected = [65536,1634,32713,32823,2,1,1,1,0.4991607666015625,0.42716962675388986,0.23717581433983226,0.7821297429620563,0.870266,1.0,1278,1278,1634,512,512,0.8437318497233516,0.7345154312524754]
+    i = 0
+
+    for d in descript:
+        if descript[d] != expected[i]:
+            print(f"The computed descriptors was not what was expected. Failed on Discriptor: {d} Expected: {descript[d]} Computed: {expected[i]} :( ")
+            return
+        i += 1
+
+    print(f"All the computed descriptors are the same as expected values :) ")
+    
+    
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ packages/py-graspi/py_graspi-0.1.1.2.tar.gz/py_graspi-0.1.1.2/tests/descriptor_testing.py
@@ -0,0 +1,20 @@
+# from py_graspi import igraph_testing as ig
+# from py_graspi import descriptors as d
+import os
+import sys
+
+sys.path.append(os.path.abspath('../src'))
+import src.igraph_testing as ig
+import src.descriptors as d
+
+
+(g, is_2D, black_vertices, white_vertices, black_green, black_interface_red, white_interface_blue,
+             dim, interface_edge_comp_paths, shortest_path_to_red, shortest_path_to_blue,
+             CT_n_D_adj_An, CT_n_A_adj_Ca) = ig.generateGraphAdj(sys.argv[1])
+
+dic = d.descriptors(g, sys.argv[1], black_vertices, white_vertices, black_green, black_interface_red, white_interface_blue,
+                dim, interface_edge_comp_paths, shortest_path_to_red, shortest_path_to_blue,
+                CT_n_D_adj_An, CT_n_A_adj_Ca)
+
+for key, value in dic.items():
+    print(key, value)
