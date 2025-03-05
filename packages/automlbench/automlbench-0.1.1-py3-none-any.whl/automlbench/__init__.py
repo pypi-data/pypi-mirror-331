6114692784b78ca46daaa08501fd8dd41fb8d3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 07:06:22 +0000

    Add package automlbench/0.1.1/automlbench-0.1.1-py3-none-any.whl

---
+++ packages/automlbench/automlbench-0.1.1-py3-none-any.whl/automlbench/__init__.py
@@ -0,0 +1,32 @@
+### __init__.py
+from .data_loader import load_data
+from .preprocessing import preprocess_data
+from .models import get_models, get_hyperparameter_grids
+from .evaluation import evaluate_model
+from .visualization import plot_performance
+from .hyperparameter_tuning import tune_hyperparameters
+from .utils import time_execution, log_message
+
+import os
+import warnings
+
+def suppress_warnings(suppress=True):
+    if suppress:
+        warnings.filterwarnings("ignore")
+        os.environ["PYTHONWARNINGS"] = "ignore"
+    else:
+        warnings.resetwarnings()
+        os.environ.pop("PYTHONWARNINGS", None)
+
+__all__ = [
+    "load_data",
+    "preprocess_data",
+    "get_models",
+    "get_hyperparameter_grids",
+    "evaluate_model",
+    "plot_performance",
+    "tune_hyperparameters",
+    "time_execution",
+    "log_message",
+    "suppress_warnings"
+]
---
+++ packages/automlbench/automlbench-0.1.1-py3-none-any.whl/automlbench/utils.py
@@ -0,0 +1,14 @@
+### utils.py
+import time
+
+def time_execution(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time for {func.__name__}: {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
+
+def log_message(message):
+    print(f"[AutoMLBench]: {message}")
