49a97cbc33d736ff110609db0dcdf6c7de1fe513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 05:04:26 +0000

    Add package uipath_sdk/0.0.66/uipath_sdk-0.0.66.tar.gz

---
+++ packages/uipath_sdk/uipath_sdk-0.0.66.tar.gz/uipath_sdk-0.0.66/uipath_sdk/_execution_context.py
@@ -0,0 +1,35 @@
+from os import environ as env
+from typing import Optional
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class ExecutionContext:
+    def __init__(self) -> None:
+        try:
+            self._instance_id: Optional[str] = env["UIPATH_JOB_KEY"]
+        except KeyError:
+            self._instance_id = None
+
+        try:
+            self._robot_key: Optional[str] = env["UIPATH_ROBOT_KEY"]
+        except KeyError:
+            self._robot_key = None
+
+        super().__init__()
+
+    @property
+    def instance_id(self) -> Optional[str]:
+        if self._instance_id is None:
+            raise ValueError("Instance ID is not set (UIPATH_JOB_KEY)")
+
+        return self._instance_id
+
+    @property
+    def robot_key(self) -> Optional[str]:
+        if self._robot_key is None:
+            raise ValueError("Robot key is not set (UIPATH_ROBOT_KEY)")
+
+        return self._robot_key
