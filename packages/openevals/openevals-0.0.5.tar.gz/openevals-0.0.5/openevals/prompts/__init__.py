ad2aef898b2cd75cb379e3b60fe6e47e3ca326a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 14:04:48 +0000

    Add package openevals/0.0.5/openevals-0.0.5.tar.gz

---
+++ packages/openevals/openevals-0.0.5.tar.gz/openevals-0.0.5/openevals/prompts/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "CONCISENESS_PROMPT",
+    "CORRECTNESS_PROMPT",
+    "HALLUCINATION_PROMPT",
+]
+
+from .conciseness import CONCISENESS_PROMPT
+from .correctness import CORRECTNESS_PROMPT
+from .hallucination import HALLUCINATION_PROMPT
