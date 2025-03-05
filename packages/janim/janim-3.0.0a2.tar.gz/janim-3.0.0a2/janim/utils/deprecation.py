0af1bde27364ba4385f98962bfa9bb4e8ee020a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 15:05:00 +0000

    Add package janim/3.0.0a2/janim-3.0.0a2.tar.gz

---
+++ packages/janim/janim-3.0.0a2.tar.gz/janim-3.0.0a2/janim/utils/deprecation.py
@@ -0,0 +1,40 @@
+from janim import __version__
+from janim.logger import log
+from janim.locale.i18n import get_local_strings
+
+_ = get_local_strings('deprecation')
+
+type VersionTuple = tuple[int, int]
+
+
+def _get_version_tuple() -> VersionTuple:
+    try:
+        idx = __version__.index('-')
+        public = __version__[:idx]
+    except ValueError:
+        public = __version__
+    dot1 = public.index('.')
+    dot2 = public.index('.', dot1 + 1)
+    return (
+        int(public[:dot1]),
+        int(public[dot1 + 1: dot2])
+    )
+
+
+_version_tuple = _get_version_tuple()
+
+_DEPRECATED_MSG = _('{name!r} is deprecated and will be removed in JAnim {remove}')
+_DEPRECATED_MSG_I = _('{name!r} is deprecated and will be removed in JAnim {remove}, use {instead!r} instead')
+
+
+def deprecated(name, instead=None, *, remove: tuple[int, int]):
+    remove_formatted = f"{remove[0]}.{remove[1]}"
+    if (_version_tuple[0] > remove[0]) or (_version_tuple[0] == remove[0] and _version_tuple[1] >= remove[1]):
+        msg = f'{name!r} has been deprecated and removed since JAnim {remove_formatted}'
+        raise RuntimeError(msg)
+    else:
+        if instead is None:
+            msg = _DEPRECATED_MSG.format(name=name, remove=remove_formatted)
+        else:
+            msg = _DEPRECATED_MSG_I.format(name=name, remove=remove_formatted, instead=instead)
+        log.warning(msg, stacklevel=3)
