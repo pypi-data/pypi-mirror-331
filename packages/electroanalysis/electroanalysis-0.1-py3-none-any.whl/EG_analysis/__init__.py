66150cc566fed171db4cc1e33ab900af13ceea52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:05:43 +0000

    Add package electroanalysis/0.1/electroanalysis-0.1-py3-none-any.whl

---
+++ packages/electroanalysis/electroanalysis-0.1-py3-none-any.whl/EG_analysis/__init__.py
@@ -0,0 +1,11 @@
+# EG_analysis/__init__.py
+from .EG_analysis import open_cv2_image
+from .EG_analysis import assign_mol_wei
+from .EG_analysis import get_exp_ladder
+from .EG_analysis import get_band_info
+from .EG_analysis import get_calib_param
+from .EG_analysis import get_new_name
+from .EG_analysis import verify_correct_gel
+from .EG_analysis import interactive_image
+from .EG_analysis import excel_band_results
+from .EG_analysis import resize_image
