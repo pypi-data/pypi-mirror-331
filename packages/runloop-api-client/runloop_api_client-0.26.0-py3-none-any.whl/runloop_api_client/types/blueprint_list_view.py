7572901f82f8a412633472db354714618f45e59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:06:30 +0000

    Add package runloop-api-client/0.26.0/runloop_api_client-0.26.0-py3-none-any.whl

---
+++ packages/runloop-api-client/runloop_api_client-0.26.0-py3-none-any.whl/runloop_api_client/types/blueprint_list_view.py
@@ -0,0 +1,17 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .._models import BaseModel
+from .blueprint_view import BlueprintView
+
+__all__ = ["BlueprintListView"]
+
+
+class BlueprintListView(BaseModel):
+    blueprints: List[BlueprintView]
+    """List of blueprints matching filter."""
+
+    has_more: bool
+
+    total_count: int
---
+++ packages/runloop-api-client/runloop_api_client-0.26.0-py3-none-any.whl/runloop_api_client/types/devbox_list_view.py
@@ -0,0 +1,17 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .._models import BaseModel
+from .devbox_view import DevboxView
+
+__all__ = ["DevboxListView"]
+
+
+class DevboxListView(BaseModel):
+    devboxes: List[DevboxView]
+    """List of devboxes matching filter."""
+
+    has_more: bool
+
+    total_count: int
