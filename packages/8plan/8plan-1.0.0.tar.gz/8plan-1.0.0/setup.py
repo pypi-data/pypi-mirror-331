e923253e04c6ed720b1b0be31f5905ffb4faccd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 03:12:15 +0000

    Add package 8plan/1.0.0/8plan-1.0.0.tar.gz

---
+++ packages/8plan/8plan-1.0.0.tar.gz/8plan-1.0.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Kutina
+# Mail: wang@kutina.cn
+# Created Time:  2024-3-3
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "8plan",      #这里是pip项目发布的名称
+    version = "1.0.0",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "Kutina"),
+    description = "FOVBPS-Kutina",
+    long_description = "FOVBPSK-Kutin",
+    license = "MIT Licence",
+
+    url = "https://github.com/Cache-Cloud/FOVBPSK-Kutina",
+    author = "Kutina",
+    author_email = "wang@kutina.cn",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["setuptools", "setuptools-scm", "tkinter", "fovbpsk"]
+)
