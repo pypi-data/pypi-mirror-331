2da4f15231ac0bd06b1a7312be838e41df2b13e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:06:01 +0000

    Add package xdsl/0.29.0/xdsl-0.29.0.tar.gz

---
+++ packages/xdsl/xdsl-0.29.0.tar.gz/xdsl-0.29.0/tests/test_pass_to_arg_and_types_str.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass, field
+from typing import Literal
+
+import pytest
+
+from xdsl.context import Context
+from xdsl.dialects import builtin
+from xdsl.passes import ModulePass, get_pass_argument_names_and_types
+
+
+@dataclass(frozen=True)
+class CustomPass(ModulePass):
+    name = "custom-pass"
+
+    number: int | float
+
+    single_number: int
+
+    int_list: tuple[int, ...]
+
+    non_init_thing: int = field(init=False)
+
+    str_thing: str
+
+    nullable_str: str | None
+
+    literal: Literal["yes", "no", "maybe"] = "no"
+
+    optional_bool: bool = False
+
+    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        pass
+
+
+@dataclass(frozen=True)
+class EmptyPass(ModulePass):
+    name = "empty"
+
+    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        pass
+
+
+@dataclass(frozen=True)
+class SimplePass(ModulePass):
+    name = "simple"
+
+    a: int | float
+    b: int | None
+    c: int = 5
+
+    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        pass
+
+
+@pytest.mark.parametrize(
+    "str_arg, pass_arg",
+    [
+        (
+            "number=int|float single_number=int int_list=tuple[int, ...] "
+            "non_init_thing=int str_thing=str nullable_str=str|None literal=no "
+            "optional_bool=false",
+            CustomPass,
+        ),
+        ("", EmptyPass),
+        ("""a=int|float b=int|None c=5""", SimplePass),
+    ],
+)
+def test_pass_to_arg_and_type_str(str_arg: str, pass_arg: type[ModulePass]):
+    assert get_pass_argument_names_and_types(pass_arg) == str_arg
---
+++ packages/xdsl/xdsl-0.29.0.tar.gz/xdsl-0.29.0/xdsl/backend/register_type.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+
+from typing_extensions import Self
+
+from xdsl.dialects.builtin import (
+    IntAttr,
+    NoneAttr,
+    StringAttr,
+)
+from xdsl.ir import (
+    Attribute,
+    ParametrizedAttribute,
+    TypeAttribute,
+)
+from xdsl.irdl import ParameterDef
+from xdsl.parser import AttrParser
+from xdsl.printer import Printer
+
+
+class RegisterType(ParametrizedAttribute, TypeAttribute, ABC):
+    """
+    An abstract register type for target ISA-specific dialects.
+    """
+
+    index: ParameterDef[IntAttr | NoneAttr]
+    spelling: ParameterDef[StringAttr]
+
+    def __init__(self, spelling: str):
+        super().__init__(self._parameters_from_spelling(spelling))
+
+    @classmethod
+    def unallocated(cls) -> Self:
+        """
+        Returns an unallocated register of this type.
+        """
+        return cls("")
+
+    @classmethod
+    def _parameters_from_spelling(
+        cls, spelling: str
+    ) -> tuple[IntAttr | NoneAttr, StringAttr]:
+        """
+        Returns the parameter list required to construct a register instance from the given spelling.
+        """
+        index = cls.abi_index_by_name().get(spelling)
+        index_attr = NoneAttr() if index is None else IntAttr(index)
+        return index_attr, StringAttr(spelling)
+
+    @property
+    def register_name(self) -> str:
+        """Returns name if allocated, raises ValueError if not"""
+        if not self.is_allocated:
+            raise ValueError("Cannot get name for unallocated register")
+        return self.spelling.data
+
+    @property
+    def is_allocated(self) -> bool:
+        """Returns true if the register is allocated, otherwise false"""
+        return bool(self.spelling.data)
+
+    @classmethod
+    def parse_parameters(cls, parser: AttrParser) -> Sequence[Attribute]:
+        if parser.parse_optional_punctuation("<"):
+            name = parser.parse_identifier()
+            parser.parse_punctuation(">")
+        else:
+            name = ""
+        return cls._parameters_from_spelling(name)
+
+    def print_parameters(self, printer: Printer) -> None:
+        if self.spelling.data:
+            with printer.in_angle_brackets():
+                printer.print_string(self.spelling.data)
+
+    def verify(self) -> None:
+        raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def instruction_set_name(cls) -> str:
+        raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def abi_index_by_name(cls) -> dict[str, int]:
+        raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def infinite_register_prefix(cls) -> str:
+        """
+        Provide the prefix for the spelling for a register at the given index in the
+        "infinite" register set.
+        For a prefix `x`, the spelling of the first infinite register will be `x0`.
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def infinite_register(cls, index: int) -> Self:
+        """
+        Provide the register at the given index in the "infinite" register set.
+        """
+        spelling = cls.infinite_register_prefix() + str(index)
+        res = cls(spelling)
+        assert isinstance(res.index, NoneAttr), (
+            f"Invalid 'infinite' register name: {spelling} clashes with finite register set"
+        )
+        return res
