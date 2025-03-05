adb751bcfda693cdd98016b86fadb0bbe2d6d8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:30 +0000

    Add package fedcore/0.0.5.2/fedcore-0.0.5.2-py3-none-any.whl

---
+++ packages/fedcore/fedcore-0.0.5.2-py3-none-any.whl/fedcore/algorithm/pruning/pruners.py
@@ -0,0 +1,256 @@
+from copy import deepcopy
+
+import numpy as np
+from fedot.core.data.data import InputData
+from pymonad.either import Either
+from torch import nn, optim
+
+from fedcore.algorithm.base_compression_model import BaseCompressionModel
+import torch_pruning as tp
+from typing import Optional
+from fedot.core.operations.operation_parameters import OperationParameters
+
+from fedcore.algorithm.pruning.pruning_validation import PruningValidator
+from fedcore.architecture.comptutaional.devices import default_device
+from fedcore.models.network_impl.base_nn_model import BaseNeuralModel
+from fedcore.repository.constanst_repository import (
+    PRUNERS,
+    PRUNING_IMPORTANCE,
+    PRUNER_REQUIRED_REG,
+    PRUNER_WITHOUT_REQUIREMENTS,
+    PRUNING_FUNC,
+    # MANUAL_PRUNING_STRATEGY,
+)
+
+
+class BasePruner(BaseCompressionModel):
+    """Class responsible for Pruning model implementation.
+    Example:
+    """
+
+    def __init__(self, params: Optional[OperationParameters] = {}):
+        super().__init__(params)
+        # finetune params
+        self.epochs = params.get("epochs", 5)
+        self.ft_params = params.get("finetune_params", None)
+        if self.ft_params is None:
+            self.ft_params = {}
+            self.ft_params["custom_loss"] = None
+            self.ft_params["epochs"] = round(self.epochs / 3)
+        self.criterion = params.get("loss", nn.CrossEntropyLoss())
+        self.optimizer = params.get("optimizer", optim.Adam)
+        self.learning_rate = params.get("lr", 0.001)
+
+        # pruning gradients params
+        self.criterion_for_grad = params.get("loss", nn.CrossEntropyLoss())
+        self.learning_rate_for_grad = params.get("lr", 0.001)
+
+        # pruning params
+        self.pruner_name = params.get("pruner_name", "meta_pruner")
+        self.importance_name = params.get("importance", "MagnitudeImportance")
+
+        # pruning hyperparams
+        self.pruning_ratio = params.get("pruning_ratio", 0.5)
+        self.pruning_iterations = params.get("pruning_iterations", 2)
+        self.importance_norm = params.get("importance_norm", 1)
+        self.importance_reduction = params.get("importance_reduction", "mean")
+        self.importance_normalize = params.get("importance_normalize", "mean")
+
+        # importance criterion for parameter selections
+        self.importance = PRUNING_IMPORTANCE[self.importance_name](
+            group_reduction=self.importance_reduction,
+            normalizer=self.importance_normalize,
+        )
+        self.trainer = BaseNeuralModel(params)
+
+    def __repr__(self):
+        return self.pruner_name
+
+    def _init_model(self, input_data):
+        print("==============Prepare original model for pruning=================")
+        self.model = input_data.target
+        self.trainer.model = self.model
+        print(
+            f"==============Initialisation of {self.pruner_name} pruning agent================="
+        )
+        print(
+            f"==============Pruning importance - {self.importance_name} ================="
+        )
+        print(f"==============Pruning ratio -  {self.pruning_ratio} =================")
+        print(
+            f"==============Pruning importance norm -  {self.importance_norm} ================="
+        )
+        # Pruner initialization
+        self.pruner = PRUNERS[self.pruner_name]
+        # list of tensors with dim size n_samples x n_channel x height x width
+        batch_generator = (b for b in input_data.features.calib_dataloader)
+        # take first batch
+        input, target = next(batch_generator)
+        self.data_batch_for_calib = input.to(self.trainer.device)
+        self.validator = PruningValidator(self.model, input_data.features.num_classes)
+        self.ignored_layers = self.validator.filter_ignored_layers(
+            self.model, str(self.model.__class__)
+        )
+        self.channel_groups = self.validator.validate_channel_groups()
+        self.optimizer_for_grad = optim.Adam(
+            self.model.parameters(), lr=self.learning_rate_for_grad
+        )
+
+    def _accumulate_grads(self, data, target, return_false=False):
+        data, target = data.to(default_device()), target.to(default_device())
+        out = self.model(data)
+        loss = self.criterion_for_grad(out, target)
+        loss.backward()
+        if return_false:
+            return loss
+
+    def _define_root_layer(self):
+        root_layer_dict = {
+            "manual_conv": self.optimised_model.conv1,
+        }
+
+        return root_layer_dict[self.pruner_name]
+
+    def _pruner_step_with_grads(self, pruner, input_data):
+        for i, (data, target) in enumerate(input_data.features.calib_dataloader):
+            if i != 0:
+                print(f"Gradients accumulation iter- {i}")
+                print(f"==========================================")
+                # we using 1 batch as example of pruning quality
+                self._accumulate_grads(data, target)
+        return pruner
+
+    def _pruner_step_with_reg(self, pruner, input_data):
+        pruner.update_regularizer()
+        # <== initialize regularizer
+        for i, (data, target) in enumerate(input_data.features.calib_dataloader):
+            if i != 0:
+                print(f"Pruning reg iter- {i}")
+                print(f"==========================================")
+                # we using 1 batch as example of pruning quality
+                self.optimizer_for_grad.zero_grad()
+                loss = self._accumulate_grads(
+                    data, target, True
+                )  # after loss.backward()
+                pruner.regularize(self.optimised_model, loss)  # <== for sparse training
+                self.optimizer_for_grad.step()
+        return pruner
+
+    def _dependency_graph_pruner(self):
+        DG = tp.DependencyGraph().build_dependency(
+            self.optimised_model,
+            example_inputs=self.data_batch_for_calib,
+            ignored_layers=self.ignored_layers,
+        )
+        group = DG.get_pruning_group(
+            self.optimised_model.conv1, tp.prune_conv_out_channels, idxs=[2, 6, 9]
+        )
+
+        print(group.details())
+
+        if DG.check_pruning_group(group):  # avoid over-pruning, i.e., channels=0.
+            group.prune()
+
+    def pruner_step(self, pruner: callable, input_data):
+
+        pruner_without_grads = isinstance(
+            self.importance, tuple(PRUNER_WITHOUT_REQUIREMENTS.values())
+        )
+        pruner_with_reg = isinstance(
+            self.importance, tuple(PRUNER_REQUIRED_REG.values())
+        )
+        pruner_with_grads = not all([pruner_with_reg, pruner_without_grads])
+
+        pruner = Either(value=pruner, monoid=[input_data, pruner_without_grads]).either(
+            left_function=lambda data: (
+                self._pruner_step_with_grads(pruner, data)
+                if pruner_with_grads
+                else self._pruner_step_with_reg(pruner, data)
+            ),
+            right_function=lambda pruner_agent: pruner_agent,
+        )
+        return pruner
+
+    def fit(self, input_data: InputData):
+        self._init_model(input_data)
+        if input_data.target.training:
+            self.model = input_data.target
+        else:
+            self.model = self.trainer.fit(input_data)
+        self.optimised_model = deepcopy(self.model)
+        self.compress(input_data=input_data)
+        return self.optimised_model
+
+    def _manual_pruning_iter(self, pruner):
+        for i in range(self.pruning_iterations):
+            for pruning_func in MANUAL_PRUNING_STRATEGY[self.pruner_name]:
+                root_layer = self._define_root_layer()
+                pruner.manual_prune(
+                    root_layer, PRUNING_FUNC[pruning_func], self.pruning_ratio
+                )
+
+    def _default_pruning_iter(self, pruner):
+        pruning_hist = []
+        for i in range(self.pruning_iterations):
+            for group in pruner.step(interactive=True):
+                dep, idxs = group[0]
+                layer = dep.layer
+                pruning_fn = dep.pruning_fn
+                valid_to_prune = self.validator.validate_pruned_layers(
+                    layer, pruning_fn
+                )
+                if valid_to_prune:
+                    pruning_hist.append((layer, idxs, pruning_fn))
+                    group.prune()
+                else:
+                    continue
+
+    def compress(self, input_data: InputData) -> np.array:
+        self.pruner = self.pruner(
+            self.optimised_model,
+            self.data_batch_for_calib,
+            # global_pruning=False,  # If False, a uniform ratio will be assigned to different layers.
+            importance=self.importance,  # importance criterion for parameter selection
+            iterative_steps=self.pruning_iterations,  # the number of iterations to achieve target ratio
+            pruning_ratio=self.pruning_ratio,
+            ignored_layers=self.ignored_layers,
+            channel_groups=self.channel_groups,
+            round_to=None,
+            unwrapped_parameters=None,
+        )
+
+        base_macs, base_nparams = tp.utils.count_ops_and_params(
+            self.model, self.data_batch_for_calib
+        )
+        manual_pruning = self.pruner_name.__contains__("manual")
+
+        self.pruner = self.pruner_step(self.pruner, input_data)
+        self.pruner = Either(
+            value=self.pruner, monoid=[self.pruner, manual_pruning]
+        ).either(
+            left_function=lambda pruner_agent: self._default_pruning_iter(pruner_agent),
+            right_function=lambda pruner_agent: self._manual_pruning_iter(pruner_agent),
+        )
+
+        print("==============Finetune pruned model=================")
+        self.trainer.model = self.optimised_model
+        self.trainer.custom_loss = self.ft_params["custom_loss"]
+        self.trainer.epochs = self.ft_params["epochs"]
+        self.optimised_model = self.trainer.fit(input_data)
+
+        print("==============After pruning=================")
+        macs, nparams = tp.utils.count_ops_and_params(
+            self.optimised_model, self.data_batch_for_calib
+        )
+        print("Params: %.2f M => %.2f M" % (base_nparams / 1e6, nparams / 1e6))
+        print("MACs: %.2f G => %.2f G" % (base_macs / 1e9, macs / 1e9))
+        return self.model
+
+    def predict_for_fit(self, input_data: InputData, output_mode: str = "compress"):
+        return self.optimised_model if output_mode == "compress" else self.model
+
+    def predict(self, input_data: InputData, output_mode: str = "compress"):
+        self.trainer.model = (
+            self.optimised_model if output_mode == "compress" else self.model
+        )
+        return self.trainer.predict(input_data, output_mode)
---
+++ packages/fedcore/fedcore-0.0.5.2-py3-none-any.whl/fedcore/tools/ruler.py
@@ -0,0 +1,283 @@
+import time
+
+import numpy as np
+import torch
+import torch.utils
+from fedot.core.pipelines.pipeline import Pipeline
+from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
+
+from fedcore.architecture.comptutaional.devices import default_device
+from fedcore.inference.onnx import ONNXInferenceModel
+from fedcore.metrics.metric_impl import (
+    ClassificationMetricCounter,
+    MetricCounter,
+    ObjectDetectionMetricCounter,
+)
+from functools import partial
+from fedcore.api.utils.data import DataLoaderHandler
+
+
+
+class PerformanceEvaluator:
+    def __init__(
+        self,
+        model,
+        data,
+        device=None,
+        batch_size=32,
+        n_batches=8,
+        collate_fn=None,
+    ):
+        is_class_container = hasattr(model, "model")
+        is_pipeline_class = isinstance(model, Pipeline)
+        dataset_from_directory = isinstance(
+            data, str
+        )  ### where's func for string dataset loading
+        self.model = model.model if is_class_container else model
+        self.model = (
+            model.operator.root_node.fitted_operation.model
+            if is_pipeline_class
+            else model
+        )
+        self.n_batches = n_batches
+        if isinstance(data, DataLoader):
+            collate_fn = data.collate_fn
+            dataset = data.dataset
+        elif dataset_from_directory:
+            pass  # TODO some logic for string dataset downloading
+        else:
+            dataset = data
+        self.data_loader = partial(DataLoaderHandler.check_convert, dataloader=DataLoader(
+            dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+        )
+        self.batch_size = batch_size# or self.data_loader.batch_size
+        self.cuda_allowed =  not getattr(self.model, '_is_quantized', False) and torch.cuda.is_available()
+        self.device = device or default_device('cpu' if not self.cuda_allowed else None)
+        self.cuda_allowed = self.cuda_allowed and self.device.type != 'cpu'
+        self.model.to(self.device)
+        # Measured performance metrics
+        self.latency = None
+        self.throughput = None
+        self.model_size = None
+        self.target_metrics = None
+
+    def eval(self):
+        self.warm_up_cuda()
+        lat, thr = self.measure_latency_throughput(1, self.n_batches)
+        result = dict(latency=lat, throughput=thr, model_size=self.measure_model_size())
+        self.report()
+        return result
+
+    @torch.no_grad()
+    def throughput_eval(self, num_iterations=30):
+        self.model.eval()
+        thr_list = []
+        for batch, _ in tqdm(self.data_loader(max_batches=self.n_batches), desc="batches", unit="batch"
+        ):
+            X = (
+                batch.cuda(non_blocking=True)
+                if hasattr(batch, "cuda") and self.cuda_allowed
+                else batch.to(self.device)
+            )
+            batch_size = len(X)
+            if self.cuda_allowed:
+                torch.cuda.synchronize(self.device)
+            tic1 = time.time()
+            for i in range(num_iterations):
+                self.model(X)
+            if self.cuda_allowed:
+                torch.cuda.synchronize(self.device)
+            tic2 = time.time()
+            thr_list.append(num_iterations * batch_size / (tic2 - tic1))
+        return thr_list
+
+    @torch.no_grad()
+    def latency_eval(self, max_samples=None):
+        self.model.eval()
+        lat_list = []
+        for batch, _ in tqdm(self.data_loader(max_batches=max_samples or self.batch_size)):
+            if isinstance(batch, torch.Tensor):
+                for sample in batch:
+                    sample = (
+                        sample.cuda(non_blocking=True)
+                        if hasattr(sample, "cuda") and self.cuda_allowed
+                        else sample.to(self.device)
+                    )
+                    sample_batch = sample[None, ...]
+                    tic1 = time.time()
+                    self.model(sample_batch)
+                    if self.cuda_allowed:
+                        torch.cuda.synchronize()
+                    tic2 = time.time()
+                    lat_list.append((tic2 - tic1))
+            else:
+                tic1 = time.time()
+                self.model(batch.to(self.device))
+                if self.cuda_allowed:
+                    torch.cuda.synchronize()
+                tic2 = time.time()
+                lat_list.append((tic2 - tic1))
+        return lat_list
+
+    def measure_latency_throughput(self, reps: int = 3, batches: int = 10):
+        timings_lat = []
+        timings_thr = []
+        with tqdm(
+            total=reps, desc="Measuring latency and throughput", unit="rep"
+        ) as pbar:
+            for rep in range(reps):
+                timings_thr.append(self.throughput_eval(reps))
+                timings_lat.append(self.latency_eval())
+                pbar.update(1)
+
+        latency = np.array([[np.mean(x), np.std(x)] for x in timings_lat])
+        throughput = np.array([[np.mean(x), np.std(x)] for x in timings_thr])
+        self.latency, self.throughput = np.mean(latency, axis=0), np.mean(
+            throughput, axis=0
+        )
+        return self.latency, self.throughput
+
+    def measure_model_size(self):
+        size_constant = 1024**2
+        if isinstance(self.model, ONNXInferenceModel):
+            size_all_mb = round(self.model.size(), 3) / size_constant
+        else:
+            param_size = 0
+            for param in self.model.parameters():
+                param_size += param.nelement() * param.element_size()
+            buffer_size = 0
+            for buffer in self.model.buffers():
+                buffer_size += buffer.nelement() * buffer.element_size()
+
+            size_all_mb = (param_size + buffer_size) / size_constant
+        self.model_size = round(size_all_mb, 3)
+        return self.model_size
+
+    @torch.no_grad()
+    def warm_up_cuda(self, n_batches=3):
+        """Warm up CUDA by performing some dummy computations"""
+        if self.cuda_allowed:
+            for inputs, _ in tqdm(self.data_loader(max_batches=n_batches), desc="warming"):
+                _ = self.model(inputs.to(self.model.device))
+
+    def report(self):
+        print(f"Latency: {self.latency} ms/sample with batch_size {self.batch_size}")
+        print(
+            f"Throughput: {self.throughput} samples/s with batch_size {self.batch_size}"
+        )
+        print(f"Model size: {self.model_size} MB")
+
+class PerformanceEvaluatorOD:
+    def __init__(self, model, data_loader, device=None, batch_size=32):
+        self.model = model.model if hasattr(model, "model") else model
+        self.data_loader = data_loader
+        # self.dataset = dataset
+        self.batch_size = batch_size
+        # self.data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+        self.device = default_device() if not device else device
+        self.model.to(self.device)
+
+        # Measured performance metrics
+        self.latency = None
+        self.throughput = None
+        self.model_size = None
+        self.target_metrics = None
+
+    def eval(self):
+
+        result = dict(
+            latency=self.measure_latency(),
+            throughput=self.measure_throughput(),
+            model_size=self.measure_model_size(),
+            target_metrics=self.measure_target_metric(),
+        )
+        self.report()
+        return result
+
+    def measure_latency(self, reps: int = 50):
+        timings = np.zeros((reps, 1))
+        if torch.cuda.is_available():
+            self.warm_up_cuda()
+        with torch.no_grad():
+            with tqdm(total=reps, desc="Measuring latency", unit="rep") as pbar:
+                for rep in range(reps):
+                    for inputs, _ in self.data_loader:
+                        start_time = time.time()
+                        _ = self.model(list(input.to(self.device) for input in inputs))
+                        end_time = time.time()
+                        if torch.cuda.is_available():
+                            torch.cuda.synchronize()
+                        curr_time = (end_time - start_time) * 1000
+                        timings[rep] = curr_time / inputs[0].size(0)
+                        break
+                    pbar.update(1)
+        self.latency = round(np.mean(timings) / reps, 5)
+        return self.latency
+
+    def measure_throughput(self, batches: int = 5):
+        total_data_size = 0
+        start_time = time.time()
+        # measure for n batches
+        with torch.no_grad():
+            with tqdm(total=batches, desc="Measuring throughput", unit="batch") as pbar:
+                for inputs, _ in self.data_loader:
+                    inputs = list(input.to(self.device) for input in inputs)
+                    if batches == 0:
+                        break
+                    total_data_size += inputs[0].size(0)
+                    _ = self.model(inputs)
+                    batches -= 1
+                    pbar.update(1)
+        if self.device == "cuda":
+            torch.cuda.synchronize()
+        total_time = (time.time() - start_time) / 1000
+        self.throughput = round(total_data_size / total_time, 0)
+        return self.throughput
+
+    def measure_target_metric(self, metric_counter: MetricCounter = None):
+        if not metric_counter:
+            metric_counter = ObjectDetectionMetricCounter()
+        with torch.no_grad():
+            with tqdm(desc="Measuring target metric", unit="batch") as pbar:
+                for batch in self.data_loader:
+                    inputs, targets = batch
+                    inputs = list(input.to(self.device) for input in inputs)
+                    prediction = self.model(inputs)
+                    metric_counter.update(prediction, targets)
+                    pbar.update(1)
+        if self.device == "cuda":
+            torch.cuda.synchronize()
+        self.target_metrics = metric_counter.compute()
+        return self.target_metrics
+
+    def measure_model_size(self):
+        if isinstance(self.model, ONNXInferenceModel):
+            size_all_mb = round(self.model.size(), 3) / 1024**2
+        else:
+            param_size = 0
+            for param in self.model.parameters():
+                param_size += param.nelement() * param.element_size()
+            buffer_size = 0
+            for buffer in self.model.buffers():
+                buffer_size += buffer.nelement() * buffer.element_size()
+
+            size_all_mb = (param_size + buffer_size) / 1024**2
+        self.model_size = round(size_all_mb, 3)
+        return self.model_size
+
+    def warm_up_cuda(self, num_iterations=10):
+        """Warm up CUDA by performing some dummy computations"""
+        if torch.cuda.is_available():
+            for _ in range(num_iterations):
+                inputs, _ = next(iter(self.data_loader))
+                inputs = list(input.to(self.device) for input in inputs)
+                _ = self.model(inputs)
+
+    def report(self):
+        print(f"Latency: {self.latency} ms/sample with batch_size {self.batch_size}")
+        print(
+            f"Throughput: {self.throughput} samples/s with batch_size {self.batch_size}"
+        )
+        print(f"Model size: {self.model_size} MB")
