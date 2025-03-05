e42d03b50c7b1b57c04284b37c672ee5c2dedf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 14:04:37 +0000

    Add package livekit-agents/1.0.0.dev1/livekit_agents-1.0.0.dev1-py3-none-any.whl

---
+++ packages/livekit-agents/livekit_agents-1.0.0.dev1-py3-none-any.whl/livekit/agents/llm/chat_context.py
@@ -0,0 +1,169 @@
+# Copyright 2023 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from typing import (
+    Any,
+    Literal,
+    Optional,
+    Union,
+)
+
+from livekit import rtc
+from livekit.agents.types import NOT_GIVEN, NotGivenOr
+from livekit.agents.utils.misc import is_given
+from pydantic import BaseModel, Field, PrivateAttr
+from typing_extensions import TypeAlias
+
+from .. import utils
+
+
+class ImageContent(BaseModel):
+    """
+    ChatImage is used to input images into the ChatContext on supported LLM providers / plugins.
+
+    You may need to consult your LLM provider's documentation on supported URL types.
+
+    ```python
+    # Pass a VideoFrame directly, which will be automatically converted to a JPEG data URL internally
+    async for event in rtc.VideoStream(video_track):
+        chat_image = ChatImage(image=event.frame)
+        # this instance is now available for your ChatContext
+
+    # Encode your VideoFrame yourself for more control, and pass the result as a data URL (see EncodeOptions for more details)
+    from livekit.agents.utils.images import encode, EncodeOptions, ResizeOptions
+
+    image_bytes = encode(
+        event.frame,
+        EncodeOptions(
+            format="PNG",
+            resize_options=ResizeOptions(width=512, height=512, strategy="scale_aspect_fit"),
+        ),
+    )
+    chat_image = ChatImage(
+        image=f"data:image/png;base64,{base64.b64encode(image_bytes).decode('utf-8')}"
+    )
+
+    # With an external URL
+    chat_image = ChatImage(image="https://example.com/image.jpg")
+    ```
+    """
+
+    type: Literal["image_content"] = Field(default="image_content")
+
+    image: Union[str, rtc.VideoFrame]
+    """
+    Either a string URL or a VideoFrame object
+    """
+    inference_width: Optional[int] = None
+    """
+    Resizing parameter for rtc.VideoFrame inputs (ignored for URL images)
+    """
+    inference_height: Optional[int] = None
+    """
+    Resizing parameter for rtc.VideoFrame inputs (ignored for URL images)
+    """
+    inference_detail: Literal["auto", "high", "low"] = "auto"
+    """
+    Detail parameter for LLM provider, if supported.
+    
+    Currently only supported by OpenAI (see https://platform.openai.com/docs/guides/vision?lang=node#low-or-high-fidelity-image-understanding)
+    """
+    _cache: dict[int, Any] = PrivateAttr(default_factory=dict)
+
+
+class AudioContent(BaseModel):
+    type: Literal["audio_content"] = Field(default="audio_content")
+    frame: list[rtc.AudioFrame]
+    transcript: Optional[str] = None
+
+
+ChatRole: TypeAlias = Literal["developer", "system", "user", "assistant"]
+
+
+class ChatMessage(BaseModel):
+    id: str = Field(default_factory=lambda: utils.shortuuid("item_"))
+    type: Literal["message"] = "message"
+    role: ChatRole
+    content: list[ChatContent]
+    interrupted: bool = False
+    hash: Optional[bytes] = None
+
+
+ChatContent: TypeAlias = Union[str, ImageContent, AudioContent]
+
+
+class FunctionCall(BaseModel):
+    id: str = Field(default_factory=lambda: utils.shortuuid("item_"))
+    type: Literal["function_call"] = "function_call"
+    call_id: str
+    arguments: str
+    name: str
+
+
+class FunctionCallOutput(BaseModel):
+    id: str = Field(default_factory=lambda: utils.shortuuid("item_"))
+    type: Literal["function_call_output"] = Field(default="function_call_output")
+    call_id: str
+    output: str
+    is_error: bool
+
+
+ChatItem: TypeAlias = Union[ChatMessage, FunctionCall, FunctionCallOutput]
+
+
+class ChatContext:
+    def __init__(self, items: list[ChatItem]):
+        self._items: list[ChatItem] = items
+
+    @classmethod
+    def empty(cls) -> "ChatContext":
+        return cls([])
+
+    @property
+    def items(self) -> list[ChatItem]:
+        return self._items
+
+    def add_message(
+        self,
+        *,
+        role: ChatRole,
+        content: list[ChatContent] | str,
+        id: NotGivenOr[str] = NOT_GIVEN,
+    ) -> ChatMessage:
+        kwargs = {}
+        if is_given(id):
+            kwargs["id"] = id
+
+        if isinstance(content, str):
+            message = ChatMessage(role=role, content=[content], **kwargs)
+        else:
+            message = ChatMessage(role=role, content=content, **kwargs)
+
+        self._items.append(message)
+        return message
+
+    def get_by_id(self, item_id: str) -> ChatItem | None:
+        return next((item for item in self.items if item.id == item_id), None)
+
+    def copy(self) -> "ChatContext":
+        return ChatContext(self.items.copy())
+
+    def to_dict(self) -> dict:
+        raise NotImplementedError
+
+    @classmethod
+    def from_dict(cls, _: dict) -> "ChatContext":
+        raise NotImplementedError
---
+++ packages/livekit-agents/livekit_agents-1.0.0.dev1-py3-none-any.whl/livekit/agents/llm/llm.py
@@ -0,0 +1,232 @@
+from __future__ import annotations
+
+import asyncio
+import time
+from abc import ABC, abstractmethod
+from types import TracebackType
+from typing import (
+    Any,
+    AsyncIterable,
+    AsyncIterator,
+    Generic,
+    Literal,
+    Optional,
+    TypedDict,
+    TypeVar,
+    Union,
+)
+
+from livekit import rtc
+from livekit.agents._exceptions import APIConnectionError, APIError
+from pydantic import BaseModel, Field
+from typing_extensions import Required
+
+from .. import utils
+from ..log import logger
+from ..metrics import LLMMetrics
+from ..types import (
+    DEFAULT_API_CONNECT_OPTIONS,
+    NOT_GIVEN,
+    APIConnectOptions,
+    NotGivenOr,
+)
+from ..utils import aio
+from .chat_context import ChatContext, ChatRole
+from .function_context import AIFunction
+
+
+class CompletionUsage(BaseModel):
+    completion_tokens: int
+    prompt_tokens: int
+    total_tokens: int
+
+
+class FunctionToolCall(BaseModel):
+    type: Literal["function"] = "function"
+    name: str
+    arguments: str
+    call_id: str
+
+
+class ChoiceDelta(BaseModel):
+    role: Optional[ChatRole] = None
+    content: Optional[str] = None
+    tool_calls: list[FunctionToolCall] = Field(default_factory=list)
+
+
+class ChatChunk(BaseModel):
+    id: str
+    delta: Optional[ChoiceDelta] = None
+    usage: Optional[CompletionUsage] = None
+
+
+# Used by ToolChoice
+class Function(TypedDict, total=False):
+    name: Required[str]
+
+
+class ToolChoice(TypedDict, total=False):
+    type: Required[Literal["function"]]
+    function: Required[Function]
+
+
+TEvent = TypeVar("TEvent")
+
+
+class LLM(
+    ABC,
+    rtc.EventEmitter[Union[Literal["metrics_collected"], TEvent]],
+    Generic[TEvent],
+):
+    def __init__(self) -> None:
+        super().__init__()
+        self._label = f"{type(self).__module__}.{type(self).__name__}"
+
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @abstractmethod
+    def chat(
+        self,
+        *,
+        chat_ctx: ChatContext,
+        fnc_ctx: list[AIFunction] | None = None,
+        conn_options: APIConnectOptions = DEFAULT_API_CONNECT_OPTIONS,
+        parallel_tool_calls: NotGivenOr[bool] = NOT_GIVEN,
+        tool_choice: NotGivenOr[Union[ToolChoice, Literal["auto", "required", "none"]]] = NOT_GIVEN,
+        extra_kwargs: NotGivenOr[dict[str, Any]] = NOT_GIVEN,
+    ) -> "LLMStream": ...
+
+    async def aclose(self) -> None: ...
+
+    async def __aenter__(self) -> LLM:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        await self.aclose()
+
+
+class LLMStream(ABC):
+    def __init__(
+        self,
+        llm: LLM,
+        *,
+        chat_ctx: ChatContext,
+        fnc_ctx: list[AIFunction],
+        conn_options: APIConnectOptions,
+    ) -> None:
+        self._llm = llm
+        self._chat_ctx = chat_ctx
+        self._fnc_ctx = fnc_ctx
+        self._conn_options = conn_options
+
+        self._event_ch = aio.Chan[ChatChunk]()
+        self._event_aiter, monitor_aiter = aio.itertools.tee(self._event_ch, 2)
+        self._metrics_task = asyncio.create_task(
+            self._metrics_monitor_task(monitor_aiter), name="LLM._metrics_task"
+        )
+
+        self._task = asyncio.create_task(self._main_task())
+        self._task.add_done_callback(lambda _: self._event_ch.close())
+
+    @abstractmethod
+    async def _run(self) -> None: ...
+
+    async def _main_task(self) -> None:
+        for i in range(self._conn_options.max_retry + 1):
+            try:
+                return await self._run()
+            except APIError as e:
+                if self._conn_options.max_retry == 0 or not e.retryable:
+                    raise
+                elif i == self._conn_options.max_retry:
+                    raise APIConnectionError(
+                        f"failed to generate LLM completion after {self._conn_options.max_retry + 1} attempts",
+                    ) from e
+                else:
+                    logger.warning(
+                        f"failed to generate LLM completion, retrying in {self._conn_options.retry_interval}s",
+                        exc_info=e,
+                        extra={
+                            "llm": self._llm._label,
+                            "attempt": i + 1,
+                        },
+                    )
+
+                await asyncio.sleep(self._conn_options.retry_interval)
+
+    @utils.log_exceptions(logger=logger)
+    async def _metrics_monitor_task(self, event_aiter: AsyncIterable[ChatChunk]) -> None:
+        start_time = time.perf_counter()
+        ttft = -1.0
+        request_id = ""
+        usage: CompletionUsage | None = None
+
+        async for ev in event_aiter:
+            request_id = ev.id
+            if ttft == -1.0:
+                ttft = time.perf_counter() - start_time
+
+            if ev.usage is not None:
+                usage = ev.usage
+
+        duration = time.perf_counter() - start_time
+        metrics = LLMMetrics(
+            timestamp=time.time(),
+            request_id=request_id,
+            ttft=ttft,
+            duration=duration,
+            cancelled=self._task.cancelled(),
+            label=self._llm._label,
+            completion_tokens=usage.completion_tokens if usage else 0,
+            prompt_tokens=usage.prompt_tokens if usage else 0,
+            total_tokens=usage.total_tokens if usage else 0,
+            tokens_per_second=usage.completion_tokens / duration if usage else 0.0,
+            error=None,
+        )
+        self._llm.emit("metrics_collected", metrics)
+
+    @property
+    def chat_ctx(self) -> ChatContext:
+        """The chat context of this stream."""
+        return self._chat_ctx
+
+    @property
+    def fnc_ctx(self) -> list[AIFunction]:
+        """The function context of this stream."""
+        return self._fnc_ctx
+
+    async def aclose(self) -> None:
+        await aio.cancel_and_wait(self._task)
+        await self._metrics_task
+
+    async def __anext__(self) -> ChatChunk:
+        try:
+            val = await self._event_aiter.__anext__()
+        except StopAsyncIteration:
+            if not self._task.cancelled() and (exc := self._task.exception()):
+                raise exc from None
+
+            raise StopAsyncIteration
+
+        return val
+
+    def __aiter__(self) -> AsyncIterator[ChatChunk]:
+        return self
+
+    async def __aenter__(self) -> LLMStream:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        await self.aclose()
