63d4ceecf04f5622c2497c243aec0a25b37ded64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 14:04:27 +0000

    Add package pathling/7.2.0.dev0/pathling-7.2.0.dev0.tar.gz

---
+++ packages/pathling/pathling-7.2.0.dev0.tar.gz/pathling-7.2.0.dev0/pathling/_version.py
@@ -0,0 +1,9 @@
+#
+# Auto generated from POM project version.
+# Please do not modify.
+#
+__version__="7.2.0.dev0"
+__java_version__="7.2.0-SNAPSHOT"
+__scala_version__="2.12"
+__delta_version__="3.3.0"
+__hadoop_version__="3.3.4"
