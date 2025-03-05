0abc0723e39bd83227e7fe1ad5bbd00c80d1a008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 17:05:46 +0000

    Add package encord-agents/0.1.9/encord_agents-0.1.9.tar.gz

---
+++ packages/encord-agents/encord_agents-0.1.9.tar.gz/encord_agents-0.1.9/encord_agents/core/constants.py
@@ -0,0 +1,4 @@
+ENCORD_DOMAIN_REGEX = (
+    r"^https:\/\/(?:(?:cord-ai-development--[\w\d]+-[\w\d]+\.web.app)|(?:(?:dev|staging|app)\.(us\.)?encord\.com))$"
+)
+EDITOR_URL_PARTS_REGEX = r"(?<domain>https:\/\/app.(us\.)?encord.com)\/label_editor\/(?P<projectHash>.*?)\/(?P<dataHash>[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12})(\/(?P<frame>\d+))?\??"
