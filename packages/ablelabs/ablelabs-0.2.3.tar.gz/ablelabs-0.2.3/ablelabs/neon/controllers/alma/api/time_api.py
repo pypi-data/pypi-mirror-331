63c7609372257460f0ee1b5f763c953e82fd9754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 03:11:34 +0000

    Add package ablelabs/0.2.3/ablelabs-0.2.3.tar.gz

---
+++ packages/ablelabs/ablelabs-0.2.3.tar.gz/ablelabs-0.2.3/ablelabs/neon/controllers/alma/api/time_api.py
@@ -0,0 +1,13 @@
+import sys, os
+
+sys.path.append(os.path.abspath(os.curdir))
+from ablelabs.neon.utils.network.messenger import MessengerClient, run_server_func
+from ablelabs.neon.utils.network.tcp_client import TcpClient
+from ablelabs.neon.controllers.alma.api.robot_router import RobotRouter
+from ablelabs.neon.controllers.alma.api.time.upper_module import TimeUpperModuleAPI
+
+
+class TimeAPI(MessengerClient):
+    def __init__(self, tcp_client: TcpClient) -> None:
+        super().__init__(tcp_client)
+        self.upper_module = TimeUpperModuleAPI(tcp_client)
