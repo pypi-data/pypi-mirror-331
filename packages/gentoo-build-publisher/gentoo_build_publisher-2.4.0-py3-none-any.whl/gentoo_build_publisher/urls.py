e50fcf2609d22bc544077aa5c8f972dadfdba393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 03:12:06 +0000

    Add package gentoo-build-publisher/2.4.0/gentoo_build_publisher-2.4.0-py3-none-any.whl

---
+++ packages/gentoo-build-publisher/gentoo_build_publisher-2.4.0-py3-none-any.whl/gentoo_build_publisher/urls.py
@@ -0,0 +1,32 @@
+"""Default urlconf for gentoo_build_publisher"""
+
+from importlib import import_module
+
+from django.urls import URLPattern
+
+from gentoo_build_publisher.plugins import Plugin, get_plugins
+from gentoo_build_publisher.views.utils import ViewFinder
+
+urlpatterns = ViewFinder.find()
+
+
+def plugin_urlpatterns(plugin: Plugin) -> list[URLPattern]:
+    """Return the urlpatterns defined in the given plugin"""
+    if not plugin.urls:
+        return []
+
+    try:
+        module = import_module(plugin.urls)
+    except ImportError:
+        return []
+
+    return getattr(module, "urlpatterns", [])
+
+
+def init() -> None:
+    """Initialize plugin urls"""
+    for plugin in get_plugins():
+        urlpatterns.extend(plugin_urlpatterns(plugin))
+
+
+init()
