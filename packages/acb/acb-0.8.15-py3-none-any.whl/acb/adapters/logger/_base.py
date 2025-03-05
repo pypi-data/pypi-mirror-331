a1bcdbd79d9ccc32913521c4197ee8a648320f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 06:05:32 +0000

    Add package acb/0.8.15/acb-0.8.15-py3-none-any.whl

---
+++ packages/acb/acb-0.8.15-py3-none-any.whl/acb/adapters/logger/_base.py
@@ -0,0 +1,13 @@
+import typing as t
+
+from acb.config import Settings
+
+
+class LoggerBaseSettings(Settings):
+    verbose: bool = False
+    deployed_level: str = "WARNING"
+    log_level: t.Optional[str] = "INFO"
+
+
+class LoggerBase(t.Protocol):
+    async def init(self) -> None: ...
