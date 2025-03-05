dcaff848b797ab4af53466ccb957448e501e0e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 15:04:52 +0000

    Add package pybinlock/0.5.0/pybinlock-0.5.0-py3-none-any.whl

---
+++ packages/pybinlock/pybinlock-0.5.0-py3-none-any.whl/binlock/__init__.py
@@ -0,0 +1,8 @@
+"""
+A lil' library for reading and writing bin lock (.lck) files
+By Michael Jordan <michael@glowingpixel.com>
+https://github.com/mjiggidy/pybinlock
+"""
+
+from . import defaults, exceptions
+from .binlock import BinLock
