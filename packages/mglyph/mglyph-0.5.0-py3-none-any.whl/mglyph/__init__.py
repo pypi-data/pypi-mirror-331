e82e95a5530508722a93311f428b9827fd00f8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-05 03:11:37 +0000

    Add package mglyph/0.5.0/mglyph-0.5.0-py3-none-any.whl

---
+++ packages/mglyph/mglyph-0.5.0-py3-none-any.whl/mglyph/__init__.py
@@ -0,0 +1,11 @@
+'''
+mglyph
+
+The Malleable Glyph librarty.
+'''
+
+__version__ = "1.0.0"
+__author__ = 'Vojtech Bartl, Adam Herout'
+__credits__ = 'FIT BUT'
+
+from .mglyph import Canvas, export, interact, lerp, orbit, show
