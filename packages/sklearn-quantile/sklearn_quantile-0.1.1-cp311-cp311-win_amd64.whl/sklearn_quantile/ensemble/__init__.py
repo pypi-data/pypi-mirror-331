27949b37b30f9bc410602a2fd0ef01e51be019db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 05:06:04 +0000

    Add package sklearn-quantile/0.1.1/sklearn_quantile-0.1.1-cp311-cp311-win_amd64.whl

---
+++ packages/sklearn-quantile/sklearn_quantile-0.1.1-cp311-cp311-win_amd64.whl/sklearn_quantile/ensemble/__init__.py
@@ -0,0 +1,7 @@
+from sklearn_quantile.ensemble.quantile import (
+    RandomForestQuantileRegressor,
+    ExtraTreesQuantileRegressor,
+    SampleRandomForestQuantileRegressor,
+    SampleExtraTreesQuantileRegressor
+)
+from sklearn_quantile.ensemble.maximum import RandomForestMaximumRegressor
