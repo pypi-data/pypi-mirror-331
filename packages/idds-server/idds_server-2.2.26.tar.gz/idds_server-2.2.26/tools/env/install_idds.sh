52c976917eeb69ba83ab46d78a7d9f005a75a64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 07:05:42 +0000

    Add package idds-server/2.2.26/idds_server-2.2.26.tar.gz

---
+++ packages/idds-server/idds_server-2.2.26.tar.gz/idds_server-2.2.26/tools/env/install_idds.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0OA
+#
+# Authors:
+# - Wen Guan, <wen.guan@cern.ch>, 2019
+
+python setup.py install --old-and-unmanageable --force
+
+bash workflow/tools/workflow/make/make.sh
+
+echo cp workflow/bin/run_workflow_wrapper ~/www/wiscgroup/
+cp workflow/bin/run_workflow_wrapper ~/www/wiscgroup/
+echo cp workflow/bin/run_workflow_wrapper /eos/user/w/wguan/www/
+cp workflow/bin/run_workflow_wrapper /eos/user/w/wguan/www/
+
+# echo scp workflow/bin/run_workflow_wrapper root@ai-idds-04:/data/iddssv1/srv/var/trf/user/
+# scp workflow/bin/run_workflow_wrapper root@ai-idds-04:/data/iddssv1/srv/var/trf/user/
+
+rm -fr workflow/bin/run_workflow_wrapper
