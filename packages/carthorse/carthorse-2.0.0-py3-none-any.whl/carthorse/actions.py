4ae78fce27717f193b6fbb014f9e777ca0c9d03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 05:06:23 +0000

    Add package carthorse/2.0.0/carthorse-2.0.0-py3-none-any.whl

---
+++ packages/carthorse/carthorse-2.0.0-py3-none-any.whl/carthorse/actions.py
@@ -0,0 +1,25 @@
+import os
+import re
+from subprocess import check_call
+
+
+def run(command):
+    print('$ '+command)
+    check_call(command, shell=True)
+
+
+def create_tag(remote='origin', update=False):
+    tag = os.environ['TAG']
+    force = '--force ' if update else ''
+    run(f"git tag {force}{tag}")
+    run(f'git push {force}{remote} tag {tag}')
+
+
+def update_major_tag(remote='origin', pattern='v[0-9]+'):
+    env_tag = os.environ['TAG']
+    match = re.match(pattern, env_tag)
+    if match is None:
+        raise ValueError(f"pattern {pattern!r} does not match {env_tag!r}")
+    tag = match.group(0)
+    run(f"git tag --force {tag}")
+    run(f'git push --force {remote} tag {tag}')
