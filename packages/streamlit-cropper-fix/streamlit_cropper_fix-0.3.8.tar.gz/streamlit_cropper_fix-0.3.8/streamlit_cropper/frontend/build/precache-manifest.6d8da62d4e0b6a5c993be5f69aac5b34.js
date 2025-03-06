243c5200fd18a48b0fe5e05b68db658e8bd1b155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 16:07:05 +0000

    Add package streamlit-cropper-fix/0.3.8/streamlit_cropper_fix-0.3.8.tar.gz

---
+++ packages/streamlit-cropper-fix/streamlit_cropper_fix-0.3.8.tar.gz/streamlit_cropper_fix-0.3.8/streamlit_cropper/frontend/build/precache-manifest.6d8da62d4e0b6a5c993be5f69aac5b34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f0949716245ab303fd8b6af6ab5512",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d55cbcdd3775660adc8c",
+    "url": "./static/css/main.1b9fbc2f.chunk.css"
+  },
+  {
+    "revision": "a241b59f5e6fc058b1ca",
+    "url": "./static/js/2.87df79f8.chunk.js"
+  },
+  {
+    "revision": "a120daea67c98f2907b7f8aedcea87dd",
+    "url": "./static/js/2.87df79f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55cbcdd3775660adc8c",
+    "url": "./static/js/main.93656913.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.93656913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
