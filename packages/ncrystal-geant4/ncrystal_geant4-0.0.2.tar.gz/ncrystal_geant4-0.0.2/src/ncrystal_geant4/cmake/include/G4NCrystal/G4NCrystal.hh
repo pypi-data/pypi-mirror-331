053553a60700795a0bb1b28318a63d08373178c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 07:05:29 +0000

    Add package ncrystal-geant4/0.0.2/ncrystal_geant4-0.0.2.tar.gz

---
+++ packages/ncrystal-geant4/ncrystal_geant4-0.0.2.tar.gz/ncrystal_geant4-0.0.2/src/ncrystal_geant4/cmake/include/G4NCrystal/G4NCrystal.hh
@@ -0,0 +1,15 @@
+#ifndef G4NCrystal_hh
+#define G4NCrystal_hh
+
+//////////////////////////////////////////////////////
+// Convenience header for all G4NCrystal interfaces //
+//////////////////////////////////////////////////////
+
+#ifndef G4NCrystal_Install_hh
+#  include "G4NCrystal/G4NCInstall.hh"
+#endif
+#ifndef G4NCrystal_MatHelper_hh
+#  include "G4NCrystal/G4NCMatHelper.hh"
+#endif
+
+#endif
