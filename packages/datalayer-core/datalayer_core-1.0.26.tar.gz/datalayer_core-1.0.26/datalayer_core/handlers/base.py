8bc717f13acc3f68dc74972f77b4b25b2487a3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 16:06:50 +0000

    Add package datalayer-core/1.0.26/datalayer_core-1.0.26.tar.gz

---
+++ packages/datalayer-core/datalayer_core-1.0.26.tar.gz/datalayer_core-1.0.26/datalayer_core/handlers/base.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2023-2024 Datalayer, Inc.
+# Distributed under the terms of the Modified BSD License.
+
+# Copyright (c) Datalayer Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+"""Base handler."""
+
+from jupyter_server.base.handlers import JupyterHandler
+from jupyter_server.extension.handler import ExtensionHandlerMixin, ExtensionHandlerJinjaMixin
+
+
+# pylint: disable=W0223
+class BaseTemplateHandler(ExtensionHandlerJinjaMixin, ExtensionHandlerMixin, JupyterHandler):
+    """The Base handler for the templates."""
+    
