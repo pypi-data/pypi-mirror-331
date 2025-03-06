380f7e8c7527af7adae2db8a2ff9628c750c9dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:05:16 +0000

    Add package superlinked/19.17.1/superlinked-19.17.1-py3-none-any.whl

---
+++ packages/superlinked/superlinked-19.17.1-py3-none-any.whl/superlinked/framework/common/util/math_util.py
@@ -0,0 +1,29 @@
+# Copyright 2025 Superlinked, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from beartype.typing import Any, Sequence
+
+
+class MathUtil:
+    @staticmethod
+    def get_max_signed_sum(values: Sequence[int | float]) -> float:
+        over_0 = sum(val for val in values if val > 0)
+        below_0 = abs(sum(val for val in values if val < 0))
+        return max(below_0, over_0)
+
+    @staticmethod
+    def convert_float_typed_integers_to_int(value: float | Any) -> int | Any:
+        if isinstance(value, float) and value.is_integer():
+            return int(value)
+        return value
