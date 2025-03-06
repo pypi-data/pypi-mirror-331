1fb2c3d11abadd22e105a9d716043756e1c911ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:05:52 +0000

    Add package etformat/1.1.1/etformat-1.1.1-py3-none-any.whl

---
+++ packages/etformat/etformat-1.1.1-py3-none-any.whl/etformat/__init__.py
@@ -0,0 +1,25 @@
+"""
+Welcome to etformat!
+Use it and feel free.
+"""
+
+# Dynamically retrieve version from setuptools-scm
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:
+    __version__ = "dev"  # Fallback for development mode
+
+# Print a message when the package is imported
+print(f"📖 etformat {__version__} - For Documentation, visit: https://ahsankhodami.github.io/etformat/intro.html")
+
+# Importing necessary modules to make them accessible when using `import etformat`
+from .calibration import *
+from .channels import *
+from .describe import *
+from .edfdata import *
+from .edfdata_containers import *
+from .edffile import *
+from .edfinfo import *
+from .export import export  # Ensure export is accessible
+from .plot_gaze import *
+from .plot_saccades import *
