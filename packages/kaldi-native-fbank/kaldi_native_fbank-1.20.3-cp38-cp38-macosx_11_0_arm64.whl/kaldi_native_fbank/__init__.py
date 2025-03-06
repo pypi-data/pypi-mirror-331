8ce38f90aa6b10dc0d27b57dd37967b2eb1d7b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 16:06:42 +0000

    Add package kaldi-native-fbank/1.20.3/kaldi_native_fbank-1.20.3-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/kaldi-native-fbank/kaldi_native_fbank-1.20.3-cp38-cp38-macosx_11_0_arm64.whl/kaldi_native_fbank/__init__.py
@@ -0,0 +1,14 @@
+from _kaldi_native_fbank import (
+    FbankOptions,
+    FeatureWindowFunction,
+    FrameExtractionOptions,
+    MelBanksOptions,
+    MfccOptions,
+    OnlineFbank,
+    OnlineMfcc,
+    OnlineWhisperFbank,
+    Rfft,
+    WhisperFeatureOptions,
+)
+__version__ = '1.20.3'
+__version__ = '1.20.3'
