e8b83afd2d446d1fcd876587faa48e090e7ddb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:07:38 +0000

    Add package osidb-bindings/4.8.0/osidb_bindings-4.8.0-py3-none-any.whl

---
+++ packages/osidb-bindings/osidb_bindings-4.8.0-py3-none-any.whl/osidb_bindings/bindings/python_client/models/flaw_reference_put.py
@@ -0,0 +1,260 @@
+import datetime
+import json
+from typing import TYPE_CHECKING, Any, TypeVar, Union
+from uuid import UUID
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+from dateutil.parser import isoparse
+
+from ..models.flaw_reference_type import FlawReferenceType
+from ..types import UNSET, OSIDBModel, Unset
+
+if TYPE_CHECKING:
+    from ..models.alert import Alert
+
+
+T = TypeVar("T", bound="FlawReferencePut")
+
+
+@_attrs_define
+class FlawReferencePut(OSIDBModel):
+    """FlawReference serializer
+
+    Attributes:
+        url (str):
+        uuid (UUID):
+        embargoed (bool): The embargoed boolean attribute is technically read-only as it just indirectly modifies the
+            ACLs but is mandatory as it controls the access to the resource.
+        alerts (list['Alert']):
+        created_dt (datetime.datetime):
+        updated_dt (datetime.datetime): The updated_dt timestamp attribute is mandatory on update as it is used to
+            detect mit-air collisions.
+        description (Union[Unset, str]):
+        type_ (Union[Unset, FlawReferenceType]):
+    """
+
+    url: str
+    uuid: UUID
+    embargoed: bool
+    alerts: list["Alert"]
+    created_dt: datetime.datetime
+    updated_dt: datetime.datetime
+    description: Union[Unset, str] = UNSET
+    type_: Union[Unset, FlawReferenceType] = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        url = self.url
+
+        uuid: str = UNSET
+        if not isinstance(self.uuid, Unset):
+            uuid = str(self.uuid)
+
+        embargoed = self.embargoed
+
+        alerts: list[dict[str, Any]] = UNSET
+        if not isinstance(self.alerts, Unset):
+            alerts = []
+            for alerts_item_data in self.alerts:
+                alerts_item: dict[str, Any] = UNSET
+                if not isinstance(alerts_item_data, Unset):
+                    alerts_item = alerts_item_data.to_dict()
+
+                alerts.append(alerts_item)
+
+        created_dt: str = UNSET
+        if not isinstance(self.created_dt, Unset):
+            created_dt = self.created_dt.isoformat()
+
+        updated_dt: str = UNSET
+        if not isinstance(self.updated_dt, Unset):
+            updated_dt = self.updated_dt.isoformat()
+
+        description = self.description
+
+        type_: Union[Unset, str] = UNSET
+        if not isinstance(self.type_, Unset):
+            type_ = FlawReferenceType(self.type_).value
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        if not isinstance(url, Unset):
+            field_dict["url"] = url
+        if not isinstance(uuid, Unset):
+            field_dict["uuid"] = uuid
+        if not isinstance(embargoed, Unset):
+            field_dict["embargoed"] = embargoed
+        if not isinstance(alerts, Unset):
+            field_dict["alerts"] = alerts
+        if not isinstance(created_dt, Unset):
+            field_dict["created_dt"] = created_dt
+        if not isinstance(updated_dt, Unset):
+            field_dict["updated_dt"] = updated_dt
+        if not isinstance(description, Unset):
+            field_dict["description"] = description
+        if not isinstance(type_, Unset):
+            field_dict["type"] = type_
+
+        return field_dict
+
+    def to_multipart(self) -> dict[str, Any]:
+        url = (None, str(self.url).encode(), "text/plain")
+
+        uuid: bytes = UNSET
+        if not isinstance(self.uuid, Unset):
+            uuid = str(self.uuid)
+
+        embargoed = (None, str(self.embargoed).encode(), "text/plain")
+
+        alerts: Union[Unset, tuple[None, bytes, str]] = UNSET
+        if not isinstance(self.alerts, Unset):
+            _temp_alerts = []
+            for alerts_item_data in self.alerts:
+                alerts_item: dict[str, Any] = UNSET
+                if not isinstance(alerts_item_data, Unset):
+                    alerts_item = alerts_item_data.to_dict()
+
+                _temp_alerts.append(alerts_item)
+            alerts = (None, json.dumps(_temp_alerts).encode(), "application/json")
+
+        created_dt: bytes = UNSET
+        if not isinstance(self.created_dt, Unset):
+            created_dt = self.created_dt.isoformat().encode()
+
+        updated_dt: bytes = UNSET
+        if not isinstance(self.updated_dt, Unset):
+            updated_dt = self.updated_dt.isoformat().encode()
+
+        description = (
+            self.description
+            if isinstance(self.description, Unset)
+            else (None, str(self.description).encode(), "text/plain")
+        )
+
+        type_: Union[Unset, tuple[None, bytes, str]] = UNSET
+        if not isinstance(self.type_, Unset):
+            type_ = (None, str(self.type_.value).encode(), "text/plain")
+        # CHANGE END (3) #}
+
+        field_dict: dict[str, Any] = {}
+        for prop_name, prop in self.additional_properties.items():
+            field_dict[prop_name] = (None, str(prop).encode(), "text/plain")
+
+        if not isinstance(url, Unset):
+            field_dict["url"] = url
+        if not isinstance(uuid, Unset):
+            field_dict["uuid"] = uuid
+        if not isinstance(embargoed, Unset):
+            field_dict["embargoed"] = embargoed
+        if not isinstance(alerts, Unset):
+            field_dict["alerts"] = alerts
+        if not isinstance(created_dt, Unset):
+            field_dict["created_dt"] = created_dt
+        if not isinstance(updated_dt, Unset):
+            field_dict["updated_dt"] = updated_dt
+        if not isinstance(description, Unset):
+            field_dict["description"] = description
+        if not isinstance(type_, Unset):
+            field_dict["type"] = type_
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        from ..models.alert import Alert
+
+        d = src_dict.copy()
+        url = d.pop("url", UNSET)
+
+        # }
+        _uuid = d.pop("uuid", UNSET)
+        uuid: UUID
+        if isinstance(_uuid, Unset):
+            uuid = UNSET
+        else:
+            uuid = _uuid if isinstance(_uuid, UUID) else UUID(_uuid)
+
+        embargoed = d.pop("embargoed", UNSET)
+
+        alerts = []
+        _alerts = d.pop("alerts", UNSET)
+        for alerts_item_data in _alerts or []:
+            # }
+            _alerts_item = alerts_item_data
+            alerts_item: Alert
+            if isinstance(_alerts_item, Unset):
+                alerts_item = UNSET
+            else:
+                alerts_item = Alert.from_dict(_alerts_item)
+
+            alerts.append(alerts_item)
+
+        # }
+        _created_dt = d.pop("created_dt", UNSET)
+        created_dt: datetime.datetime
+        if isinstance(_created_dt, Unset):
+            created_dt = UNSET
+        else:
+            created_dt = isoparse(_created_dt)
+
+        # }
+        _updated_dt = d.pop("updated_dt", UNSET)
+        updated_dt: datetime.datetime
+        if isinstance(_updated_dt, Unset):
+            updated_dt = UNSET
+        else:
+            updated_dt = isoparse(_updated_dt)
+
+        description = d.pop("description", UNSET)
+
+        # }
+        _type_ = d.pop("type", UNSET)
+        type_: Union[Unset, FlawReferenceType]
+        if isinstance(_type_, Unset):
+            type_ = UNSET
+        else:
+            type_ = FlawReferenceType(_type_)
+
+        flaw_reference_put = cls(
+            url=url,
+            uuid=uuid,
+            embargoed=embargoed,
+            alerts=alerts,
+            created_dt=created_dt,
+            updated_dt=updated_dt,
+            description=description,
+            type_=type_,
+        )
+
+        flaw_reference_put.additional_properties = d
+        return flaw_reference_put
+
+    @staticmethod
+    def get_fields():
+        return {
+            "url": str,
+            "uuid": UUID,
+            "embargoed": bool,
+            "alerts": list["Alert"],
+            "created_dt": datetime.datetime,
+            "updated_dt": datetime.datetime,
+            "description": str,
+            "type": FlawReferenceType,
+        }
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
