e56eb5bbe18eb53c29981342de62941bb9c270ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 15:06:31 +0000

    Add package PandaAuthPy/1.0.0/pandaauthpy-1.0.0.tar.gz

---
+++ packages/PandaAuthPy/pandaauthpy-1.0.0.tar.gz/pandaauthpy-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PandaAuthPy",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["requests"],
+    author="Rice Jay",
+    author_email="yvor.exe@example.com",
+    description="A Panda-Auth library for Python.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/PandaAuthPy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
