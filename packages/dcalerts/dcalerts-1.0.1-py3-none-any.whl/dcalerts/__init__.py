567e87a338498087923a0c1d547ee6f84d84d23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 03:14:35 +0000

    Add package dcalerts/1.0.1/dcalerts-1.0.1-py3-none-any.whl

---
+++ packages/dcalerts/dcalerts-1.0.1-py3-none-any.whl/dcalerts/__init__.py
@@ -0,0 +1,5 @@
+from .decorators import notify
+from .contexts import Notifier
+from .settings import DcalertsSettings, DcalertsSettings as Settings, DEFAULTS
+from .messages import MessageHandler, send_message, make_message
+from . import utils
