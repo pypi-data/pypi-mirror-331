b21a4d767121d009ac9ea062c71a0c65bf62f893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 03:15:08 +0000

    Add package fb-video/0.1.4/fb_video-0.1.4.tar.gz

---
+++ packages/fb-video/fb_video-0.1.4.tar.gz/fb_video-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fb-video',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'fb-scraper = fb_video.fb:main',  # Updated to fb_video.fb
+        ],
+    },
+    author='Kehem IT',
+    author_email='support@kehem.com',
+    description='A Facebook video information scraper by Kehem IT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kehem/fb-download',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
