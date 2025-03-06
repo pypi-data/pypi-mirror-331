188f8b0384468e195dc1fed016523c940a879f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 06:05:52 +0000

    Add package chessminal/1.5/chessminal-1.5.tar.gz

---
+++ packages/chessminal/chessminal-1.5.tar.gz/chessminal-1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chessminal",
+    version="1.5",
+    packages=find_packages(include=["chessminal", "chessminal.*"]),
+    include_package_data=True,
+    install_requires=[
+        "chess",
+        "python-chess",
+        "stockfish",
+        "requests",
+        "rich",
+    ],
+    entry_points={
+        "console_scripts": [
+            "chessminal=chessminal.main:main",
+        ],
+    },
+)
