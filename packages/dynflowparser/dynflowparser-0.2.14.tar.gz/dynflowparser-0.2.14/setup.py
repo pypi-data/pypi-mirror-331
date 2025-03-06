45e389e1f1f51e397157f187be01815a267b09c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 05:07:32 +0000

    Add package dynflowparser/0.2.14/dynflowparser-0.2.14.tar.gz

---
+++ packages/dynflowparser/dynflowparser-0.2.14.tar.gz/dynflowparser-0.2.14/setup.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+
+setuptools.setup(
+    version="0.0.2",
+    setup_requires=['pbr>=2.0.0'],
+    long_description="Get sosreport dynflow files and generates user friendly html pages for tasks, plans, actions and steps.",
+    long_description_content_type='text/markdown',
+    pbr=True)
