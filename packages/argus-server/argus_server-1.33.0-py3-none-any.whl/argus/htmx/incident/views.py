73ded12b053a25b83631de21e15a3ef3a795d9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:05:55 +0000

    Add package argus-server/1.33.0/argus_server-1.33.0-py3-none-any.whl

---
+++ packages/argus-server/argus_server-1.33.0-py3-none-any.whl/argus/htmx/incident/views.py
@@ -0,0 +1,186 @@
+from __future__ import annotations
+
+import logging
+from datetime import datetime
+
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.utils.timezone import now as tznow
+from django.shortcuts import render, get_object_or_404
+
+from django.views.decorators.http import require_POST, require_GET
+from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpResponseBadRequest
+from django_htmx.http import HttpResponseClientRefresh, reswap, retarget
+
+from argus.auth.utils import get_or_update_preference
+from argus.incident.models import Incident
+from argus.notificationprofile.models import Filter
+from argus.util.datetime_utils import make_aware
+
+from ..request import HtmxHttpRequest
+
+from .customization import get_incident_table_columns
+from .utils import get_filter_function
+from .forms import AckForm, DescriptionOptionalForm, EditTicketUrlForm, AddTicketUrlForm
+from ..utils import (
+    bulk_change_incidents,
+    bulk_ack_queryset,
+    bulk_close_queryset,
+    bulk_reopen_queryset,
+    bulk_change_ticket_url_queryset,
+)
+
+User = get_user_model()
+LOG = logging.getLogger(__name__)
+
+
+# Map request trigger to parameters for incidents update
+INCIDENT_UPDATE_ACTIONS = {
+    "ack": (AckForm, bulk_ack_queryset),
+    "close": (DescriptionOptionalForm, bulk_close_queryset),
+    "reopen": (DescriptionOptionalForm, bulk_reopen_queryset),
+    "update-ticket": (EditTicketUrlForm, bulk_change_ticket_url_queryset),
+    "add-ticket": (AddTicketUrlForm, bulk_change_ticket_url_queryset),
+}
+
+
+def prefetch_incident_daughters():
+    return Incident.objects.select_related("source").prefetch_related(
+        "incident_tag_relations",
+        "incident_tag_relations__tag",
+        "events",
+        "events__ack",
+    )
+
+
+# fetch with htmx
+@require_GET
+def incident_detail(request, pk: int):
+    incident = get_object_or_404(Incident, id=pk)
+    duration = None
+    now = tznow()
+    if incident.end_time:
+        if incident.end_time <= now:
+            duration = incident.end_time - incident.start_time
+        else:
+            duration = now - incident.start_time
+    incident.duration = duration
+    context = {
+        "incident": incident,
+        "page_title": str(incident),
+    }
+    return render(request, "htmx/incident/incident_detail.html", context=context)
+
+
+def get_form_data(request, formclass: forms.Form):
+    formdata = request.POST or None
+    incident_ids = []
+    cleaned_form = None
+    if formdata:
+        incident_ids = request.POST.getlist("incident_ids", [])
+        form = formclass(formdata)
+        if form.is_valid():
+            cleaned_form = form.cleaned_data
+    return cleaned_form, incident_ids
+
+
+@require_POST
+def incident_update(request: HtmxHttpRequest, action: str):
+    try:
+        formclass, callback_func = INCIDENT_UPDATE_ACTIONS[action]
+    except KeyError:
+        LOG.error("Unrecognized action name %s when updating incidents.", action)
+        return HttpResponseBadRequest("Invalid update action")
+    formdata, incident_ids = get_form_data(request, formclass)
+    if formdata:
+        bulk_change_incidents(request.user, incident_ids, formdata, callback_func)
+    return HttpResponseClientRefresh()
+
+
+@require_GET
+def filter_form(request: HtmxHttpRequest):
+    request.session["selected_filter"] = None
+    incident_list_filter = get_filter_function()
+    filter_form, _ = incident_list_filter(request, None)
+    context = {"filter_form": filter_form}
+    return render(request, "htmx/incident/_incident_filterbox.html", context=context)
+
+
+@require_POST
+def create_filter(request: HtmxHttpRequest):
+    from argus.htmx.incident.filter import create_named_filter
+
+    filter_name = request.POST.get("filter_name", None)
+    incident_list_filter = get_filter_function()
+    filter_form, _ = incident_list_filter(request, None)
+    if filter_name and filter_form.is_valid():
+        filterblob = filter_form.to_filterblob()
+        _, filter_obj = create_named_filter(request, filter_name, filterblob)
+        if filter_obj:
+            request.session["selected_filter"] = str(filter_obj.id)
+            return HttpResponseClientRefresh()
+    messages.error(request, "Failed to create filter")
+    return HttpResponseBadRequest()
+
+
+@require_GET
+def filter_select(request: HtmxHttpRequest):
+    filter_id = request.GET.get("filter", None)
+    if filter_id and get_object_or_404(Filter, id=filter_id):
+        request.session["selected_filter"] = filter_id
+        incident_list_filter = get_filter_function()
+        filter_form, _ = incident_list_filter(request, None)
+        context = {"filter_form": filter_form}
+        return render(request, "htmx/incident/_incident_filterbox.html", context=context)
+    else:
+        request.session["selected_filter"] = None
+        if request.htmx.trigger:
+            return reswap(HttpResponse(), "none")
+        else:
+            return retarget(HttpResponse(), "#incident-filter-select")
+
+
+@require_GET
+def incident_list(request: HtmxHttpRequest) -> HttpResponse:
+    columns = get_incident_table_columns()
+
+    # Load incidents
+    qs = prefetch_incident_daughters().order_by("-start_time")
+    total_count = qs.count()
+    last_refreshed = make_aware(datetime.now())
+
+    params = dict(request.GET.items())
+
+    incident_list_filter = get_filter_function()
+    filter_form, qs = incident_list_filter(request, qs)
+    filtered_count = qs.count()
+
+    # Standard Django pagination
+
+    page_size, _ = get_or_update_preference(request, request.GET, "argus_htmx", "page_size")
+
+    paginator = Paginator(object_list=qs, per_page=page_size)
+    page_num = params.pop("page", "1")
+    page = paginator.get_page(page_num)
+
+    # The htmx magic - use a different, minimal base template for htmx
+    # requests, allowing us to skip rendering the unchanging parts of the
+    # template.
+    if request.htmx:
+        base_template = "htmx/incident/responses/_incident_list_refresh.html"
+    else:
+        base_template = "htmx/incident/_base.html"
+    context = {
+        "columns": columns,
+        "filtered_count": filtered_count,
+        "count": total_count,
+        "filter_form": filter_form,
+        "page_title": "Incidents",
+        "base": base_template,
+        "page": page,
+        "last_refreshed": last_refreshed,
+    }
+
+    return render(request, "htmx/incident/incident_list.html", context=context)
---
+++ packages/argus-server/argus_server-1.33.0-py3-none-any.whl/argus/incident/V1/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+
+from rest_framework import routers
+
+from . import views as views_V1
+from .. import views
+
+
+router = routers.SimpleRouter()
+router.register(r"sources", views.SourceSystemViewSet)
+router.register(r"source-types", views.SourceSystemTypeViewSet)
+router.register(r"", views_V1.IncidentViewSetV1)
+
+sourced_incident_list = views_V1.SourceLockedIncidentViewSetV1.as_view({"get": "list", "post": "create"})
+
+event_list = views.EventViewSet.as_view({"get": "list", "post": "create"})
+event_detail = views.EventViewSet.as_view({"get": "retrieve"})
+
+ack_list = views_V1.AcknowledgementViewSetV1.as_view({"get": "list", "post": "create"})
+ack_detail = views_V1.AcknowledgementViewSetV1.as_view({"get": "retrieve", "put": "update", "patch": "partial_update"})
+
+tag_list = views.IncidentTagViewSet.as_view({"get": "list", "post": "create"})
+tag_detail = views.IncidentTagViewSet.as_view({"get": "retrieve", "delete": "destroy"})
+
+app_name = "incident"
+urlpatterns = [
+    path("mine/", sourced_incident_list, name="source_locked_incidents"),
+    path("<int:incident_pk>/events/", event_list, name="incident-events"),
+    path("<int:incident_pk>/events/<int:pk>/", event_detail, name="incident-event"),
+    path("<int:incident_pk>/acks/", ack_list, name="incident-acks"),
+    path("<int:incident_pk>/acks/<int:pk>/", ack_detail, name="incident-ack"),
+    path("<int:incident_pk>/tags/", tag_list, name="incident-tags"),
+    path("<int:incident_pk>/tags/<str:tag>/", tag_detail, name="incident-tag"),
+    path("open/", views_V1.OpenIncidentListV1.as_view(), name="incidents-open"),
+    path("open+unacked/", views_V1.OpenUnAckedIncidentListV1.as_view(), name="incidents-open-unacked"),
+] + router.urls
