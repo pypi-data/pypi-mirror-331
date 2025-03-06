9560d446ad033605ea2115da7831a088a51f0593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 15:06:27 +0000

    Add package ariana/0.2.1/ariana-0.2.1.tar.gz

---
+++ packages/ariana/ariana-0.2.1.tar.gz/ariana-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import sys
+import platform
+
+setup(
+    name='ariana',
+    version='0.2.1',
+    description='Ariana CLI - A tool for code instrumentalization and execution with observability',
+    packages=['ariana'],
+    package_data={
+        'ariana': ['bin/ariana-linux-x64', 'bin/ariana-macos-x64', 'bin/ariana-macos-arm64', 'bin/ariana-windows-x64.exe'],
+    },
+    entry_points={
+        'console_scripts': [
+            'ariana = ariana:main',
+        ],
+    },
+    license='BSD-3-Clause',
+    url='https://github.com/dedale-dev/ariana',
+)
