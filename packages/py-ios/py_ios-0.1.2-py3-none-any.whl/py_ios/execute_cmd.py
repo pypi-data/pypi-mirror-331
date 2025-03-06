27b3adae06fc65335d853aae1c7bdea6d97d1d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 15:07:00 +0000

    Add package py-ios/0.1.2/py_ios-0.1.2-py3-none-any.whl

---
+++ packages/py-ios/py_ios-0.1.2-py3-none-any.whl/py_ios/execute_cmd.py
@@ -0,0 +1,40 @@
+﻿import os
+import platform
+import subprocess
+import argparse
+
+
+def run_exe(*cmd_args):
+    os_name = platform.system()
+    current_dir = os.path.dirname(__file__)
+    if os_name == "Darwin":
+        exe_path = os.path.join(current_dir, "lib", "mac", "go-ios")
+        command = f'{exe_path} ' + ' '.join(cmd_args)
+
+    elif os_name == "Windows":
+
+        # Get the full path of the executable
+        exe_path = os.path.join(current_dir, "lib", "windows", "go-ios.exe")
+        # Construct the command with the executable path and the variable arguments
+        if all(arg in cmd_args for arg in ("start", "tunnel")):
+
+            command = f'{exe_path} --userspace ' + ' '.join(cmd_args)
+        else:
+            command = f'{exe_path} ' + ' '.join(cmd_args)
+
+    # Execute the command
+    subprocess.run(command, shell=True)
+
+
+def main():
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Run go-ios with specified arguments.")
+
+    # Add arguments to the parser
+    parser.add_argument('cmd_args', nargs=argparse.REMAINDER, help="Arguments to pass to go-ios")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Call run_exe with the parsed arguments
+    run_exe(*args.cmd_args)
