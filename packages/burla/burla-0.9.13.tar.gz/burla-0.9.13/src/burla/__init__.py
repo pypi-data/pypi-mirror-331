3df9239c741f9191e264f0a3867283276eb9b132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 15:06:27 +0000

    Add package burla/0.9.13/burla-0.9.13.tar.gz

---
+++ packages/burla/burla-0.9.13.tar.gz/burla-0.9.13/src/burla/__init__.py
@@ -0,0 +1,14 @@
+import os
+from fire import Fire
+
+
+# needed so main_service can associate a client version with a request
+__version__ = "0.9.13"
+_BURLA_BACKEND_URL = "https://backend.burla.dev"
+
+from burla._auth import login
+from burla._remote_parallel_map import remote_parallel_map
+
+
+def init_cli():
+    Fire({"login": login, "version": lambda: print(__version__)})
