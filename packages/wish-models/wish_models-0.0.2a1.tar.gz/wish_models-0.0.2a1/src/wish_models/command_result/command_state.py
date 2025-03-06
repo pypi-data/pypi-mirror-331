4c22574f824405b230cd29ba24698aeffeebeeba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:08:17 +0000

    Add package wish-models/0.0.2a1/wish_models-0.0.2a1.tar.gz

---
+++ packages/wish-models/wish_models-0.0.2a1.tar.gz/wish_models-0.0.2a1/src/wish_models/command_result/command_state.py
@@ -0,0 +1,34 @@
+import enum
+
+
+class CommandState(str, enum.Enum):
+    """Enumeration of ExitClass."""
+
+    DOING = "DOING"
+    """The command is being executed."""
+
+    SUCCESS = "SUCCESS"
+    """The command succeeded, with meaningful insights."""
+
+    USER_CANCELLED = "USER_CANCELLED"
+    """The user cancelled the command.
+
+    Wish uses this state (wish assists human operators)"""
+
+    COMMAND_NOT_FOUND = "COMMAND_NOT_FOUND"
+    """The command was not found on the local machine."""
+
+    FILE_NOT_FOUND = "FILE_NOT_FOUND"
+    """A file was not found on the local machine."""
+
+    REMOTE_OPERATION_FAILED = "REMOTE_OPERATION_FAILED"
+    """An operation on a remote machine failed."""
+
+    TIMEOUT = "TIMEOUT"
+    """The command timed out."""
+
+    NETWORK_ERROR = "NETWORK_ERROR"
+    """A network error occurred."""
+
+    OTHERS = "OTHERS"
+    """Other errors."""
