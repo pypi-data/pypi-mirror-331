1b224501e03ed8f0b6be92b82d2a7076bb2d555a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 16:07:25 +0000

    Add package wiz-trader/0.2.0/wiz_trader-0.2.0-py3-none-any.whl

---
+++ packages/wiz-trader/wiz_trader-0.2.0-py3-none-any.whl/wiz_trader/__init__.py
@@ -0,0 +1,7 @@
+"""WizTrader SDK for connecting to the Wizzer."""
+
+from .quotes import QuotesClient
+
+__version__ = "0.2.0"
+
+__all__ = ["QuotesClient"]
---
+++ packages/wiz-trader/wiz_trader-0.2.0-py3-none-any.whl/wiz_trader/apis/__init__.py
@@ -0,0 +1,5 @@
+# apis/__init__.py
+# Provision for REST API functionalities in future.
+# Currently, no APIs are implemented.
+
+"""Module for REST API integrations (future functionality)."""
