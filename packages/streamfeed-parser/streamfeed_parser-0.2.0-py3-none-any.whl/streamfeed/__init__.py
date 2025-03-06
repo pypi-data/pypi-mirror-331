321d3a1b6a3f8a4de65bc3e998b63956fe58c8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 03:14:44 +0000

    Add package streamfeed-parser/0.2.0/streamfeed_parser-0.2.0-py3-none-any.whl

---
+++ packages/streamfeed-parser/streamfeed_parser-0.2.0-py3-none-any.whl/streamfeed/__init__.py
@@ -0,0 +1,20 @@
+# Import standard library modules first to avoid any circular import issues
+import gzip
+import bz2
+import zipfile
+
+# Then import your package components
+from .detect import detect_compression
+from .csv_parser import stream_csv_lines
+from .xml_parser import stream_xml_items_iterparse
+from .ftp_handler import stream_from_ftp
+from .feed_streamer import stream_feed, preview_feed
+
+__all__ = [
+    "detect_compression",
+    "stream_csv_lines",
+    "stream_xml_items_iterparse",
+    "stream_from_ftp",
+    "stream_feed",
+    "preview_feed",
+]
