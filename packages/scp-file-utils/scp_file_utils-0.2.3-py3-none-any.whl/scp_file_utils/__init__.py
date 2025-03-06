5720645df05782ef8765003b0af5c8488fc3550e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:06:11 +0000

    Add package scp-file-utils/0.2.3/scp_file_utils-0.2.3-py3-none-any.whl

---
+++ packages/scp-file-utils/scp_file_utils-0.2.3-py3-none-any.whl/scp_file_utils/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for SCP File Utils."""
+
+__author__ = """Jaideep Sundaram"""
+__email__ = "jai.python3@gmail.com"
+__version__ = "0.2.3"
+
+# from .[new_project] import Something as Something
