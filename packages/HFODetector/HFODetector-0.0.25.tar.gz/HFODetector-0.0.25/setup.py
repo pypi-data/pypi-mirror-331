ec2183974c741071533066b07c4be19f5319d72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:06:12 +0000

    Add package HFODetector/0.0.25/HFODetector-0.0.25.tar.gz

---
+++ packages/HFODetector/HFODetector-0.0.25.tar.gz/HFODetector-0.0.25/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='HFODetector',
+    version='0.0.25',
+    description='Package for detecting HFOs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/roychowdhuryresearch/HFO_Detector',
+    author='Xin Chen, Hoyoung Chung',
+    author_email='xinchen98@g.ucla.edu, taylorchung@ucla.edu',
+    license='TDG Attribution Non-Commercial No Distrib',
+    python_requires='>=3.6',
+    packages=find_packages(include=['HFODetector']),
+    install_requires=['mne',
+                      'numpy',
+                      'scipy',
+                      'matplotlib',
+                      'pandas',
+                      'scikit-image'
+                      ],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
