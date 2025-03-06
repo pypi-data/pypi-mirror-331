04624d462591b38b699535e161b6bc6d609a5665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:08:30 +0000

    Add package yonoma/0.1.5/yonoma-0.1.5.tar.gz

---
+++ packages/yonoma/yonoma-0.1.5.tar.gz/yonoma-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yonoma",
+    version="0.1.5",
+    # packages=find_packages(),
+    package_dir={'': 'src'}, 
+    packages=find_packages(where='src'),
+    install_requires=["requests"],
+    author="YONOMAHQ",
+    author_email="tools@yonoma.io",
+    description="A Python client for the Yonoma API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YonomaHQ/yonoma-email-marketing-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.13.2",
+)
