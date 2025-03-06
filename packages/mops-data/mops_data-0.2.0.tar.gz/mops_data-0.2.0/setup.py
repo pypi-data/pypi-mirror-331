50048d6e21ed76e98677fa75fd5e1f9fda39038b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:07:50 +0000

    Add package mops-data/0.2.0/mops_data-0.2.0.tar.gz

---
+++ packages/mops-data/mops_data-0.2.0.tar.gz/mops_data-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='mops_data',  # 自設名稱或與自定義函式相同名稱皆可
+    version='0.2.0',
+    py_modules=['mops_data'],   # mops_data.py  
+    install_requires=[
+        # The mops_data package was designed in the Department of Accounting by Chuan-Fu Chuang.
+    ],
+)
