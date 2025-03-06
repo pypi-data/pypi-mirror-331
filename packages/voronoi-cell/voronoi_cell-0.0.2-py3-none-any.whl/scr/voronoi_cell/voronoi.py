1380d927a6f825554f502982be2f5937735a6835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 05:07:24 +0000

    Add package voronoi-cell/0.0.2/voronoi_cell-0.0.2-py3-none-any.whl

---
+++ packages/voronoi-cell/voronoi_cell-0.0.2-py3-none-any.whl/scr/voronoi_cell/voronoi.py
@@ -0,0 +1,35 @@
+import itertools import product
+import numpy as np
+
+def VCell(Q, **rangeorlist):
+    if np.linalg.det(np.array(Q)) <=0: 
+        raise Exception("your matrix is not positive-definite.")
+        return "your matrix is not positive-definite."
+    if (np.array(Q).transpose() != np.array(Q)).any():
+        raise Exception("your matrix is not symmetric.")
+        return "your matrix is not symmetric."
+    d = len(Q)
+    if "range" in rangeorlist:
+        r = rangeorlist["range"]
+        if (r <= 0):
+            raise Exception("The range must be a positive integer.")
+        p = [list(vec) for vec in product(range(-r, r+1), repeat=d)]
+        p.remove([0] * d)
+    elif "list" in rangeorlist:
+        p = rangeorlist["list"]
+    else:
+        raise Exception("A range or list of potential relevant vectors needs to be given.")
+    ineqs = []
+    for vert in p:
+        #Collect an inequality for each potential relevant vector (not all may be useful)
+        #equation is -2 v^T.Q.x + v^T . Q . v >= 0
+        b = 0
+        for i in range(d):
+            for j in range(d):
+                b += vert[i]*Q[i][j]*vert[j]
+        vec = [-2*sum([vert[i]*Q[i][j] for i in range(d)]) for j in range(d)]
+        ineqs += [tuple([b] + vec)]
+    VC = Polyhedron(ieqs = ineqs)
+    if VC.volume() != 1:
+        raise Exception("relevant vector list not complete")
+    return VC
