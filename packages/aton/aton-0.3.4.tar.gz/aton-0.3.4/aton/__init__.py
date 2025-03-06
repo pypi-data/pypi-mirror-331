71315f682295c14df8c436f707e48f6931e0dfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:05:50 +0000

    Add package aton/0.3.4/aton-0.3.4.tar.gz

---
+++ packages/aton/aton-0.3.4.tar.gz/aton-0.3.4/aton/__init__.py
@@ -0,0 +1,13 @@
+"""
+.. include:: ../_README_temp.md
+"""
+
+
+from ._version import __version__ as version
+from . import st
+from . import phys
+from . import txt
+from . import interface
+from . import spx
+from . import qrotor
+
