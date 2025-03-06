b9579906b84d3daa87013c99411c7a56a92c0b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:07:56 +0000

    Add package canonicalwebteam.sitemaps-parser/1.0.0/canonicalwebteam_sitemaps_parser-1.0.0.tar.gz

---
+++ packages/canonicalwebteam.sitemaps-parser/canonicalwebteam_sitemaps_parser-1.0.0.tar.gz/canonicalwebteam_sitemaps_parser-1.0.0/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="canonicalwebteam.sitemaps-parser",
+    version="1.0.0",
+    author="Canonical webteam",
+    author_email="webteam@canonical.com",
+    url="https://github.com/canonical/canonicalwebteam.sitemaps-parser",
+    description=(
+        "Flask extension to parse websites and extract structured data"
+    ),
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "Flask>=1.0.2",
+        "beautifulsoup4",
+        "humanize",
+        "lxml",
+        "requests",
+        "python-dateutil",
+        "validators",
+        "python-slugify",
+    ],
+)
