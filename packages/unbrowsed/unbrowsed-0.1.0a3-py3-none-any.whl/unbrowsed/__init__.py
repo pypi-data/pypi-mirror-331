fa947ef218c70e6cba7898a554862ca09c1ac03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:05:51 +0000

    Add package unbrowsed/0.1.0a3/unbrowsed-0.1.0a3-py3-none-any.whl

---
+++ packages/unbrowsed/unbrowsed-0.1.0a3-py3-none-any.whl/unbrowsed/__init__.py
@@ -0,0 +1,14 @@
+from unbrowsed.parser import parse_html
+from unbrowsed.queries import (
+    query_by_label_text,
+    query_by_text,
+    MultipleElementsFoundError,
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "parse_html",
+    "query_by_label_text",
+    "query_by_text",
+    "MultipleElementsFoundError",
+]
