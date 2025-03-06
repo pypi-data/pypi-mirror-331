d1303fb8ae9e8acc186e2b61256afea8d4e6657c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 06:05:49 +0000

    Add package dolma/1.2.0.dev7/dolma-1.2.0.dev7-pp39-pypy39_pp73-manylinux_2_28_armv7l.whl

---
+++ packages/dolma/dolma-1.2.0.dev7-pp39-pypy39_pp73-manylinux_2_28_armv7l.whl/dolma/taggers/code/__init__.py
@@ -0,0 +1,17 @@
+from .code_taggers import (
+    CodeCopyrightTagger,
+    CodeRedPajamaTaggers,
+    CodeSecretsTagger,
+    CodeStarCoderTaggers,
+    CodeStarCoderTaggers2,
+    Learn2CodeTaggers,
+)
+
+__all__ = [
+    "CodeSecretsTagger",
+    "CodeCopyrightTagger",
+    "CodeRedPajamaTaggers",
+    "CodeStarCoderTaggers",
+    "CodeStarCoderTaggers2",
+    "Learn2CodeTaggers",
+]
