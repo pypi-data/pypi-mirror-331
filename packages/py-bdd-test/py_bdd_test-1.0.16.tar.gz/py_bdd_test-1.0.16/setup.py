afd0d2b917019f3dca64cc2a2e659ce789c7119d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:08:20 +0000

    Add package py-bdd-test/1.0.16/py_bdd_test-1.0.16.tar.gz

---
+++ packages/py-bdd-test/py_bdd_test-1.0.16.tar.gz/py_bdd_test-1.0.16/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-bdd-test",
+    version="1.0.16",
+    packages=find_packages(exclude=["bdd-test", "bdd-test.*"]),
+    install_requires=[
+        "behave",
+        "kafka-python",
+        "parse",
+        "parse_type",
+        "pyhamcrest",
+        "behavex",
+        "requests",
+        "PyHamcrest",
+        "pyyaml",
+        "fastapi",
+        "uvicorn",
+        "python-multipart",
+        "pymongo",
+        "pydantic",
+        "typing",
+        "python-dotenv",
+        "playwright",
+        "kafka-python",
+    ],
+    entry_points={
+        "console_scripts": [
+            "list-bdd-steps=py_bdd_test.list_steps:main"
+        ]
+    },
+    python_requires=">=3.7",
+)
