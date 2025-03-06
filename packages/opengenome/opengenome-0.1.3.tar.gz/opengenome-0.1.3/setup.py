e0d6b39af9eeba26cdb6e3eb7d142a22ab924c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 05:06:18 +0000

    Add package opengenome/0.1.3/opengenome-0.1.3.tar.gz

---
+++ packages/opengenome/opengenome-0.1.3.tar.gz/opengenome-0.1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_namespace_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='opengenome',
+    version='0.1.3',
+    long_description=description,
+    long_description_content_type="text/markdown",
+    packages=find_namespace_packages(where="src"),
+    license_files=('LICENSE.txt', ),
+    package_dir={'': 'src'},
+    install_requires=[],
+    include_package_data=True,
+    package_data={"": ["*.yaml"]},
+    entry_points={
+        "console_scripts": [
+            "opengenome-about = opengenome:about",
+        ],
+    },
+    extras_require={
+        "dev": [
+            "mypy",
+            "pytest",
+            "pytest-cov",
+            "jupyter",
+            "sphinx",
+            "myst-nb",
+            "sphinx-autoapi",
+            "pydata_sphinx_theme",
+        ]
+    },
+)
