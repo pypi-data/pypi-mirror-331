5f509d9cbdc3f01d9d4c27de4d8766f6974d2803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 15:06:37 +0000

    Add package aiotaskqueue/0.5.0/aiotaskqueue-0.5.0-py3-none-any.whl

---
+++ packages/aiotaskqueue/aiotaskqueue-0.5.0-py3-none-any.whl/aiotaskqueue/publisher.py
@@ -0,0 +1,24 @@
+from aiotaskqueue._types import P, TResult
+from aiotaskqueue.broker.abc import Broker
+from aiotaskqueue.config import Configuration
+from aiotaskqueue.serialization import serialize_task
+from aiotaskqueue.tasks import RunningTask, TaskInstance
+
+
+class Publisher:
+    def __init__(
+        self,
+        broker: Broker,
+        config: Configuration,
+    ) -> None:
+        self._broker = broker
+        self._config = config
+
+    async def enqueue(self, task: TaskInstance[P, TResult]) -> RunningTask[TResult]:
+        record = serialize_task(
+            task,
+            default_backend=self._config.default_serialization_backend,
+            serialization_backends=self._config.serialization_backends,
+        )
+        await self._broker.enqueue(record)
+        return RunningTask(instance=task, id=record.id)
