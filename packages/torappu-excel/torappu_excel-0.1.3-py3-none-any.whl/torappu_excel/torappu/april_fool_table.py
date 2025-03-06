93046220ce89318e8502d3e780586bf1774f56fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:07:53 +0000

    Add package torappu-excel/0.1.3/torappu_excel-0.1.3-py3-none-any.whl

---
+++ packages/torappu-excel/torappu_excel-0.1.3-py3-none-any.whl/torappu_excel/torappu/april_fool_table.py
@@ -0,0 +1,14 @@
+from .act_4fun_data import Act4funData
+from .act_5fun_data import Act5FunData
+from .april_fool_const import AprilFoolConst
+from .april_fool_score_data import AprilFoolScoreData
+from .april_fool_stage_data import AprilFoolStageData
+from ..common import BaseStruct
+
+
+class AprilFoolTable(BaseStruct):
+    stages: dict[str, AprilFoolStageData]
+    scoreDict: dict[str, list[AprilFoolScoreData]]
+    constant: AprilFoolConst
+    act4FunData: Act4funData
+    act5FunData: Act5FunData
