dda5c678a79a6a581fdb71769a36637ac758465b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:05:52 +0000

    Add package spdl-io/0.0.9/spdl_io-0.0.9-cp313-cp313-manylinux_2_27_x86_64.whl

---
+++ packages/spdl-io/spdl_io-0.0.9-cp313-cp313-manylinux_2_27_x86_64.whl/spdl/io/utils/_build.py
@@ -0,0 +1,42 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-unsafe
+
+from spdl.io.lib import _libspdl
+
+__all__ = [
+    "is_cuda_available",
+    "is_nvcodec_available",
+    "is_nvjpeg_available",
+]
+
+
+def is_cuda_available() -> bool:
+    """Check if SPDL is compiled with CUDA support.
+
+    Returns:
+        True if SPDL is compiled with CUDA support.
+    """
+    return _libspdl.is_cuda_available()
+
+
+def is_nvcodec_available() -> bool:
+    """Check if SPDL is compiled with NVCODEC support.
+
+    Returns:
+        True if SPDL is compiled with NVCODEC support.
+    """
+    return _libspdl.is_nvcodec_available()
+
+
+def is_nvjpeg_available() -> bool:
+    """Check if SPDL is compiled with NVJPEG support.
+
+    Returns:
+        True if SPDL is compiled with NVJPEG support.
+    """
+    return _libspdl.is_nvjpeg_available()
