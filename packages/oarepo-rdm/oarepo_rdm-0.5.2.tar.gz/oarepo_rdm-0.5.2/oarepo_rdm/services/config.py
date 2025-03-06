75879578e93b6dd2aafc3c45475433fa994dc6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 03:14:59 +0000

    Add package oarepo-rdm/0.5.2/oarepo_rdm-0.5.2.tar.gz

---
+++ packages/oarepo-rdm/oarepo_rdm-0.5.2.tar.gz/oarepo_rdm-0.5.2/oarepo_rdm/services/config.py
@@ -0,0 +1,10 @@
+from invenio_rdm_records.services.config import RDMRecordServiceConfig
+from oarepo_runtime.services.config.service import PermissionsPresetsConfigMixin
+from oarepo_runtime.services.results import RecordItem, RecordList
+
+
+class OARepoRDMServiceConfig(RDMRecordServiceConfig):
+    """"""
+    # PERMISSIONS_PRESETS = ["everyone"]
+    # result_item_cls = RecordItem
+    # result_list_cls = RecordList
