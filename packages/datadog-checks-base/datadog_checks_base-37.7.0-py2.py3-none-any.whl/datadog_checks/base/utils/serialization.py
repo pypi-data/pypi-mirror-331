661807330a0131694e429d00a4f485c6c986ab04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 03:13:35 +0000

    Add package datadog-checks-base/37.7.0/datadog_checks_base-37.7.0-py2.py3-none-any.whl

---
+++ packages/datadog-checks-base/datadog_checks_base-37.7.0-py2.py3-none-any.whl/datadog_checks/base/utils/serialization.py
@@ -0,0 +1,34 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import logging
+
+try:
+    import orjson as json
+
+    impl = 'orjson'
+    sort_keys_kwargs = {'option': json.OPT_SORT_KEYS}
+
+    def from_json(s, **kwargs):
+        return json.loads(s, **kwargs)
+
+    def to_json(d, **kwargs):
+        return json.dumps(d, **kwargs).decode()
+
+except ImportError:
+    import json
+
+    impl = 'stdlib'
+    sort_keys_kwargs = {'sort_keys': True}
+
+    def from_json(s, **kwargs):
+        return json.loads(s, **kwargs)
+
+    def to_json(d, **kwargs):
+        return json.dumps(d, **kwargs)
+
+
+logger = logging.getLogger(__name__)
+logger.debug('Using JSON implementation from %s', impl)
+
+__all__ = ['from_json', 'json', 'sort_keys_kwargs', 'to_json']
