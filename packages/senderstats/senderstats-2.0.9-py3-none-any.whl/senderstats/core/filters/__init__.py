3e498d1176764f3b35c3c02df235183539ceb264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 05:07:20 +0000

    Add package senderstats/2.0.9/senderstats-2.0.9-py3-none-any.whl

---
+++ packages/senderstats/senderstats-2.0.9-py3-none-any.whl/senderstats/core/filters/__init__.py
@@ -0,0 +1,15 @@
+from .exclude_domain_filter import ExcludeDomainFilter
+from .exclude_empty_sender_filter import ExcludeEmptySenderFilter
+from .exclude_invalid_size_filter import ExcludeInvalidSizeFilter
+from .exclude_ip_filter import ExcludeIPFilter
+from .exclude_sender_filter import ExcludeSenderFilter
+from .restrict_domain_filter import RestrictDomainFilter
+
+__all__ = [
+    'ExcludeDomainFilter',
+    'ExcludeEmptySenderFilter',
+    'ExcludeInvalidSizeFilter',
+    'ExcludeIPFilter',
+    'ExcludeSenderFilter',
+    'RestrictDomainFilter'
+]
