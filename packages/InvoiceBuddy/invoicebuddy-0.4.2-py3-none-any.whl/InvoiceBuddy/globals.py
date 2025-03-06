34f33af48cee286d2d29a08238cb50bb0f38505d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:08:03 +0000

    Add package InvoiceBuddy/0.4.2/invoicebuddy-0.4.2-py3-none-any.whl

---
+++ packages/InvoiceBuddy/invoicebuddy-0.4.2-py3-none-any.whl/InvoiceBuddy/globals.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+# General
+APPLICATION_NAME = 'InvoiceBuddy'
+APPLICATION_VERSION = '0.4.2'
+JSON_RESPONSE_FORMAT_VERSION = 1
+SELLER_LOGO_FILENAME = "seller-logo.png"
+INVOICE_TEMPLATE_FILENAME = "invoice_template.html"
+PROPOSAL_TEMPLATE_FILENAME = "proposal_template.html"
+INTERNAL_SERVER_ERROR = "An error occurred on the server. Please check the server logs for more details"
+
+# Caching
+FLASK_WEB_SERVER_CACHE_INTERVAL = 5    # in seconds
+
+# Logging
+LOG_FILENAME = 'InvoiceBuddy.log'
+LOG_FILE_SIZE = 1024 * 1024  # Log file size 1MB
+LOGGER_NAME = 'InvoiceBuddy'
+
+# DB
+DATABASE_NAME = "InvoiceBuddy"
+INVOICE_FOLDER = "invoices"
+PROPOSAL_FOLDER = "proposals"
+
+# Presentation
+ITEMS_ITEMS_PER_PAGE = 10
+CUSTOMERS_ITEMS_PER_PAGE = 10
+PAST_INVOICES_TABLE_ITEMS_PER_PAGE = 10
+PAST_PROPOSALS_TABLE_ITEMS_PER_PAGE = 10
+
+
+# Invoices
+class InvoiceType(Enum):
+    CREDIT = 'credit'
+    DEBIT = 'debit'
+
+
+class InvoiceStatus(Enum):
+    UNPAID = 0
+    PAID = 1
+    CANCELED = 2
+
+
+# Proposals
+class ProposalStatus(Enum):
+    UNACCEPTED = 0
+    ACCEPTED = 1
+    REJECTED = 2
