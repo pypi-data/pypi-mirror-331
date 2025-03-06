804f47b2a611f487cc568721a62326104a7a143e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 07:05:53 +0000

    Add package scp-file-utils/0.2.1/scp_file_utils-0.2.1.tar.gz

---
+++ packages/scp-file-utils/scp_file_utils-0.2.1.tar.gz/scp_file_utils-0.2.1/scp_file_utils/constants.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+from datetime import datetime
+
+DEFAULT_PROJECT = "scp-file-utils"
+
+DEFAULT_CONFIG_FILE = os.path.join(os.path.dirname("__file__"), "conf", "config.yaml")
+
+DEFAULT_TIMESTAMP = str(datetime.today().strftime("%Y-%m-%d-%H%M%S"))
+
+# Get the operating system
+OS = os.uname().sysname
+
+DEFAULT_DOWNLOADS_DIR = None
+DEFAULT_OUTDIR_BASE = None
+
+# If the operating system is Windows, set the downloads directory to the user's Downloads folder
+if OS == "Windows":
+    DEFAULT_DOWNLOADS_DIR = os.path.join(
+        os.getenv("USERPROFILE"),
+        "Downloads",
+    )
+    DEFAULT_OUTDIR_BASE = os.path.join(
+        os.getenv("USERPROFILE"),
+        "Downloads",
+        DEFAULT_PROJECT,
+    )
+# Otherwise, set the downloads directory to the user's home directory
+else:
+    DEFAULT_DOWNLOADS_DIR = os.path.join(
+        os.getenv("HOME"),
+        "Downloads",
+    )
+    DEFAULT_OUTDIR_BASE = os.path.join(
+        "/tmp/",
+        os.getenv("USER"),
+        DEFAULT_PROJECT,
+    )
+
+DEFAULT_LOGGING_FORMAT = (
+    "%(levelname)s : %(asctime)s : %(pathname)s : %(lineno)d : %(message)s"
+)
+
+DEFAULT_LOGGING_LEVEL = logging.INFO
+
+DEFAULT_VERBOSE = False
---
+++ packages/scp-file-utils/scp_file_utils-0.2.1.tar.gz/scp_file_utils-0.2.1/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for scp_file_utils."""
