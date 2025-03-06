474a341bc1fd6c1e662468dc9386b9b6c82ae16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:06:18 +0000

    Add package ecofreq/0.0.2/ecofreq-0.0.2-py3-none-any.whl

---
+++ packages/ecofreq/ecofreq-0.0.2-py3-none-any.whl/ecofreq/config.py
@@ -0,0 +1,11 @@
+import pathlib
+
+JOULES_IN_KWH = 3.6e6
+OPTION_DISABLED = ["none", "off"]
+
+TS_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+HOMEDIR = pathlib.Path(__file__).parent
+DATADIR = HOMEDIR / "data"
+LOG_FILE = "/var/log/ecofreq.log"
+SHM_FILE = "/dev/shm/ecofreq"
