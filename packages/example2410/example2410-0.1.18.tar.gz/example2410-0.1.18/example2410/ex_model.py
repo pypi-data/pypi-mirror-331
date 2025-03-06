10839a52cf46eaa7dde1772226d8ef8e7f05d645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:05:56 +0000

    Add package example2410/0.1.18/example2410-0.1.18.tar.gz

---
+++ packages/example2410/example2410-0.1.18.tar.gz/example2410-0.1.18/example2410/ex_model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from example2410.helper import helper
+
+class sum_tf:
+    def __init__(self, numbers):
+        for i in numbers:
+            helper.checknumber(i)
+        self.sum = tf.reduce_sum(numbers)
+
