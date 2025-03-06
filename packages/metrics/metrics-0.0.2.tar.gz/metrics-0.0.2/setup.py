8ea58e8270ee62ff246b0a0135f5f2bdaaef3053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 03:15:21 +0000

    Add package metrics/0.0.2/metrics-0.0.2.tar.gz

---
+++ packages/metrics/metrics-0.0.2.tar.gz/metrics-0.0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='metrics',
+    version='0.0.2',
+    description='PyPi package created by Schibsted\'s Product & Application Security team.',
+    long_description='',
+    url='https://schibsted.com',
+    author='Product Security Team',
+    author_email='application-security+pypi@schibsted.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
