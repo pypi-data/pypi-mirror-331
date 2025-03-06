6a50730b2fdaa7f1059149fdacafa1e2e2611130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:06:11 +0000

    Add package querysource/3.15.12/querysource-3.15.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/querysource/querysource-3.15.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/querysource/datasources/drivers/rethink.py
@@ -0,0 +1,47 @@
+from datamodel import Column
+from ...conf import (
+    RT_DRIVER,
+    RT_HOST,
+    RT_PORT,
+    RT_DATABASE,
+    RT_USER,
+    RT_PASSWORD,
+)
+from .abstract import NoSQLDriver
+
+class rethinkDriver(NoSQLDriver):
+    driver: str = RT_DRIVER
+    port: int = Column(required=True, default=RT_PORT)
+    database: str = Column(required=False)
+
+    def params(self) -> dict:
+        """params
+
+        Returns:
+            dict: params required for AsyncDB.
+        """
+        if self.username:
+            return {
+                "host": self.host,
+                "port": self.port,
+                "user": self.username,
+                "password": self.password,
+                "db": self.database,
+            }
+        else:
+            return {
+                "host": self.host,
+                "port": self.port,
+                "db": self.database
+            }
+
+try:
+    rethink_default = rethinkDriver(
+        host=RT_HOST,
+        port=RT_PORT,
+        database=RT_DATABASE,
+        username=RT_USER,
+        password=RT_PASSWORD
+    )
+except (TypeError, ValueError):
+    rethink_default = None
