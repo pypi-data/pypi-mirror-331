bddff7a4ba3231343e87613bc1ac5e3eb287c935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 16:07:05 +0000

    Add package datatransfer-client/2.10.15/datatransfer_client-2.10.15.tar.gz

---
+++ packages/datatransfer-client/datatransfer_client-2.10.15.tar.gz/datatransfer_client-2.10.15/src/datatransfer/source/enroll_get_data/apps.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from importlib import import_module
+
+from django.apps.config import AppConfig
+
+
+class EnrollGetDataAppConfig(AppConfig):
+
+    name = __package__
+
+    def _register_related_objects_views(self):
+        """Добавляет представления для моделей приложения."""
+        from educommon.django.db.model_view import registries
+
+        if registries.get('related_objects'):
+            model_views = import_module(self.name + '.model_views')
+            registries['related_objects'].register(
+                *model_views.related_model_views
+            )
+
+    def _register_tasks(self):
+        """Регистрирует асинхронные задачи."""
+        from .tasks import enroll_get_data_autorun_task
+        from .tasks import enroll_get_data_push_rules
+        from .tasks import enroll_get_data_push_task
+        from .tasks import enroll_get_data_task
+
+    def ready(self):
+        super(EnrollGetDataAppConfig, self).ready()
+        self._register_related_objects_views()
+        self._register_tasks()
