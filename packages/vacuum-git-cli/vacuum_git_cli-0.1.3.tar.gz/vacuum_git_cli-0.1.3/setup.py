0bed240ec9e5c97af2acfac5768a7bc5f9c91682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 06:06:04 +0000

    Add package vacuum-git-cli/0.1.3/vacuum_git_cli-0.1.3.tar.gz

---
+++ packages/vacuum-git-cli/vacuum_git_cli-0.1.3.tar.gz/vacuum_git_cli-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="vacuum-git-cli",
+    version="0.1.3",
+    author="Ayash",
+    author_email="ayashbera@gmail.com",
+    description="A tool to clone specific folders from a Git repository",
+    url="https://github.com/Ayash-Bera/vaccum",
+    packages=find_packages(),
+    py_modules=["vacuum_script"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "vacuum=vacuum_script:main",
+        ],
+    },
+)
