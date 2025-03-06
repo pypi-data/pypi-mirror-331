fd4f4de146187e5b134a2570472557ae0fe6e289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:05:52 +0000

    Add package scp-file-utils/0.2.4/scp_file_utils-0.2.4.tar.gz

---
+++ packages/scp-file-utils/scp_file_utils-0.2.4.tar.gz/scp_file_utils-0.2.4/README.rst
@@ -0,0 +1,17 @@
+==============
+SCP File Utils
+==============
+
+A collection of Python scripts for downloading/uploading files via scp.
+
+Usage
+=====
+
+.. code-block:: bash
+
+    pip install scp-file-utils
+
+Exported scripts
+================
+
+* scp-download-file
---
+++ packages/scp-file-utils/scp_file_utils-0.2.4.tar.gz/scp_file_utils-0.2.4/tests/test_scp_file_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `scp_file_utils` package."""
+
+
+import unittest
+
+from scp_file_utils import scp_file_utils
+
+
+class TestScp_file_utils(unittest.TestCase):
+    """Tests for `scp_file_utils` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
