627e5edc0209715300bc634dab575ea553113e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 06:06:35 +0000

    Add package forensics/0.1.0/forensics-0.1.0.tar.gz

---
+++ packages/forensics/forensics-0.1.0.tar.gz/forensics-0.1.0/src/feedback_forensics/app/main.py
@@ -0,0 +1,46 @@
+import argparse
+import gradio.themes.utils.fonts
+import gradio as gr
+from loguru import logger
+
+import feedback_forensics.app.interface as interface
+from feedback_forensics.app.constants import USERNAME, PASSWORD
+import feedback_forensics.app.datasets
+
+# make gradio work offline
+gradio.themes.utils.fonts.GoogleFont.stylesheet = lambda self: None
+
+
+def run():
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datapath", "-d", type=str, help="Path to dataset")
+    args = parser.parse_args()
+    if args.datapath:
+        feedback_forensics.app.datasets.BUILTIN_DATASETS.append(
+            feedback_forensics.app.datasets.create_local_dataset(args.datapath)
+        )
+        logger.info(f"Added local dataset to available datasets ({args.datapath}).")
+
+    if len(feedback_forensics.app.datasets.BUILTIN_DATASETS) == 0:
+        logger.error(
+            "No datasets available. No local or standard datasets could be loaded. Please either provide a path to a local dataset via --datapath (-d) flag or provide the correct HuggingFace token via the HF_TOKEN environment variable."
+        )
+        return
+
+    # setup the gradio app
+    demo = interface.generate()
+
+    # run the app
+    if USERNAME and PASSWORD:
+        auth = (USERNAME, PASSWORD)
+        auth_message = "Welcome to the ICAI App demo!"
+    else:
+        auth = None
+        auth_message = None
+
+    demo.launch(auth=auth, auth_message=auth_message)
+
+
+if __name__ == "__main__":
+    run()  # pylint: disable=no-value-for-parameter
