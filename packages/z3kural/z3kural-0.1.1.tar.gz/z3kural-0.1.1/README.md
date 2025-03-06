0630816a6f0c4462ecea4a97fa212a6c98f6afdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 03:14:05 +0000

    Add package z3kural/0.1.1/z3kural-0.1.1.tar.gz

---
+++ packages/z3kural/z3kural-0.1.1.tar.gz/z3kural-0.1.1/README.md
@@ -0,0 +1,17 @@
+# Z3Kural - Thirukkural API by Z3Connect
+
+A Python package to fetch Thirukkural verses.
+
+## Installation
+
+pip install z3kural 
+
+
+
+## Usage
+
+```python
+from z3kural import get_kural
+
+kural = get_kural(1)
+print(kural)
---
+++ packages/z3kural/z3kural-0.1.1.tar.gz/z3kural-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="z3kural",  
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A Python package to fetch Thirukkural verses",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Z3Connect",
+    author_email="z3connect.z3connect@gmail.com",  
+    url="https://github.com/z3connect/z3kural",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
