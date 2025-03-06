80ba64c15e36107817bbc10edfc722c5b7c5c69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:05:28 +0000

    Add package aioecowitt/2025.3.1/aioecowitt-2025.3.1.tar.gz

---
+++ packages/aioecowitt/aioecowitt-2025.3.1.tar.gz/aioecowitt-2025.3.1/tests/test_sensor.py
@@ -0,0 +1,56 @@
+"""Test ecowitt sensor module."""
+
+import pytest
+
+from aioecowitt import server
+
+from .const import GW2000A_V3_DATA
+from aioecowitt import sensor
+
+
+def test_update_listener() -> None:
+    """Test on change get updates from callback."""
+    ecowit_sensor = sensor.EcoWittSensor(
+        "test", "test", sensor.EcoWittSensorTypes.TEMPERATURE_C, "test"
+    )
+
+    called = False
+
+    def on_change() -> None:
+        """Test callback."""
+        nonlocal called
+        called = True
+
+    ecowit_sensor.update_cb.append(on_change)
+
+    ecowit_sensor.update_value(10, 0, 0)
+    assert called
+
+    called = False
+    ecowit_sensor.update_value(10, 0, 0)
+    assert not called
+
+    ecowit_sensor.update_value(11, 0, 0)
+    assert called
+
+
+@pytest.mark.asyncio
+async def test_heap_field(ecowitt_server, ecowitt_http) -> None:
+    """Test handling of heap field."""
+    heap_sensor = None
+
+    def on_change(sensor: server.EcoWittSensor) -> None:
+        """Test callback."""
+        if sensor.key == "heap":
+            nonlocal heap_sensor
+            heap_sensor = sensor
+
+    ecowitt_server.new_sensor_cb.append(on_change)
+
+    resp = await ecowitt_http.post("/", data=GW2000A_V3_DATA)
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "OK"
+
+    assert heap_sensor
+    assert heap_sensor.value == GW2000A_V3_DATA["heap"]
---
+++ packages/aioecowitt/aioecowitt-2025.3.1.tar.gz/aioecowitt-2025.3.1/tests/test_server.py
@@ -0,0 +1,84 @@
+"""EcoWitt server tests."""
+
+import pytest
+
+from aioecowitt import server
+
+from .const import GW2000A_DATA, EASYWEATHER_DATA
+
+# pylint: disable=redefined-outer-name
+
+
+@pytest.mark.asyncio
+async def test_server_start(ecowitt_server, ecowitt_http) -> None:
+    """Test server start."""
+    sensors = []
+
+    def on_change(sensor: server.EcoWittSensor) -> None:
+        """Test callback."""
+        sensors.append(sensor)
+
+    ecowitt_server.new_sensor_cb.append(on_change)
+
+    resp = await ecowitt_http.post("/", data=GW2000A_DATA)
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "OK"
+
+    assert len(sensors) == 52
+    assert len(ecowitt_server.sensors) == 52
+    assert len(ecowitt_server.stations) == 1
+
+    assert "PASSKEY" not in ecowitt_server.last_values[GW2000A_DATA["PASSKEY"]]
+
+
+@pytest.mark.asyncio
+async def test_server_token(ecowitt_server, ecowitt_http) -> None:
+    """Test server start."""
+    sensors = []
+    path = "/test"
+    ecowitt_server.path = path
+
+    def on_change(sensor: server.EcoWittSensor) -> None:
+        """Test callback."""
+        sensors.append(sensor)
+
+    ecowitt_server.new_sensor_cb.append(on_change)
+
+    resp = await ecowitt_http.post("/", data=GW2000A_DATA)
+    assert resp.status == 404
+
+    resp = await ecowitt_http.post(path, data=GW2000A_DATA)
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "OK"
+
+    assert len(sensors) == 52
+    assert len(ecowitt_server.sensors) == 52
+    assert len(ecowitt_server.stations) == 1
+
+
+@pytest.mark.asyncio
+async def test_server_multi_stations(ecowitt_server, ecowitt_http) -> None:
+    """Test server start and multiple stations."""
+    sensors = []
+
+    def on_change(sensor: server.EcoWittSensor) -> None:
+        """Test callback."""
+        sensors.append(sensor)
+
+    ecowitt_server.new_sensor_cb.append(on_change)
+
+    resp = await ecowitt_http.post("/", data=GW2000A_DATA)
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "OK"
+
+    resp = await ecowitt_http.post("/", data=EASYWEATHER_DATA)
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "OK"
+
+    assert len(sensors) == 91
+    assert len(ecowitt_server.sensors) == 91
+    assert len(ecowitt_server.stations) == 2
