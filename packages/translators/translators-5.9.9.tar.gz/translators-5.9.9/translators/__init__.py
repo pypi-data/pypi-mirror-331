805acd860ef121b0b98748697ac1d3d72c45e95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 06:06:07 +0000

    Add package translators/5.9.9/translators-5.9.9.tar.gz

---
+++ packages/translators/translators-5.9.9.tar.gz/translators-5.9.9/translators/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "5.9.9"
+__author__ = "UlionTse"
+
+
+from translators.server import (
+    translate_text,
+    translate_html,
+    translators_pool,
+    get_languages,
+    get_region_of_server,
+    preaccelerate_and_speedtest,
+)
+
+
+__all__ = (
+    "__version__",
+    "__author__",
+    "translate_text",
+    "translate_html",
+    "translators_pool",
+    "get_languages",
+    "get_region_of_server",
+    "preaccelerate_and_speedtest",
+)
