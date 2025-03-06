db2e82f3342f3db3490fdaee31542c7d67b38754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:06:17 +0000

    Add package threedi-mi-utils/0.1.7/threedi_mi_utils-0.1.7-py3-none-any.whl

---
+++ packages/threedi-mi-utils/threedi_mi_utils-0.1.7-py3-none-any.whl/threedi_mi_utils/tests/conftest.py
@@ -0,0 +1,17 @@
+import logging
+
+import pytest
+from qgis.core import QgsApplication
+
+_singletons = {}
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(autouse=True)
+def qgis_app_initialized():
+    """Make sure qgis is initialized for testing."""
+    if "app" not in _singletons:
+        app = QgsApplication([], False)
+        app.initQgis()
+        logger.debug("Initialized qgis (for testing): %s", app.showSettings())
+        _singletons["app"] = app
