ee7f12d1535e7371d6948311fc2382f9ec07abf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:06:23 +0000

    Add package test_pypi_donny_test/0.1.1/test_pypi_donny_test-0.1.1-py3-none-any.whl

---
+++ packages/test_pypi_donny_test/test_pypi_donny_test-0.1.1-py3-none-any.whl/main.py
@@ -0,0 +1,16 @@
+import torch
+import numpy
+import matplotlib.pyplot as plt
+import seaborn as sns
+import transformers
+import datasets
+import safetensors
+import pandas as pd
+
+
+def test():
+    return 1
+
+
+if __name__ == "__main__":
+    test()
