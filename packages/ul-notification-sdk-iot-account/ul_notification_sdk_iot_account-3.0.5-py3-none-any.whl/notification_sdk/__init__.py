2e234260ec01db7d0211e77eea86ca2aa6043be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:07:38 +0000

    Add package ul-notification-sdk-iot-account/3.0.5/ul_notification_sdk_iot_account-3.0.5-py3-none-any.whl

---
+++ packages/ul-notification-sdk-iot-account/ul_notification_sdk_iot_account-3.0.5-py3-none-any.whl/notification_sdk/__init__.py
@@ -0,0 +1,7 @@
+from notification_sdk.notification_sdk import NotificationSdk
+from notification_sdk.notification_sdk_config import NotificationSdkConfig
+
+__all__ = (
+    "NotificationSdk",
+    "NotificationSdkConfig",
+)
---
+++ packages/ul-notification-sdk-iot-account/ul_notification_sdk_iot_account-3.0.5-py3-none-any.whl/notification_sdk/notification_sdk_config.py
@@ -0,0 +1,6 @@
+from typing import NamedTuple
+
+
+class NotificationSdkConfig(NamedTuple):
+    api_url: str
+    api_token: str
