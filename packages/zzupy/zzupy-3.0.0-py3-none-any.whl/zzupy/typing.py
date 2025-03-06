918e4b1f04587d883c7a1003f896e0152ab5321a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 04:06:08 +0000

    Add package zzupy/3.0.0/zzupy-3.0.0-py3-none-any.whl

---
+++ packages/zzupy/zzupy-3.0.0-py3-none-any.whl/zzupy/typing.py
@@ -0,0 +1,9 @@
+from typing_extensions import TypedDict
+
+
+class DeviceParams(TypedDict):
+    deviceName: str
+    deviceId: str
+    deviceInfo: str
+    deviceInfos: str
+    userAgentPrecursor: int
