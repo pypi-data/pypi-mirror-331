7b20ec61ede2a2be54626c9d499f750957cf826b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 17:08:01 +0000

    Add package kia-hyundai-api/1.4.3/kia_hyundai_api-1.4.3-py3-none-any.whl

---
+++ packages/kia-hyundai-api/kia_hyundai_api-1.4.3-py3-none-any.whl/kia_hyundai_api/const.py
@@ -0,0 +1,13 @@
+SENSITIVE_FIELD_NAMES = [
+    "username",
+    "password",
+    "userid",
+    "vin",
+    "sid",
+    "vinkey",
+    "lat",
+    "lon",
+]
+
+API_URL_HOST = "api.owners.kia.com"
+API_URL_BASE = "https://"+API_URL_HOST+"/apigw/v1/"
