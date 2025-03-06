dc64db673060c557c8868bb90e2bbbf27be2c925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 05:06:25 +0000

    Add package gamr-backend-api-service/1.0.10/gamr_backend_api_service-1.0.10-py3-none-any.whl

---
+++ packages/gamr-backend-api-service/gamr_backend_api_service-1.0.10-py3-none-any.whl/gamr_backend_api_service/api/routes/exception_handlers.py
@@ -0,0 +1,27 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+from gamr_backend_api_service.auth.exceptions import (
+    TokenNotDecoded,
+    UserNotExists,
+)
+from gamr_backend_api_service.model_services.hugging_face.errors import (
+    HuggingFaceException,
+)
+from gamr_backend_api_service.models.errors import UserBlank
+
+
+def user_not_exists_exception_handler(request: Request, exc: UserNotExists):
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.message})
+
+
+def user_blank_incorrect_exception_handler(request: Request, exc: UserBlank):
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.message})
+
+
+def token_not_decoded_exception_handler(request: Request, exc: TokenNotDecoded):
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.message})
+
+
+def hugging_face_api_exception_handler(request: Request, exc: HuggingFaceException):
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.message})
---
+++ packages/gamr-backend-api-service/gamr_backend_api_service-1.0.10-py3-none-any.whl/gamr_backend_api_service/model_services/hugging_face/service.py
@@ -0,0 +1,37 @@
+import json
+from dataclasses import dataclass
+
+from gradio_client import Client
+
+from gamr_backend_api_service.model_services.abstract_model_service import (
+    AbstractModelService,
+)
+from gamr_backend_api_service.model_services.hugging_face.errors import (
+    HuggingFaceException,
+)
+from gamr_backend_api_service.models import ImageData
+from gamr_backend_api_service.models.image_response import ImageResponse
+
+
+@dataclass
+class HuggingFaceInterface(AbstractModelService):
+    model_api_url: str = (
+        "https://gastonamengual-object-detection-app.hf.space/"  # TODO save as secret
+    )
+
+    @property
+    def _client(self):
+        return Client(self.model_api_url)
+
+    def predict(self, image_data: ImageData) -> ImageResponse:
+        try:
+            result = self._client.predict(
+                json.dumps(image_data.list_encoded_image),
+                api_name="/predict",
+            )
+            bytes_image = bytes(json.loads(result))
+            image_response = ImageResponse(bytes_image)
+            return image_response
+
+        except Exception as ex:
+            raise HuggingFaceException(message=f"HuggingFace API Error: {ex}")
