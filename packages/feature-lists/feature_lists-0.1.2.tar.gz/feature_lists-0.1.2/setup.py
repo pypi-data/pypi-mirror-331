97d42051669d4f2dfd9638b44a02692cf92e8ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 15:06:38 +0000

    Add package feature-lists/0.1.2/feature_lists-0.1.2.tar.gz

---
+++ packages/feature-lists/feature_lists-0.1.2.tar.gz/feature_lists-0.1.2/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='feature-lists',  # Name of the library
+    version='0.1.2',  # Version of your library
+    packages=['feature_lists'],  # Automatically find all packages
+    install_requires=[],  # List any dependencies (e.g., Django)
+    description='A simple video bookmarking and rating system for Django.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Omkar Mahajan',
+    author_email='mahajanomkar86@gmail.com',
+    url='https://github.com/omkar130/feature_lists.git',  # Update with your GitHub repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
