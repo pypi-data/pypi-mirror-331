c64b54829055855668308424d586feb91527e827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 15:06:37 +0000

    Add package evil-pkg2/0.2/evil_pkg2-0.2.tar.gz

---
+++ packages/evil-pkg2/evil_pkg2-0.2.tar.gz/evil_pkg2-0.2/setup.py
@@ -0,0 +1,14 @@
+
+import requests
+
+
+requests.get("http://127.0.0.1:8000/evil")
+
+from setuptools import setup
+
+setup(
+    name="evil-pkg2",
+    version="0.2",
+    packages=["evil_pkg2"],
+    description="Just a harmless package :)",
+)
