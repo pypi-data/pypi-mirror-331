3c6d9394afbf410d1efdd0cbf6951316c3c4d2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 16:06:50 +0000

    Add package datalayer-core/1.0.26/datalayer_core-1.0.26-py3-none-any.whl

---
+++ packages/datalayer-core/datalayer_core-1.0.26-py3-none-any.whl/datalayer_core/authn/keys.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2023-2024 Datalayer, Inc.
+# Distributed under the terms of the Modified BSD License.
+
+# Copyright (c) Datalayer Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+from __future__ import annotations
+
+
+DATALAYER_IAM_USER_KEY = "@datalayer/iam:user"
+
+DATALAYER_IAM_TOKEN_KEY = "@datalayer/iam:token"
