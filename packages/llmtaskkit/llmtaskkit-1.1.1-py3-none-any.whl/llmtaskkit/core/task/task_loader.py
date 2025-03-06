f78d074d5b0832e5cfe27d2422e77e89ebee1117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 07:05:40 +0000

    Add package llmtaskkit/1.1.1/llmtaskkit-1.1.1-py3-none-any.whl

---
+++ packages/llmtaskkit/llmtaskkit-1.1.1-py3-none-any.whl/llmtaskkit/core/task/task_loader.py
@@ -0,0 +1,8 @@
+# llmtaskkit/core/task/task_loader.py
+import yaml
+from llmtaskkit.core.task import RawTask
+
+def load_raw_tasks_from_yaml(yaml_path: str) -> dict:
+    with open(yaml_path, "r", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+    return RawTask.from_yaml(data)
