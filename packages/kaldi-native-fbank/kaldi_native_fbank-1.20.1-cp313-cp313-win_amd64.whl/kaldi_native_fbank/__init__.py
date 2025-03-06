afc4cccac83b9fc4c770025785bd6e729f539f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 16:06:39 +0000

    Add package kaldi-native-fbank/1.20.1/kaldi_native_fbank-1.20.1-cp313-cp313-win_amd64.whl

---
+++ packages/kaldi-native-fbank/kaldi_native_fbank-1.20.1-cp313-cp313-win_amd64.whl/kaldi_native_fbank/__init__.py
@@ -0,0 +1,13 @@
+from _kaldi_native_fbank import (
+    FbankOptions,
+    FeatureWindowFunction,
+    FrameExtractionOptions,
+    MelBanksOptions,
+    MfccOptions,
+    OnlineFbank,
+    OnlineMfcc,
+    OnlineWhisperFbank,
+    Rfft,
+    WhisperFeatureOptions,
+)
+__version__ = '1.20.1'
