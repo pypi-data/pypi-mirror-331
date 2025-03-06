8b148f18b7045ddb31fb1a90b338671b7984f521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-06 14:05:49 +0000

    Add package yteva/0.0.1/yteva-0.0.1.tar.gz

---
+++ packages/yteva/yteva-0.0.1.tar.gz/yteva-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yteva",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pyrofork"
+    ],
+    author="Eslam",
+    author_email="your_email@example.com",
+    description="A simple library to fetch and download audio.",
+    url="https://t.me/sourceeva",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
