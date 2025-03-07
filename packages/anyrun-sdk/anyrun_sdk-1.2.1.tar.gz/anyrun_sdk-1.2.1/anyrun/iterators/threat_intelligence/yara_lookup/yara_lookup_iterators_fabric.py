57df29311c6117e3f86b05467f2975b092962379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:14:05 +0000

    Add package anyrun-sdk/1.2.1/anyrun_sdk-1.2.1.tar.gz

---
+++ packages/anyrun-sdk/anyrun_sdk-1.2.1.tar.gz/anyrun_sdk-1.2.1/anyrun/iterators/threat_intelligence/yara_lookup/yara_lookup_iterators_fabric.py
@@ -0,0 +1,52 @@
+from anyrun.iterators.threat_intelligence.yara_lookup import StixYaraIterator, JsonYaraIterator
+from anyrun.connectors import YaraLookupConnector
+
+
+class YaraIterator:
+    """ Iterator Factory. Creates a concrete iterator instance according to the method called """
+    @staticmethod
+    def stix(
+            connector: YaraLookupConnector,
+            yara_rule: str,
+            chunk_size: int = 1,
+            ssl: bool = False,
+    ) -> StixYaraIterator:
+        """
+        Iterates through the yara search matches. Returns matches in **json** format
+
+        :param connector: Connector instance
+        :param yara_rule: Valid YARA rule
+        :param chunk_size: The number of feed objects to be retrieved each iteration.
+            If greater than one, returns the list of objects
+        :param ssl: Enable/disable ssl verification
+        :return: StixYaraIterator instance
+        """
+        return StixYaraIterator(
+            connector=connector,
+            chunk_size=chunk_size,
+            ssl=ssl,
+            yara_rule=yara_rule
+        )
+
+    @staticmethod
+    def json(
+            connector: YaraLookupConnector,
+            yara_rule: str,
+            chunk_size: int = 1,
+            ssl: bool = False,
+    ) -> JsonYaraIterator:
+        """
+        Iterates through the yara search matches. Returns matches in **json** format
+
+        :param connector: Connector instance
+        :param yara_rule: Valid YARA rule
+        :param chunk_size: The number of feed objects to be retrieved each iteration.
+            If greater than one, returns the list of objects
+        :param ssl: Enable/disable ssl verification
+        """
+        return JsonYaraIterator(
+            connector=connector,
+            chunk_size=chunk_size,
+            ssl=ssl,
+            yara_rule=yara_rule
+        )
