538a54b4f47f82f3682b6191e0132751796cbd8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:15:10 +0000

    Add package pygrafical/0.0.12/pygrafical-0.0.12.tar.gz

---
+++ packages/pygrafical/pygrafical-0.0.12.tar.gz/pygrafical-0.0.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygrafical',
+    version='0.0.12',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'pygrafical': ['lib.so'],
+        'pygrafical.hooks': ['*.py']# Укажите путь к вашему файлу библиотеки
+    },
+    install_requires=[
+        # Список зависимостей, если есть
+    ],
+    author='tetredial_9274',
+    author_email='system32@mail.ru',
+    description='графическая библиотека основаная на Windows API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ananimys784/Kingurio',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
