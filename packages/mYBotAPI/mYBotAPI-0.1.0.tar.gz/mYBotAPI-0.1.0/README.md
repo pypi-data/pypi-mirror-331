8a1ee744fb4185ce3344189c8b60e0ac40e931ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:06:07 +0000

    Add package mYBotAPI/0.1.0/mYBotAPI-0.1.0.tar.gz

---
+++ packages/mYBotAPI/mYBotAPI-0.1.0.tar.gz/mYBotAPI-0.1.0/README.md
@@ -0,0 +1,9 @@
+# mYBot
+
+Dieses Modul ermöglicht den Zugriff auf die mYBot-API.
+
+## Installation
+
+```bash
+pip install mYBot
+
---
+++ packages/mYBotAPI/mYBotAPI-0.1.0.tar.gz/mYBotAPI-0.1.0/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mYBotAPI',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Ein Modul für den Zugriff auf die mYBot-API.',
+    author='Yannic Basin',
+    author_email='yannicbasin@gmail.com',
+    url='https://mYBot.yb-tech.de',
+    install_requires=[
+        'requests',  # Abhängigkeiten hier hinzufügen
+    ],
+)
+
