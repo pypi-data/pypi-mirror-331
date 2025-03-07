faac07c3581864be6cdca65400a25b808df74436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:07:35 +0000

    Add package kurzaj/0.2.0/kurzaj-0.2.0-py3-none-any.whl

---
+++ packages/kurzaj/kurzaj-0.2.0-py3-none-any.whl/kurzaj/main.py
@@ -0,0 +1,19 @@
+identificator = ["secretphrase"]
+
+class kurzajfunctions:
+    def getbmi():
+        return 16
+
+    def say(thing):
+        print(f'kurzaj: {thing}')
+
+    def zajebidentifikator():
+        print("successfully jewed")
+        return identificator
+
+    def wpierdolidentifikator(iden):
+        if iden == identificator:
+            return True
+
+def kurzaj():
+    return kurzajfunctions
