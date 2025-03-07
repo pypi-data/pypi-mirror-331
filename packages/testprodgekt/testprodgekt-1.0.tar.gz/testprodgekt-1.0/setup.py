6a0a16505b09ca2144d9c5b28bd5f69a324030a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:07:20 +0000

    Add package testprodgekt/1.0/testprodgekt-1.0.tar.gz

---
+++ packages/testprodgekt/testprodgekt-1.0.tar.gz/testprodgekt-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='testprodgekt',
+    version='1.0',
+    packages = ['testprodgekt'],
+    install_requires=['keyboard'],
+    python_requires=">=3.11",
+    author="DavidYdin",
+    author_email="David.2280@yandex.ru"
+)
---
+++ packages/testprodgekt/testprodgekt-1.0.tar.gz/testprodgekt-1.0/testprodgekt/__init__.py
@@ -0,0 +1,4 @@
+from testprodgekt import*
+__author__ = 'DavidYdin'
+__version__ = '1.0'
+__email__ = 'David.2280@yandex.ru'
