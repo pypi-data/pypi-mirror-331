310f817c98f5e5510d5bd99b063db16b76955623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:13:43 +0000

    Add package edprompt/0.0.9/edprompt-0.0.9.tar.gz

---
+++ packages/edprompt/edprompt-0.0.9.tar.gz/edprompt-0.0.9/edprompt/__init__.py
@@ -0,0 +1,3 @@
+__version__, __all__ = "0.0.8", ["edprompt"]
+
+from edprompt import *
---
+++ packages/edprompt/edprompt-0.0.9.tar.gz/edprompt-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='edprompt',
+    version='0.0.9',
+    description='python shell prompt cli editor joke saver prompt bloody hell lmao 🤣🤣🤣',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/edprompt',
+    packages=find_packages(exclude=[]),
+    install_requires=['clro'],
+    keywords=['python', 'shell', 'prompt', 'cli', 'editer', 'joke' 'saver', 'edprompt'],
+    python_requires='>=3.6',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
