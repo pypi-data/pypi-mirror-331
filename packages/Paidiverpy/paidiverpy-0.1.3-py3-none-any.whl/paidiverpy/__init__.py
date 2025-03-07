e1380b874864be5944ab254ff153b569cb64eb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:07:43 +0000

    Add package Paidiverpy/0.1.3/paidiverpy-0.1.3-py3-none-any.whl

---
+++ packages/Paidiverpy/paidiverpy-0.1.3-py3-none-any.whl/paidiverpy/__init__.py
@@ -0,0 +1,8 @@
+"""Paidiverpy base package."""
+
+__version__ = "0.1.3"
+__author__ = "Tobias Ferreira"
+__credits__ = "National Oceanography Centre"
+from .paidiverpy import Paidiverpy
+
+__all__ = ["Paidiverpy"]
