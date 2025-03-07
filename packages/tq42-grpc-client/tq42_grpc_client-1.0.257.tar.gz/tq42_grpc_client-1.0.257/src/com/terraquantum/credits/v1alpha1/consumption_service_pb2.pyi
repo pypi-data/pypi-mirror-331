3520816420b70b11a5ec35e8ed40ae443a1380d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:14:37 +0000

    Add package tq42-grpc-client/1.0.257/tq42_grpc_client-1.0.257.tar.gz

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.257.tar.gz/tq42_grpc_client-1.0.257/src/com/terraquantum/credits/v1alpha1/consumption_service_pb2.pyi
@@ -0,0 +1,7 @@
+from com.terraquantum.credits.v1alpha1 import check_experiment_run_pb2 as _check_experiment_run_pb2
+from com.terraquantum.credits.v1alpha1 import list_transactions_by_org_pb2 as _list_transactions_by_org_pb2
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
