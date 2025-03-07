a29d3c2a1d8f3b405d0bca68667b5371eeb29aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:07:02 +0000

    Add package server-3-free-threads/0.1.1/server_3_free_threads-0.1.1-py3-none-any.whl

---
+++ packages/server-3-free-threads/server_3_free_threads-0.1.1-py3-none-any.whl/Server_3_free_threads/initial_func.py
@@ -0,0 +1,39 @@
+import logging
+import dotenv
+from threading import Thread
+import os
+from load_app import load_app as l_app
+from parser_args import ParserCommandLineArgs as p_args
+import importlib
+from server_actions import ServerActions
+
+
+def main():
+    dotenv.load_dotenv()
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(filename)s:%(funcName)s] %(message)s')
+    log = logging.getLogger(__name__)
+
+    host = os.getenv("HOST")
+    port_default = int(os.getenv("PORT"))
+    connection_queue = int(os.getenv("CONNECTION_QUEUE"))
+
+    module = None
+    parser_args = p_args(port_default)
+    parser_args.find_args()
+    port, path_app = parser_args.port, parser_args.app
+    if path_app:
+        if l_app(path_app):
+            module = importlib.import_module(path_app)
+            importlib.invalidate_caches()
+    else:
+        log.info("относительный путь к приложению не указан, сервер запущен в тестовом режиме")
+    server_actions = ServerActions(host=host, port=port, connection_queue=connection_queue)
+    threads = (Thread(target=server_actions.accepting_connections),
+               Thread(target=server_actions.reading_from_socket),
+               Thread(target=server_actions.sending_to_socket, args=(module, )),
+               Thread(target=server_actions.close_client_sock),)
+
+    for elem in threads:
+        elem.start()
+    for elem in threads:
+        elem.join()
