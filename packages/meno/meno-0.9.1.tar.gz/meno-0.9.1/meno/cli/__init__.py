c1d5f107e194008d19a71cea6e887583c59381bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:14:59 +0000

    Add package meno/0.9.1/meno-0.9.1.tar.gz

---
+++ packages/meno/meno-0.9.1.tar.gz/meno-0.9.1/meno/cli/__init__.py
@@ -0,0 +1,6 @@
+"""Command-line interface for Meno.
+
+This package provides command-line interfaces for various components of Meno.
+"""
+
+from .team_config_cli import main as team_config_cli_main
