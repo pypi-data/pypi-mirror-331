a1e55c924608d565882f7c4c4e7ef80c542cf82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:14:15 +0000

    Add package py-qj-robots/0.1.2/py_qj_robots-0.1.2.tar.gz

---
+++ packages/py-qj-robots/py_qj_robots-0.1.2.tar.gz/py_qj_robots-0.1.2/README.md
@@ -0,0 +1,9 @@
+# py-qj-robots
+
+千诀机器人 Python SDK
+
+## 安装
+
+```bash
+pip install py-qj-robots# perception-python-sdk
+# perception-python-sdk
---
+++ packages/py-qj-robots/py_qj_robots-0.1.2.tar.gz/py_qj_robots-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-qj-robots",
+    version="0.1.2",
+    author="QJ ROBOTS",
+    author_email="github@qj-robots.com",
+    description="千诀机器人 Python SDK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/QJ-ROBOTS/perception-python-sdk",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.25.1"
+    ],
+)
