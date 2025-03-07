a7303a596e96b5892384b7e353e0ae5445789598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:07:26 +0000

    Add package glean-requests/0.3.0/glean_requests-0.3.0-py3-none-any.whl

---
+++ packages/glean-requests/glean_requests-0.3.0-py3-none-any.whl/glean_requests/__init__.py
@@ -0,0 +1,10 @@
+"""
+A client session for interacting with Glean's REST API.
+
+This module provides a simple interface for making authenticated requests to Glean's API endpoints.
+"""
+
+from .glean_client import GleanBaseClient, GleanIndexingClient, GleanRestClient
+
+__all__ = ["GleanBaseClient", "GleanRestClient", "GleanIndexingClient"]
+__version__ = "0.1.0"
