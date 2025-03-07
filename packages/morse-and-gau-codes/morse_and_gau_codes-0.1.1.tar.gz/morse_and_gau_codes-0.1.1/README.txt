22b07a937066f0af99d4196192713d248a45ad4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:14:35 +0000

    Add package morse-and-gau-codes/0.1.1/morse_and_gau_codes-0.1.1.tar.gz

---
+++ packages/morse-and-gau-codes/morse_and_gau_codes-0.1.1.tar.gz/morse_and_gau_codes-0.1.1/README.txt
@@ -0,0 +1,5 @@
+# Your Package
+This package provides a simple Morse code dictionary
+Navigieren Sie zu dem Verzeichnis, das setup.py enthält.
+
+Laufen im Terminal:pip install morse-and-gau-codes==0.1.0
---
+++ packages/morse-and-gau-codes/morse_and_gau_codes-0.1.1.tar.gz/morse_and_gau_codes-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name="morse_and_gau_codes",  # Package name
+    version="0.1.1",  # Version number
+    packages=find_packages(),  # Automatically finds modules
+    description="A simple Morse and Gauß code dictionary package",
+    author="Artem",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        
+    ],
+)
